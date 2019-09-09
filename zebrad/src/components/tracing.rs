92d9d9dfd77e12ef36d5f2a3a51a2f3d8cc2451d
Henry de Valence <hdevalence@hdevalence.ca>
2019-09-08 21:47:57 -0700

    Add filter reloading handling.

--- zebrad/src/components/tracing.rs
+++ zebrad/src/components/tracing.rs
@@ -7,6 +7,7 @@ use abscissa_core::{err, Component, FrameworkError, FrameworkErrorKind};
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Request, Response, Server};
 
+use tracing::Subscriber;
 use tracing_log::LogTracer;
 use tracing_subscriber::{filter::Filter, reload::Handle, FmtSubscriber};
 
@@ -63,6 +64,8 @@ impl TracingEndpoint {
 
     /// Do setup after receiving a tokio runtime.
     pub fn init_tokio(&mut self, tokio_component: &TokioComponent) -> Result<(), FrameworkError> {
+        info!("Initializing tracing endpoint");
+
         // Clone the filter handle so it can be moved into make_service_fn closure
         let handle = self.filter_handle.clone();
         let service = make_service_fn(move |_| {
@@ -73,6 +76,7 @@ impl TracingEndpoint {
             }
         });
 
+        // XXX load tracing addr from config
         let addr = "127.0.0.1:3000".parse().unwrap();
 
         let server = Server::bind(&addr).serve(service);
@@ -87,22 +91,54 @@ impl TracingEndpoint {
     }
 }
 
-async fn filter_handler<S>(
+fn reload_filter_from_chunk<S: Subscriber>(
+    handle: Handle<Filter, S>,
+    chunk: hyper::Chunk,
+) -> Result<(), String> {
+    let bytes = chunk.into_bytes();
+    let body = std::str::from_utf8(bytes.as_ref()).map_err(|e| format!("{}", e))?;
+    trace!(request.body = ?body);
+    let filter = body.parse::<Filter>().map_err(|e| format!("{}", e))?;
+    handle.reload(filter).map_err(|e| format!("{}", e))
+}
+
+async fn filter_handler<S: Subscriber>(
     handle: Handle<Filter, S>,
     req: Request<Body>,
 ) -> Result<Response<Body>, hyper::Error> {
+    use futures_util::TryStreamExt;
     use hyper::{Method, StatusCode};
-    match (req.method(), req.uri().path()) {
+
+    // We can't use #[instrument] because Handle<_,_> is not Debug,
+    // so we create a span manually.
+    let handler_span =
+        info_span!("filter_handler", method = ?req.method(), path = ?req.uri().path());
+    let _enter = handler_span.enter(); // dropping _enter closes the span
+
+    let rsp = match (req.method(), req.uri().path()) {
+        (&Method::GET, "/") => Response::new(Body::from(
+            r#"
+This HTTP endpoint allows dynamic control of the filter applied to
+tracing events.  To set the filter, POST it to /filter:
+
+curl -X POST localhost:3000/filter -d "zebrad=trace"
+"#,
+        )),
         (&Method::POST, "/filter") => {
-            let rsp = Response::new(Body::from("OK"));
-            Ok(rsp)
+            // Combine all HTTP request chunks into one
+            let whole_chunk = req.into_body().try_concat().await?;
+            match reload_filter_from_chunk(handle, whole_chunk) {
+                Err(e) => Response::builder()
+                    .status(StatusCode::BAD_REQUEST)
+                    .body(Body::from(e))
+                    .expect("response with known status code cannot fail"),
+                Ok(()) => Response::new(Body::from("")),
+            }
         }
-        _ => {
-            let rsp = Response::builder()
-                .status(StatusCode::BAD_REQUEST)
-                .body(Body::from(""))
-                .expect("response with known status cannot fail");
-            Ok(rsp)
-        }
-    }
+        _ => Response::builder()
+            .status(StatusCode::NOT_FOUND)
+            .body(Body::from(""))
+            .expect("response with known status cannot fail"),
+    };
+    Ok(rsp)
 }
