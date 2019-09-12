f2357deaff0fa8b524d06a764a3744de75959ab9
Henry de Valence <hdevalence@hdevalence.ca>
2019-09-11 22:31:14 -0700

    DROP changing hyper made futures extension traits fall out of order?
    This seems to be an issue with `pin-project` resolved in the git branch
    of `hyper` (but not yet released).

--- zebrad/src/components/tracing.rs
+++ zebrad/src/components/tracing.rs
@@ -126,7 +126,12 @@ curl -X POST localhost:3000/filter -d "zebrad=trace"
         )),
         (&Method::POST, "/filter") => {
             // Combine all HTTP request chunks into one
-            let whole_chunk = req.into_body().try_concat().await?;
+            //let whole_chunk = req.into_body().try_concat().await?;
+            // XXX try_concat extension trait is not applying for some reason,
+            // just pull one chunk
+            let mut body = req.into_body();
+            let maybe_chunk = body.next().await;
+            let whole_chunk = maybe_chunk.unwrap()?;
             match reload_filter_from_chunk(handle, whole_chunk) {
                 Err(e) => Response::builder()
                     .status(StatusCode::BAD_REQUEST)
