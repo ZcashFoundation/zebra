0dde3b351b2a08f74d45da3bab4462f3089a6346
Henry de Valence <hdevalence@hdevalence.ca>
2019-09-13 05:28:38 -0700

    Add a Magic type for network magics.

--- zebra-network/src/constants.rs
+++ zebra-network/src/constants.rs
@@ -2,5 +2,14 @@
 
 use crate::types::*;
 
-/// The Zcash version used on mainnet.
-pub const ZCASH_VERSION: Version = Version(170_007);
+/// The Zcash network protocol version used on mainnet.
+pub const CURRENT_VERSION: Version = Version(170_007);
+
+/// Magic numbers used to identify different Zcash networks.
+pub mod magics {
+    use super::*;
+    /// The production mainnet.
+    pub const MAINNET: Magic = Magic(0x6427e924);
+    /// The testnet.
+    pub const TESTNET: Magic = Magic(0xbff91afa);
+}
