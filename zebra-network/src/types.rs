c01cd4b29158eeaf63c464ae2e20a0bdcbb2de41
Henry de Valence <hdevalence@hdevalence.ca>
2019-09-14 08:15:22 -0700

    Add derived impls to newtypes

--- zebra-network/src/types.rs
+++ zebra-network/src/types.rs
@@ -1,15 +1,19 @@
 //! Newtype wrappers assigning semantic meaning to primitive types.
 
 /// A magic number identifying the network.
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub struct Magic(pub u32);
 
 /// A protocol version number.
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub struct Version(pub u32);
 
 /// Bitfield of features to be enabled for this connection.
 // Tower provides utilities for service discovery, so this might go
 // away in the future in favor of that.
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub struct Services(pub u64);
 
 /// A nonce used in the networking layer to identify messages.
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub struct Nonce(pub u64);
