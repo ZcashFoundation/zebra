3af32ab26163ed4fa7eaac8cf99f9a7abaec08d8
Deirdre Connolly <deirdre@zfnd.org>
2019-09-25 18:54:38 -0400

    Remove unused imports

--- zebra-chain/src/merkle_tree.rs
+++ zebra-chain/src/merkle_tree.rs
@@ -1,11 +1,7 @@
 //! A binary hash tree of SHA256d (two rounds of SHA256) hashes for
 //! node values.
 
-use std::io;
-
 use sha2::Sha256;
-
-use crate::serialization::{SerializationError, ZcashDeserialize, ZcashSerialize};
 
 /// A binary hash tree of SHA256d (two rounds of SHA256) hashes for
 /// node values.
