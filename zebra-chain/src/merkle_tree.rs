e06cccde70f203f8104eac516bfa9605e09f8852
Deirdre Connolly <deirdre@zfnd.org>
2019-09-25 18:51:29 -0400

    Implement Zcash(De)serialize traits specifically for MerkleTree<Transaction>
    This is a general placeholder for now.

--- zebra-chain/src/merkle_tree.rs
+++ zebra-chain/src/merkle_tree.rs
@@ -2,9 +2,8 @@
 //! node values.
 
 use std::io;
-use std::io::prelude::*;
 
-use sha2::{Digest, Sha256};
+use sha2::Sha256;
 
 use crate::serialization::{SerializationError, ZcashDeserialize, ZcashSerialize};
 
@@ -17,18 +16,6 @@ pub struct MerkleTree<T> {
 
 impl<T> MerkleTree<T> {
     pub fn get_root(&self) -> Sha256 {
-        unimplemented!();
-    }
-}
-
-impl<T> ZcashSerialize for MerkleTree<T> {
-    fn zcash_serialize<W: io::Write>(&self, writer: W) -> Result<(), SerializationError> {
-        unimplemented!();
-    }
-}
-
-impl<T> ZcashDeserialize for MerkleTree<T> {
-    fn zcash_deserialize<R: io::Read>(reader: R) -> Result<Self, SerializationError> {
         unimplemented!();
     }
 }
