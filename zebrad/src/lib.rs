119f881907fa7df0f0f7c66d9f04c1b6831c3664
Henry de Valence <hdevalence@hdevalence.ca>
2019-09-25 16:03:56 -0700

    nit: suppress async_await nightly warning.

--- zebrad/src/lib.rs
+++ zebrad/src/lib.rs
@@ -6,6 +6,9 @@
 
 //#![deny(warnings, missing_docs, trivial_casts, unused_qualifications)]
 #![forbid(unsafe_code)]
+// async_await is considered stable on nightly releases, but the corresponding
+// release (1.39.0) is still in beta. Suppress the warning for now.
+#![allow(stable_features)]
 #![feature(async_await)]
 
 #[macro_use]
