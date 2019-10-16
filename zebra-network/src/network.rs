63cf340ab496d6b90912af0451fab4ed85ecff19
Henry de Valence <hdevalence@hdevalence.ca>
2019-10-16 15:16:29 -0700

    Add fields to zebra-network Config.

--- zebra-network/src/network.rs
+++ zebra-network/src/network.rs
@@ -1,7 +1,7 @@
 use crate::{constants::magics, protocol::types::Magic};
 
 /// An enum describing the possible network choices.
-#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash, Serialize, Deserialize)]
 pub enum Network {
     /// The production mainnet.
     Mainnet,
