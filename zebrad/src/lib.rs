cfba5e961a89841ef86dd2e0f822ebd1c3bf8473
Henry de Valence <hdevalence@hdevalence.ca>
2019-11-14 17:33:08 -0800

    Remove nightly toolchain pin since async/await is stable.

--- zebrad/src/lib.rs
+++ zebrad/src/lib.rs
@@ -6,10 +6,6 @@
 
 //#![deny(warnings, missing_docs, trivial_casts, unused_qualifications)]
 #![forbid(unsafe_code)]
-// async_await is considered stable on nightly releases, but the corresponding
-// release (1.39.0) is still in beta. Suppress the warning for now.
-#![allow(stable_features)]
-#![feature(async_await)]
 
 #[macro_use]
 extern crate tracing;
