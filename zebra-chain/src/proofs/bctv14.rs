7d520f8133089595396e26a643858cbef99180d4
Deirdre Connolly <deirdre@zfnd.org>
2020-01-21 18:18:13 -0500

    Impl Arbitrary for Bctv14Proof

--- zebra-chain/src/proofs/bctv14.rs
+++ zebra-chain/src/proofs/bctv14.rs
@@ -1,5 +1,8 @@
 use std::{fmt, io};
 
+#[cfg(test)]
+use proptest::{arbitrary::Arbitrary, collection::vec, prelude::*};
+
 use crate::serialization::{SerializationError, ZcashDeserialize, ZcashSerialize};
 
 /// An encoding of a BCTV14 proof, as used in Zcash.
@@ -46,4 +49,21 @@ impl ZcashDeserialize for Bctv14Proof {
         reader.read_exact(&mut bytes[..])?;
         Ok(Self(bytes))
     }
+}
+
+#[cfg(test)]
+impl Arbitrary for Bctv14Proof {
+    type Parameters = ();
+
+    fn arbitrary_with(_args: Self::Parameters) -> Self::Strategy {
+        (vec(any::<u8>(), 296))
+            .prop_map(|v| {
+                let mut bytes = [0; 296];
+                bytes.copy_from_slice(v.as_slice());
+                return Self(bytes);
+            })
+            .boxed()
+    }
+
+    type Strategy = BoxedStrategy<Self>;
 }
