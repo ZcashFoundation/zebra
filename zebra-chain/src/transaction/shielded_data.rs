a93e2db44b26806c5dd3070189bccb01063e846c
Deirdre Connolly <deirdre@zfnd.org>
2020-01-27 23:25:17 -0500

    Add and use OutCiphertext
    Instead of [u64; 10].

--- zebra-chain/src/transaction/shielded_data.rs
+++ zebra-chain/src/transaction/shielded_data.rs
@@ -91,10 +91,7 @@ pub struct OutputDescription {
     /// A ciphertext component for the encrypted output note.
     pub enc_ciphertext: EncryptedCiphertext,
     /// A ciphertext component for the encrypted output note.
-    ///
-    /// XXX refine to a specific type.
-    /// XXX this is a [u64; 10] rather than a [u8; 80] to get trait impls
-    pub out_ciphertext: [u64; 10],
+    pub out_ciphertext: OutCiphertext,
     /// The ZK output proof.
     pub zkproof: Groth16Proof,
 }
@@ -259,6 +256,69 @@ impl Arbitrary for EncryptedCiphertext {
         (vec(any::<u8>(), 580))
             .prop_map(|v| {
                 let mut bytes = [0; 580];
+                bytes.copy_from_slice(v.as_slice());
+                return Self(bytes);
+            })
+            .boxed()
+    }
+
+    type Strategy = BoxedStrategy<Self>;
+}
+
+/// A ciphertext component for encrypted output notes.
+pub struct OutCiphertext(pub [u8; 80]);
+
+impl fmt::Debug for OutCiphertext {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.debug_tuple("OutCiphertext")
+            .field(&hex::encode(&self.0[..]))
+            .finish()
+    }
+}
+
+// These impls all only exist because of array length restrictions.
+
+impl Copy for OutCiphertext {}
+
+impl Clone for OutCiphertext {
+    fn clone(&self) -> Self {
+        let mut bytes = [0; 80];
+        bytes[..].copy_from_slice(&self.0[..]);
+        Self(bytes)
+    }
+}
+
+impl PartialEq for OutCiphertext {
+    fn eq(&self, other: &Self) -> bool {
+        self.0[..] == other.0[..]
+    }
+}
+
+impl Eq for OutCiphertext {}
+
+impl ZcashSerialize for OutCiphertext {
+    fn zcash_serialize<W: io::Write>(&self, mut writer: W) -> Result<(), SerializationError> {
+        writer.write_all(&self.0[..])?;
+        Ok(())
+    }
+}
+
+impl ZcashDeserialize for OutCiphertext {
+    fn zcash_deserialize<R: io::Read>(mut reader: R) -> Result<Self, SerializationError> {
+        let mut bytes = [0; 80];
+        reader.read_exact(&mut bytes[..])?;
+        Ok(Self(bytes))
+    }
+}
+
+#[cfg(test)]
+impl Arbitrary for OutCiphertext {
+    type Parameters = ();
+
+    fn arbitrary_with(_args: Self::Parameters) -> Self::Strategy {
+        (vec(any::<u8>(), 80))
+            .prop_map(|v| {
+                let mut bytes = [0; 80];
                 bytes.copy_from_slice(v.as_slice());
                 return Self(bytes);
             })
