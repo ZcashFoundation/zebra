d4d1cf8d7bb5a0c9a282d7c45b1e1dbd3c95992d
Deirdre Connolly <deirdre@zfnd.org>
2020-02-08 17:45:07 -0500

    Add memo type with Debug impl and some test cases

--- zebra-chain/src/lib.rs
+++ zebra-chain/src/lib.rs
@@ -7,6 +7,7 @@ mod sha256d_writer;
 pub mod block;
 pub mod equihash_solution;
 pub mod note_commitment_tree;
+pub mod note_encryption;
 pub mod proofs;
 pub mod serialization;
 pub mod transaction;
