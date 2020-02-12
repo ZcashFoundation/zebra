a877b09a4676af50ee660bbd86fe57d2b3edde4f
Henry de Valence <hdevalence@hdevalence.ca>
2020-02-12 12:40:08 -0800

    Update zebrad docs and README.

--- zebrad/src/lib.rs
+++ zebrad/src/lib.rs
@@ -1,8 +1,16 @@
-//! Zebrad
+//! Hello! I am Zebra, an ongoing Rust implementation of a Zcash node.
 //!
-//! Application based on the [Abscissa] framework.
+//! Zebra is a work in progress.  It is developed as a collection of `zebra-*`
+//! libraries implementing the different components of a Zcash node (networking,
+//! chain structures, consensus rules, etc), and a `zebrad` binary which uses them.
 //!
-//! [Abscissa]: https://github.com/iqlusioninc/abscissa
+//! Most of our work so far has gone into `zebra-network`, building a new
+//! networking stack for Zcash, and `zebra-chain`, building foundational data
+//! structures.
+//!
+//! [Rendered docs from the `main` branch](https://doc.zebra.zfnd.org).
+//!
+//! [Join us on Discord](https://discord.gg/na6QZNd).
 
 //#![deny(warnings, missing_docs, trivial_casts, unused_qualifications)]
 #![forbid(unsafe_code)]
