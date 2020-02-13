c5ed962ddf4643e437217a95d3ead20a90b46478
Henry de Valence <hdevalence@hdevalence.ca>
2020-02-13 10:11:39 -0800

    Fix clippy lints.

--- zebrad/src/lib.rs
+++ zebrad/src/lib.rs
@@ -6,6 +6,9 @@
 
 //#![deny(warnings, missing_docs, trivial_casts, unused_qualifications)]
 #![forbid(unsafe_code)]
+// Tracing causes false positives on this lint:
+// https://github.com/tokio-rs/tracing/issues/553
+#![allow(clippy::cognitive_complexity)]
 
 #[macro_use]
 extern crate tracing;
