56b47de0b5f848fa06333bba1765a649d629177d
Deirdre Connolly <deirdre@zfnd.org>
2020-02-25 18:14:59 -0500

    Stub out sprout, sapling key modules

---
+++ zebra-chain/src/keys.rs
@@ -0,0 +1,4 @@
+//! Key types.
+
+mod sapling;
+mod sprout;
---
+++ zebra-chain/src/keys/sprout.rs
@@ -0,0 +1,18 @@
+use std::{
+    fmt,
+    io::{self},
+};
+
+#[cfg(test)]
+use proptest::{array, collection::vec, prelude::*};
+#[cfg(test)]
+use proptest_derive::Arbitrary;
+
+use crate::serialization::{SerializationError, ZcashDeserialize, ZcashSerialize};
+
+#[cfg(test)]
+proptest! {
+
+    #[test]
+    fn test() {}
+}
