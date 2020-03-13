7af9b80f3be7cf17e40f2dc64a956177b690fa70
Deirdre Connolly <deirdre@zfnd.org>
2020-03-13 18:58:02 -0400

    Add the beginnings of Sprout and Sapling key types

--- zebra-chain/src/keys/sprout.rs
+++ zebra-chain/src/keys/sprout.rs
@@ -1,4 +1,10 @@
 //! Sprout key types
+//!
+//! "The receiving key sk_enc, the incoming viewing key ivk = (apk,
+//! sk_enc), and the shielded payment address addr_pk = (a_pk, pk_enc) are
+//! derived from a_sk, as described in [‘Sprout Key Components’][ps]
+//!
+//! [ps]: https://zips.z.cash/protocol/protocol.pdf#sproutkeycomponents
 
 use std::{
     fmt,
@@ -11,6 +17,20 @@ use proptest::{array, collection::vec, prelude::*};
 use proptest_derive::Arbitrary;
 
 use crate::serialization::{SerializationError, ZcashDeserialize, ZcashSerialize};
+
+/// All other Sprout key types derive from the SpendingKey value.
+pub struct SpendingKey;
+
+pub struct ReceivingKey;
+
+pub struct PayingKey;
+
+pub struct TransmissionKey;
+
+pub struct IncomingViewingKey {
+    paying_key: PayingKey,
+    receiving_key: ReceivingKey,
+}
 
 #[cfg(test)]
 proptest! {
