4857680946bc92ba25228f710a0032741a430511
Deirdre Connolly <deirdre@zfnd.org>
2020-03-26 22:32:33 -0400

    Fix typos and doc comments

--- zebra-chain/src/addresses/transparent.rs
+++ zebra-chain/src/addresses/transparent.rs
@@ -35,7 +35,7 @@ mod magics {
 /// In Bitcoin a single byte is used for the version field identifying
 /// the address type. In Zcash two bytes are used. For addresses on
 /// the production network, this and the encoded length cause the first
-/// two characters of the Base58Check encoding to be xed as “t3” for
+/// two characters of the Base58Check encoding to be fixed as “t3” for
 /// P2SH addresses, and as “t1” for P2PKH addresses. (This does not
 /// imply that a transparent Zcash address can be parsed identically
 /// to a Bitcoin address just by removing the “t”.)
@@ -66,7 +66,7 @@ impl TransparentAddress {
     /// addresses.
     ///
     /// The resulting hash in both of these cases is always exactly 20
-    /// bytes. These are big-endian.
+    /// bytes.
     /// https://en.bitcoin.it/Base58Check_encoding#Encoding_a_Bitcoin_address
     fn hash_payload(bytes: &[u8]) -> [u8; 20] {
         let sha_hash = Sha256::digest(bytes);
