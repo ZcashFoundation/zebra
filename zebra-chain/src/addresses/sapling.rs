4f25572a1600fae158f472e8c06f028f3ea4f797
Deirdre Connolly <deirdre@zfnd.org>
2020-03-28 04:06:55 -0400

    Add stub for SaplingShieldedAddress

--- zebra-chain/src/addresses/sapling.rs
+++ zebra-chain/src/addresses/sapling.rs
@@ -1,1 +1,17 @@
 //! Sapling Shielded Payment Address types.
+
+use std::{fmt, io};
+
+#[cfg(test)]
+use proptest::{arbitrary::Arbitrary, array, prelude::*};
+
+use crate::{
+    keys::sapling,
+    serialization::{ReadZcashExt, SerializationError, ZcashDeserialize, ZcashSerialize},
+};
+
+///
+pub struct SaplingShieldedAddress {
+    diversifier: sapling::Diversifier,
+    transmission_key: sapling::TransmissionKey,
+}
