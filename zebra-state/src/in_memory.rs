dbd7045b4df7c8ca0854f56803b7145c749f026d
Jane Lusby <jane@zfnd.org>
2020-06-03 11:29:31 -0700

    make blocks queryable by height

--- zebra-state/src/in_memory.rs
+++ zebra-state/src/in_memory.rs
@@ -1,7 +1,7 @@
 use super::{Request, Response};
 use futures::prelude::*;
 use std::{
-    collections::HashMap,
+    collections::{BTreeMap, HashMap},
     error::Error,
     future::Future,
     pin::Pin,
@@ -9,11 +9,44 @@ use std::{
     task::{Context, Poll},
 };
 use tower::{buffer::Buffer, Service};
-use zebra_chain::block::{Block, BlockHeaderHash};
+use zebra_chain::{
+    block::{Block, BlockHeaderHash},
+    types::BlockHeight,
+};
 
 #[derive(Default)]
 struct ZebraState {
-    blocks: HashMap<BlockHeaderHash, Arc<Block>>,
+    by_hash: HashMap<BlockHeaderHash, Arc<Block>>,
+    by_height: BTreeMap<BlockHeight, Arc<Block>>,
+}
+
+impl ZebraState {
+    fn insert(&mut self, block: impl Into<Arc<Block>>) {
+        let block = block.into();
+        let hash = block.as_ref().into();
+        let height = block.coinbase_height().unwrap();
+
+        self.by_hash.insert(hash, block.clone());
+        self.by_height.insert(height, block);
+    }
+
+    fn get(
+        &mut self,
+        query: impl Into<BlockQuery>,
+    ) -> Result<Arc<Block>, Box<dyn Error + Send + Sync + 'static>> {
+        match query.into() {
+            BlockQuery::ByHash(hash) => self
+                .by_hash
+                .get(&hash)
+                .cloned()
+                .ok_or_else(|| "block could not be found".into()),
+            BlockQuery::ByHeight(height) => self
+                .by_height
+                .get(&height)
+                .cloned()
+                .ok_or_else(|| "block could not be found".into()),
+        }
+    }
 }
 
 impl Service<Request> for ZebraState {
@@ -29,22 +62,31 @@ impl Service<Request> for ZebraState {
     fn call(&mut self, req: Request) -> Self::Future {
         match req {
             Request::AddBlock { block } => {
-                let hash = block.as_ref().into();
-                self.blocks.insert(hash, block);
-
+                self.insert(block);
                 async { Ok(Response::Added) }.boxed()
             }
             Request::GetBlock { hash } => {
-                let result = self
-                    .blocks
-                    .get(&hash)
-                    .cloned()
-                    .map(|block| Response::Block { block })
-                    .ok_or_else(|| "block could not be found".into());
-
+                let result = self.get(hash).map(|block| Response::Block { block });
                 async move { result }.boxed()
             }
         }
+    }
+}
+
+enum BlockQuery {
+    ByHash(BlockHeaderHash),
+    ByHeight(BlockHeight),
+}
+
+impl From<BlockHeaderHash> for BlockQuery {
+    fn from(hash: BlockHeaderHash) -> Self {
+        Self::ByHash(hash)
+    }
+}
+
+impl From<BlockHeight> for BlockQuery {
+    fn from(height: BlockHeight) -> Self {
+        Self::ByHeight(height)
     }
 }
 
