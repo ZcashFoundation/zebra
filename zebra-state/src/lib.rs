e2fad5d3b6b9a25089792f5a3e68856264836841
Jane Lusby <jane@zfnd.org>
2020-06-03 12:58:10 -0700

    Add test for Tip query

--- zebra-state/src/lib.rs
+++ zebra-state/src/lib.rs
@@ -63,4 +63,55 @@ mod tests {
 
         Ok(())
     }
+
+    #[tokio::test]
+    async fn get_tip() -> Result<(), Report> {
+        let block0: Arc<_> =
+            Block::zcash_deserialize(&zebra_test_vectors::BLOCK_MAINNET_GENESIS_BYTES[..])?.into();
+        let block1: Arc<_> =
+            Block::zcash_deserialize(&zebra_test_vectors::BLOCK_MAINNET_1_BYTES[..])?.into();
+
+        let expected_hash: BlockHeaderHash = block1.as_ref().into();
+
+        let mut service = in_memory::init();
+
+        // insert the higher block first
+        let response = service
+            .call(Request::AddBlock {
+                block: block1.clone(),
+            })
+            .await
+            .map_err(|e| eyre!(e))?;
+
+        ensure!(
+            matches!(response, Response::Added),
+            "unexpected response kind: {:?}",
+            response
+        );
+
+        // genesis block second
+        let response = service
+            .call(Request::AddBlock {
+                block: block0.clone(),
+            })
+            .await
+            .map_err(|e| eyre!(e))?;
+
+        ensure!(
+            matches!(response, Response::Added),
+            "unexpected response kind: {:?}",
+            response
+        );
+
+        let block_response = service.call(Request::GetTip).await.map_err(|e| eyre!(e))?;
+
+        // assert that the higher block is returned as the tip even tho it was
+        // least recently inserted
+        match block_response {
+            Response::Tip { hash } => assert_eq!(expected_hash, hash),
+            _ => bail!("unexpected response kind: {:?}", block_response),
+        }
+
+        Ok(())
+    }
 }
