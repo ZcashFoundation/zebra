b263489af668efdf8b9360a561d0955c3e09e478
Deirdre Connolly <deirdre@zfnd.org>
2020-06-06 18:10:32 -0400

    Export ed25519 zebra (#445)
    And use that one, and our similar redjubjub export, everywhere.
    
    Fixes #405
    
    * Move PRs to 'In Progress' column on project board when created

--- zebra-chain/src/transaction/joinsplit.rs
+++ zebra-chain/src/transaction/joinsplit.rs
@@ -1,7 +1,7 @@
 #[cfg(test)]
 use proptest::{array, collection::vec, prelude::*};
 
-use crate::{notes::sprout, proofs::ZkSnarkProof};
+use crate::{ed25519_zebra, notes::sprout, proofs::ZkSnarkProof};
 
 /// A _JoinSplit Description_, as described in [protocol specification §7.2][ps].
 ///
