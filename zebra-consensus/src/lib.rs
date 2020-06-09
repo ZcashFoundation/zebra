997201c9c7fe784097f5c27bcebf55eb09dc6b68
teor <teor@riseup.net>
2020-06-09 21:17:16 +1000

    consensus: Add a mempool stub

--- zebra-consensus/src/lib.rs
+++ zebra-consensus/src/lib.rs
@@ -3,7 +3,8 @@
 //! `verify::BlockVerifier` verifies blocks and their transactions, then adds them to
 //! `zebra_state::ZebraState`.
 //!
-//! `mempool::ZebraMempool` verifies transactions, and adds them to the mempool state.
+//! `mempool::MempoolTransactionVerifier` verifies transactions, and adds them to
+//! `mempool::ZebraMempoolState`.
 //!
 //! Consensus handling is provided using `tower::Service`s, to support backpressure
 //! and batch verification.
@@ -12,4 +13,5 @@
 #![doc(html_root_url = "https://doc.zebra.zfnd.org/zebra_consensus")]
 #![deny(missing_docs)]
 
+pub mod mempool;
 pub mod verify;
