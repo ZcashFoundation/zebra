3f5e2695e3c3c06298009fc89316652bc8451c7c
teor <teor@riseup.net>
2020-06-17 21:10:02 +1000

    chain: Tweak the version and time test bounds
    Use MAX constants for the block header version and time arbitrary test
    ranges. Reduces the block header time arbitrary test range from 2**32 to
    2**32-1 (u32::MAX). (2**32 is an invalid time value, which gets
    truncated to 0 during serialization.)
    
    Also add some comments about DateTime conversions.
    
    Part of #477.

--- zebra-chain/src/block/tests.rs
+++ zebra-chain/src/block/tests.rs
@@ -16,11 +16,13 @@ impl Arbitrary for BlockHeader {
 
     fn arbitrary_with(_args: ()) -> Self::Strategy {
         (
-            (4u32..2_147_483_647u32),
+            // version is interpreted as i32 in the spec, so we are limited to i32::MAX here
+            (4u32..(i32::MAX as u32)),
             any::<BlockHeaderHash>(),
             any::<MerkleTreeRootHash>(),
             any::<SaplingNoteTreeRootHash>(),
-            (0i64..4_294_967_296i64),
+            // time is interpreted as u32 in the spec, but rust timestamps are i64
+            (0i64..(u32::MAX as i64)),
             any::<u32>(),
             any::<[u8; 32]>(),
             any::<EquihashSolution>(),
