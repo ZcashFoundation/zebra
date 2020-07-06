d22e6eeb73e5c8aa3dfdd71fd9ee86dd9f6a707a
Jane Lusby <jane@zfnd.org>
2020-07-06 13:16:21 -0700

    Start work implementing sprout note commitment function

--- zebra-chain/src/notes.rs
+++ zebra-chain/src/notes.rs
@@ -6,3 +6,9 @@ pub mod sprout;
 /// The randomness used in the Pedersen Hash for note commitment.
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub struct NoteCommitmentRandomness(pub [u8; 32]);
+
+impl AsRef<[u8]> for NoteCommitmentRandomness {
+    fn as_ref(&self) -> &[u8] {
+        &self.0
+    }
+}
