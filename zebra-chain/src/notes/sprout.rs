d6aa0d4ea484dfe0bfc6172c91d6ca61832bfa7f
Deirdre Connolly <deirdre@zfnd.org>
2020-07-09 21:20:50 -0400

    More Note refinements

--- zebra-chain/src/notes/sprout.rs
+++ zebra-chain/src/notes/sprout.rs
@@ -53,6 +53,16 @@ impl ZcashSerialize for Nullifier {
     }
 }
 
+/// The randomness used in the Pedersen Hash for note commitment.
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct NoteCommitmentRandomness(pub [u8; 32]);
+
+impl AsRef<[u8]> for NoteCommitmentRandomness {
+    fn as_ref(&self) -> &[u8] {
+        &self.0
+    }
+}
+
 /// A Note represents that a value is spendable by the recipient who
 /// holds the spending key corresponding to a given shielded payment
 /// address.
