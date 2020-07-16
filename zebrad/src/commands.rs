49a3a7d6d1868a152fecdb68a993243e8eaa1dd8
teor <teor@riseup.net>
2020-07-16 17:04:42 +1000

    fix: Only launch network endpoints for server commands
    Fixes #669.

--- zebrad/src/commands.rs
+++ zebrad/src/commands.rs
@@ -7,11 +7,14 @@ mod seed;
 mod start;
 mod version;
 
+use self::ZebradCmd::*;
 use self::{
     connect::ConnectCmd, generate::GenerateCmd, revhex::RevhexCmd, seed::SeedCmd, start::StartCmd,
     version::VersionCmd,
 };
+
 use crate::config::ZebradConfig;
+
 use abscissa_core::{
     config::Override, Command, Configurable, FrameworkError, Help, Options, Runnable,
 };
@@ -61,11 +64,25 @@ impl ZebradCmd {
     /// If the command is `None`, then abscissa writes zebrad usage information
     /// to stdout.
     pub(crate) fn uses_stdout(&self) -> bool {
-        use ZebradCmd::*;
         match self {
             // List all the commands, so new commands have to make a choice here
             Generate(_) | Help(_) | Revhex(_) | Version(_) => true,
             Connect(_) | Seed(_) | Start(_) => false,
+        }
+    }
+
+    /// Returns true if this command is a server command.
+    ///
+    /// For example, `Start` acts as a Zcash node.
+    ///
+    /// Usage note: `abscissa_core::EntryPoint` stores an `Option<ZerbradCmd>`.
+    /// If the command is `None`, then abscissa prints zebrad's usage
+    /// information, then exits.
+    pub(crate) fn is_server(&self) -> bool {
+        match self {
+            // List all the commands, so new commands have to make a choice here
+            Connect(_) | Seed(_) | Start(_) => true,
+            Generate(_) | Help(_) | Revhex(_) | Version(_) => false,
         }
     }
 }
