1df13a8f9b8a10bf79d14d5b931f783f3b0da329
teor <teor@riseup.net>
2020-07-17 08:28:51 +1000

    feature: Limit the maximum checkpoint height gap

--- zebra-utils/calculate-checkpoints.sh
+++ zebra-utils/calculate-checkpoints.sh
@@ -20,18 +20,34 @@ set -euo pipefail
 
 # zebra-consensus accepts an ordered list of checkpoints, starting with the
 # genesis block. Checkpoint heights can be chosen arbitrarily.
+
+# We limit the memory usage for each checkpoint, based on the cumulative size of
+# the serialized blocks in the chain. Deserialized blocks are larger, because
+# they contain pointers and non-compact integers. But they should be within a
+# constant factor of the serialized size.
+MAX_CHECKPOINT_BYTE_COUNT=$((256*1024*1024))
+
+# We limit the maximum number of blocks in each checkpoint. Each block uses a
+# constant amount of memory for the supporting data structures and futures.
 #
-# We select checkpoints that have approximately equal memory usage, based on the
-# cumulative size of the blocks in the chain. To support incremental list
-# updates, we choose the first block that is at least $CHECKPOINT_SIZE_SPACING
-# bytes after the previous checkpoint.
-MIN_CHECKPOINT_BYTE_COUNT=$((256*1024*1024))
+# TODO: set this gap to half the sync lookahead distance.
+MAX_CHECKPOINT_HEIGHT_GAP=2000
 
 cumulative_bytes=0
+height_gap=0
 while read -r height size hash; do
     cumulative_bytes=$((cumulative_bytes + size))
+    height_gap=$((height_gap + 1))
+
+    # Checkpoints can be slightly larger the maximum byte count. That's ok,
+    # because the memory usage is only approximate. (This is a bash-specific
+    # optimisation, to avoid keeping a copy of the previous height and hash.
+    # Since exact sizes don't matter, we can use the same check in the Rust
+    # implementation. Or choose a simpler alternative.)
     if [ "$height" -eq 0 ] || \
-       [ "$cumulative_bytes" -ge "$MIN_CHECKPOINT_BYTE_COUNT" ]; then
+       [ "$cumulative_bytes" -ge "$MAX_CHECKPOINT_BYTE_COUNT" ] || \
+       [ "$height_gap" -ge "$MAX_CHECKPOINT_HEIGHT_GAP" ]; then
+
         # Reverse the byte order of hash.
         #
         # We reverse the hash after selecting the checkpoints, because launching
@@ -39,7 +55,10 @@ while read -r height size hash; do
         # optimisation, the Rust implementation should reverse hashes as it loads
         # them.)
         hash=$(zebrad revhex "$hash")
+
         echo "$height $hash"
+
         cumulative_bytes=0
+        height_gap=0
     fi
 done
