66d1b780f6dd30908e6d2eea08ead16c0508e9ce
Alfredo Garcia <oxarbitrage@gmail.com>
2020-07-21 15:05:52 -0300

    rustfmt

--- zebrad/src/commands/revhex.rs
+++ zebrad/src/commands/revhex.rs
@@ -26,10 +26,7 @@ impl Runnable for RevhexCmd {
             // We can distinguish EOF from an empty line, because the newline is
             // included in the buffer, so empty lines return Ok(1).
             while stdin().read_line(&mut input).unwrap_or(0) > 0 {
-                println!(
-                    "{}",
-                    zebra_chain::utils::byte_reverse_hex(&input.trim())
-                );
+                println!("{}", zebra_chain::utils::byte_reverse_hex(&input.trim()));
             }
         } else {
             println!("{}", zebra_chain::utils::byte_reverse_hex(&self.input));
