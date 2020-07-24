af7500f2e2e5d3b3581aba884b888a7fc579712c
Alfredo Garcia <oxarbitrage@gmail.com>
2020-07-24 10:19:58 -0300

    refine used types

--- zebra-utils/src/bin/zebra-checkpoints/main.rs
+++ zebra-utils/src/bin/zebra-checkpoints/main.rs
@@ -15,6 +15,8 @@ use std::process::Stdio;
 use structopt::StructOpt;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
+use zebra_chain::types::BlockHeight;
+
 mod args;
 
 /// We limit the maximum number of blocks in each checkpoint. Each block uses a
@@ -25,11 +27,11 @@ const MAX_CHECKPOINT_HEIGHT_GAP: usize = zebrad::commands::LOOKAHEAD_LIMIT / 2;
 /// the serialized blocks in the chain. Deserialized blocks are larger, because
 /// they contain pointers and non-compact integers. But they should be within a
 /// constant factor of the serialized size.
-const MAX_CHECKPOINT_BYTE_COUNT: i64 = 256 * 1024 * 1024;
+const MAX_CHECKPOINT_BYTE_COUNT: u64 = 256 * 1024 * 1024;
 
 /// Checkpoints must be on the main chain, so we skip blocks that are within the
 /// zcashd reorg limit.
-const BLOCK_REORG_LIMIT: i64 = 100;
+const BLOCK_REORG_LIMIT: BlockHeight = BlockHeight(100);
 
 // Passthrough arguments if needed
 fn passthrough(mut cmd: std::process::Command, args: &args::Args) -> std::process::Command {
@@ -50,21 +52,26 @@ fn main() -> Result<()> {
     cmd = passthrough(cmd, &args);
 
     // set up counters
-    let mut cumulative_bytes: i64 = 0;
-    let mut height_gap: usize = 0;
+    let mut cumulative_bytes: u64 = 0;
+    let mut height_gap: BlockHeight = BlockHeight(0);
 
     // get the current block count
     cmd.arg("getblockcount");
     cmd.stdout(Stdio::piped()).spawn().unwrap();
     let output = cmd.output().unwrap();
-    let mut block_count: i64 = String::from_utf8_lossy(&output.stdout)
+    let mut requested_height: BlockHeight = String::from_utf8_lossy(&output.stdout)
         .trim()
         .parse()
         .unwrap();
-    block_count -= BLOCK_REORG_LIMIT;
+    requested_height = BlockHeight(
+        requested_height
+            .0
+            .checked_sub(BLOCK_REORG_LIMIT.0)
+            .expect("zcashd has some mature blocks: wait for zcashd to sync more blocks"),
+    );
 
     // loop through all blocks
-    for x in 0..block_count {
+    for x in 0..requested_height.0 {
         // unfortunatly we need to create a process for each block
         let mut cmd = std::process::Command::new(&args.cli);
         cmd = passthrough(cmd, &args);
@@ -80,24 +87,31 @@ fn main() -> Result<()> {
 
         // get the values we are interested in
         let hash = &v["hash"].as_str().unwrap();
-        let height = &v["height"].as_i64().unwrap();
-        let size = &v["size"].as_i64().unwrap();
+        let height = BlockHeight(v["height"].as_u64().unwrap() as u32);
+        assert!(height <= BlockHeight::MAX);
+        assert_eq!(x, height.0);
+        let size = v["size"].as_u64().unwrap();
+        assert!(size <= zebra_chain::block::MAX_BLOCK_BYTES);
 
         // compute
         cumulative_bytes += size;
-        height_gap += 1;
+        height_gap = BlockHeight(height_gap.0 + 1);
 
         // check if checkpoint
-        if *height == 0
+        if height == BlockHeight(0)
             || cumulative_bytes >= MAX_CHECKPOINT_BYTE_COUNT
-            || height_gap >= MAX_CHECKPOINT_HEIGHT_GAP
+            || height_gap.0 >= MAX_CHECKPOINT_HEIGHT_GAP as u32
         {
             // print to output
-            println!("{} {}", height, zebra_chain::utils::byte_reverse_hex(hash));
+            println!(
+                "{} {}",
+                height.0,
+                zebra_chain::utils::byte_reverse_hex(hash)
+            );
 
             // reset counters
             cumulative_bytes = 0;
-            height_gap = 0;
+            height_gap = BlockHeight(0);
         }
     }
 
