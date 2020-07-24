487cfebfdec919609df1ee0d918e7faecbbea1dd
Henry de Valence <hdevalence@hdevalence.ca>
2020-07-24 11:12:23 -0700

    Align crate versions and user-agent with NU numbers.
    We had a brief discussion on discord and it seemed like we had consensus on the
    following versioning policy:
    
    * zebrad: match major version to NU version, so we will start by releasing
      zebrad 3.0.0;
    
    * zebra-* libraries: start by matching zebrad's version, then increment major
      versions of each library as we need to make breaking changes (potentially
      faster than the zebrad version, always respecting semver but making no
      guarantees about the longevity of major releases).
    
    This commit sets all of the crate versions to 3.0.0-alpha.0 -- the -alpha.0
    marks it as a prerelease not subject to perfect adherence to compatibility
    guarantees.

--- zebra-network/src/constants.rs
+++ zebra-network/src/constants.rs
@@ -34,7 +34,7 @@ pub const HEARTBEAT_INTERVAL: Duration = Duration::from_secs(60);
 pub const TIMESTAMP_TRUNCATION_SECONDS: i64 = 30 * 60;
 
 /// The User-Agent string provided by the node.
-pub const USER_AGENT: &str = "🦓Zebra v2.0.0-alpha.0🦓";
+pub const USER_AGENT: &str = "🦓 Zebra 3.0.0-alpha.0 🦓";
 
 /// The Zcash network protocol version implemented by this crate.
 ///
