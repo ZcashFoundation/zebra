1e787aecb938db3835e50b10f953b49c4e1d4fee
teor <teor@riseup.net>
2020-07-24 13:17:39 +1000

    feature: Check the previous block height in BlockVerifier
    This is a temporary busy-waiting fix.

--- zebra-consensus/src/checkpoint/tests.rs
+++ zebra-consensus/src/checkpoint/tests.rs
@@ -305,7 +305,7 @@ async fn continuous_blockchain(restart_height: Option<BlockHeight>) -> Result<()
                 break;
             }
 
-            /// SPANDOC: Make sure the verifier service is ready
+            /// SPANDOC: Make sure the verifier service is ready for block {?height}
             let ready_verifier_service = checkpoint_verifier
                 .ready_and()
                 .map_err(|e| eyre!(e))
@@ -317,7 +317,7 @@ async fn continuous_blockchain(restart_height: Option<BlockHeight>) -> Result<()
                 ready_verifier_service.call(block.clone()),
             );
 
-            /// SPANDOC: spawn verification future in the background
+            /// SPANDOC: spawn verification future in the background for block {?height}
             let handle = tokio::spawn(verify_future.in_current_span());
             handles.push(handle);
 
