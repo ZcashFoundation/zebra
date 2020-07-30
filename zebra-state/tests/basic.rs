c5e83dae69a5902471b771a6248229869aa83f61
Alfredo Garcia <oxarbitrage@gmail.com>
2020-07-30 10:30:42 -0300

    remove mutex and just add more transcripts

--- zebra-state/tests/basic.rs
+++ zebra-state/tests/basic.rs
@@ -1,7 +1,6 @@
 use color_eyre::eyre::Report;
-use lazy_static::lazy_static;
 use once_cell::sync::Lazy;
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
 use tempdir::TempDir;
 
 use zebra_chain::{block::Block, serialization::ZcashDeserialize, Network, Network::*};
@@ -9,25 +8,11 @@ use zebra_test::transcript::Transcript;
 
 use zebra_state::*;
 
-lazy_static! {
-    pub static ref MODE: Mutex<Network> = Mutex::new(Network::Mainnet);
-}
-
-pub fn read_mode() -> Network {
-    *MODE.lock().unwrap()
-}
-
-pub fn write_mode(network: Network) {
-    *MODE.lock().unwrap() = network;
-}
-
-static ADD_BLOCK_TRANSCRIPT: Lazy<Vec<(Request, Response)>> = Lazy::new(|| {
-    let block: Arc<_> = Block::zcash_deserialize(match read_mode() {
-        Network::Testnet => &zebra_test::vectors::BLOCK_TESTNET_10_BYTES[..],
-        _ => &zebra_test::vectors::BLOCK_MAINNET_415000_BYTES[..],
-    })
-    .unwrap()
-    .into();
+static ADD_BLOCK_TRANSCRIPT_MAINNET: Lazy<Vec<(Request, Response)>> = Lazy::new(|| {
+    let block: Arc<_> =
+        Block::zcash_deserialize(&zebra_test::vectors::BLOCK_MAINNET_415000_BYTES[..])
+            .unwrap()
+            .into();
     let hash = block.as_ref().into();
     vec![
         (
@@ -40,19 +25,54 @@ static ADD_BLOCK_TRANSCRIPT: Lazy<Vec<(Request, Response)>> = Lazy::new(|| {
     ]
 });
 
-static GET_TIP_TRANSCRIPT: Lazy<Vec<(Request, Response)>> = Lazy::new(|| {
-    let block0: Arc<_> = Block::zcash_deserialize(match read_mode() {
-        Network::Testnet => &zebra_test::vectors::BLOCK_TESTNET_GENESIS_BYTES[..],
-        _ => &zebra_test::vectors::BLOCK_MAINNET_GENESIS_BYTES[..],
-    })
-    .unwrap()
-    .into();
-    let block1: Arc<_> = Block::zcash_deserialize(match read_mode() {
-        Network::Testnet => &zebra_test::vectors::BLOCK_TESTNET_1_BYTES[..],
-        _ => &zebra_test::vectors::BLOCK_MAINNET_1_BYTES[..],
-    })
-    .unwrap()
-    .into();
+static ADD_BLOCK_TRANSCRIPT_TESTNET: Lazy<Vec<(Request, Response)>> = Lazy::new(|| {
+    let block: Arc<_> = Block::zcash_deserialize(&zebra_test::vectors::BLOCK_TESTNET_10_BYTES[..])
+        .unwrap()
+        .into();
+    let hash = block.as_ref().into();
+    vec![
+        (
+            Request::AddBlock {
+                block: block.clone(),
+            },
+            Response::Added { hash },
+        ),
+        (Request::GetBlock { hash }, Response::Block { block }),
+    ]
+});
+
+static GET_TIP_TRANSCRIPT_MAINNET: Lazy<Vec<(Request, Response)>> = Lazy::new(|| {
+    let block0: Arc<_> =
+        Block::zcash_deserialize(&zebra_test::vectors::BLOCK_MAINNET_GENESIS_BYTES[..])
+            .unwrap()
+            .into();
+    let block1: Arc<_> = Block::zcash_deserialize(&zebra_test::vectors::BLOCK_MAINNET_1_BYTES[..])
+        .unwrap()
+        .into();
+    let hash0 = block0.as_ref().into();
+    let hash1 = block1.as_ref().into();
+    vec![
+        // Insert higher block first, lower block second
+        (
+            Request::AddBlock { block: block1 },
+            Response::Added { hash: hash1 },
+        ),
+        (
+            Request::AddBlock { block: block0 },
+            Response::Added { hash: hash0 },
+        ),
+        (Request::GetTip, Response::Tip { hash: hash1 }),
+    ]
+});
+
+static GET_TIP_TRANSCRIPT_TESTNET: Lazy<Vec<(Request, Response)>> = Lazy::new(|| {
+    let block0: Arc<_> =
+        Block::zcash_deserialize(&zebra_test::vectors::BLOCK_TESTNET_GENESIS_BYTES[..])
+            .unwrap()
+            .into();
+    let block1: Arc<_> = Block::zcash_deserialize(&zebra_test::vectors::BLOCK_TESTNET_10_BYTES[..])
+        .unwrap()
+        .into();
     let hash0 = block0.as_ref().into();
     let hash1 = block1.as_ref().into();
     vec![
@@ -76,7 +96,6 @@ async fn check_transcripts_mainnet() -> Result<(), Report> {
 
 #[tokio::test]
 async fn check_transcripts_testnet() -> Result<(), Report> {
-    write_mode(Network::Testnet);
     check_transcripts(Testnet).await
 }
 
@@ -84,7 +103,13 @@ async fn check_transcripts_testnet() -> Result<(), Report> {
 async fn check_transcripts(network: Network) -> Result<(), Report> {
     zebra_test::init();
 
-    for transcript_data in &[&ADD_BLOCK_TRANSCRIPT, &GET_TIP_TRANSCRIPT] {
+    let mainnet_transcript = &[&ADD_BLOCK_TRANSCRIPT_TESTNET, &GET_TIP_TRANSCRIPT_TESTNET];
+    let testnet_transcript = &[&ADD_BLOCK_TRANSCRIPT_MAINNET, &GET_TIP_TRANSCRIPT_MAINNET];
+
+    for transcript_data in match network {
+        Network::Testnet => testnet_transcript,
+        _ => mainnet_transcript,
+    } {
         let service = in_memory::init();
         let transcript = Transcript::from(transcript_data.iter().cloned());
         /// SPANDOC: check the in memory service against the transcript
