98fe712babe9e34cc436d930b0560dd2ad5bf16d
Jane Lusby <jane@zfnd.org>
2020-08-10 19:59:26 -0700

    make the test useful

--- zebra-chain/src/transaction/sighash.rs
+++ zebra-chain/src/transaction/sighash.rs
@@ -32,14 +32,23 @@ pub struct SigHasher<'a> {
 impl<'a> SigHasher<'a> {
     pub fn sighash(self) -> Hash {
         use NetworkUpgrade::*;
+        let mut hash = blake2b_simd::Params::new()
+            .hash_length(32)
+            .personal(&self.personal())
+            .to_state();
+
         match self.network_upgrade() {
             Genesis => unimplemented!(),
             BeforeOverwinter => unimplemented!(),
-            Overwinter | Sapling => self.sighash_zip143(),
+            Overwinter | Sapling => self
+                .hash_sighash_zip143(&mut hash)
+                .expect("serialization into hasher never fails"),
             Blossom => unimplemented!(),
             Heartwood => unimplemented!(),
             Canopy => unimplemented!(),
         }
+
+        hash.finalize()
     }
 
     fn network_upgrade(&self) -> NetworkUpgrade {
@@ -51,48 +60,16 @@ impl<'a> SigHasher<'a> {
     }
 
     /// Sighash implementation for the overwinter and sapling consensus branches
-    fn sighash_zip143(&self) -> Hash {
-        let mut personal = [0; 16];
-        (&mut personal[..12]).copy_from_slice(ZCASH_SIGHASH_PERSONALIZATION_PREFIX);
-        (&mut personal[12..])
-            .write_u32::<LittleEndian>(self.consensus_branch_id().into())
-            .unwrap();
-
-        let mut hash = blake2b_simd::Params::new()
-            .hash_length(32)
-            .personal(&personal)
-            .to_state();
-
-        hash.write_u32::<LittleEndian>(self.trans.header())
-            .expect("write to hasher will never fail");
-
-        hash.write_u32::<LittleEndian>(self.trans.group_id().expect("fOverwintered is always set"))
-            .expect("write to hasher will never fail");
-
-        self.hash_prevouts(&mut hash)
-            .expect("write to hasher will never fail");
-        self.hash_sequence(&mut hash)
-            .expect("write to hasher will never fail");
-        self.hash_outputs(&mut hash)
-            .expect("write to hasher will never fail");
-        self.hash_joinsplits(&mut hash)
-            .expect("write to hasher will never fail");
-
-        self.trans
-            .lock_time()
-            .zcash_serialize(&mut hash)
-            .expect("write to hasher will never fail");
-
-        hash.write_u32::<LittleEndian>(
-            self.trans
-                .expiry_height()
-                .expect("fOverwintered is always set")
-                .0,
-        )
-        .expect("write to hasher will never fail");
-
-        hash.write_u32::<LittleEndian>(self.hash_type)
-            .expect("write to hasher will never fail");
+    fn hash_sighash_zip143<W: io::Write>(&self, mut writer: W) -> Result<(), io::Error> {
+        self.hash_header(&mut writer)?;
+        self.hash_groupid(&mut writer)?;
+        self.hash_prevouts(&mut writer)?;
+        self.hash_sequence(&mut writer)?;
+        self.hash_outputs(&mut writer)?;
+        self.hash_joinsplits(&mut writer)?;
+        self.hash_lock_time(&mut writer)?;
+        self.hash_expiry_height(&mut writer)?;
+        self.hash_hash_type(&mut writer)?;
 
         // if let Some((n, script_code, amount)) = transparent_input {
         //     let mut data = vec![];
@@ -105,11 +82,29 @@ impl<'a> SigHasher<'a> {
         //     h.update(&data);
         // }
 
-        hash.finalize()
+        Ok(())
+    }
+
+    pub(super) fn personal(&self) -> [u8; 16] {
+        let mut personal = [0; 16];
+        (&mut personal[..12]).copy_from_slice(ZCASH_SIGHASH_PERSONALIZATION_PREFIX);
+        (&mut personal[12..])
+            .write_u32::<LittleEndian>(self.consensus_branch_id().into())
+            .unwrap();
+        personal
+    }
+
+    fn hash_header<W: io::Write>(&self, mut writer: W) -> Result<(), io::Error> {
+        writer.write_u32::<LittleEndian>(self.trans.header())
+    }
+
+    fn hash_groupid<W: io::Write>(&self, mut writer: W) -> Result<(), io::Error> {
+        writer
+            .write_u32::<LittleEndian>(self.trans.group_id().expect("fOverwintered is always set"))
     }
 
     fn hash_prevouts<W: io::Write>(&self, mut writer: W) -> Result<(), io::Error> {
-        if self.hash_type & SIGHASH_ANYONECANPAY == 0 {
+        if self.hash_type & SIGHASH_ANYONECANPAY != 0 {
             return writer.write_all(&[0; 32]);
         }
 
@@ -134,16 +129,16 @@ impl<'a> SigHasher<'a> {
     }
 
     fn hash_sequence<W: io::Write>(&self, mut writer: W) -> Result<(), io::Error> {
-        if self.hash_type & SIGHASH_ANYONECANPAY == 0
-            && (self.hash_type & SIGHASH_MASK) != SIGHASH_SINGLE
-            && (self.hash_type & SIGHASH_MASK) != SIGHASH_NONE
+        if self.hash_type & SIGHASH_ANYONECANPAY != 0
+            || (self.hash_type & SIGHASH_MASK) == SIGHASH_SINGLE
+            || (self.hash_type & SIGHASH_MASK) == SIGHASH_NONE
         {
             return writer.write_all(&[0; 32]);
         }
 
         let mut hash = blake2b_simd::Params::new()
             .hash_length(32)
-            .personal(ZCASH_PREVOUTS_HASH_PERSONALIZATION)
+            .personal(ZCASH_SEQUENCE_HASH_PERSONALIZATION)
             .to_state();
 
         let mut buf = vec![];
@@ -162,19 +157,20 @@ impl<'a> SigHasher<'a> {
     }
 
     /// Writes the u256 hash of the transactions outputs to the provided Writer
-    fn hash_outputs<W: io::Write>(&self, mut writer: W) -> Result<(), io::Error> {
-        if (self.hash_type & SIGHASH_MASK) != SIGHASH_SINGLE
-            && (self.hash_type & SIGHASH_MASK) != SIGHASH_NONE
-        {
-            self.outputs_hash(writer)
-        // } else if (self.hash_type & SIGHASH_MASK) == SIGHASH_SINGLE
-        //     && transparent_input.is_some()
-        //     && transparent_input.as_ref().unwrap().0 < tx.vout.len()
+    fn hash_outputs<W: io::Write>(&self, writer: W) -> Result<(), io::Error> {
+        todo!()
+        // if (self.hash_type & SIGHASH_MASK) == SIGHASH_SINGLE
+        //     || (self.hash_type & SIGHASH_MASK) == SIGHASH_NONE
         // {
-        //     self.single_output_hash(writer);
-        } else {
-            writer.write_all(&[0; 32])
-        }
+        //     self.outputs_hash(writer)
+        // } else if (self.hash_type & SIGHASH_MASK) == SIGHASH_SINGLE
+        // //     && transparent_input.is_some()
+        // //     && transparent_input.as_ref().unwrap().0 < tx.vout.len()
+        // {
+        //     self.single_output_hash(writer)
+        // } else {
+        //     writer.write_all(&[0; 32])
+        // }
     }
 
     fn outputs_hash<W: io::Write>(&self, writer: W) -> Result<(), io::Error> {
@@ -196,6 +192,23 @@ impl<'a> SigHasher<'a> {
 
         // todo!()
     }
+
+    fn hash_lock_time<W: io::Write>(&self, mut writer: W) -> Result<(), io::Error> {
+        self.trans.lock_time().zcash_serialize(&mut writer)
+    }
+
+    fn hash_expiry_height<W: io::Write>(&self, mut writer: W) -> Result<(), io::Error> {
+        writer.write_u32::<LittleEndian>(
+            self.trans
+                .expiry_height()
+                .expect("fOverwintered is always set")
+                .0,
+        )
+    }
+
+    fn hash_hash_type<W: io::Write>(&self, mut writer: W) -> Result<(), io::Error> {
+        writer.write_u32::<LittleEndian>(self.hash_type)
+    }
 }
 
 #[cfg(test)]
@@ -214,7 +227,7 @@ mod test {
             $hasher
                 .$f(&mut buf)
                 .expect("hashing into a vec never fails");
-            assert_eq!($expected.as_ref(), buf.as_slice());
+            assert_eq!(hex::decode($expected.as_ref()).unwrap(), buf);
         };
     }
 
@@ -228,14 +241,48 @@ mod test {
             trans: &transaction,
             hash_type: 1,
             network: Network::Mainnet,
-            height: BlockHeight(1),
+            height: BlockHeight(653600 - 1),
         };
+
+        assert_hash_eq!(b"03000080", hasher, hash_header);
+
+        assert_hash_eq!(b"7082c403", hasher, hash_groupid);
 
         assert_hash_eq!(
             b"d53a633bbecf82fe9e9484d8a0e727c73bb9e68c96e72dec30144f6a84afa136",
             hasher,
             hash_prevouts
         );
+
+        assert_hash_eq!(
+            b"a5f25f01959361ee6eb56a7401210ee268226f6ce764a4f10b7f29e54db37272",
+            hasher,
+            hash_sequence
+        );
+
+        // assert_hash_eq!(
+        //     b"ab6f7f6c5ad6b56357b5f37e16981723db6c32411753e28c175e15589172194a",
+        //     hasher,
+        //     hash_outputs
+        // );
+
+        // assert_hash_eq!(
+        //     b"0000000000000000000000000000000000000000000000000000000000000000",
+        //     hasher,
+        //     hash_joinsplits
+        // );
+
+        assert_hash_eq!(b"481cdd86", hasher, hash_lock_time);
+
+        assert_hash_eq!(b"b3cc4318", hasher, hash_expiry_height);
+
+        assert_hash_eq!(b"01000000", hasher, hash_hash_type);
+
+        // assert_hash_eq!(
+        //     b"a1f1a4e5cd9bd522322d661edd2af1bf2a7019cfab94ece18f4ba935b0a19073",
+        //     hasher,
+        //     hash_sighash_zip143
+        // );
 
         Ok(())
     }
