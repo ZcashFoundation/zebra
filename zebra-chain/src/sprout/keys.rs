38a55dc2be8382553b738640980d1bceacb20254
Ramana Venkata <vramana@users.noreply.github.com>
2020-08-22 04:39:47 +0530

    Update zebra-chain/src/sprout/keys.rs
    Co-authored-by: Jane Lusby <jlusby42@gmail.com>

--- zebra-chain/src/sprout/keys.rs
+++ zebra-chain/src/sprout/keys.rs
@@ -38,7 +38,7 @@ mod sk_magics {
 /// https://zips.z.cash/protocol/protocol.pdf#sproutkeycomponents
 fn prf_addr(x: [u8; 32], t: u8) -> [u8; 32] {
     let mut state = [0u32; 8];
-    let mut block: GenericArray<u8, U64> = GenericArray::default();
+    let mut block = GenericArray::<u8, U64>::default();
 
     block.as_mut_slice()[0..32].copy_from_slice(&x[..]);
     // The first four bits –i.e. the most signicant four bits of the
