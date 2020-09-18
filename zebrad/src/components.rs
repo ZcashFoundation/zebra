4f02fd8cf2db1c732cbc6b848f212686be6ba981
Henry de Valence <hdevalence@hdevalence.ca>
2020-09-18 16:13:57 -0700

    zebrad: document Inbound, ChainSync responsibilities

--- zebrad/src/components.rs
+++ zebrad/src/components.rs
@@ -1,3 +1,10 @@
+//! Holds components of a Zebra node.
+//!
+//! Some, but not all, of these components are structured as Abscissa components,
+//! while the others are just ordinary structures. This is because Abscissa's
+//! component and dependency injection models are designed to work together, but
+//! don't fit the async context well.
+
 mod inbound;
 pub mod metrics;
 mod sync;
