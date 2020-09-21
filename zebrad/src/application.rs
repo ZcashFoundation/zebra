b94b1d1fafb481f265359ad59b8a014e2467eaad
Jane Lusby <jane@zfnd.org>
2020-09-21 15:17:16 -0700

    disable reporting url for timeout errors

--- zebrad/src/application.rs
+++ zebrad/src/application.rs
@@ -92,6 +92,12 @@ impl Application for ZebradApp {
         color_eyre::config::HookBuilder::default()
             .issue_url(concat!(env!("CARGO_PKG_REPOSITORY"), "/issues/new"))
             .add_issue_metadata("version", env!("CARGO_PKG_VERSION"))
+            .issue_filter(|kind| match kind {
+                color_eyre::ErrorKind::NonRecoverable(_) => true,
+                color_eyre::ErrorKind::Recoverable(error) => {
+                    !error.is::<tower::timeout::error::Elapsed>()
+                }
+            })
             .install()
             .unwrap();
 
