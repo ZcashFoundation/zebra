f4ca8e16e3fbbff0a43fc2b19292be19c982cdb0
teor <teor@riseup.net>
2020-09-30 14:57:13 +1000

    Add testnet and halving subsidy tests

--- zebra-consensus/src/block/subsidy/general.rs
+++ zebra-consensus/src/block/subsidy/general.rs
@@ -93,45 +93,174 @@ mod test {
     use color_eyre::Report;
 
     #[test]
-    fn test_halving() -> Result<(), Report> {
-        let network = Network::Mainnet;
+    fn halving_test() -> Result<(), Report> {
+        halving_for_network(Network::Mainnet)?;
+        halving_for_network(Network::Testnet)?;
+
+        Ok(())
+    }
+
+    fn halving_for_network(network: Network) -> Result<(), Report> {
         let blossom_height = Blossom.activation_height(network).unwrap();
-        let canopy_height = Canopy.activation_height(network).unwrap();
+        let first_halving_height = match network {
+            Network::Mainnet => Canopy.activation_height(network).unwrap(),
+            // Based on "7.7 Calculation of Block Subsidy and Founders’ Reward"
+            Network::Testnet => Height(1_116_000),
+        };
 
         assert_eq!(1, halving_divisor((blossom_height - 1).unwrap(), network));
         assert_eq!(1, halving_divisor(blossom_height, network));
-        assert_eq!(1, halving_divisor((canopy_height - 1).unwrap(), network));
-        assert_eq!(2, halving_divisor(canopy_height, network));
-        assert_eq!(2, halving_divisor((canopy_height + 1).unwrap(), network));
+        assert_eq!(
+            1,
+            halving_divisor((first_halving_height - 1).unwrap(), network)
+        );
+
+        assert_eq!(2, halving_divisor(first_halving_height, network));
+        assert_eq!(
+            2,
+            halving_divisor((first_halving_height + 1).unwrap(), network)
+        );
+
+        assert_eq!(
+            4,
+            halving_divisor(
+                (first_halving_height + POST_BLOSSOM_HALVING_INTERVAL).unwrap(),
+                network
+            )
+        );
+        assert_eq!(
+            8,
+            halving_divisor(
+                (first_halving_height + (POST_BLOSSOM_HALVING_INTERVAL.0 as i32 * 2)).unwrap(),
+                network
+            )
+        );
+
+        assert_eq!(
+            1024,
+            halving_divisor(
+                (first_halving_height + (POST_BLOSSOM_HALVING_INTERVAL.0 as i32 * 9)).unwrap(),
+                network
+            )
+        );
+        assert_eq!(
+            1024 * 1024,
+            halving_divisor(
+                (first_halving_height + (POST_BLOSSOM_HALVING_INTERVAL.0 as i32 * 19)).unwrap(),
+                network
+            )
+        );
+        assert_eq!(
+            1024 * 1024 * 1024,
+            halving_divisor(
+                (first_halving_height + (POST_BLOSSOM_HALVING_INTERVAL.0 as i32 * 29)).unwrap(),
+                network
+            )
+        );
+        assert_eq!(
+            1024 * 1024 * 1024 * 1024,
+            halving_divisor(
+                (first_halving_height + (POST_BLOSSOM_HALVING_INTERVAL.0 as i32 * 39)).unwrap(),
+                network
+            )
+        );
+
+        // The largest possible divisor
+        assert_eq!(
+            1 << 63,
+            halving_divisor(
+                (first_halving_height + (POST_BLOSSOM_HALVING_INTERVAL.0 as i32 * 62)).unwrap(),
+                network
+            )
+        );
 
         Ok(())
     }
 
     #[test]
-    fn test_block_subsidy() -> Result<(), Report> {
-        let network = Network::Mainnet;
-        let blossom_height = Blossom.activation_height(network).unwrap();
-        let canopy_height = Canopy.activation_height(network).unwrap();
+    fn block_subsidy_test() -> Result<(), Report> {
+        block_subsidy_for_network(Network::Mainnet)?;
+        block_subsidy_for_network(Network::Testnet)?;
 
-        // After slow-start mining and before Blossom the block reward is 12.5 ZEC
+        Ok(())
+    }
+
+    fn block_subsidy_for_network(network: Network) -> Result<(), Report> {
+        let blossom_height = Blossom.activation_height(network).unwrap();
+        let first_halving_height = match network {
+            Network::Mainnet => Canopy.activation_height(network).unwrap(),
+            // Based on "7.7 Calculation of Block Subsidy and Founders’ Reward"
+            Network::Testnet => Height(1_116_000),
+        };
+
+        // After slow-start mining and before Blossom the block subsidy is 12.5 ZEC
         // https://z.cash/support/faq/#what-is-slow-start-mining
         assert_eq!(
             Amount::try_from(1_250_000_000),
             block_subsidy((blossom_height - 1).unwrap(), network)
         );
 
-        // After Blossom the block reward is reduced to 6.25 ZEC without halving
+        // After Blossom the block subsidy is reduced to 6.25 ZEC without halving
         // https://z.cash/upgrade/blossom/
         assert_eq!(
             Amount::try_from(625_000_000),
             block_subsidy(blossom_height, network)
         );
 
-        // After 1st halving(coinciding with Canopy) the block reward is reduced to 3.125 ZEC
+        // After the 1st halving, the block subsidy is reduced to 3.125 ZEC
         // https://z.cash/upgrade/canopy/
         assert_eq!(
             Amount::try_from(312_500_000),
-            block_subsidy(canopy_height, network)
+            block_subsidy(first_halving_height, network)
+        );
+
+        // After the 2nd halving, the block subsidy is reduced to 1.5625 ZEC
+        // See "7.7 Calculation of Block Subsidy and Founders’ Reward"
+        assert_eq!(
+            Amount::try_from(156_250_000),
+            block_subsidy(
+                (first_halving_height + POST_BLOSSOM_HALVING_INTERVAL).unwrap(),
+                network
+            )
+        );
+
+        // After the 7th halving, the block subsidy is reduced to 0.04882812 ZEC
+        // Check that the block subsidy rounds down correctly, and there are no errors
+        assert_eq!(
+            Amount::try_from(4_882_812),
+            block_subsidy(
+                (first_halving_height + (POST_BLOSSOM_HALVING_INTERVAL.0 as i32 * 6)).unwrap(),
+                network
+            )
+        );
+
+        // After the 29th halving, the block subsidy is 1 zatoshi
+        // Check that the block subsidy is calculated correctly at the limit
+        assert_eq!(
+            Amount::try_from(1),
+            block_subsidy(
+                (first_halving_height + (POST_BLOSSOM_HALVING_INTERVAL.0 as i32 * 28)).unwrap(),
+                network
+            )
+        );
+
+        // After the 30th halving, there is no block subsidy
+        // Check that there are no errors
+        assert_eq!(
+            Amount::try_from(0),
+            block_subsidy(
+                (first_halving_height + (POST_BLOSSOM_HALVING_INTERVAL.0 as i32 * 29)).unwrap(),
+                network
+            )
+        );
+
+        // The largest possible divisor
+        assert_eq!(
+            Amount::try_from(0),
+            block_subsidy(
+                (first_halving_height + (POST_BLOSSOM_HALVING_INTERVAL.0 as i32 * 62)).unwrap(),
+                network
+            )
         );
 
         Ok(())
@@ -175,17 +304,20 @@ mod test {
         // TODO: After second halving, there will be no funding streams, and
         // miners will get all the block reward
 
+        // TODO: also add some very large halvings
+
         Ok(())
     }
 
     #[test]
     fn subsidy_is_correct_test() -> Result<(), Report> {
-        subsidy_is_correct(Network::Mainnet)?;
-        subsidy_is_correct(Network::Testnet)?;
+        subsidy_is_correct_for_network(Network::Mainnet)?;
+        subsidy_is_correct_for_network(Network::Testnet)?;
 
         Ok(())
     }
-    fn subsidy_is_correct(network: Network) -> Result<(), Report> {
+
+    fn subsidy_is_correct_for_network(network: Network) -> Result<(), Report> {
         use crate::block::check;
         use zebra_chain::{block::Block, serialization::ZcashDeserializeInto};
 
@@ -198,6 +330,7 @@ mod test {
                 .zcash_deserialize_into::<Block>()
                 .expect("block is structurally valid");
 
+            // TODO: first halving, second halving, and very large halvings
             if Height(height) > SLOW_START_INTERVAL
                 && Height(height) < Canopy.activation_height(network).unwrap()
             {
