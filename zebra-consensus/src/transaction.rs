07b7c9eccfe675b144eb28a79caf9e8914fb3f4e
Deirdre Connolly <deirdre@zfnd.org>
2020-10-09 00:31:42 -0400

    Clean up some things and validate all signatures

--- zebra-consensus/src/transaction.rs
+++ zebra-consensus/src/transaction.rs
@@ -12,21 +12,22 @@
 //! This is an internal module. Use `verify::BlockVerifier` for blocks and their
 //! transactions, or `mempool::MempoolTransactionVerifier` for mempool transactions.
 
-use displaydoc::Display;
-use futures::{FutureExt, TryFutureExt};
 use std::{
     future::Future,
     pin::Pin,
     sync::Arc,
     task::{Context, Poll},
 };
+
+use displaydoc::Display;
+use futures::{FutureExt, TryFutureExt};
 use thiserror::Error;
 use tower::{buffer::Buffer, util::BoxService, Service, ServiceExt};
 use tracing::instrument;
 
 use zebra_chain::{
     parameters::{Network, NetworkUpgrade::Sapling},
-    primitives::{ed25519, redjubjub::Error as RedjubjubError},
+    primitives::{ed25519, groth16, redjubjub},
     sapling,
     sighash::HashType,
     sprout,
@@ -40,10 +41,8 @@ use crate::{primitives::redjubjub, BoxError, Config};
 
 /// Internal transaction verification service.
 ///
-/// After verification, the transaction future completes. State changes are handled by
-/// `BlockVerifier` or `MempoolTransactionVerifier`.
-///
-/// `TransactionVerifier` is not yet implemented.
+/// After verification, the transaction future completes. State changes are
+/// handled by `BlockVerifier` or `MempoolTransactionVerifier`.
 #[derive(Default)]
 pub(crate) struct TransactionVerifier<S>
 where
@@ -51,28 +50,20 @@ where
     S::Future: Send + 'static,
 {
     script: ScriptVerifier<S>,
-    joinsplit: JoinSplitVerifier<S>,
-    spend: SpendVerifier<S>,
-    output: OutputVerifier<S>,
-    redjubjub: redjubjub::Verifier<S>,
-    ed25519: ed25519::batch::Verifier<S>,
+    groth16: groth16::Verifier<S>,
 }
 
 #[non_exhaustive]
 #[derive(Debug, Display, Error)]
 pub enum VerifyTransactionError {
-    ///
+    /// Could not verify a transparent script
     Script(VerifyScriptError),
-    ///
-    JoinSplit(VerifyJoinSplitError),
-    ///
+    /// Could not verify a Groth16 proof of a JoinSplit/Spend/Output description
+    Groth16(VerifyGroth16Error),
+    /// Could not verify a Ed25519 signature with JoinSplitData
     Ed25519(ed25519::Error),
-    ///
-    Spend(VerifySpendError),
-    ///
-    Output(VerifyOutputError),
-    ///
-    Redjubjub(RedjubjubError),
+    /// Could not verify a RedJubjub signature with ShieldedData
+    RedJubjub(redjubjub::Error),
 }
 
 impl<S> Service<Arc<Transaction>> for TransactionVerifier<S>
@@ -86,21 +77,13 @@ where
         Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send + 'static>>;
 
     fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-        // TODO: This lorge and must be busted up
-        match (
-            self.script.poll_ready(cx),
-            self.joinsplit.poll_ready(cx),
-            self.ed25519.poll_ready(cx),
-            self.spend.poll_ready(cx),
-            self.output.poll_ready(cx),
-            self.redjubjub.poll_ready(cx),
-        ) {
-            (Poll::Ready(Err(e)), _) => Poll::Ready(Err(VerifyTransactionError::Checkpoint(e))),
-
-            (_, Poll::Ready(Err(e))) => Poll::Ready(Err(VerifyTransactionError::Block(e))),
-
+        match (self.script.poll_ready(cx), self.groth16.poll_ready(cx)) {
+            // First, fail if either service fails.
+            (Poll::Ready(Err(e)), _) => Poll::Ready(Err(VerifyTransactionError::Script(e))),
+            (_, Poll::Ready(Err(e))) => Poll::Ready(Err(VerifyTransactionError::Groth16(e))),
+            // Second, we're unready if either service is unready.
             (Poll::Pending, _) | (_, Poll::Pending) => Poll::Pending,
-
+            // Finally, we're ready if both services are ready and OK.
             (Poll::Ready(Ok(())), Poll::Ready(Ok(()))) => Poll::Ready(Ok(())),
         }
     }
@@ -151,25 +134,44 @@ where
                 sig,
             } = joinsplit_d;
 
-            let msg = tx.data_to_be_signed();
-            self.ed25519
-                .call((pub_key.into(), sig.into(), msg.into()))
+            ed25519::VerificationKey::try_From(pub_key)
+                .and_then(|vk| vk.verify(sig, tx.data_to_be_signed()))
                 .map_err(VerifyTransactionError::Ed25519)
-                .boxed();
         }
 
         if let Some(shielded_data_d) = shielded_data {
+            let sighash = tx.sighash(
+                Network::Sapling, // TODO: pass this in
+                HashType::ALL,    // TODO: check these
+                None,             // TODO: check these
+            );
+
             shielded_data_d.spends().for_each(|spend| {
-                self.spend
-                    .call(spend)
-                    .map_err(VerifyTransactionError::VerifySpendError)
+                // TODO: check that spend.cv and spend.rk are NOT of small
+                // order.
+                // https://zips.z.cash/protocol/canopy.pdf#spenddesc
+
+                // Verify the spend authorization signature for each Spend
+                // description.
+                spend
+                    .rk
+                    .verify(sighash, spend.spend_auth_sig)
+                    .map_err(VerifyTransactionError::Redjubjub);
+
+                self.groth16
+                    .call(spend.into())
+                    .map_err(VerifyTransactionError::VerifyGroth16Error)
                     .boxed()
             });
 
             shielded_data_d.outputs().for_each(|output| {
-                self.output
-                    .call(output)
-                    .map_err(VerifyTransactionError::VerifyOutputError)
+                // TODO: check that output.cv and output.epk are NOT of small
+                // order.
+                // https://zips.z.cash/protocol/canopy.pdf#outputdesc
+
+                self.groth16
+                    .call(output.into())
+                    .map_err(VerifyTransactionError::VerifyGroth16Error)
                     .boxed()
             });
 
@@ -179,12 +181,6 @@ where
                 rest_outputs,
                 binding_sig,
             } = shielded_data_d;
-
-            let sighash = tx.sighash(
-                Network::Sapling, // TODO: pass this in
-                HashType::ALL,    // TODO: check these
-                None,             // TODO: check these
-            );
 
             // Checks the balance.
             //
@@ -202,7 +198,8 @@ where
             //
             // https://zips.z.cash/protocol/canopy.pdf#saplingbalance
             let bsk = shielded_data_d.binding_validating_key(value_balance);
-            bsk.verify(sighash, &binding_sig);
+            bsk.verify(sighash, &binding_sig)
+                .map_err(VerifyTransactionError::Redjubjub);
         }
     }
 }
