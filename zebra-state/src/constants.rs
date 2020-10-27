207562d5a38f37ebd21a0cfae0880f6e4e13e953
Jane Lusby <jane@zfnd.org>
2020-10-27 14:24:00 -0700

    state: add SLED_FORMAT_VERSION prefix to db path
    Also removes a redundant test.

--- zebra-state/src/constants.rs
+++ zebra-state/src/constants.rs
@@ -13,3 +13,5 @@ pub const MIN_TRASPARENT_COINBASE_MATURITY: block::Height = block::Height(100);
 /// coinbase transactions.
 pub const MAX_BLOCK_REORG_HEIGHT: block::Height =
     block::Height(MIN_TRASPARENT_COINBASE_MATURITY.0 - 1);
+
+pub const SLED_FORMAT_VERSION: u32 = 0;
