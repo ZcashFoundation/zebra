8ceeb29ea69cc9098b78e02c85b3c3ff8dca6665
Jane Lusby <jane@zfnd.org>
2020-10-27 14:23:31 -0700

    clippy: remove generated tests in empty crates

--- zebra-rpc/src/lib.rs
+++ zebra-rpc/src/lib.rs
@@ -1,11 +1,3 @@
 #![doc(html_favicon_url = "https://www.zfnd.org/images/zebra-favicon-128.png")]
 #![doc(html_logo_url = "https://www.zfnd.org/images/zebra-icon.png")]
 #![doc(html_root_url = "https://doc.zebra.zfnd.org/zebra_rpc")]
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
