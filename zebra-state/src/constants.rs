e11e8e1373092e77bd2a91cd7f76ce9f39b8179e
Deirdre Connolly <deirdre@zfnd.org>
2020-11-25 17:12:01 -0500

    s/TRASPARENT/TRANSPARENT/g

--- zebra-state/src/constants.rs
+++ zebra-state/src/constants.rs
@@ -3,13 +3,13 @@
 /// A transaction MUST NOT spend a transparent output of a coinbase transaction
 /// from a block less than 100 blocks prior to the spend. Note that transparent
 /// outputs of coinbase transactions include Founders' Reward outputs.
-pub const MIN_TRASPARENT_COINBASE_MATURITY: u32 = 100;
+pub const MIN_TRANSPARENT_COINBASE_MATURITY: u32 = 100;
 
 /// The maximum chain reorganisation height.
 ///
 /// Allowing reorganisations past this height could allow double-spends of
 /// coinbase transactions.
-pub const MAX_BLOCK_REORG_HEIGHT: u32 = MIN_TRASPARENT_COINBASE_MATURITY - 1;
+pub const MAX_BLOCK_REORG_HEIGHT: u32 = MIN_TRANSPARENT_COINBASE_MATURITY - 1;
 
 /// The database format version, incremented each time the database format changes.
 pub const DATABASE_FORMAT_VERSION: u32 = 3;
