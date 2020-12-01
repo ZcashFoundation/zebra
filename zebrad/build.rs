df89153a9b50b42a86d71b8c4020775f5240c4bd
Jane Lusby <jlusby42@gmail.com>
2020-12-01 11:30:11 -0800

    Update zebrad/build.rs
    Co-authored-by: teor <teor@riseup.net>

--- zebrad/build.rs
+++ zebrad/build.rs
@@ -6,6 +6,7 @@ fn main() {
     // Setup the flags, toggling off the 'SEMVER_FROM_CARGO_PKG' flag
     let mut flags = ConstantsFlags::empty();
     flags.toggle(ConstantsFlags::SHA_SHORT);
+    flags.toggle(ConstantsFlags::REBUILD_ON_HEAD_CHANGE);
 
     // Generate the 'cargo:' key output
     generate_cargo_keys(flags).expect("Unable to generate the cargo keys!");
