47d78d4cf429eea75e1c91e18ef5c213b38ce5b5
Deirdre Connolly <deirdre@zfnd.org>
2020-12-04 03:47:52 -0500

    Try sentry::init()

--- zebrad/src/bin/zebrad/main.rs
+++ zebrad/src/bin/zebrad/main.rs
@@ -7,5 +7,15 @@ use zebrad::application::APPLICATION;
 
 /// Boot Zebrad
 fn main() {
+    let _guard = sentry::init((
+        "https://94059ee72a44420286310990b7c614b5@o485484.ingest.sentry.io/5540918",
+        sentry::ClientOptions {
+            debug: true,
+            ..Default::default()
+        },
+    ));
+
+    sentry::capture_message("Hello World!", sentry::Level::Info);
+
     abscissa_core::boot(&APPLICATION);
 }
