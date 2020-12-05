44e1051deed6be27dde0f63af56233f1956961a2
Deirdre Connolly <deirdre@zfnd.org>
2020-12-05 14:45:37 -0500

    Debug

--- zebrad/src/bin/zebrad/main.rs
+++ zebrad/src/bin/zebrad/main.rs
@@ -3,23 +3,32 @@
 #![deny(warnings, missing_docs, trivial_casts, unused_qualifications)]
 #![forbid(unsafe_code)]
 
-use zebrad::application::APPLICATION;
+// use zebrad::application::APPLICATION;
 
 /// Boot Zebrad
 fn main() {
-    if cfg!(feature = "enable-sentry") {
-        let tracing_integration = sentry_tracing::TracingIntegration::default();
+    // if cfg!(feature = "enable-sentry") {
 
-        // The Sentry default config pulls in the DSN from the `SENTRY_DSN`
-        // environment variable.
-        std::mem::forget(sentry::init(
-            sentry::ClientOptions {
-                debug: true,
-                ..Default::default()
-            }
-            .add_integration(tracing_integration),
-        ));
-    }
+    // The Sentry default config pulls in the DSN from the `SENTRY_DSN`
+    // environment variable.
+    let _guard = sentry::init(
+        sentry::ClientOptions {
+            debug: true,
+            ..Default::default()
+        }, // .add_integration(sentry_tracing::TracingIntegration::default()),
+    );
+    // }
 
-    abscissa_core::boot(&APPLICATION);
+    let next = std::panic::take_hook();
+    std::panic::set_hook(Box::new(move |info| {
+        println!("Caught panic");
+        sentry::integrations::panic::panic_handler(info);
+        next(info);
+    }));
+
+    sentry::capture_message("Hello World!", sentry::Level::Info);
+
+    panic!("Everything is on fire!");
+
+    //abscissa_core::boot(&APPLICATION);
 }
