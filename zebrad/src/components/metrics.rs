d8ba2fd88e5343bd6ba7c334e310ad7016263df2
teor <teor@riseup.net>
2020-12-19 13:19:58 +1000

    Add state, metrics, and tracing conflict tests

--- zebrad/src/components/metrics.rs
+++ zebrad/src/components/metrics.rs
@@ -12,12 +12,14 @@ impl MetricsEndpoint {
     /// Create the component.
     pub fn new(config: &ZebradConfig) -> Result<Self, FrameworkError> {
         if let Some(addr) = config.metrics.endpoint_addr {
-            info!("Initializing metrics endpoint at {}", addr);
             let endpoint_result = metrics_exporter_prometheus::PrometheusBuilder::new()
                 .listen_address(addr)
                 .install();
             match endpoint_result {
-                Ok(endpoint) => endpoint,
+                Ok(endpoint) => {
+                    info!("Opened metrics endpoint at {}", addr);
+                    endpoint
+                }
                 Err(e) => panic!(
                     "Opening metrics endpoint listener {:?} failed: {:?}. \
                      Hint: Check if another zebrad or zcashd process is running. \
