391c53aa60a38c5c975de22671b7ee56490858f5
teor <teor@riseup.net>
2021-01-25 19:33:47 +1000

    Move BoxError to zebrad's lib.rs
    For consistency with other crates.

--- zebrad/src/lib.rs
+++ zebrad/src/lib.rs
@@ -26,6 +26,13 @@
 #[macro_use]
 extern crate tracing;
 
+/// Error type alias to make working with tower traits easier.
+///
+/// Note: the 'static lifetime bound means that the *type* cannot have any
+/// non-'static lifetimes, (e.g., when a type contains a borrow and is
+/// parameterized by 'a), *not* that the object itself has 'static lifetime.
+pub type BoxError = Box<dyn std::error::Error + Send + Sync + 'static>;
+
 mod components;
 
 pub mod application;
