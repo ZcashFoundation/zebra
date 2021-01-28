cbdcddfdadeccc6a45320647ae0e199d0cda45a5
Alfredo Garcia <oxarbitrage@gmail.com>
2021-01-28 13:43:58 -0300

    escape

--- zebrad/tests/acceptance.rs
+++ zebrad/tests/acceptance.rs
@@ -1161,8 +1161,8 @@ fn zcash_state_conflict() -> Result<()> {
     check_config_conflict(
         dir_conflict.path(),
         format!(
-            "(Opened Zebra state cache at).*({})",
-            dir_conflict_full.to_str().unwrap()
+            r"(Opened Zebra state cache at ).*({})",
+            regex::escape(dir_conflict_full.to_str().unwrap())
         )
         .as_str(),
         dir_conflict.path(),
