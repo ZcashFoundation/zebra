bb872bca74e13dcac18de62606ebdde0140642f1
Jane Lusby <jane@zfnd.org>
2021-02-18 14:33:06 -0800

    remove unnecessary Option around request timeout

--- zebra-network/src/peer/handshake.rs
+++ zebra-network/src/peer/handshake.rs
@@ -435,7 +435,6 @@ where
                 svc: inbound_service,
                 client_rx: server_rx.into(),
                 peer_tx,
-                request_timer: None,
             };
 
             tokio::spawn(
