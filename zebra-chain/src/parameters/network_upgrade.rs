fa215fb0a66664ec8d4494e2f2f35af120821d6a
Jane Lusby <jane@zfnd.org>
2021-02-26 14:12:36 -0800

    Add consensus branch ID for NU5

--- zebra-chain/src/parameters/network_upgrade.rs
+++ zebra-chain/src/parameters/network_upgrade.rs
@@ -96,6 +96,7 @@ pub(crate) const CONSENSUS_BRANCH_IDS: &[(NetworkUpgrade, ConsensusBranchId)] = 
     (Blossom, ConsensusBranchId(0x2bb40e60)),
     (Heartwood, ConsensusBranchId(0xf5b9230b)),
     (Canopy, ConsensusBranchId(0xe9ff75a6)),
+    (NU5, ConsensusBranchId(0xf919a198)),
 ];
 
 /// The target block spacing before Blossom.
