41d0c695c3c977c46d6e8037d440e80f54894250
Alfredo Garcia <oxarbitrage@gmail.com>
2021-03-01 14:57:46 -0300

    rustfmt

--- zebra-chain/src/transaction.rs
+++ zebra-chain/src/transaction.rs
@@ -195,7 +195,7 @@ impl Transaction {
             ),
             // No JoinSplits
             Transaction::V1 { .. } | Transaction::V5 { .. } => Box::new(std::iter::empty()),
-            _ => Box::new(std::iter::empty())
+            _ => Box::new(std::iter::empty()),
         }
     }
 
@@ -209,10 +209,14 @@ impl Transaction {
                 shielded_data: Some(shielded_data),
                 ..
             } => Box::new(shielded_data.nullifiers()),
-            Transaction::V5 { .. } => unimplemented!("v5 transaction format as specified in ZIP-225"),
+            Transaction::V5 { .. } => {
+                unimplemented!("v5 transaction format as specified in ZIP-225")
+            }
             // No JoinSplits
-            Transaction::V1 { .. } | Transaction::V2 { .. } | Transaction::V3 { .. } => Box::new(std::iter::empty()),
-            _ => Box::new(std::iter::empty())
+            Transaction::V1 { .. } | Transaction::V2 { .. } | Transaction::V3 { .. } => {
+                Box::new(std::iter::empty())
+            }
+            _ => Box::new(std::iter::empty()),
         }
     }
 
