3817ecdc5a5bb104afc475fd39cc57e3d775f0cd
teor <teor@riseup.net>
2021-03-01 13:44:49 +1000

    rustfmt

--- zebra-chain/src/transaction/sighash.rs
+++ zebra-chain/src/transaction/sighash.rs
@@ -245,7 +245,9 @@ impl<'a> SigHasher<'a> {
             Transaction::V1 { .. } | Transaction::V2 { .. } => unreachable!(ZIP143_EXPLANATION),
             Transaction::V3 { joinsplit_data, .. } => joinsplit_data.is_some(),
             Transaction::V4 { joinsplit_data, .. } => joinsplit_data.is_some(),
-            Transaction::V5 { .. } => unimplemented!("v5 transaction format as specified in ZIP-225"),
+            Transaction::V5 { .. } => {
+                unimplemented!("v5 transaction format as specified in ZIP-225")
+            }
         };
 
         if !has_joinsplits {
