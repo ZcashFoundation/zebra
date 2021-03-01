a143434032f6874a88ea92111dc38ceffba298e6
Deirdre Connolly <deirdre@zfnd.org>
2021-03-01 02:37:13 -0500

    Comment out Sprout params stuff for now

--- zebra-consensus/src/primitives/groth16/params.rs
+++ zebra-consensus/src/primitives/groth16/params.rs
@@ -7,7 +7,7 @@ use super::HashReader;
 
 const SAPLING_SPEND_HASH: &str = "8270785a1a0d0bc77196f000ee6d221c9c9894f55307bd9357c3f0105d31ca63991ab91324160d8f53e2bbd3c2633a6eb8bdf5205d822e7f3f73edac51b2b70c";
 const SAPLING_OUTPUT_HASH: &str = "657e3d38dbb5cb5e7dd2970e8b03d69b4787dd907285b5a7f0790dcc8072f60bf593b32cc2d1c030e00ff5ae64bf84c5c3beb84ddc841d48264b4a171744d028";
-const SPROUT_HASH: &str = "e9b238411bd6c0ec4791e9d04245ec350c9c5744f5610dfcce4365d5ca49dfefd5054e371842b3f88fa1b9d7e8e075249b3ebabd167fa8b0f3161292d36c180a";
+// const SPROUT_HASH: &str = "e9b238411bd6c0ec4791e9d04245ec350c9c5744f5610dfcce4365d5ca49dfefd5054e371842b3f88fa1b9d7e8e075249b3ebabd167fa8b0f3161292d36c180a";
 
 lazy_static::lazy_static! {
     pub static ref PARAMS: Groth16Params = Groth16Params::new();
@@ -28,45 +28,45 @@ impl Groth16Params {
     }
 }
 
-#[non_exhaustive]
-pub struct SproutParams {
-    pub verifying_key: groth16::VerifyingKey<Bls12>,
-    pub prepared_verifying_key: groth16::PreparedVerifyingKey<Bls12>,
-}
+// #[non_exhaustive]
+// pub struct SproutParams {
+//     pub verifying_key: groth16::VerifyingKey<Bls12>,
+//     pub prepared_verifying_key: groth16::PreparedVerifyingKey<Bls12>,
+// }
 
-impl SproutParams {
-    fn new() -> Self {
-        let sprout_fs = SproutParams::load();
-        let sprout_fs = BufReader::with_capacity(1024 * 1024, &sprout_fs[..]);
-        Self::read(sprout_fs).expect("reading parameters from vec will always succeed")
-    }
+// impl SproutParams {
+//     fn new() -> Self {
+//         let sprout_fs = SproutParams::load();
+//         let sprout_fs = BufReader::with_capacity(1024 * 1024, &sprout_fs[..]);
+//         Self::read(sprout_fs).expect("reading parameters from vec will always succeed")
+//     }
 
-    fn load() -> Vec<u8> {
-        todo!("figure out how we want to actually load these, file, download, include, etc");
-    }
+//     fn load() -> Vec<u8> {
+//         todo!("figure out how we want to actually load these, file, download, include, etc");
+//     }
 
-    fn read<R: io::Read>(fs: R) -> Result<Self, io::Error> {
-        let mut fs = HashReader::new(fs);
-        let verifying_key = groth16::VerifyingKey::<Bls12>::read(&mut fs)?;
+//     fn read<R: io::Read>(fs: R) -> Result<Self, io::Error> {
+//         let mut fs = HashReader::new(fs);
+//         let verifying_key = groth16::VerifyingKey::<Bls12>::read(&mut fs)?;
 
-        // There is extra stuff (the transcript) at the end of the parameter file which is
-        // used to verify the parameter validity, but we're not interested in that. We do
-        // want to read it, though, so that the BLAKE2b computed afterward is consistent
-        // with `b2sum` on the files.
-        io::copy(&mut fs, &mut io::sink())?;
+//         // There is extra stuff (the transcript) at the end of the parameter file which is
+//         // used to verify the parameter validity, but we're not interested in that. We do
+//         // want to read it, though, so that the BLAKE2b computed afterward is consistent
+//         // with `b2sum` on the files.
+//         io::copy(&mut fs, &mut io::sink())?;
 
-        if fs.into_hash() != SPROUT_HASH {
-            panic!("Sprout groth16 parameter is not correct.");
-        }
+//         if fs.into_hash() != SPROUT_HASH {
+//             panic!("Sprout groth16 parameter is not correct.");
+//         }
 
-        let prepared_verifying_key = groth16::prepare_verifying_key(&verifying_key);
+//         let prepared_verifying_key = groth16::prepare_verifying_key(&verifying_key);
 
-        Ok(Self {
-            verifying_key,
-            prepared_verifying_key,
-        })
-    }
-}
+//         Ok(Self {
+//             verifying_key,
+//             prepared_verifying_key,
+//         })
+//     }
+// }
 
 #[non_exhaustive]
 pub struct SaplingParams {
