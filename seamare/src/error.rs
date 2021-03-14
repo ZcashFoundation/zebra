034cb34be2d6f1cc112580161e5b9255f62239d9
WindSoilder <WindSoilder@outlook.com>
2021-03-15 07:18:39 +0800

    add NotValidUtf8Path error

--- seamare/src/error.rs
+++ seamare/src/error.rs
@@ -1,14 +1,14 @@
+use std::path::PathBuf;
 use thiserror::Error;
 
 pub type Result<T, E = SeamareError> = std::result::Result<T, E>;
 
 #[derive(Error, Debug)]
 pub enum SeamareError {
-    #[error("loading {path:?} failed, reason: {detail:?}")]
-    IO {
-        path: String,
-        detail: std::io::Error,
-    },
+    #[error("Loading path failed")]
+    IO(#[from] std::io::Error),
     #[error("Build package graph failed")]
     Guppy(#[from] guppy::Error),
+    #[error("Detect invalid UTF-8 path, get {0}")]
+    NotValidUtf8Path(PathBuf),
 }
--- seamare/src/file_path.rs
+++ seamare/src/file_path.rs
@@ -53,10 +53,7 @@ impl<'l> FilePathContext<'l> {
     /// `pub(super)` is to dissuade individual linters from loading file contexts.
     pub(super) fn load(self) -> Result<Option<ContentContext<'l>>> {
         let full_path = self.project_ctx.full_path(self.file_path);
-        let contents_opt = read_file(&full_path).map_err(|err| SeamareError::IO {
-            path: format!("{}", full_path),
-            detail: err,
-        })?;
+        let contents_opt = read_file(&full_path)?;
         Ok(contents_opt.map(|content| ContentContext::new(self, content)))
     }
 }
