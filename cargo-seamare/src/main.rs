97ee3cae0193079ea63441ca62d94e64b858ee3a
WindSoilder <WindSoilder@outlook.com>
2021-03-15 07:19:28 +0800

    finish initialize context

--- cargo-seamare/src/main.rs
+++ cargo-seamare/src/main.rs
@@ -1,17 +1,21 @@
 mod lint_engine;
 
+use camino::Utf8PathBuf;
 use lint_engine::{LintEngine, LintEngineBuilder};
-use guppy::MetadataCommand;
-use guppy::graph::PackageGraph;
+use seamare::prelude::{CoreContext, Result, SeamareError};
+use std::env;
 
-fn main() {
+fn main() -> Result<()> {
     run_lint_engine()
-    // basic progress:
-    // check if it's running inside a cargo project.
-    // Initialize a lint engine, load linter.
-    // Run this lint engine.
 }
 
-
-fn run_lint_engine() {
+fn run_lint_engine() -> Result<()> {
+    // basic progress:
+    // check if it's running inside a cargo project, build context can handle this.
+    let path = env::current_dir()?;
+    let path = Utf8PathBuf::from_path_buf(path).map_err(|e| SeamareError::NotValidUtf8Path(e))?;
+    let context = CoreContext::new(path.as_path());
+    // Initialize a lint engine, load linter.
+    // Run this lint engine.
+    Ok(())
 }
