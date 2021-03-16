62610278b28927f01619a738ccf50df8b7319e40
WindSoilder <WindSoilder@outlook.com>
2021-03-16 09:45:01 +0800

    fix code style

--- cargo-seamare/src/main.rs
+++ cargo-seamare/src/main.rs
@@ -17,7 +17,7 @@ fn run_lint_engine() -> Result<()> {
     // basic progress:
     // check if it's running inside a cargo project, build context can handle this.
     let path = env::current_dir()?;
-    let path = Utf8PathBuf::from_path_buf(path).map_err(|e| SeamareError::NotValidUtf8Path(e))?;
+    let path = Utf8PathBuf::from_path_buf(path).map_err(SeamareError::NotValidUtf8Path)?;
     let context = CoreContext::new(path.as_path())?;
     // Initialize a lint engine, load linter.
     let engine = LintEngineConfig::new(&context)
