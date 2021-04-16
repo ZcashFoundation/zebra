ea70bf50fab3651c124b2d8b2925339cbd107753
teor <teor@riseup.net>
2021-04-16 13:08:01 +1000

    Test fake v5 blocks made out of pre-NU5 block test vectors

--- zebra-chain/src/transaction/tests/vectors.rs
+++ zebra-chain/src/transaction/tests/vectors.rs
@@ -6,7 +6,9 @@ use crate::{
     serialization::{WriteZcashExt, ZcashDeserialize, ZcashDeserializeInto, ZcashSerialize},
 };
 
+use block::MAX_BLOCK_BYTES;
 use itertools::Itertools;
+use std::convert::TryInto;
 
 #[test]
 fn librustzcash_tx_deserialize_and_round_trip() {
@@ -156,6 +158,132 @@ fn empty_v4_round_trip() {
     assert_eq!(data, data2, "data must be equal if structs are equal");
 }
 
+/// Do a round-trip test on fake v5 transactions created from v4 transactions
+/// in the block test vectors.
+///
+/// Covers Sapling only, Transparent only, and Sapling/Transparent v5
+/// transactions.
+#[test]
+fn fake_v5_round_trip() {
+    zebra_test::init();
+
+    for original_bytes in zebra_test::vectors::BLOCKS.iter() {
+        let original_block = original_bytes
+            .zcash_deserialize_into::<Block>()
+            .expect("block is structurally valid");
+
+        // skip this block if it only contains v5 transactions,
+        // the block round-trip test covers it already
+        if original_block
+            .transactions
+            .iter()
+            .all(|trans| matches!(trans.as_ref(), &Transaction::V5 { .. }))
+        {
+            continue;
+        }
+
+        let mut fake_block = original_block.clone();
+        fake_block.transactions = fake_block
+            .transactions
+            .iter()
+            .map(AsRef::as_ref)
+            .map(transaction_to_fake_v5)
+            .map(Into::into)
+            .collect();
+
+        // test each transaction
+        for (original_tx, fake_tx) in original_block
+            .transactions
+            .iter()
+            .zip(fake_block.transactions.iter())
+        {
+            assert_ne!(
+                &original_tx, &fake_tx,
+                "v1-v4 transactions must change when converted to fake v5"
+            );
+
+            let fake_bytes = fake_tx
+                .zcash_serialize_to_vec()
+                .expect("vec serialization is infallible");
+
+            assert_ne!(
+                &original_bytes[..],
+                fake_bytes,
+                "v1-v4 transaction data must change when converted to fake v5"
+            );
+
+            let fake_tx2 = fake_bytes
+                .zcash_deserialize_into::<Transaction>()
+                .expect("tx is structurally valid");
+
+            assert_eq!(fake_tx.as_ref(), &fake_tx2);
+
+            let fake_bytes2 = fake_tx2
+                .zcash_serialize_to_vec()
+                .expect("vec serialization is infallible");
+
+            assert_eq!(
+                fake_bytes, fake_bytes2,
+                "data must be equal if structs are equal"
+            );
+        }
+
+        // test full blocks
+        assert_ne!(
+            &original_block, &fake_block,
+            "v1-v4 transactions must change when converted to fake v5"
+        );
+
+        let fake_bytes = fake_block
+            .zcash_serialize_to_vec()
+            .expect("vec serialization is infallible");
+
+        assert_ne!(
+            &original_bytes[..],
+            fake_bytes,
+            "v1-v4 transaction data must change when converted to fake v5"
+        );
+
+        // skip fake blocks which exceed the block size limit
+        // because of the changes we made
+        if fake_bytes.len() > MAX_BLOCK_BYTES.try_into().unwrap() {
+            continue;
+        }
+
+        let fake_block2 = match fake_bytes.zcash_deserialize_into::<Block>() {
+            Ok(fake_block2) => fake_block2,
+            Err(err) => {
+                // TODO: work out why transaction parsing succeeds,
+                //       but block parsing doesn't
+                tracing::info!(
+                    ?err,
+                    ?original_block,
+                    ?fake_block,
+                    hex_original_bytes = ?hex::encode(&original_bytes),
+                    hex_fake_bytes = ?hex::encode(&fake_bytes),
+                    original_bytes_len = %original_bytes.len(),
+                    fake_bytes_len = %fake_bytes.len(),
+                    %MAX_BLOCK_BYTES,
+                    "unexpected structurally invalid block during deserialization"
+                );
+
+                continue;
+            }
+        };
+
+        assert_eq!(fake_block, fake_block2);
+
+        let fake_bytes2 = fake_block2
+            .zcash_serialize_to_vec()
+            .expect("vec serialization is infallible");
+
+        assert_eq!(
+            fake_bytes, fake_bytes2,
+            "data must be equal if structs are equal"
+        );
+    }
+}
+
 // Utility functions
 
 /// Return serialized empty Transaction::V5 Orchard data.
@@ -170,4 +298,131 @@ fn empty_v5_orchard_data() -> Vec<u8> {
 
     // all other orchard fields are only present when `nActionsOrchard > 0`
     buf
+}
+
+/// Convert `trans` into a fake v5 transaction,
+/// converting sapling shielded data from v4 to v5 if possible.
+fn transaction_to_fake_v5(trans: &Transaction) -> Transaction {
+    use Transaction::*;
+
+    match trans {
+        V1 {
+            inputs,
+            outputs,
+            lock_time,
+        } => V5 {
+            inputs: inputs.to_vec(),
+            outputs: outputs.to_vec(),
+            lock_time: *lock_time,
+            expiry_height: block::Height(0),
+            sapling_shielded_data: None,
+            rest: empty_v5_orchard_data(),
+        },
+        V2 {
+            inputs,
+            outputs,
+            lock_time,
+            ..
+        } => V5 {
+            inputs: inputs.to_vec(),
+            outputs: outputs.to_vec(),
+            lock_time: *lock_time,
+            expiry_height: block::Height(0),
+            sapling_shielded_data: None,
+            rest: empty_v5_orchard_data(),
+        },
+        V3 {
+            inputs,
+            outputs,
+            lock_time,
+            expiry_height,
+            ..
+        } => V5 {
+            inputs: inputs.to_vec(),
+            outputs: outputs.to_vec(),
+            lock_time: *lock_time,
+            expiry_height: *expiry_height,
+            sapling_shielded_data: None,
+            rest: empty_v5_orchard_data(),
+        },
+        V4 {
+            inputs,
+            outputs,
+            lock_time,
+            expiry_height,
+            sapling_shielded_data,
+            ..
+        } => V5 {
+            inputs: inputs.to_vec(),
+            outputs: outputs.to_vec(),
+            lock_time: *lock_time,
+            expiry_height: *expiry_height,
+            sapling_shielded_data: sapling_shielded_data
+                .clone()
+                .map(sapling_shielded_v4_to_fake_v5)
+                .flatten(),
+            rest: empty_v5_orchard_data(),
+        },
+        v5 @ V5 { .. } => v5.clone(),
+    }
+}
+
+/// Convert a v4 sapling shielded data into a fake v5 sapling shielded data,
+/// if possible.
+fn sapling_shielded_v4_to_fake_v5(
+    v4_shielded: sapling::ShieldedData<PerSpendAnchor>,
+) -> Option<sapling::ShieldedData<SharedAnchor>> {
+    use futures::future::Either::*;
+    use sapling::ShieldedData;
+
+    let unique_anchors: Vec<_> = v4_shielded
+        .spends()
+        .map(|spend| spend.per_spend_anchor)
+        .unique()
+        .collect();
+
+    let shared_anchor = match unique_anchors.as_slice() {
+        [unique_anchor] => *unique_anchor,
+        // TODO: remove shared anchor when there are no spends
+        [] => Default::default(),
+        // Multiple different anchors, can't convert to v5
+        _ => return None,
+    };
+
+    let first = match v4_shielded.first {
+        Left(spend) => Left(sapling_spend_v4_to_fake_v5(spend)),
+        Right(output) => Right(output),
+    };
+
+    let fake_shielded_v5 = ShieldedData::<SharedAnchor> {
+        value_balance: v4_shielded.value_balance,
+        shared_anchor,
+        first,
+        rest_spends: v4_shielded
+            .rest_spends
+            .iter()
+            .cloned()
+            .map(sapling_spend_v4_to_fake_v5)
+            .collect(),
+        rest_outputs: v4_shielded.rest_outputs,
+        binding_sig: v4_shielded.binding_sig,
+    };
+
+    Some(fake_shielded_v5)
+}
+
+/// Convert a v4 sapling spend into a fake v5 sapling spend.
+fn sapling_spend_v4_to_fake_v5(
+    v4_spend: sapling::Spend<PerSpendAnchor>,
+) -> sapling::Spend<SharedAnchor> {
+    use sapling::Spend;
+
+    Spend::<SharedAnchor> {
+        cv: v4_spend.cv,
+        per_spend_anchor: FieldNotPresent,
+        nullifier: v4_spend.nullifier,
+        rk: v4_spend.rk,
+        zkproof: v4_spend.zkproof,
+        spend_auth_sig: v4_spend.spend_auth_sig,
+    }
 }
