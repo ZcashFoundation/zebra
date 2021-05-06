9daf14d6e49417c4f6aa762b91d2554b9f68718b
Deirdre Connolly <durumcrustulum@gmail.com>
2021-05-06 09:54:05 -0400

    Some comments

--- zebra-chain/src/primitives/redpallas/tests/basepoints.rs
+++ zebra-chain/src/primitives/redpallas/tests/basepoints.rs
@@ -6,6 +6,7 @@ use super::super::constants;
 #[test]
 fn orchard_spendauth_basepoint() {
     assert_eq!(
+        // An instance of _GroupHash^P_
         pallas::Point::hash_to_curve("z.cash:Orchard")(b"G").to_bytes(),
         constants::SPENDAUTHSIG_BASEPOINT_BYTES
     );
@@ -14,6 +15,7 @@ fn orchard_spendauth_basepoint() {
 #[test]
 fn orchard_binding_basepoint() {
     assert_eq!(
+        // An instance of _GroupHash^P_
         pallas::Point::hash_to_curve("z.cash:Orchard-cv")(b"r").to_bytes(),
         constants::BINDINGSIG_BASEPOINT_BYTES
     );
