84378b2d150f3e241cabad283861e0f8ff67342c
Alfredo Garcia <oxarbitrage@gmail.com>
2021-05-31 16:24:00 -0300

    modify test

--- zebra-state/src/service/finalized_state/tests/prop.rs
+++ zebra-state/src/service/finalized_state/tests/prop.rs
@@ -1,47 +1,34 @@
 use std::env;
 
-use zebra_chain::{
-    block::{Block, Height, LedgerState},
-    parameters::Network,
-};
+use zebra_chain::block::Height;
 use zebra_test::prelude::*;
 
 use crate::{
     config::Config,
-    service::finalized_state::{FinalizedBlock, FinalizedState},
+    service::{
+        finalized_state::{FinalizedBlock, FinalizedState},
+        non_finalized_state::arbitrary::PreparedChain,
+    },
 };
 
-const DEFAULT_PARTIAL_CHAIN_PROPTEST_CASES: u32 = 2;
-const PARTIAL_CHAIN_SIZE: usize = 200;
+const DEFAULT_PARTIAL_CHAIN_PROPTEST_CASES: u32 = 32;
 
 #[test]
 fn blocks_with_v5_transactions() -> Result<()> {
     zebra_test::init();
-
-    blocks_with_v5_transactions_for_network(Network::Mainnet)?;
-    blocks_with_v5_transactions_for_network(Network::Testnet)?;
-
-    Ok(())
-}
-
-fn blocks_with_v5_transactions_for_network(network: Network) -> Result<()> {
-    let strategy = LedgerState::genesis_strategy(None)
-        .prop_flat_map(|init| Block::partial_chain_strategy(init, PARTIAL_CHAIN_SIZE));
-
     proptest!(ProptestConfig::with_cases(env::var("PROPTEST_CASES")
         .ok()
         .and_then(|v| v.parse().ok())
         .unwrap_or(DEFAULT_PARTIAL_CHAIN_PROPTEST_CASES)),
-        |(chain in strategy)| {
-
+        |((chain, count, network) in PreparedChain::default())| {
             let mut state = FinalizedState::new(&Config::ephemeral(), network);
-
             let mut height = Height(0);
-            for block in chain {
+            // use `count` to minimize test failures, so they are easier to diagnose
+            for block in chain.iter().take(count) {
                 let hash = state.commit_finalized_direct(FinalizedBlock::from(block.clone()));
                 prop_assert_eq!(Some(height), state.finalized_tip_height());
-                prop_assert_eq!(hash.unwrap(), block.hash());
-
+                prop_assert_eq!(hash.unwrap(), block.hash);
+                // TODO: check that the nullifiers were correctly inserted (#1287)
                 height = Height(height.0 + 1);
             }
     });
