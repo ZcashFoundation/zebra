45cd458abd0cf3f678dbfbf0d48e0c0f7b790ef8
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-06-01 18:40:52 +0000

    Add consensus rule link to field documentation
    Describe how the consensus rule is validated structurally by
    `ShieldedData`.

--- zebra-chain/src/sapling/shielded_data.rs
+++ zebra-chain/src/sapling/shielded_data.rs
@@ -86,6 +86,16 @@ where
     AnchorV: AnchorVariant + Clone,
 {
     /// The net value of Sapling spend transfers minus output transfers.
+    ///
+    /// [`ShieldedData`] structurally validates this [value balance consensus
+    /// rule](https://zips.z.cash/protocol/nu5.pdf#txnencodingandconsensus):
+    /// "If effectiveVersion = 4 and there are no Spend descriptions or Output
+    /// descriptions, then valueBalanceSapling MUST be 0."
+    ///
+    /// `value_balance` is a field in [`ShieldedData`], which must have at least
+    /// one spend or output in its first field, so it is impossible to have a
+    /// [`ShieldedData`] with no spends, no outputs, and a non-zero value
+    /// balance.
     pub value_balance: Amount,
 
     /// A bundle of spends and outputs, containing at least one spend or
