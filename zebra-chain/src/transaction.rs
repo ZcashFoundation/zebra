4392f6ee2d3b8a4f0da54fe774e1cd79847d539f
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-06-01 19:30:24 +0000

    Add a `Transaction::orchard_shielded_data` getter
    Allows accessing the Orchard shielded data if it is present in the
    transaction, regardless of the transaction version.

--- zebra-chain/src/transaction.rs
+++ zebra-chain/src/transaction.rs
@@ -395,6 +395,24 @@ impl Transaction {
 
     // orchard
 
+    /// Access the [`orchard::ShieldedData`] in this transaction, if the transaction has it,
+    /// regardless of version.
+    pub fn orchard_shielded_data(&self) -> Option<&orchard::ShieldedData> {
+        match self {
+            // Maybe Orchard shielded data
+            Transaction::V5 {
+                orchard_shielded_data,
+                ..
+            } => orchard_shielded_data.as_ref(),
+
+            // No Orchard shielded data
+            Transaction::V1 { .. }
+            | Transaction::V2 { .. }
+            | Transaction::V3 { .. }
+            | Transaction::V4 { .. } => None,
+        }
+    }
+
     /// Iterate over the [`orchard::Action`]s in this transaction, if there are any.
     pub fn orchard_actions(&self) -> Box<dyn Iterator<Item = &orchard::Action> + '_> {
         match self {
