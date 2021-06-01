b199cc79569bad8fee5383fea778e709832a9936
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-06-01 15:29:44 +0000

    Implement `Transaction::orchard_actions()` getter
    Returns an iterator to iterator over the actions in the Orchard shielded
    data (if there is one, otherwise it returns an empty iterator).

--- zebra-chain/src/transaction.rs
+++ zebra-chain/src/transaction.rs
@@ -395,6 +395,27 @@ impl Transaction {
 
     // orchard
 
+    /// Iterate over the [`orchard::Action`]s in this transaction, if there are any.
+    pub fn orchard_actions(&self) -> Box<dyn Iterator<Item = &orchard::Action> + '_> {
+        match self {
+            // Actions
+            Transaction::V5 {
+                orchard_shielded_data: Some(orchard_shielded_data),
+                ..
+            } => Box::new(orchard_shielded_data.actions()),
+
+            // No Actions
+            Transaction::V1 { .. }
+            | Transaction::V2 { .. }
+            | Transaction::V3 { .. }
+            | Transaction::V4 { .. }
+            | Transaction::V5 {
+                orchard_shielded_data: None,
+                ..
+            } => Box::new(std::iter::empty()),
+        }
+    }
+
     /// Access the [`orchard::Nullifier`]s in this transaction, regardless of version.
     pub fn orchard_nullifiers(&self) -> Box<dyn Iterator<Item = &orchard::Nullifier> + '_> {
         // This function returns a boxed iterator because the different
