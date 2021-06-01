5140b8cd3064afe1df50cf2511d4257917b898ec
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-06-01 01:33:34 +0000

    Refactor transaction test vectors
    Make it easier to reuse the fake V5 transaction converter in other test
    vectors.

--- zebra-consensus/src/transaction/tests.rs
+++ zebra-consensus/src/transaction/tests.rs
@@ -5,55 +5,30 @@ use zebra_chain::{
     transaction::{arbitrary::transaction_to_fake_v5, Transaction},
 };
 
-use crate::error::TransactionError::*;
+use super::check;
+
+use crate::error::TransactionError;
 use color_eyre::eyre::Report;
 
 #[test]
 fn v5_fake_transactions() -> Result<(), Report> {
     zebra_test::init();
 
-    v5_fake_transactions_for_network(Network::Mainnet)?;
-    v5_fake_transactions_for_network(Network::Testnet)?;
+    let networks = vec![
+        (Network::Mainnet, zebra_test::vectors::MAINNET_BLOCKS.iter()),
+        (Network::Testnet, zebra_test::vectors::TESTNET_BLOCKS.iter()),
+    ];
 
-    Ok(())
-}
-
-fn v5_fake_transactions_for_network(network: Network) -> Result<(), Report> {
-    zebra_test::init();
-
-    // get all the blocks we have available
-    let block_iter = match network {
-        Network::Mainnet => zebra_test::vectors::MAINNET_BLOCKS.iter(),
-        Network::Testnet => zebra_test::vectors::TESTNET_BLOCKS.iter(),
-    };
-
-    for (height, original_bytes) in block_iter {
-        let original_block = original_bytes
-            .zcash_deserialize_into::<Block>()
-            .expect("block is structurally valid");
-
-        // convert all transactions from the block to V5
-        let transactions: Vec<Transaction> = original_block
-            .transactions
-            .iter()
-            .map(AsRef::as_ref)
-            .map(|t| transaction_to_fake_v5(t, network, Height(*height)))
-            .map(Into::into)
-            .collect();
-
-        // after the conversion some transactions end up with no inputs nor outputs.
-        for transaction in transactions {
-            match super::check::has_inputs_and_outputs(&transaction) {
-                Err(e) => {
-                    if e != NoInputs && e != NoOutputs {
-                        panic!("error must be NoInputs or NoOutputs")
-                    }
-                }
+    for (network, blocks) in networks {
+        for transaction in fake_v5_transactions_for_network(network, blocks) {
+            match check::has_inputs_and_outputs(&transaction) {
                 Ok(()) => (),
+                Err(TransactionError::NoInputs) | Err(TransactionError::NoOutputs) => (),
+                Err(_) => panic!("error must be NoInputs or NoOutputs"),
             };
 
             // make sure there are no joinsplits nor spends in coinbase
-            super::check::coinbase_tx_no_prevout_joinsplit_spend(&transaction)?;
+            check::coinbase_tx_no_prevout_joinsplit_spend(&transaction)?;
 
             // validate the sapling shielded data
             match transaction {
@@ -62,13 +37,13 @@ fn v5_fake_transactions_for_network(network: Network) -> Result<(), Report> {
                     ..
                 } => {
                     if let Some(s) = sapling_shielded_data {
-                        super::check::sapling_balances_match(&s)?;
+                        check::sapling_balances_match(&s)?;
 
                         for spend in s.spends_per_anchor() {
-                            super::check::spend_cv_rk_not_small_order(&spend)?
+                            check::spend_cv_rk_not_small_order(&spend)?
                         }
                         for output in s.outputs() {
-                            super::check::output_cv_epk_not_small_order(&output)?;
+                            check::output_cv_epk_not_small_order(&output)?;
                         }
                     }
                 }
@@ -76,5 +51,23 @@ fn v5_fake_transactions_for_network(network: Network) -> Result<(), Report> {
             }
         }
     }
+
     Ok(())
+}
+
+fn fake_v5_transactions_for_network<'b>(
+    network: Network,
+    blocks: impl DoubleEndedIterator<Item = (&'b u32, &'b &'static [u8])> + 'b,
+) -> impl DoubleEndedIterator<Item = Transaction> + 'b {
+    blocks.flat_map(move |(height, original_bytes)| {
+        let original_block = original_bytes
+            .zcash_deserialize_into::<Block>()
+            .expect("block is structurally valid");
+
+        original_block
+            .transactions
+            .into_iter()
+            .map(move |transaction| transaction_to_fake_v5(&*transaction, network, Height(*height)))
+            .map(Transaction::from)
+    })
 }
