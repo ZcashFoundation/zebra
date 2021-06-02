76c0b074242d717dae56aaf275862fc4c51f4da8
teor <teor@riseup.net>
2021-06-02 12:05:57 +1000

    Make SummaryDebug generic over collections and exact size iterators

--- zebra-chain/src/fmt.rs
+++ zebra-chain/src/fmt.rs
@@ -28,39 +28,49 @@ impl<T> From<T> for DisplayToDebug<T> {
     }
 }
 
+/// Wrapper to override `Debug` to display a shorter summary of the type.
+///
+/// For collections and exact size iterators, it only displays the
+/// collection/iterator type, the item type, and the length.
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct SummaryDebug<T>(pub T);
+pub struct SummaryDebug<CollectionOrIter>(pub CollectionOrIter);
 
-impl<T> fmt::Debug for SummaryDebug<Vec<T>> {
+impl<CollectionOrIter> fmt::Debug for SummaryDebug<CollectionOrIter>
+where
+    CollectionOrIter: IntoIterator + Clone,
+    <CollectionOrIter as IntoIterator>::IntoIter: ExactSizeIterator,
+{
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}, len={}", std::any::type_name::<T>(), self.0.len())
+        write!(
+            f,
+            "{}<{}>, len={}",
+            std::any::type_name::<CollectionOrIter>(),
+            std::any::type_name::<<CollectionOrIter as IntoIterator>::Item>(),
+            self.0.clone().into_iter().len()
+        )
     }
 }
 
-impl<T> fmt::Debug for SummaryDebug<&Vec<T>> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}, len={}", std::any::type_name::<T>(), self.0.len())
-    }
-}
-
-impl<T> ops::Deref for SummaryDebug<T> {
-    type Target = T;
+impl<CollectionOrIter> ops::Deref for SummaryDebug<CollectionOrIter> {
+    type Target = CollectionOrIter;
 
     fn deref(&self) -> &Self::Target {
         &self.0
     }
 }
 
-impl<T> From<T> for SummaryDebug<T> {
-    fn from(t: T) -> Self {
-        Self(t)
+impl<CollectionOrIter> From<CollectionOrIter> for SummaryDebug<CollectionOrIter> {
+    fn from(collection: CollectionOrIter) -> Self {
+        Self(collection)
     }
 }
 
-impl<T> IntoIterator for SummaryDebug<Vec<T>> {
-    type Item = T;
-
-    type IntoIter = <Vec<T> as IntoIterator>::IntoIter;
+impl<CollectionOrIter> IntoIterator for SummaryDebug<CollectionOrIter>
+where
+    CollectionOrIter: IntoIterator,
+{
+    type Item = <CollectionOrIter as IntoIterator>::Item;
+    type IntoIter = <CollectionOrIter as IntoIterator>::IntoIter;
 
     fn into_iter(self) -> Self::IntoIter {
         self.0.into_iter()
