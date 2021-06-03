20306c551c0033441bc5e892942481e2c5125513
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-06-03 14:44:29 +0000

    Test if `has_inputs_and_outputs` considers actions
    Create a dummy transaction with no inputs and no outputs, and add a
    dummy Orchard action to it. The `check::has_inputs_and_outputs`
    should succeed, because the consensus rule considers having Orchard
    actions as having inputs and/or outputs.

--- zebra-consensus/src/transaction/tests.rs
+++ zebra-consensus/src/transaction/tests.rs
@@ -1,5 +1,11 @@
+use std::convert::TryFrom;
+
 use zebra_chain::{
+    amount::Amount,
+    at_least_one, orchard,
     parameters::Network,
+    primitives::{redpallas, Halo2Proof},
+    serialization::{AtLeastOne, ZcashDeserialize},
     transaction::{arbitrary::fake_v5_transactions_for_network, Transaction},
 };
 
@@ -49,6 +55,59 @@ fn v5_fake_transactions() -> Result<(), Report> {
     }
 
     Ok(())
+}
+
+#[test]
+fn fake_v5_transaction_with_orchard_actions_has_inputs_and_outputs() {
+    // Find a transaction with no inputs or outputs to use as base
+    let mut transaction = fake_v5_transactions_for_network(
+        Network::Mainnet,
+        zebra_test::vectors::MAINNET_BLOCKS.iter(),
+    )
+    .rev()
+    .find(|transaction| {
+        transaction.inputs().is_empty()
+            && transaction.outputs().is_empty()
+            && transaction.sapling_spends_per_anchor().next().is_none()
+            && transaction.sapling_outputs().next().is_none()
+            && transaction.joinsplit_count() == 0
+    })
+    .expect("At least one fake V5 transaction with no inputs and no outputs");
+
+    // Create a dummy action, it doesn't need to be valid
+    let dummy_action_bytes = [0u8; 820];
+    let mut dummy_action_bytes_reader = &dummy_action_bytes[..];
+    let dummy_action = orchard::Action::zcash_deserialize(&mut dummy_action_bytes_reader)
+        .expect("Dummy action should deserialize");
+
+    // Pair the dummy action with a fake signature
+    let dummy_authorized_action = orchard::AuthorizedAction {
+        action: dummy_action,
+        spend_auth_sig: redpallas::Signature::from([0u8; 64]),
+    };
+
+    // Place the dummy action inside the Orchard shielded data
+    let dummy_shielded_data = orchard::ShieldedData {
+        flags: orchard::Flags::empty(),
+        value_balance: Amount::try_from(0).expect("invalid transaction amount"),
+        shared_anchor: orchard::tree::Root::default(),
+        proof: Halo2Proof(vec![]),
+        actions: at_least_one![dummy_authorized_action],
+        binding_sig: redpallas::Signature::from([0u8; 64]),
+    };
+
+    // Replace the shielded data in the transaction so that it has one action
+    match &mut transaction {
+        Transaction::V5 {
+            orchard_shielded_data,
+            ..
+        } => *orchard_shielded_data = Some(dummy_shielded_data),
+        _ => panic!("Fake V5 transaction is not V5"),
+    }
+
+    // If a transaction has at least one Orhcard shielded action, it should be considered to have
+    // inputs and/or outputs
+    assert!(check::has_inputs_and_outputs(&transaction).is_ok());
 }
 
 #[test]
