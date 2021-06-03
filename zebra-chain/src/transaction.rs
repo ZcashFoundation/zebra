bc29da74f4d7ee3382eff2673635bf7b241c21ef
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-06-02 21:29:11 -0300

    Refactor `Transaction::orchard_*` getters
    Use the fact that `Option<T>` implements `Iterator<T>` to simplify the
    code and remove the need for boxing the iterators.
    
    Co-authored-by: teor <teor@riseup.net>

--- zebra-chain/src/transaction.rs
+++ zebra-chain/src/transaction.rs
@@ -414,33 +414,18 @@ impl Transaction {
     }
 
     /// Iterate over the [`orchard::Action`]s in this transaction, if there are any.
-    pub fn orchard_actions(&self) -> Box<dyn Iterator<Item = &orchard::Action> + '_> {
-        match self {
-            // Actions
-            Transaction::V5 {
-                orchard_shielded_data: Some(orchard_shielded_data),
-                ..
-            } => Box::new(orchard_shielded_data.actions()),
-
-            // No Actions
-            Transaction::V1 { .. }
-            | Transaction::V2 { .. }
-            | Transaction::V3 { .. }
-            | Transaction::V4 { .. }
-            | Transaction::V5 {
-                orchard_shielded_data: None,
-                ..
-            } => Box::new(std::iter::empty()),
-        }
+    pub fn orchard_actions(&self) -> impl Iterator<Item = &orchard::Action> {
+        self.orchard_shielded_data()
+            .into_iter()
+            .map(orchard::ShieldedData::actions)
+            .flatten()
     }
 
     /// Access the [`orchard::Nullifier`]s in this transaction, regardless of version.
-    pub fn orchard_nullifiers(&self) -> Box<dyn Iterator<Item = &orchard::Nullifier> + '_> {
-        // This function returns a boxed iterator because the different
-        // transaction variants can have different iterator types
-        match self.orchard_shielded_data() {
-            Some(orchard_shielded_data) => Box::new(orchard_shielded_data.nullifiers()),
-            None => Box::new(std::iter::empty()),
-        }
+    pub fn orchard_nullifiers(&self) -> impl Iterator<Item = &orchard::Nullifier> {
+        self.orchard_shielded_data()
+            .into_iter()
+            .map(orchard::ShieldedData::nullifiers)
+            .flatten()
     }
 }
