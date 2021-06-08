772e12f78f922aae232d46fa3c9eb47fede4bed6
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-06-08 01:12:34 +0000

    Rename field to `wait_next_handshake`
    Make the name a bit more clear regarding to the field's purpose.

--- zebra-network/src/peer_set/candidate_set.rs
+++ zebra-network/src/peer_set/candidate_set.rs
@@ -108,7 +108,7 @@ mod tests;
 pub(super) struct CandidateSet<S> {
     pub(super) address_book: Arc<std::sync::Mutex<AddressBook>>,
     pub(super) peer_service: S,
-    next_peer_min_wait: Sleep,
+    wait_next_handshake: Sleep,
 }
 
 impl<S> CandidateSet<S>
@@ -132,7 +132,7 @@ where
         CandidateSet {
             address_book,
             peer_service,
-            next_peer_min_wait: sleep(Duration::from_secs(0)),
+            wait_next_handshake: sleep(Duration::from_secs(0)),
         }
     }
 
@@ -282,9 +282,9 @@ where
     /// new peer connections are initiated at least
     /// `MIN_PEER_CONNECTION_INTERVAL` apart.
     pub async fn next(&mut self) -> Option<MetaAddr> {
-        let current_deadline = self.next_peer_min_wait.deadline().max(Instant::now());
+        let current_deadline = self.wait_next_handshake.deadline().max(Instant::now());
         let mut sleep = sleep_until(current_deadline + Self::MIN_PEER_CONNECTION_INTERVAL);
-        mem::swap(&mut self.next_peer_min_wait, &mut sleep);
+        mem::swap(&mut self.wait_next_handshake, &mut sleep);
 
         // # Correctness
         //
