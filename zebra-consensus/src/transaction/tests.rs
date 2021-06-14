12c2a2817c218161aa5af1e563fe88a6bae6e1e7
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-06-14 19:03:49 +0000

    Test if V5 tx. is accepted after NU5 activation
    Create a fake V5 transaction and pretend it is placed in a block that
    has a height after the NU5 activation. The test should succeed, but
    since the NU5 activation height has not been specified yet (neither for
    the testnet nor the mainnet), for now this test is marked as
    `should_panic`.

--- zebra-consensus/src/transaction/tests.rs
+++ zebra-consensus/src/transaction/tests.rs
@@ -204,3 +204,40 @@ async fn v5_transaction_is_rejected_before_nu5_activation() {
         );
     }
 }
+
+#[tokio::test]
+// TODO: Remove `should_panic` once the NU5 activation heights for testnet and mainnet have been
+// defined.
+#[should_panic]
+async fn v5_transaction_is_accepted_after_nu5_activation() {
+    let nu5 = NetworkUpgrade::Nu5;
+    let networks = vec![
+        (Network::Mainnet, zebra_test::vectors::MAINNET_BLOCKS.iter()),
+        (Network::Testnet, zebra_test::vectors::TESTNET_BLOCKS.iter()),
+    ];
+
+    for (network, blocks) in networks {
+        let state_service = service_fn(|_| async { unreachable!("Service should not be called") });
+        let script_verifier = script::Verifier::new(state_service);
+        let verifier = Verifier::new(network, script_verifier);
+
+        let transaction = fake_v5_transactions_for_network(network, blocks)
+            .rev()
+            .next()
+            .expect("At least one fake V5 transaction in the test vectors");
+
+        let expected_hash = transaction.hash();
+
+        let result = verifier
+            .oneshot(Request::Block {
+                transaction: Arc::new(transaction),
+                known_utxos: Arc::new(HashMap::new()),
+                height: nu5
+                    .activation_height(network)
+                    .expect("NU5 activation height is specified"),
+            })
+            .await;
+
+        assert_eq!(result, Ok(expected_hash));
+    }
+}
