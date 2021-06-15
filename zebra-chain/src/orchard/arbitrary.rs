9bab7bf2ca0795fadaf29e2fdd825f20e7eef66e
teor <teor@riseup.net>
2021-06-15 11:41:18 +1000

    Try refactor again

--- zebra-chain/src/orchard/arbitrary.rs
+++ zebra-chain/src/orchard/arbitrary.rs
@@ -43,7 +43,7 @@ impl Arbitrary for note::Nullifier {
 
         (vec(any::<u8>(), 64))
             .prop_map(|bytes| {
-                let bytes = bytes.try_into().expect("array is the correct length");
+                let bytes = bytes.as_slice().try_into().expect("vec is the correct length");
                 Self::try_from(pallas::Scalar::from_bytes_wide(&bytes).to_bytes())
                     .expect("a valid generated nullifier")
             })
