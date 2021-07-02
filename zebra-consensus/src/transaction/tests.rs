8571f0b2f437f4b68deb19f495892269d4888b18
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-07-02 01:41:55 +0000

    Test V5 transaction with Sapling spends
    Create a fake V5 transaction that has Sapling spends and check that the
    verifier accepts the transaction.

--- zebra-consensus/src/transaction/tests.rs
+++ zebra-consensus/src/transaction/tests.rs
@@ -775,6 +775,47 @@ fn v4_with_sapling_outputs_and_no_spends() {
     });
 }
 
+/// Test if a V5 transaction with Sapling spends is accepted by the verifier.
+#[test]
+// TODO: Remove `should_panic` once V5 transaction verification is complete.
+#[should_panic]
+fn v5_with_sapling_spends() {
+    zebra_test::init();
+    zebra_test::RUNTIME.block_on(async {
+        let network = Network::Mainnet;
+
+        let transaction =
+            fake_v5_transactions_for_network(network, zebra_test::vectors::MAINNET_BLOCKS.iter())
+                .rev()
+                .filter(|transaction| !transaction.is_coinbase() && transaction.inputs().is_empty())
+                .find(|transaction| transaction.sapling_spends_per_anchor().next().is_some())
+                .expect("No transaction found with Sapling spends");
+
+        let expected_hash = transaction.hash();
+        let height = transaction
+            .expiry_height()
+            .expect("Transaction is missing expiry height");
+
+        // Initialize the verifier
+        let state_service =
+            service_fn(|_| async { unreachable!("State service should not be called") });
+        let script_verifier = script::Verifier::new(state_service);
+        let verifier = Verifier::new(network, script_verifier);
+
+        // Test the transaction verifier
+        let result = verifier
+            .clone()
+            .oneshot(Request::Block {
+                transaction: Arc::new(transaction),
+                known_utxos: Arc::new(HashMap::new()),
+                height,
+            })
+            .await;
+
+        assert_eq!(result, Ok(expected_hash));
+    });
+}
+
 // Utility functions
 
 /// Create a mock transparent transfer to be included in a transaction.
