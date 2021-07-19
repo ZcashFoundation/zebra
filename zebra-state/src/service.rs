684ce70f12ba859c1ebf4b7ce77fef3baeb61451
teor <teor@riseup.net>
2021-07-19 10:59:30 +1000

    Pass the finalized state to chain contextual validation (#2503)
    This change is required to validate UTXOs against a forked chain.
    
    Also abstract chain contextual validation into a new method.

--- zebra-state/src/service.rs
+++ zebra-state/src/service.rs
@@ -175,9 +175,9 @@ impl StateService {
         let parent_hash = prepared.block.header.previous_block_hash;
 
         if self.disk.finalized_tip_hash() == parent_hash {
-            self.mem.commit_new_chain(prepared)?;
+            self.mem.commit_new_chain(prepared, &self.disk)?;
         } else {
-            self.mem.commit_block(prepared)?;
+            self.mem.commit_block(prepared, &self.disk)?;
         }
 
         Ok(())
