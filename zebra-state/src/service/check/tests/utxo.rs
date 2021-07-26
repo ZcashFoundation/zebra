197551e6c9c705f11e29672b1d8a2bd18bec7305
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-07-26 19:59:58 +0000

    Replace usages of `tests::Prepare`
    Use the same trait but placed in a new module that's accessible based on
    the feature flag.

--- zebra-state/src/service/check/tests/utxo.rs
+++ zebra-state/src/service/check/tests/utxo.rs
@@ -13,11 +13,11 @@ use zebra_chain::{
 };
 
 use crate::{
+    arbitrary::Prepare,
     service::{
         arbitrary::{new_state_with_mainnet_genesis, transaction_v4_from_coinbase},
         StateService,
     },
-    tests::Prepare,
     FinalizedBlock,
     ValidateContextError::{
         DuplicateTransparentSpend, EarlyTransparentSpend, MissingTransparentOutput,
