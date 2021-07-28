7af2e4aebc9225713345b0a439d4e3eb5adf4810
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-07-28 11:51:47 +0000

    Return receiver endpoint from service constructor
    Make it available so that the best tip height can be watched.

--- zebrad/src/commands/start.rs
+++ zebrad/src/commands/start.rs
@@ -49,10 +49,8 @@ impl StartCmd {
         info!(?config);
 
         info!("initializing node state");
-        let state = ServiceBuilder::new().buffer(20).service(zebra_state::init(
-            config.state.clone(),
-            config.network.network,
-        ));
+        let (state_service, _) = zebra_state::init(config.state.clone(), config.network.network);
+        let state = ServiceBuilder::new().buffer(20).service(state_service);
 
         info!("initializing verifiers");
         let verifier = zebra_consensus::chain::init(
