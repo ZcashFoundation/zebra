49178a4e58a8bd1dd3bb2bf878fbae3b949102bb
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-08-06 14:19:57 +0000

    Remove redundant PoW block assertion
    It's also checked in
    `zebra_state::service::check::block_is_contextually_valid`, and it was
    getting in the way of tests that received a gossiped block before
    finalizing enough blocks.

--- zebra-state/src/service.rs
+++ zebra-state/src/service.rs
@@ -6,7 +6,6 @@ use std::{
     time::{Duration, Instant},
 };
 
-use check::difficulty::POW_MEDIAN_BLOCK_SPAN;
 use futures::future::FutureExt;
 use non_finalized_state::{NonFinalizedState, QueuedBlocks};
 use tokio::sync::{oneshot, watch};
@@ -16,7 +15,6 @@ use tower::{util::BoxService, Service};
 use tracing::instrument;
 use zebra_chain::{
     block::{self, Block},
-    parameters::POW_AVERAGING_WINDOW,
     parameters::{Network, NetworkUpgrade},
     transaction,
     transaction::Transaction,
@@ -529,14 +527,6 @@ impl StateService {
             "invalid non-finalized block height: the canopy checkpoint is mandatory, pre-canopy \
             blocks, and the canopy activation block, must be committed to the state as finalized \
             blocks"
-        );
-
-        // required by check_contextual_validity, moved here to make testing easier
-        let relevant_chain = self.any_ancestor_blocks(block.block.header.previous_block_hash);
-        assert!(
-            relevant_chain.len() >= POW_AVERAGING_WINDOW + POW_MEDIAN_BLOCK_SPAN,
-            "contextual validation requires at least 28 \
-            (POW_AVERAGING_WINDOW + POW_MEDIAN_BLOCK_SPAN) blocks"
         );
     }
 }
