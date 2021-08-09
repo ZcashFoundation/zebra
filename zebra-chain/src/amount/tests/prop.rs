31cd64faf52cb5d3a3aa63f3a3a9886dab484036
teor <teor@riseup.net>
2021-08-09 10:03:32 +1000

    Move amount proptests to amount::tests::prop

---
+++ zebra-chain/src/amount/tests/prop.rs
@@ -0,0 +1,27 @@
+//! Randomised property tests for amounts.
+
+use proptest::prelude::*;
+
+use crate::amount::*;
+
+proptest! {
+    #[test]
+    fn amount_serialization(amount in any::<Amount<NegativeAllowed>>()) {
+        zebra_test::init();
+
+        let bytes = amount.to_bytes();
+        let serialized_amount = Amount::<NegativeAllowed>::from_bytes(bytes)?;
+
+        prop_assert_eq!(amount, serialized_amount);
+    }
+
+    #[test]
+    fn amount_deserialization(bytes in any::<[u8; 8]>()) {
+        zebra_test::init();
+
+        if let Ok(deserialized) = Amount::<NegativeAllowed>::from_bytes(bytes) {
+            let bytes2 = deserialized.to_bytes();
+            prop_assert_eq!(bytes, bytes2);
+        }
+    }
+}
