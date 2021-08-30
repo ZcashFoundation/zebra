2e1d857b27dda202aca35d0c4d51bd7c09546692
teor <teor@riseup.net>
2021-08-30 12:38:41 +1000

    Add transaction IDs to the chain tip channel (#2686)
    * Re-use finalized blocks for chain tip updates
    
    This avoids serializing and deserializing blocks from the finalized state.
    
    * Optimise tip sender equality checks
    
    * Re-use precalculated block hashes and heights for chain tip updates
    
    * Add chain tip mined transaction IDs
    
    * Doc comment typo
    
    Co-authored-by: Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
    
    Co-authored-by: Deirdre Connolly <deirdre@zfnd.org>
    Co-authored-by: Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>

--- zebra-state/src/service/chain_tip.rs
+++ zebra-state/src/service/chain_tip.rs
@@ -5,13 +5,67 @@ use tokio::sync::watch;
 use zebra_chain::{
     block::{self, Block},
     chain_tip::ChainTip,
+    transaction,
 };
+
+use crate::{request::ContextuallyValidBlock, FinalizedBlock};
 
 #[cfg(test)]
 mod tests;
 
 /// The internal watch channel data type for [`ChainTipSender`] and [`ChainTipReceiver`].
-type ChainTipData = Option<Arc<Block>>;
+type ChainTipData = Option<ChainTipBlock>;
+
+/// A chain tip block, with precalculated block data.
+///
+/// Used to efficiently update the [`ChainTipSender`].
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct ChainTipBlock {
+    pub(crate) block: Arc<Block>,
+    pub(crate) hash: block::Hash,
+    pub(crate) height: block::Height,
+
+    /// The mined transaction IDs of the transactions in `block`,
+    /// in the same order as `block.transactions`.
+    pub(crate) transaction_hashes: Vec<transaction::Hash>,
+}
+
+impl From<ContextuallyValidBlock> for ChainTipBlock {
+    fn from(contextually_valid: ContextuallyValidBlock) -> Self {
+        let ContextuallyValidBlock {
+            block,
+            hash,
+            height,
+            new_outputs: _,
+            transaction_hashes,
+            chain_value_pool_change: _,
+        } = contextually_valid;
+        Self {
+            block,
+            hash,
+            height,
+            transaction_hashes,
+        }
+    }
+}
+
+impl From<FinalizedBlock> for ChainTipBlock {
+    fn from(finalized: FinalizedBlock) -> Self {
+        let FinalizedBlock {
+            block,
+            hash,
+            height,
+            new_outputs: _,
+            transaction_hashes,
+        } = finalized;
+        Self {
+            block,
+            hash,
+            height,
+            transaction_hashes,
+        }
+    }
+}
 
 /// A sender for recent changes to the non-finalized and finalized chain tips.
 #[derive(Debug)]
@@ -33,7 +87,7 @@ pub struct ChainTipSender {
 impl ChainTipSender {
     /// Create new linked instances of [`ChainTipSender`] and [`ChainTipReceiver`],
     /// using `initial_tip` as the tip.
-    pub fn new(initial_tip: impl Into<Option<Arc<Block>>>) -> (Self, ChainTipReceiver) {
+    pub fn new(initial_tip: impl Into<Option<ChainTipBlock>>) -> (Self, ChainTipReceiver) {
         let (sender, receiver) = watch::channel(None);
         let mut sender = ChainTipSender {
             non_finalized_tip: false,
@@ -50,7 +104,7 @@ impl ChainTipSender {
     /// Update the current finalized tip.
     ///
     /// May trigger an update to the best tip.
-    pub fn set_finalized_tip(&mut self, new_tip: impl Into<Option<Arc<Block>>>) {
+    pub fn set_finalized_tip(&mut self, new_tip: impl Into<Option<ChainTipBlock>>) {
         if !self.non_finalized_tip {
             self.update(new_tip);
         }
@@ -59,7 +113,7 @@ impl ChainTipSender {
     /// Update the current non-finalized tip.
     ///
     /// May trigger an update to the best tip.
-    pub fn set_best_non_finalized_tip(&mut self, new_tip: impl Into<Option<Arc<Block>>>) {
+    pub fn set_best_non_finalized_tip(&mut self, new_tip: impl Into<Option<ChainTipBlock>>) {
         let new_tip = new_tip.into();
 
         // once the non-finalized state becomes active, it is always populated
@@ -74,14 +128,18 @@ impl ChainTipSender {
     ///
     /// An update is only sent if the current best tip is different from the last best tip
     /// that was sent.
-    fn update(&mut self, new_tip: impl Into<Option<Arc<Block>>>) {
+    fn update(&mut self, new_tip: impl Into<Option<ChainTipBlock>>) {
         let new_tip = new_tip.into();
 
-        if new_tip.is_none() {
-            return;
-        }
+        let needs_update = match (new_tip.as_ref(), self.active_value.as_ref()) {
+            // since the blocks have been contextually validated,
+            // we know their hashes cover all the block data
+            (Some(new_tip), Some(active_value)) => new_tip.hash != active_value.hash,
+            (Some(_new_tip), None) => true,
+            (None, _active_value) => false,
+        };
 
-        if new_tip != self.active_value {
+        if needs_update {
             let _ = self.sender.send(new_tip.clone());
             self.active_value = new_tip;
         }
@@ -110,16 +168,23 @@ impl ChainTipReceiver {
 impl ChainTip for ChainTipReceiver {
     /// Return the height of the best chain tip.
     fn best_tip_height(&self) -> Option<block::Height> {
-        self.receiver
-            .borrow()
-            .as_ref()
-            .and_then(|block| block.coinbase_height())
+        self.receiver.borrow().as_ref().map(|block| block.height)
     }
 
     /// Return the block hash of the best chain tip.
     fn best_tip_hash(&self) -> Option<block::Hash> {
-        // TODO: get the hash from the state and store it in the sender,
-        //       so we don't have to recalculate it every time
-        self.receiver.borrow().as_ref().map(|block| block.hash())
+        self.receiver.borrow().as_ref().map(|block| block.hash)
+    }
+
+    /// Return the mined transaction IDs of the transactions in the best chain tip block.
+    ///
+    /// All transactions with these mined IDs should be rejected from the mempool,
+    /// even if their authorizing data is different.
+    fn best_tip_mined_transaction_ids(&self) -> Vec<transaction::Hash> {
+        self.receiver
+            .borrow()
+            .as_ref()
+            .map(|block| block.transaction_hashes.clone())
+            .unwrap_or_default()
     }
 }
