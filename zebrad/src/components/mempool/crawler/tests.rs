0f935daf6b22012652df9cedea864a71827daebf
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-08-30 15:07:27 +0000

    Update test to consider when crawler is paused
    Change the mempool crawler test so that it's a proptest that tests
    different chain sync. lengths. This leads to different scenarios with
    the crawler pausing and resuming.

--- zebrad/src/components/mempool/crawler/tests.rs
+++ zebrad/src/components/mempool/crawler/tests.rs
@@ -1,5 +1,6 @@
 use std::time::Duration;
 
+use proptest::prelude::*;
 use tokio::{
     sync::mpsc::{self, UnboundedReceiver},
     time::{self, timeout},
@@ -21,37 +22,60 @@ const CRAWL_ITERATIONS: usize = 4;
 /// Note that this affects the total run time of the [`crawler_requests_for_transaction_ids`] test.
 /// There are [`CRAWL_ITERATIONS`] requests that are expected to not be sent, so the test runs for
 /// at least `MAX_REQUEST_DELAY * CRAWL_ITERATIONS`.
-const MAX_REQUEST_DELAY: Duration = Duration::from_millis(250);
+const MAX_REQUEST_DELAY: Duration = Duration::from_millis(25);
 
 /// The amount of time to advance beyond the expected instant that the crawler wakes up.
 const ERROR_MARGIN: Duration = Duration::from_millis(100);
 
-#[tokio::test]
-async fn crawler_requests_for_transaction_ids() {
-    let (peer_set, mut requests) = mock_peer_set();
+proptest! {
+    #[test]
+    fn crawler_requests_for_transaction_ids(mut sync_lengths in any::<Vec<usize>>()) {
+        let runtime = tokio::runtime::Builder::new_current_thread()
+            .enable_all()
+            .build()
+            .expect("Failed to create Tokio runtime");
+        let _guard = runtime.enter();
 
-    // Mock the latest sync length in a state that enables the mempool.
-    let (sync_status, mut recent_sync_lengths) = SyncStatus::new();
-    for _ in 0..5 {
-        recent_sync_lengths.push_extend_tips_length(0);
-    }
+        // Add a dummy last element, so that all of the original values are used.
+        sync_lengths.push(0);
 
-    Crawler::spawn(peer_set, sync_status);
+        runtime.block_on(async move {
+            let (peer_set, mut requests) = mock_peer_set();
+            let (sync_status, mut recent_sync_lengths) = SyncStatus::new();
 
-    time::pause();
+            time::pause();
 
-    for _ in 0..CRAWL_ITERATIONS {
-        for _ in 0..FANOUT {
-            let request = timeout(MAX_REQUEST_DELAY, requests.recv()).await;
+            Crawler::spawn(peer_set, sync_status.clone());
 
-            assert!(matches!(request, Ok(Some(Request::MempoolTransactionIds))));
-        }
+            for sync_length in sync_lengths {
+                let mempool_is_enabled = sync_status.is_close_to_tip();
 
-        let extra_request = timeout(MAX_REQUEST_DELAY, requests.recv()).await;
+                for _ in 0..CRAWL_ITERATIONS {
+                    for _ in 0..FANOUT {
+                        let request = timeout(MAX_REQUEST_DELAY, requests.recv()).await;
 
-        assert!(extra_request.is_err());
+                        if mempool_is_enabled {
+                            prop_assert!(matches!(request, Ok(Some(Request::MempoolTransactionIds))));
+                        } else {
+                            prop_assert!(request.is_err());
+                        }
+                    }
 
-        time::advance(RATE_LIMIT_DELAY + ERROR_MARGIN).await;
+                    let extra_request = timeout(MAX_REQUEST_DELAY, requests.recv()).await;
+
+                    prop_assert!(extra_request.is_err());
+
+                    time::sleep(RATE_LIMIT_DELAY + ERROR_MARGIN).await;
+                }
+
+                // Applying the update event at the end of the test iteration means that the first
+                // iteration runs with an empty recent sync. lengths vector. A dummy element is
+                // appended to the events so that all of the original values are applied.
+                recent_sync_lengths.push_extend_tips_length(sync_length);
+            }
+
+            Ok(())
+        })?;
     }
 }
 
