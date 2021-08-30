83a2e30e337a4904f50ee143f96f8eb04bab75ce
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-08-29 21:01:33 -0300

    Create a `SyncStatus` helper type (#2685)
    * Create a `SyncStatus` helper type
    
    Keeps track if the synchronizer is close to the chain tip or not.
    
    * Refactor `ChainSync` ctor. to return `SyncStatus`
    
    Change the constructor API so that it returns a higher level construct.
    
    * Test if `SyncStatus` waits for the chain tip
    
    Test if waiting for the chain tip to be reached correctly finishes when
    the chain tip is reached. This is done by sending recent sync lengths to
    the `SyncStatus` instance, and checking that every time a separate
    `SyncStatus` instance determines it has reached the tip the original
    instance wakes up.
    
    * Add a temporary attribute to allow dead code
    
    The code added isn't used yet, so we'll add a temporary waiver until
    another PR is merged to use them.

---
+++ zebrad/src/components/sync/status.rs
@@ -0,0 +1,49 @@
+// TODO: Remove this attribute once this type is used (#2603).
+#![allow(dead_code)]
+
+use tokio::sync::watch;
+
+use super::RecentSyncLengths;
+
+#[cfg(test)]
+mod tests;
+
+/// A helper type to determine if the synchronizer has likely reached the chain tip.
+///
+/// This type can be used as a handle, so cloning it is cheap.
+#[derive(Clone, Debug)]
+pub struct SyncStatus {
+    latest_sync_length: watch::Receiver<Vec<usize>>,
+}
+
+impl SyncStatus {
+    /// Create an instance of [`SyncStatus`].
+    ///
+    /// The status is determined based on the latest counts of synchronized blocks, observed
+    /// through `latest_sync_length`.
+    pub fn new() -> (Self, RecentSyncLengths) {
+        let (recent_sync_lengths, latest_sync_length) = RecentSyncLengths::new();
+        let status = SyncStatus { latest_sync_length };
+
+        (status, recent_sync_lengths)
+    }
+
+    /// Wait until the synchronization is likely close to the tip.
+    ///
+    /// Returns an error if communication with the synchronizer is lost.
+    pub async fn wait_until_close_to_tip(&mut self) -> Result<(), watch::error::RecvError> {
+        while !self.is_close_to_tip() {
+            self.latest_sync_length.changed().await?;
+        }
+
+        Ok(())
+    }
+
+    /// Check if the synchronization is likely close to the chain tip.
+    pub fn is_close_to_tip(&self) -> bool {
+        let _sync_lengths = self.latest_sync_length.borrow();
+
+        // TODO: Determine if the synchronization is actually close to the tip (#2592).
+        true
+    }
+}
