303c8cf5ef273db7e11b2d177f66e39d1fdd6cea
teor <teor@riseup.net>
2021-10-19 05:23:21 +1000

    Add a queue checker task, to make sure mempool transactions propagate (#2888)
    * Guarantee unique IDs in mempool service responses
    
    * Guarantee unique IDs in crawler task mempool Queue requests
    
    Also update the tests to use unique IDs.
    
    * Add a CheckForVerifiedTransactions mempool request
    
    Also document the mempool request and response variants.
    
    * Spawn a QueueChecker task to check for newly verified transactions
    
    This task makes sure that transactions reliably propagate,
    rather than relying on peer requests or responses to trigger propagation.
    
    * Update the start command documentation
    
    Co-authored-by: Alfredo Garcia <oxarbitrage@gmail.com>

---
+++ zebrad/src/components/mempool/queue_checker.rs
@@ -0,0 +1,86 @@
+//! Zebra Mempool queue checker.
+//!
+//! The queue checker periodically sends a request to the mempool,
+//! so that newly verified transactions are added to the mempool,
+//! and gossiped to peers.
+//!
+//! The mempool performs these actions on every request,
+//! but we can't guarantee that requests will arrive from peers
+//! on a regular basis.
+//!
+//! Crawler queue requests are also too infrequent,
+//! and they only happen if peers respond within the timeout.
+
+use std::time::Duration;
+
+use tokio::{task::JoinHandle, time::sleep};
+use tower::{BoxError, Service, ServiceExt};
+
+use crate::components::mempool;
+
+/// The delay between queue check events.
+///
+/// This interval is chosen so that there are a significant number of
+/// queue checks in each target block interval.
+///
+/// This allows transactions to propagate across the network for each block,
+/// even if some peers are poorly connected.
+const RATE_LIMIT_DELAY: Duration = Duration::from_secs(5);
+
+/// The mempool queue checker.
+///
+/// The queue checker relies on the mempool to ignore requests when the mempool is inactive.
+pub struct QueueChecker<Mempool> {
+    /// The mempool service that receives crawled transaction IDs.
+    mempool: Mempool,
+}
+
+impl<Mempool> QueueChecker<Mempool>
+where
+    Mempool:
+        Service<mempool::Request, Response = mempool::Response, Error = BoxError> + Send + 'static,
+    Mempool::Future: Send,
+{
+    /// Spawn an asynchronous task to run the mempool queue checker.
+    pub fn spawn(mempool: Mempool) -> JoinHandle<Result<(), BoxError>> {
+        let queue_checker = QueueChecker { mempool };
+
+        tokio::spawn(queue_checker.run())
+    }
+
+    /// Periodically check if the mempool has newly verified transactions.
+    ///
+    /// Runs until the mempool returns an error,
+    /// which happens when Zebra is shutting down.
+    pub async fn run(mut self) -> Result<(), BoxError> {
+        info!("initializing mempool queue checker task");
+
+        loop {
+            sleep(RATE_LIMIT_DELAY).await;
+            self.check_queue().await?;
+        }
+    }
+
+    /// Check if the mempool has newly verified transactions.
+    async fn check_queue(&mut self) -> Result<(), BoxError> {
+        debug!("checking for newly verified mempool transactions");
+
+        // Since this is an internal request, we don't expect any errors.
+        // So we propagate any unexpected errors to the task that spawned us.
+        let response = self
+            .mempool
+            .ready_and()
+            .await?
+            .call(mempool::Request::CheckForVerifiedTransactions)
+            .await?;
+
+        match response {
+            mempool::Response::CheckedForVerifiedTransactions => {}
+            _ => {
+                unreachable!("mempool did not respond with checked queue to mempool queue checker")
+            }
+        };
+
+        Ok(())
+    }
+}
