aad14df14ef01d28c05feaf8e5a88b1a7e00ded1
teor <teor@riseup.net>
2021-10-28 09:04:31 +1000

    Remove duplicate code in rebase

--- zebra-network/src/peer_set/initialize.rs
+++ zebra-network/src/peer_set/initialize.rs
@@ -492,11 +492,6 @@ where
             // Zebra can't control how many queued connections are waiting,
             // but most OSes also limit the number of queued inbound connections on a listener port.
             tokio::time::sleep(constants::MIN_PEER_CONNECTION_INTERVAL).await;
-        } else {
-            // Security: Let other tasks run after each errored connection is processed.
-            //
-            // Avoids remote peers starving other Zebra tasks using inbound connection errors.
-            tokio::task::yield_now().await;
         }
 
         // Security: Let other tasks run after each connection is processed.
