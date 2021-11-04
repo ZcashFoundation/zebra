5f44e026d6a8b95a16bd25a5a9a2a3a2f8ca84ea
teor <teor@riseup.net>
2021-11-04 14:38:01 +1000

    Add tests for CompactSize64

--- zebra-chain/src/serialization/tests/prop.rs
+++ zebra-chain/src/serialization/tests/prop.rs
@@ -5,13 +5,15 @@ use std::io::Cursor;
 use proptest::prelude::*;
 
 use crate::{
-    serialization::{CompactSizeMessage, ZcashDeserialize, ZcashDeserializeInto, ZcashSerialize},
+    serialization::{
+        CompactSize64, CompactSizeMessage, ZcashDeserialize, ZcashDeserializeInto, ZcashSerialize,
+    },
     transaction::UnminedTx,
 };
 
 proptest! {
     #[test]
-    fn compactsize_write_then_read_round_trip(s in any::<CompactSizeMessage>()) {
+    fn compact_size_message_write_then_read_round_trip(s in any::<CompactSizeMessage>()) {
         zebra_test::init();
 
         let buf = s.zcash_serialize_to_vec().unwrap();
@@ -23,11 +25,42 @@ proptest! {
     }
 
     #[test]
-    fn compactsize_read_then_write_round_trip(bytes in prop::array::uniform9(0u8..)) {
+    fn compact_size_message_read_then_write_round_trip(bytes in prop::array::uniform9(0u8..)) {
         zebra_test::init();
 
         // Only do the test if the bytes were valid.
         if let Ok(s) = CompactSizeMessage::zcash_deserialize(Cursor::new(&bytes[..])) {
+            // The CompactSize encoding is variable-length, so we may not even
+            // read all of the input bytes, and therefore we can't expect that
+            // the encoding will reproduce bytes that were never read. Instead,
+            // copy the input bytes, and overwrite them with the encoding of s,
+            // so that if the encoding is different, we'll catch it on the part
+            // that's written.
+            let mut expect_bytes = bytes;
+            s.zcash_serialize(Cursor::new(&mut expect_bytes[..])).unwrap();
+
+            prop_assert_eq!(bytes, expect_bytes);
+        }
+    }
+
+    #[test]
+    fn compact_size_64_write_then_read_round_trip(s in any::<CompactSize64>()) {
+        zebra_test::init();
+
+        let buf = s.zcash_serialize_to_vec().unwrap();
+        // Maximum encoding size of a CompactSize is 9 bytes.
+        prop_assert!(buf.len() <= 9);
+
+        let expect_s: CompactSize64 = buf.zcash_deserialize_into().unwrap();
+        prop_assert_eq!(s, expect_s);
+    }
+
+    #[test]
+    fn compact_size_64_read_then_write_round_trip(bytes in prop::array::uniform9(0u8..)) {
+        zebra_test::init();
+
+        // Only do the test if the bytes were valid.
+        if let Ok(s) = CompactSize64::zcash_deserialize(Cursor::new(&bytes[..])) {
             // The CompactSize encoding is variable-length, so we may not even
             // read all of the input bytes, and therefore we can't expect that
             // the encoding will reproduce bytes that were never read. Instead,
