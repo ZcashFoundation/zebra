a7f11c2101cf926f86225154589ae232a01a5602
teor <teor@riseup.net>
2021-11-12 09:40:34 +1000

    Add comments with expected values of address test vectors

--- zebra-test/src/network_addr.rs
+++ zebra-test/src/network_addr.rs
@@ -21,13 +21,13 @@ lazy_static! {
 
                 "61bc6649",                         // time, Fri Jan  9 02:54:25 UTC 2009
                 "0000000000000000",                 // service flags, NODE_NONE
-                "00000000000000000000000000000001", // address, fixed 16 bytes (IPv4 embedded in IPv6)
-                "0000",                             // port
+                "00000000000000000000000000000001", // address, fixed 16 bytes (IPv4-mapped IPv6), ::1
+                "0000",                             // port, 0
 
                 "79627683",                         // time, Tue Nov 22 11:22:33 UTC 2039
                 "0100000000000000",                 // service flags, NODE_NETWORK
-                "00000000000000000000000000000001", // address, fixed 16 bytes (IPv6)
-                "00f1",                             // port
+                "00000000000000000000000000000001", // address, fixed 16 bytes (IPv4-mapped IPv6), ::1
+                "00f1",                             // port, 241
             )
         ).expect("Message bytes are in valid hex representation"),
 
@@ -38,8 +38,8 @@ lazy_static! {
 
                 "79627683",                         // time, Tue Nov 22 11:22:33 UTC 2039
                 "0100000000000000",                 // service flags, NODE_NETWORK
-                "00000000000000000000000000000000", // address, fixed 16 bytes (IPv6)
-                "235a",                             // port
+                "00000000000000000000000000000000", // address, fixed 16 bytes (IPv4-mapped IPv6), ::
+                "235a",                             // port, 9050
             )
         ).expect("Message bytes are in valid hex representation"),
 
@@ -52,8 +52,8 @@ lazy_static! {
 
                 "61bc6649",                         // time, Fri Jan  9 02:54:25 UTC 2009
                 "ffffffffffffffff",                 // service flags, all set
-                "00000000000000000000000000000001", // address, fixed 16 bytes (IPv4 embedded in IPv6)
-                "0000",                             // port
+                "00000000000000000000000000000001", // address, fixed 16 bytes (IPv4-mapped IPv6), ::1
+                "0000",                             // port, 0
             )
         ).expect("Message bytes are in valid hex representation"),
 
@@ -64,9 +64,10 @@ lazy_static! {
 
                 "61bc6649",                         // time, Fri Jan  9 02:54:25 UTC 2009
                 "0100000000000000",                 // service flags, NODE_NETWORK
-                "00000000000000000000ffff",         // address, fixed 16 bytes (IPv4 embedded in IPv6)
+                                                    // address, fixed 16 bytes (IPv4-mapped IPv6),
+                "00000000000000000000ffff",         // IPv4-mapped IPv6 prefix, ::ffff...
                 "7f000001",                         // IPv4, 127.0.0.1
-                "0000",                             // port
+                "0000",                             // port, 0
             )
         ).expect("Message bytes are in valid hex representation"),
 
@@ -100,22 +101,23 @@ lazy_static! {
                 "00",                               // service flags, COMPACTSIZE(NODE_NONE)
                 "02",                               // network id, IPv6
                 "10",                               // address length, COMPACTSIZE(16)
-                "00000000000000000000000000000001", // address
-                "0000",                             // port
+                "00000000000000000000000000000001", // address, ::1
+                "0000",                             // port, 0
 
                 "79627683",                         // time, Tue Nov 22 11:22:33 UTC 2039
                 "01",                               // service flags, COMPACTSIZE(NODE_NETWORK)
                 "02",                               // network id, IPv6
                 "10",                               // address length, COMPACTSIZE(16)
-                "00000000000000000000000000000001", // address
-                "00f1",                             // port
+                "00000000000000000000000000000001", // address, ::1
+                "00f1",                             // port, 241
 
                 "79627683",                         // time, Tue Nov 22 11:22:33 UTC 2039
                 "01",                               // service flags, COMPACTSIZE(NODE_NETWORK)
                 "04",                               // network id, TorV3
                 "20",                               // address length, COMPACTSIZE(32)
-                "53cd5648488c4707914182655b7664034e09e66f7e8cbf1084e654eb56c5bd88", // 32 bytes of key + 2 bytes checksum
-                "235a",                             // port
+                "53cd5648488c4707914182655b7664034e09e66f7e8cbf1084e654eb56c5bd88",
+                                                    // address, (32 byte Tor v3 onion service public key)
+                "235a",                             // port, 9050
             )
         ).expect("Message bytes are in valid hex representation"),
 
@@ -130,16 +132,16 @@ lazy_static! {
                 "01",                               // service flags, COMPACTSIZE(NODE_NETWORK)
                 "01",                               // network id, IPv4
                 "04",                               // address length, COMPACTSIZE(4)
-                "7f000001",                         // address
-                "0001",                             // port
+                "7f000001",                         // address, 127.0.0.1
+                "0001",                             // port, 1
 
                 // check that variable-length encoding works
                 "79627683",                         // time, Tue Nov 22 11:22:33 UTC 2039
                 "01",                               // service flags, COMPACTSIZE(NODE_NETWORK)
                 "02",                               // network id, IPv6
                 "10",                               // address length, COMPACTSIZE(16)
-                "00000000000000000000000000000001", // address
-                "00f1",                             // port
+                "00000000000000000000000000000001", // address, ::1
+                "00f1",                             // port, 241
             )
         ).expect("Message bytes are in valid hex representation"),
 
@@ -152,8 +154,8 @@ lazy_static! {
                 "ffffffffffffffffff",               // service flags, COMPACTSIZE(all flags set)
                 "02",                               // network id, IPv6
                 "10",                               // address length, COMPACTSIZE(16)
-                "00000000000000000000000000000001", // address
-                "0000",                             // port
+                "00000000000000000000000000000001", // address, ::1
+                "0000",                             // port, 0
             )
         ).expect("Message bytes are in valid hex representation"),
 
@@ -164,18 +166,18 @@ lazy_static! {
 
                 "79627683",                         // time, Tue Nov 22 11:22:33 UTC 2039
                 "01",                               // service flags, COMPACTSIZE(NODE_NETWORK)
-                "fb",                               // network id, unknown
+                "fb",                               // network id, (unknown)
                 "08",                               // address length, COMPACTSIZE(8)
-                "0000000000000000",                 // address
-                "0001",                             // port
+                "0000000000000000",                 // address, (8 zero bytes)
+                "0001",                             // port, 1
 
                 // check that variable-length encoding works
                 "79627683",                         // time, Tue Nov 22 11:22:33 UTC 2039
                 "01",                               // service flags, COMPACTSIZE(NODE_NETWORK)
                 "02",                               // network id, IPv6
                 "10",                               // address length, COMPACTSIZE(16)
-                "00000000000000000000000000000001", // address
-                "00f1",                             // port
+                "00000000000000000000000000000001", // address, ::1
+                "00f1",                             // port, 241
             )
         ).expect("Message bytes are in valid hex representation"),
 
@@ -186,18 +188,18 @@ lazy_static! {
 
                 "79627683",                         // time, Tue Nov 22 11:22:33 UTC 2039
                 "01",                               // service flags, COMPACTSIZE(NODE_NETWORK)
-                "fc",                               // network id, unknown
-                "00",                               // address length, COMPACTSIZE(8)
-                "",                                 // address
-                "0001",                             // port
+                "fc",                               // network id, (unknown)
+                "00",                               // address length, COMPACTSIZE(0)
+                "",                                 // address, (no bytes)
+                "0001",                             // port, 1
 
                 // check that variable-length encoding works
                 "79627683",                         // time, Tue Nov 22 11:22:33 UTC 2039
                 "01",                               // service flags, COMPACTSIZE(NODE_NETWORK)
                 "02",                               // network id, IPv6
                 "10",                               // address length, COMPACTSIZE(16)
-                "00000000000000000000000000000001", // address
-                "00f1",                             // port
+                "00000000000000000000000000000001", // address, ::1
+                "00f1",                             // port, 241
             )
         ).expect("Message bytes are in valid hex representation"),
 
@@ -209,20 +211,20 @@ lazy_static! {
 
                         "79627683",                         // time, Tue Nov 22 11:22:33 UTC 2039
                         "01",                               // service flags, COMPACTSIZE(NODE_NETWORK)
-                        "fd",                               // network id, unknown
+                        "fd",                               // network id, (unknown)
                         "fd0002",                           // address length, COMPACTSIZE(512)
                     ),
-                    "00".repeat(512),                       // address
+                    "00".repeat(512),                       // address, (512 zero bytes)
                     concat!(
-                        "0001",                             // port
+                        "0001",                             // port, 1
 
                         // check that variable-length encoding works
                         "79627683",                         // time, Tue Nov 22 11:22:33 UTC 2039
                         "01",                               // service flags, COMPACTSIZE(NODE_NETWORK)
                         "02",                               // network id, IPv6
                         "10",                               // address length, COMPACTSIZE(16)
-                        "00000000000000000000000000000001", // address
-                        "00f1",                             // port
+                        "00000000000000000000000000000001", // address, ::1
+                        "00f1",                             // port, 241
                     )
             )
         ).expect("Message bytes are in valid hex representation"),
@@ -243,8 +245,9 @@ lazy_static! {
                 "01",                               // service flags, COMPACTSIZE(NODE_NETWORK)
                 "04",                               // network id, TorV3
                 "20",                               // address length, COMPACTSIZE(32)
-                "53cd5648488c4707914182655b7664034e09e66f7e8cbf1084e654eb56c5bd88", // 32 bytes of key + 2 bytes checksum
-                "235a",                             // port
+                "53cd5648488c4707914182655b7664034e09e66f7e8cbf1084e654eb56c5bd88",
+                                                    // address, (32 byte Tor v3 onion service public key)
+                "235a",                             // port, 9050
             )
         ).expect("Message bytes are in valid hex representation"),
 
@@ -269,20 +272,20 @@ lazy_static! {
 
                         "79627683",                         // time, Tue Nov 22 11:22:33 UTC 2039
                         "01",                               // service flags, COMPACTSIZE(NODE_NETWORK)
-                        "fe",                               // network id, unknown
+                        "fe",                               // network id, (unknown)
                         "fd0102",                           // invalid address length, COMPACTSIZE(513)
                     ),
-                    "00".repeat(513),                       // address
+                    "00".repeat(513),                       // address, (513 zero bytes)
                     concat!(
-                        "0001",                             // port
+                        "0001",                             // port, 1
 
                         // check that the entire message is ignored
                         "79627683",                         // time, Tue Nov 22 11:22:33 UTC 2039
                         "01",                               // service flags, COMPACTSIZE(NODE_NETWORK)
                         "02",                               // network id, IPv6
                         "10",                               // address length, COMPACTSIZE(16)
-                        "00000000000000000000000000000001", // address
-                        "00f1",                             // port
+                        "00000000000000000000000000000001", // address, ::1
+                        "00f1",                             // port, 241
                     )
             )
         ).expect("Message bytes are in valid hex representation"),
@@ -294,9 +297,9 @@ lazy_static! {
 
                 "79627683",                         // time, Tue Nov 22 11:22:33 UTC 2039
                 "01",                               // service flags, COMPACTSIZE(NODE_NETWORK)
-                "ff",                               // network id, unknown
+                "ff",                               // network id, (unknown)
                 "feffffff7f",                       // invalid address length, COMPACTSIZE(2^31 - 1)
-                // no address, won't fit in memory
+                // no address, generated bytes wouldn't fit in memory
             ),
         ).expect("Message bytes are in valid hex representation"),
     ];
