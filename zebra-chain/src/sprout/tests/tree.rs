cd9a3570ba9ad1d6829ec4f69297cbf0f1b9b215
Marek <mail@marek.onl>
2021-11-11 16:17:37 +0100

    Add tests for Sprout note commitment tree

---
+++ zebra-chain/src/sprout/tests/tree.rs
@@ -0,0 +1,45 @@
+//! Tests for Sprout Note Commitment Trees.
+
+use hex::FromHex;
+
+use crate::sprout::commitment::NoteCommitment;
+use crate::sprout::tests::test_vectors;
+use crate::sprout::tree;
+
+#[test]
+fn empty_roots() {
+    zebra_test::init();
+
+    for i in 0..tree::EMPTY_ROOTS.len() {
+        assert_eq!(
+            hex::encode(tree::EMPTY_ROOTS[i]),
+            test_vectors::HEX_EMPTY_ROOTS[i]
+        );
+    }
+}
+
+#[test]
+fn incremental_roots() {
+    zebra_test::init();
+
+    let mut leaves = vec![];
+
+    let mut incremental_tree = tree::NoteCommitmentTree::default();
+
+    for (i, cm) in test_vectors::COMMITMENTS.iter().enumerate() {
+        let bytes = <[u8; 32]>::from_hex(cm).unwrap();
+
+        let cm = NoteCommitment::from(bytes);
+
+        leaves.push(cm);
+
+        let _ = incremental_tree.append(cm);
+
+        assert_eq!(hex::encode(incremental_tree.hash()), test_vectors::ROOTS[i]);
+
+        // assert_eq!(
+        //     hex::encode((NoteCommitmentTree::from(leaves.clone())).hash()),
+        //     roots[i]
+        // );
+    }
+}
