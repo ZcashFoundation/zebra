b26558238e68a64f31b4b70b25c6bf3f5f834db0
Marek <mail@marek.onl>
2021-11-17 21:58:36 +0100

    Finish the tests for the note commitment trees

--- zebra-chain/src/sprout/tests/tree.rs
+++ zebra-chain/src/sprout/tests/tree.rs
@@ -6,7 +6,7 @@ use color_eyre::eyre;
 use eyre::Result;
 
 use hex::FromHex;
-use zebra_test::vectors::{MAINNET_BLOCKS, MAINNET_FINAL_SPROUT_ROOTS};
+use zebra_test::vectors;
 
 use crate::block::Block;
 use crate::parameters::{Network, NetworkUpgrade};
@@ -82,46 +82,52 @@ fn incremental_roots() {
 #[test]
 fn incremental_roots_with_blocks() -> Result<()> {
     incremental_roots_with_blocks_for_network(Network::Mainnet)?;
-
-    // TODO Get final Sprout roots for Testnet and uncomment the line below.
-
-    // incremental_roots_with_blocks_for_network(Network::Testnet)?;
+    incremental_roots_with_blocks_for_network(Network::Testnet)?;
 
     Ok(())
 }
 
 fn incremental_roots_with_blocks_for_network(network: Network) -> Result<()> {
-    let (blocks, sprout_roots) = match network {
-        Network::Mainnet => (&*MAINNET_BLOCKS, &*MAINNET_FINAL_SPROUT_ROOTS),
+    // The mainnet block height at which the first JoinSplit occurred.
+    const MAINNET_FIRST_JOINSPLIT_HEIGHT: u32 = 396;
 
-        // TODO Use `TESTNET_BLOCKS` and `TESTNET_FINAL_SPROUT_ROOTS` once they are
-        // available.
-        Network::Testnet => (&*MAINNET_BLOCKS, &*MAINNET_FINAL_SPROUT_ROOTS),
+    // The testnet block height at which the first JoinSplit occurred.
+    const TESTNET_FIRST_JOINSPLIT_HEIGHT: u32 = 2259;
+
+    // Load the test data.
+    let (blocks, sprout_roots, next_height) = match network {
+        Network::Mainnet => (
+            &*vectors::MAINNET_BLOCKS,
+            &*vectors::MAINNET_FINAL_SPROUT_ROOTS,
+            MAINNET_FIRST_JOINSPLIT_HEIGHT,
+        ),
+        Network::Testnet => (
+            &*vectors::TESTNET_BLOCKS,
+            &*vectors::TESTNET_FINAL_SPROUT_ROOTS,
+            TESTNET_FIRST_JOINSPLIT_HEIGHT,
+        ),
     };
 
-    // We pick the initial blocks of the Overwinter network upgrade. This is a
-    // reasonable choice since Overwinter still uses the Sprout shielded pool,
-    // and the block heights have a significant meaning in comparison to a
-    // random Sprout block.
-    let height = NetworkUpgrade::Overwinter
+    // Load the Genesis height.
+    let genesis_height = NetworkUpgrade::Genesis
         .activation_height(network)
         .unwrap()
         .0;
 
-    // Build an empty note commitment tree
-    let mut note_commitment_tree = tree::NoteCommitmentTree::default();
-
-    // Load the Overwinter activation block.
-    let overwinter_activation_block = Arc::new(
+    // Load the Genesis block.
+    let genesis_block = Arc::new(
         blocks
-            .get(&height)
+            .get(&genesis_height)
             .expect("test vector exists")
             .zcash_deserialize_into::<Block>()
             .expect("block is structurally valid"),
     );
 
-    // Add note commitments from the Overwinter activation block to the tree.
-    for transaction in overwinter_activation_block.transactions.iter() {
+    // Build an empty note commitment tree.
+    let mut note_commitment_tree = tree::NoteCommitmentTree::default();
+
+    // Add note commitments from Genesis to the tree.
+    for transaction in genesis_block.transactions.iter() {
         for sprout_note_commitment in transaction.sprout_note_commitments() {
             note_commitment_tree
                 .append(*sprout_note_commitment)
@@ -129,29 +135,36 @@ fn incremental_roots_with_blocks_for_network(network: Network) -> Result<()> {
         }
     }
 
-    // Check if the root of the tree of the activation block is correct.
-    let overwinter_activation_block_root =
-        tree::Root(**sprout_roots.get(&height).expect("test vector exists"));
-    assert_eq!(
-        overwinter_activation_block_root,
-        note_commitment_tree.root()
+    // Load the Genesis note commitment tree root.
+    let genesis_anchor = tree::Root(
+        **sprout_roots
+            .get(&genesis_height)
+            .expect("test vector exists"),
     );
 
-    // Load the block immediately after Overwinter activation (activation + 1).
-    let block_after_overwinter_activation = Arc::new(
+    // Check if the root of the note commitment tree of Genesis is correct.
+    assert_eq!(genesis_anchor, note_commitment_tree.root());
+
+    // Load the first block after Genesis that contains a JoinSplit transaction
+    // so that we can add new note commitments to the tree.
+    let next_block = Arc::new(
         blocks
-            .get(&(height + 1))
+            .get(&(genesis_height + next_height))
             .expect("test vector exists")
             .zcash_deserialize_into::<Block>()
             .expect("block is structurally valid"),
     );
 
-    let block_after_overwinter_activation_root =
-        tree::Root(**sprout_roots.get(&(height + 1)).expect("test vector exists"));
+    // Load the note commitment tree root of `next_block`.
+    let next_block_anchor = tree::Root(
+        **sprout_roots
+            .get(&(genesis_height + next_height))
+            .expect("test vector exists"),
+    );
 
-    // Add note commitments from the block after Overwinter activation to the tree.
+    // Add the note commitments from `next_block` to the tree.
     let mut appended_count = 0;
-    for transaction in block_after_overwinter_activation.transactions.iter() {
+    for transaction in next_block.transactions.iter() {
         for sprout_note_commitment in transaction.sprout_note_commitments() {
             note_commitment_tree
                 .append(*sprout_note_commitment)
@@ -163,17 +176,10 @@ fn incremental_roots_with_blocks_for_network(network: Network) -> Result<()> {
     // We also want to make sure that sprout_note_commitments() is returning
     // the commitments in the right order. But this will only be actually tested
     // if there are more than one note commitment in a block.
-    // In the test vectors this applies only for the block 1 in mainnet,
-    // so we make this explicit in this assert.
-    if network == Network::Mainnet {
-        assert!(appended_count > 1);
-    }
+    assert!(appended_count > 1);
 
-    // Check if root of the second block is correct
-    assert_eq!(
-        block_after_overwinter_activation_root,
-        note_commitment_tree.root()
-    );
+    // Check if the root of `next_block` is correct.
+    assert_eq!(next_block_anchor, note_commitment_tree.root());
 
     Ok(())
 }
