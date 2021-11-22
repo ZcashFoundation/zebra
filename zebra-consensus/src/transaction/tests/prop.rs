f7af58d7e922619cb11b4e299bf5b96b82c6fcad
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-11-22 20:58:55 +0000

    Create a helper function to select block heights
    Allows generating block heights inside a range using a scale factor
    between 0 and 1.

--- zebra-consensus/src/transaction/tests/prop.rs
+++ zebra-consensus/src/transaction/tests/prop.rs
@@ -25,3 +25,35 @@ fn sapling_onwards_strategy() -> impl Strategy<Value = (Network, block::Height)>
             .prop_map(move |height_value| (network, block::Height(height_value)))
     })
 }
+
+/// Selects a [`block::Height`] between `min_height` and `max_height` using the `scale` factor.
+///
+/// The `scale` must be in the range `0.0..1.0`, where `0.0` results in the selection of
+/// `min_height` and `1.0` would select the `max_height` if the range was inclusive. The range is
+/// exclusive however, so `max_height` is never selected (unless it is equal to `min_height`).
+///
+/// # Panics
+///
+/// - if `scale` is not in the range `0.0..1.0`
+/// - if `min_height` is greater than `max_height`
+fn scale_block_height(
+    min_height: impl Into<Option<block::Height>>,
+    max_height: impl Into<Option<block::Height>>,
+    scale: f64,
+) -> block::Height {
+    assert!(scale >= 0.0);
+    assert!(scale < 1.0);
+
+    let min_height = min_height.into().unwrap_or(block::Height(0));
+    let max_height = max_height.into().unwrap_or(block::Height::MAX);
+
+    assert!(min_height <= max_height);
+
+    let min_height_value = f64::from(min_height.0);
+    let max_height_value = f64::from(max_height.0);
+    let height_range = max_height_value - min_height_value;
+
+    let new_height_value = (height_range * scale + min_height_value).floor();
+
+    block::Height(new_height_value as u32)
+}
