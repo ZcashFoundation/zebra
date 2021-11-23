734099fb933bbb0213370d24f8ad799de798ffac
teor <teor@riseup.net>
2021-11-24 06:36:53 +1000

    Add a 1 hour timeout to parameter file downloads

--- zebra-consensus/src/primitives/groth16/params.rs
+++ zebra-consensus/src/primitives/groth16/params.rs
@@ -5,6 +5,13 @@ use std::path::PathBuf;
 use bellman::groth16;
 use bls12_381::Bls12;
 
+/// The timeout for each parameter file download, in seconds.
+///
+/// Zebra assumes that it's running on at least a 10 Mbps connection.
+/// So the parameter files should download in about 15 minutes using `zebrad download`.
+/// But `zebrad start` downloads blocks at the same time, so we allow some extra time.
+pub const PARAMETER_DOWNLOAD_TIMEOUT: u64 = 60 * 60;
+
 lazy_static::lazy_static! {
     /// Groth16 Zero-Knowledge Proof parameters for the Sapling and Sprout circuits.
     ///
@@ -52,22 +59,26 @@ impl Groth16Parameters {
     /// If the downloaded or pre-existing parameter files are invalid.
     fn new() -> Groth16Parameters {
         tracing::info!("downloading Zcash Sapling parameters if needed");
-        let sapling_paths = zcash_proofs::download_sapling_parameters().unwrap_or_else(|error| {
-            panic!(
-                "error downloading Sapling parameter files: {:?}. {}",
-                error,
-                Groth16Parameters::failure_hint()
-            )
-        });
+        let sapling_paths =
+            zcash_proofs::download_sapling_parameters(Some(PARAMETER_DOWNLOAD_TIMEOUT))
+                .unwrap_or_else(|error| {
+                    panic!(
+                        "error downloading Sapling parameter files: {:?}. {}",
+                        error,
+                        Groth16Parameters::failure_hint()
+                    )
+                });
 
         tracing::info!("downloading Zcash Sprout parameters if needed");
-        let sprout_path = zcash_proofs::download_sprout_parameters().unwrap_or_else(|error| {
-            panic!(
-                "error downloading Sprout parameter files: {:?}. {}",
-                error,
-                Groth16Parameters::failure_hint()
-            )
-        });
+        let sprout_path =
+            zcash_proofs::download_sprout_parameters(Some(PARAMETER_DOWNLOAD_TIMEOUT))
+                .unwrap_or_else(|error| {
+                    panic!(
+                        "error downloading Sprout parameter files: {:?}. {}",
+                        error,
+                        Groth16Parameters::failure_hint()
+                    )
+                });
 
         // TODO: if loading fails, log a message including `failure_hint`
         tracing::info!("checking and loading Zcash Sapling and Sprout parameters");
