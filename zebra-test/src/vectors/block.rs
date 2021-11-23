dbd49a3f00173084153d422c3a8fa01e938f6dc9
Alfredo Garcia <oxarbitrage@gmail.com>
2021-11-23 02:17:05 -0300

    Validate coinbase expiration height (#3082)
    * add testnet test blocks around nu5
    
    * validate coinbase expiration height
    
    * change const name and doc
    
    Co-authored-by: teor <teor@riseup.net>
    
    * change commit location
    
    Co-authored-by: teor <teor@riseup.net>
    
    * use pre Nu5 rules when there is no activation height
    
    * add sapling final root to nu5 test vectors
    
    * fix tests
    
    Co-authored-by: teor <teor@riseup.net>

--- zebra-test/src/vectors/block.rs
+++ zebra-test/src/vectors/block.rs
@@ -220,6 +220,10 @@ lazy_static! {
             (1_116_000, BLOCK_TESTNET_1116000_BYTES.as_ref()),
             (1_116_001, BLOCK_TESTNET_1116001_BYTES.as_ref()),
             (1_326_100, BLOCK_TESTNET_1326100_BYTES.as_ref()),
+            (1_599_199, BLOCK_TESTNET_1599199_BYTES.as_ref()),
+            // Nu5
+            (1_599_200, BLOCK_TESTNET_1599200_BYTES.as_ref()),
+            (1_599_201, BLOCK_TESTNET_1599201_BYTES.as_ref()),
         ].iter().cloned().collect();
 
     /// Testnet final Sprout roots, indexed by height.
@@ -272,6 +276,10 @@ lazy_static! {
             (1_116_000, SAPLING_FINAL_ROOT_TESTNET_1116000_BYTES.as_ref().try_into().unwrap()),
             (1_116_001, SAPLING_FINAL_ROOT_TESTNET_1116001_BYTES.as_ref().try_into().unwrap()),
             (1_326_100, SAPLING_FINAL_ROOT_TESTNET_1326100_BYTES.as_ref().try_into().unwrap()),
+            (1_599_199, SAPLING_FINAL_ROOT_TESTNET_1599199_BYTES.as_ref().try_into().unwrap()),
+            // Nu5
+            (1_599_200, SAPLING_FINAL_ROOT_TESTNET_1599200_BYTES.as_ref().try_into().unwrap()),
+            (1_599_201, SAPLING_FINAL_ROOT_TESTNET_1599201_BYTES.as_ref().try_into().unwrap()),
         ].iter().cloned().collect();
 
     // Mainnet
@@ -834,6 +842,29 @@ lazy_static! {
         .expect("Block bytes are in valid hex representation");
     pub static ref SAPLING_FINAL_ROOT_TESTNET_1326100_BYTES: [u8; 32] =
         <[u8; 32]>::from_hex("2b30b19f4254709fe365bd0b381b2e3d9d0c933eb4dba4dd1d07f0f6e196a183")
+        .expect("final root bytes are in valid hex representation").rev();
+
+    // Nu5 transition
+    // for i in 1599199 1599200 1599201; do
+    //     zcash-cli -testnet getblock $i 0 > block-test-$[i/1000000]-$[i/1000%1000]-$[i%1000].txt
+    // done
+    pub static ref BLOCK_TESTNET_1599199_BYTES: Vec<u8> =
+        <Vec<u8>>::from_hex(include_str!("block-test-1-599-199.txt").trim())
+        .expect("Block bytes are in valid hex representation");
+    pub static ref BLOCK_TESTNET_1599200_BYTES: Vec<u8> =
+        <Vec<u8>>::from_hex(include_str!("block-test-1-599-200.txt").trim())
+        .expect("Block bytes are in valid hex representation");
+    pub static ref BLOCK_TESTNET_1599201_BYTES: Vec<u8> =
+        <Vec<u8>>::from_hex(include_str!("block-test-1-599-201.txt").trim())
+        .expect("Block bytes are in valid hex representation");
+    pub static ref SAPLING_FINAL_ROOT_TESTNET_1599199_BYTES: [u8; 32] =
+        <[u8; 32]>::from_hex("4de75d10def701ad22ecc17517a3adc8789ea8c214ac5bfc917b8924377e6c89")
+        .expect("final root bytes are in valid hex representation").rev();
+    pub static ref SAPLING_FINAL_ROOT_TESTNET_1599200_BYTES: [u8; 32] =
+        <[u8; 32]>::from_hex("4de75d10def701ad22ecc17517a3adc8789ea8c214ac5bfc917b8924377e6c89")
+        .expect("final root bytes are in valid hex representation").rev();
+    pub static ref SAPLING_FINAL_ROOT_TESTNET_1599201_BYTES: [u8; 32] =
+        <[u8; 32]>::from_hex("4de75d10def701ad22ecc17517a3adc8789ea8c214ac5bfc917b8924377e6c89")
         .expect("final root bytes are in valid hex representation").rev();
 }
 
