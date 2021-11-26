a298fa3c55fd2c88e4f6265ce1f651a93b748ae6
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-11-26 19:53:27 +0000

    Create `MinimumPeerVersion` helper type
    A type to track the current minimum protocol version for connected
    peers based on the current block height.

--- zebra-network/src/peer.rs
+++ zebra-network/src/peer.rs
@@ -12,6 +12,8 @@ mod error;
 mod handshake;
 /// Tracks the load on a `Client` service.
 mod load_tracked_client;
+/// Watches for chain tip height updates to determine the minimum support peer protocol version.
+mod minimum_peer_version;
 
 use client::{ClientRequest, ClientRequestReceiver, InProgressClientRequest, MustUseOneshotSender};
 
@@ -21,3 +23,4 @@ pub use connector::{Connector, OutboundConnectorRequest};
 pub use error::{ErrorSlot, HandshakeError, PeerError, SharedPeerError};
 pub use handshake::{ConnectedAddr, Handshake, HandshakeRequest};
 pub use load_tracked_client::LoadTrackedClient;
+pub use minimum_peer_version::MinimumPeerVersion;
---
+++ zebra-network/src/peer/minimum_peer_version.rs
@@ -0,0 +1,80 @@
+use zebra_chain::{chain_tip::ChainTip, parameters::Network};
+
+use crate::protocol::external::types::Version;
+
+/// A helper type to monitor the chain tip in order to determine the minimum peer protocol version
+/// that is currently supported.
+pub struct MinimumPeerVersion<C> {
+    network: Network,
+    chain_tip: C,
+    current_minimum: Version,
+    has_changed: bool,
+}
+
+impl<C> MinimumPeerVersion<C>
+where
+    C: ChainTip,
+{
+    /// Create a new [`MinimumPeerVersion`] to track the minimum supported peer protocol version
+    /// for the current `chain_tip` on the `network`.
+    pub fn new(chain_tip: C, network: Network) -> Self {
+        MinimumPeerVersion {
+            network,
+            chain_tip,
+            current_minimum: Version::min_remote_for_height(network, None),
+            has_changed: true,
+        }
+    }
+
+    /// Check if the minimum supported peer version has changed since the last time this was
+    /// called.
+    ///
+    /// The first call returns the current minimum version, and subsequent calls return [`None`]
+    /// until the minimum version changes. When that happens, the next call returns the new minimum
+    /// version, and subsequent calls return [`None`] again until the minimum version changes once
+    /// more.
+    pub fn changed(&mut self) -> Option<Version> {
+        self.update();
+
+        if self.has_changed {
+            self.has_changed = false;
+            Some(self.current_minimum)
+        } else {
+            None
+        }
+    }
+
+    /// Retrieve the current minimum supported peer protocol version.
+    pub fn current(&mut self) -> Version {
+        self.update();
+        self.current_minimum
+    }
+
+    /// Check the current chain tip height to determine the minimum peer version, and detect if it
+    /// has changed.
+    fn update(&mut self) {
+        let height = self.chain_tip.best_tip_height();
+        let new_minimum = Version::min_remote_for_height(self.network, height);
+
+        if self.current_minimum != new_minimum {
+            self.current_minimum = new_minimum;
+            self.has_changed = true;
+        }
+    }
+}
+
+/// A custom [`Clone`] implementation to ensure that the first call to
+/// [`MinimumPeerVersion::changed`] after the clone will always return the current version.
+impl<C> Clone for MinimumPeerVersion<C>
+where
+    C: Clone,
+{
+    fn clone(&self) -> Self {
+        MinimumPeerVersion {
+            network: self.network,
+            chain_tip: self.chain_tip.clone(),
+            current_minimum: self.current_minimum,
+            has_changed: true,
+        }
+    }
+}
