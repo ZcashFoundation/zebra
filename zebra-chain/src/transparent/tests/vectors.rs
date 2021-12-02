73907af994d587b420c653b659fa58d4113cb9d0
Alfredo Garcia <oxarbitrage@gmail.com>
2021-12-02 10:20:05 -0300

    extend examples in test

--- zebra-chain/src/transparent/tests/vectors.rs
+++ zebra-chain/src/transparent/tests/vectors.rs
@@ -4,6 +4,26 @@ use super::super::serialize::parse_coinbase_height;
 fn parse_coinbase_height_mins() {
     zebra_test::init();
 
+    // examples with height 1:
+
+    let case1 = vec![0x51];
+    assert!(!parse_coinbase_height(case1.clone()).is_err());
+    assert_eq!(parse_coinbase_height(case1).unwrap().0 .0, 1);
+
+    let case2 = vec![0x01, 0x01];
+    assert!(parse_coinbase_height(case2).is_err());
+
+    let case3 = vec![0x02, 0x01, 0x00];
+    assert!(parse_coinbase_height(case3).is_err());
+
+    let case4 = vec![0x03, 0x01, 0x00, 0x00];
+    assert!(parse_coinbase_height(case4).is_err());
+
+    let case5 = vec![0x04, 0x01, 0x00, 0x00, 0x00];
+    assert!(parse_coinbase_height(case5).is_err());
+
+    // examples with height 17:
+
     let case1 = vec![0x01, 0x11];
     assert!(!parse_coinbase_height(case1.clone()).is_err());
     assert_eq!(parse_coinbase_height(case1).unwrap().0 .0, 17);
