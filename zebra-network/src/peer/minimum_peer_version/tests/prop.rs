25b6989956ba582afd6fe46b34b81051a0f5aab1
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-12-03 20:41:03 +0000

    Test if minimum version changes with chain tip
    Apply an arbitrary list of chain tip height updates and check that for
    each update the minimum peer version is calculated correctly.

--- zebra-network/src/peer/minimum_peer_version/tests/prop.rs
+++ zebra-network/src/peer/minimum_peer_version/tests/prop.rs
@@ -22,4 +22,24 @@ proptest! {
 
         prop_assert_eq!(minimum_peer_version.current(), expected_minimum_version);
     }
+
+    /// Test if the calculated minimum peer version changes with the tip height.
+    #[test]
+    fn minimum_peer_version_is_updated_with_chain_tip(
+        network in any::<Network>(),
+        block_heights in any::<Vec<Option<block::Height>>>(),
+    ) {
+        let (mut minimum_peer_version, best_tip_height) =
+            MinimumPeerVersion::with_mock_chain_tip(network);
+
+        for block_height in block_heights {
+            best_tip_height
+                .send(block_height)
+                .expect("receiving endpoint lives as long as `minimum_peer_version`");
+
+            let expected_minimum_version = Version::min_remote_for_height(network, block_height);
+
+            prop_assert_eq!(minimum_peer_version.current(), expected_minimum_version);
+        }
+    }
 }
