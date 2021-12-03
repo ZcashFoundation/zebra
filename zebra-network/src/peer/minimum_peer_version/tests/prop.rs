96597c36ad9b1af0416395701ba3d30fe6294fa6
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-12-03 21:10:19 +0000

    Test minimum peer version changed reports
    Simulate a series of best chain tip height updates, and check for
    minimum peer version updates at least once between them. Changes should
    only be reported once.

--- zebra-network/src/peer/minimum_peer_version/tests/prop.rs
+++ zebra-network/src/peer/minimum_peer_version/tests/prop.rs
@@ -42,4 +42,41 @@ proptest! {
             prop_assert_eq!(minimum_peer_version.current(), expected_minimum_version);
         }
     }
+
+    /// Test if the minimum peer version changes are correctly tracked.
+    #[test]
+    fn minimum_peer_version_reports_changes_correctly(
+        network in any::<Network>(),
+        block_height_updates in any::<Vec<Option<Option<block::Height>>>>(),
+    ) {
+        let (mut minimum_peer_version, best_tip_height) =
+            MinimumPeerVersion::with_mock_chain_tip(network);
+
+        let mut current_minimum_version = Version::min_remote_for_height(network, None);
+        let mut expected_minimum_version = Some(current_minimum_version);
+
+        prop_assert_eq!(minimum_peer_version.changed(), expected_minimum_version);
+
+        for update in block_height_updates {
+            if let Some(new_block_height) = update {
+                best_tip_height
+                    .send(new_block_height)
+                    .expect("receiving endpoint lives as long as `minimum_peer_version`");
+
+                let new_minimum_version = Version::min_remote_for_height(network, new_block_height);
+
+                expected_minimum_version = if new_minimum_version != current_minimum_version {
+                    Some(new_minimum_version)
+                } else {
+                    None
+                };
+
+                current_minimum_version = new_minimum_version;
+            } else {
+                expected_minimum_version = None;
+            }
+
+            prop_assert_eq!(minimum_peer_version.changed(), expected_minimum_version);
+        }
+    }
 }
