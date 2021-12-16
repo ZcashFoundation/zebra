43df91d88aa123b543e86ef50c11759827d14d8d
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-12-16 00:50:01 +0000

    Add extension method to check for readiness error
    Checks if the `Service` isn't immediately ready because a call to
    `ready` immediately returns an error.

--- zebra-test/src/service_extensions.rs
+++ zebra-test/src/service_extensions.rs
@@ -9,6 +9,9 @@ use now_or_later::NowOrLater;
 pub trait IsReady<Request>: Service<Request> {
     /// Check if the [`Service`] is immediately ready to be called.
     fn is_ready(&mut self) -> BoxFuture<bool>;
+
+    /// Check if the [`Service`] is not immediately ready because it returns an error.
+    fn not_ready_due_to_error(&mut self) -> BoxFuture<bool>;
 }
 
 impl<S, Request> IsReady<Request> for S
@@ -19,6 +22,12 @@ where
     fn is_ready(&mut self) -> BoxFuture<bool> {
         NowOrLater(self.ready())
             .map(|ready_result| matches!(ready_result, Some(Ok(_))))
+            .boxed()
+    }
+
+    fn not_ready_due_to_error(&mut self) -> BoxFuture<bool> {
+        NowOrLater(self.ready())
+            .map(|ready_result| matches!(ready_result, Some(Err(_))))
             .boxed()
     }
 }
