83a553448d8c25ac3d54a6d1f4687d8b3561e8a2
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-12-16 15:02:54 +0000

    Add dummy background tasks to `ClientTestHarness`
    Spawn simple timeout tasks as mock connection and heartbeat tasks.

--- zebra-network/src/peer/client/tests.rs
+++ zebra-network/src/peer/client/tests.rs
@@ -3,12 +3,18 @@
 
 mod vectors;
 
+use std::time::Duration;
+
 use futures::channel::{mpsc, oneshot};
+use tokio::task::JoinHandle;
 
 use crate::{
     peer::{error::SharedPeerError, CancelHeartbeatTask, Client, ClientRequest, ErrorSlot},
     protocol::external::types::Version,
 };
+
+/// The maximum time a mocked peer connection should be alive during a test.
+const MAX_PEER_CONNECTION_TIME: Duration = Duration::from_secs(10);
 
 /// A harness with mocked channels for testing a [`Client`] instance.
 pub struct ClientTestHarness {
@@ -175,6 +181,8 @@ impl ClientTestHarnessBuilder {
             server_tx: client_request_sender,
             error_slot: error_slot.clone(),
             version,
+            connection_task: Self::mock_background_task(),
+            heartbeat_task: Self::mock_background_task(),
         };
 
         let harness = ClientTestHarness {
@@ -185,5 +193,12 @@ impl ClientTestHarnessBuilder {
         };
 
         (client, harness)
+    }
+
+    /// Spawn a dummy background task.
+    ///
+    /// The task lives as long as [`MAX_PEER_CONNECTION_TIME`].
+    fn mock_background_task() -> JoinHandle<()> {
+        tokio::spawn(tokio::time::sleep(MAX_PEER_CONNECTION_TIME))
     }
 }
