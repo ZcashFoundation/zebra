09ad8907f038b20e48cf0a87aadbef79c7d81786
teor <teor@riseup.net>
2021-12-27 09:01:17 +1000

    Add an in-memory connection test that uses AsyncReadWrite

--- zebra-network/src/isolated/tests/vectors.rs
+++ zebra-network/src/isolated/tests/vectors.rs
@@ -23,8 +23,8 @@ async fn connect_isolated_sends_anonymised_version_message_tcp() {
         return;
     }
 
-    // These tests might fail on machines with no configured IPv4 addresses
-    // (localhost should be enough).
+    // These tests might fail on machines with no configured IPv4 addresses.
+    // (Localhost should be enough.)
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:0").await.unwrap();
     let listen_addr = listener.local_addr().unwrap();
@@ -94,9 +94,92 @@ async fn connect_isolated_sends_anonymised_version_message_tcp() {
     //
     // This test doesn't send a version message on `inbound_conn`,
     // so providing a service is incorrect behaviour.
+    //
+    // A timeout error would be acceptable,
+    // but a TCP connection error indicates a potential test setup issue.
+    // So we fail on them both, because we expect this test to complete before the timeout.
+    let outbound_result = futures::poll!(outbound_join_handle);
+    assert!(matches!(outbound_result, Poll::Pending));
+}
+
+/// Test that `connect_isolated` sends a version message with minimal distinguishing features,
+/// when sent in-memory.
+///
+/// This test also:
+/// - checks `AsyncReadWrite` support, and
+/// - runs even if network tests are disabled.
+#[tokio::test]
+async fn connect_isolated_sends_anonymised_version_message_mem() {
+    zebra_test::init();
+
+    // We expect version messages to be ~100 bytes
+    let (inbound_stream, outbound_stream) = tokio::io::duplex(1024);
+
+    let outbound_join_handle = tokio::spawn(connect_isolated(outbound_stream, "".to_string()));
+
+    let mut inbound_stream = Framed::new(inbound_stream, Codec::builder().finish());
+
+    // We don't need to send any bytes to get a version message.
+    if let Message::Version {
+        version,
+        services,
+        timestamp,
+        address_recv,
+        address_from,
+        nonce: _,
+        user_agent,
+        start_height,
+        relay,
+    } = inbound_stream
+        .next()
+        .await
+        .expect("stream item")
+        .expect("item is Ok(msg)")
+    {
+        // Check that the version message sent by connect_isolated
+        // anonymises all the fields that it possibly can.
+        //
+        // The version field needs to be accurate, because it controls protocol features.
+        // The nonce must be randomised for security.
+        //
+        // SECURITY TODO: check if the timestamp field can be zeroed, to remove another distinguisher (#3300)
+        let fixed_isolated_addr: SocketAddr = "0.0.0.0:8233".parse().unwrap();
+
+        // Required fields should be accurate and match most other peers.
+        // (We can't test nonce randomness here.)
+        assert_eq!(version, CURRENT_NETWORK_PROTOCOL_VERSION);
+        assert_eq!(timestamp.timestamp() % (5 * 60), 0);
+
+        // Other fields should be empty or zeroed.
+        assert_eq!(services, PeerServices::empty());
+        assert_eq!(
+            address_recv,
+            // Since we're connecting to the peer, we expect it to have the node flag.
+            //
+            // SECURITY TODO: should this just be zeroed anyway? (#3300)
+            AddrInVersion::new(fixed_isolated_addr, PeerServices::NODE_NETWORK),
+        );
+        assert_eq!(
+            address_from,
+            AddrInVersion::new(fixed_isolated_addr, PeerServices::empty()),
+        );
+        assert_eq!(user_agent, "");
+        assert_eq!(start_height.0, 0);
+        assert!(!relay);
+    } else {
+        panic!("handshake did not send version message");
+    }
+
+    // Make sure that the isolated connection did not:
+    // - panic, or
+    // - return a service.
+    //
+    // This test doesn't send a version message on `inbound_conn`,
+    // so providing a service is incorrect behaviour.
+    // (But a timeout error would be acceptable.)
     let outbound_result = futures::poll!(outbound_join_handle);
     assert!(matches!(
         outbound_result,
-        Poll::Ready(Ok(Err(_))) | Poll::Pending,
+        Poll::Pending | Poll::Ready(Ok(Err(_)))
     ));
 }
