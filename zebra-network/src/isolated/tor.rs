7926081911a5e7f0c7d8ed63f5dd1dbe1d6e5eb8
teor <teor@riseup.net>
2021-12-27 17:06:03 +1000

    Use a shared tor client instance for all isolated connections

--- zebra-network/src/isolated/tor.rs
+++ zebra-network/src/isolated/tor.rs
@@ -1,6 +1,9 @@
 //! Uses tor to create isolated and anonymised connections to specific peers.
 
+use std::sync::{Arc, Mutex};
+
 use arti_client::{TorAddr, TorClient, TorClientConfig};
+use tor_rtcompat::tokio::TokioRuntimeHandle;
 use tower::util::BoxService;
 
 use zebra_chain::parameters::Network;
@@ -9,6 +12,15 @@ use crate::{connect_isolated, BoxError, Request, Response};
 
 #[cfg(test)]
 mod tests;
+
+lazy_static::lazy_static! {
+    /// The shared isolated [`TorClient`] instance.
+    ///
+    /// TODO: turn this into a tower service that takes a hostname, and returns an `arti_client::DataStream`
+    ///       (or a task that updates a watch channel when it's done?)
+    pub static ref SHARED_TOR_CLIENT: Arc<Mutex<Option<TorClient<TokioRuntimeHandle>>>> =
+        Arc::new(Mutex::new(None));
+}
 
 /// Creates a Zcash peer connection to `hostname` via Tor.
 /// This connection is completely isolated from all other node state.
@@ -34,10 +46,46 @@ pub async fn connect_isolated_tor(
     user_agent: String,
 ) -> Result<BoxService<Request, Response, BoxError>, BoxError> {
     let addr = TorAddr::from(hostname)?;
-    let runtime = tor_rtcompat::tokio::current_runtime()?;
 
-    let tor_client = TorClient::bootstrap(runtime, TorClientConfig::default()).await?;
+    // Initialize or clone the shared tor client instance
+    let tor_client = match cloned_tor_client() {
+        Some(tor_client) => tor_client,
+        None => new_tor_client().await?,
+    };
+
     let tor_stream = tor_client.connect(addr, None).await?;
 
     connect_isolated(network, tor_stream, user_agent).await
+}
+
+/// Returns a new tor client instance, and updates [`SHARED_TOR_CLIENT`].
+///
+/// If there is a bootstrap error, [`SHARED_TOR_CLIENT`] is not modified.
+async fn new_tor_client() -> Result<TorClient<TokioRuntimeHandle>, BoxError> {
+    let runtime = tokio::runtime::Handle::current();
+    let runtime = TokioRuntimeHandle::new(runtime);
+    let tor_client = TorClient::bootstrap(runtime, TorClientConfig::default()).await?;
+
+    // # Correctness
+    //
+    // It is ok for multiple tasks to race, because all tor clients have identical configs.
+    // And all connections are isolated, regardless of whether they use a new or cloned client.
+    // (Any replaced clients will be dropped.)
+    let mut shared_tor_client = SHARED_TOR_CLIENT
+        .lock()
+        .expect("panic in shared tor client mutex guard");
+    *shared_tor_client = Some(tor_client.isolated_client());
+
+    Ok(tor_client)
+}
+
+/// Returns an isolated tor client instance by cloning [`SHARED_TOR_CLIENT`].
+///
+/// If [`new_tor_client`] has not run successfully yet, returns `None`.
+fn cloned_tor_client() -> Option<TorClient<TokioRuntimeHandle>> {
+    SHARED_TOR_CLIENT
+        .lock()
+        .expect("panic in shared tor client mutex guard")
+        .as_ref()
+        .map(TorClient::isolated_client)
 }
