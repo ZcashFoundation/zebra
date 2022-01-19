e57f1ce677dd897322b2844b0f5efbea36a3beb7
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2022-01-19 13:30:40 +0000

    Refactor setup of `Connection` test vectors
    Add a `new_test_connection` helper function to create a `Connection`
    instance that's ready for testing.

--- zebra-network/src/peer/connection/tests/vectors.rs
+++ zebra-network/src/peer/connection/tests/vectors.rs
@@ -6,52 +6,30 @@
 
 use futures::{channel::mpsc, FutureExt};
 use tokio_util::codec::FramedWrite;
-use tower::service_fn;
+
 use zebra_chain::parameters::Network;
+use zebra_test::mock_service::{MockService, PanicAssertion};
 
 use crate::{
-    peer::{client::ClientRequestReceiver, connection::State, Connection, ErrorSlot},
+    peer::{
+        client::ClientRequestReceiver, connection::State, ClientRequest, Connection, ErrorSlot,
+    },
     peer_set::ActiveConnectionCounter,
     protocol::external::Codec,
-    PeerError,
+    PeerError, Request, Response,
 };
 
 #[tokio::test]
 async fn connection_run_loop_ok() {
     zebra_test::init();
 
-    let (client_tx, client_rx) = mpsc::channel(1);
-
     // The real stream and sink are from a split TCP connection,
     // but that doesn't change how the state machine behaves.
     let (peer_inbound_tx, peer_inbound_rx) = mpsc::channel(1);
 
     let mut peer_outbound_bytes = Vec::<u8>::new();
-    let peer_outbound_tx = FramedWrite::new(
-        &mut peer_outbound_bytes,
-        Codec::builder()
-            .for_network(Network::Mainnet)
-            .with_metrics_addr_label("test".into())
-            .finish(),
-    );
 
-    let unused_inbound_service =
-        service_fn(|_| async { unreachable!("inbound service should never be called") });
-
-    let shared_error_slot = ErrorSlot::default();
-
-    let connection = Connection {
-        state: State::AwaitingRequest,
-        request_timer: None,
-        cached_addrs: Vec::new(),
-        svc: unused_inbound_service,
-        client_rx: ClientRequestReceiver::from(client_rx),
-        error_slot: shared_error_slot.clone(),
-        peer_tx: peer_outbound_tx,
-        connection_tracker: ActiveConnectionCounter::new_counter().track_connection(),
-        metrics_label: "test".to_string(),
-        last_metrics_state: None,
-    };
+    let (connection, client_tx, shared_error_slot) = new_test_connection(&mut peer_outbound_bytes);
 
     let connection = connection.run(peer_inbound_rx);
 
@@ -83,36 +61,13 @@ async fn connection_run_loop_ok() {
 async fn connection_run_loop_future_drop() {
     zebra_test::init();
 
-    let (client_tx, client_rx) = mpsc::channel(1);
-
+    // The real stream and sink are from a split TCP connection,
+    // but that doesn't change how the state machine behaves.
     let (peer_inbound_tx, peer_inbound_rx) = mpsc::channel(1);
 
     let mut peer_outbound_bytes = Vec::<u8>::new();
-    let peer_outbound_tx = FramedWrite::new(
-        &mut peer_outbound_bytes,
-        Codec::builder()
-            .for_network(Network::Mainnet)
-            .with_metrics_addr_label("test".into())
-            .finish(),
-    );
 
-    let unused_inbound_service =
-        service_fn(|_| async { unreachable!("inbound service should never be called") });
-
-    let shared_error_slot = ErrorSlot::default();
-
-    let connection = Connection {
-        state: State::AwaitingRequest,
-        request_timer: None,
-        cached_addrs: Vec::new(),
-        svc: unused_inbound_service,
-        client_rx: ClientRequestReceiver::from(client_rx),
-        error_slot: shared_error_slot.clone(),
-        peer_tx: peer_outbound_tx,
-        connection_tracker: ActiveConnectionCounter::new_counter().track_connection(),
-        metrics_label: "test".to_string(),
-        last_metrics_state: None,
-    };
+    let (connection, client_tx, shared_error_slot) = new_test_connection(&mut peer_outbound_bytes);
 
     let connection = connection.run(peer_inbound_rx);
 
@@ -133,36 +88,14 @@ async fn connection_run_loop_future_drop() {
 async fn connection_run_loop_client_close() {
     zebra_test::init();
 
-    let (mut client_tx, client_rx) = mpsc::channel(1);
-
+    // The real stream and sink are from a split TCP connection,
+    // but that doesn't change how the state machine behaves.
     let (peer_inbound_tx, peer_inbound_rx) = mpsc::channel(1);
 
     let mut peer_outbound_bytes = Vec::<u8>::new();
-    let peer_outbound_tx = FramedWrite::new(
-        &mut peer_outbound_bytes,
-        Codec::builder()
-            .for_network(Network::Mainnet)
-            .with_metrics_addr_label("test".into())
-            .finish(),
-    );
 
-    let unused_inbound_service =
-        service_fn(|_| async { unreachable!("inbound service should never be called") });
-
-    let shared_error_slot = ErrorSlot::default();
-
-    let connection = Connection {
-        state: State::AwaitingRequest,
-        request_timer: None,
-        cached_addrs: Vec::new(),
-        svc: unused_inbound_service,
-        client_rx: ClientRequestReceiver::from(client_rx),
-        error_slot: shared_error_slot.clone(),
-        peer_tx: peer_outbound_tx,
-        connection_tracker: ActiveConnectionCounter::new_counter().track_connection(),
-        metrics_label: "test".to_string(),
-        last_metrics_state: None,
-    };
+    let (connection, mut client_tx, shared_error_slot) =
+        new_test_connection(&mut peer_outbound_bytes);
 
     let connection = connection.run(peer_inbound_rx);
 
@@ -190,36 +123,13 @@ async fn connection_run_loop_client_close() {
 async fn connection_run_loop_client_drop() {
     zebra_test::init();
 
-    let (client_tx, client_rx) = mpsc::channel(1);
-
+    // The real stream and sink are from a split TCP connection,
+    // but that doesn't change how the state machine behaves.
     let (peer_inbound_tx, peer_inbound_rx) = mpsc::channel(1);
 
     let mut peer_outbound_bytes = Vec::<u8>::new();
-    let peer_outbound_tx = FramedWrite::new(
-        &mut peer_outbound_bytes,
-        Codec::builder()
-            .for_network(Network::Mainnet)
-            .with_metrics_addr_label("test".into())
-            .finish(),
-    );
 
-    let unused_inbound_service =
-        service_fn(|_| async { unreachable!("inbound service should never be called") });
-
-    let shared_error_slot = ErrorSlot::default();
-
-    let connection = Connection {
-        state: State::AwaitingRequest,
-        request_timer: None,
-        cached_addrs: Vec::new(),
-        svc: unused_inbound_service,
-        client_rx: ClientRequestReceiver::from(client_rx),
-        error_slot: shared_error_slot.clone(),
-        peer_tx: peer_outbound_tx,
-        connection_tracker: ActiveConnectionCounter::new_counter().track_connection(),
-        metrics_label: "test".to_string(),
-        last_metrics_state: None,
-    };
+    let (connection, client_tx, shared_error_slot) = new_test_connection(&mut peer_outbound_bytes);
 
     let connection = connection.run(peer_inbound_rx);
 
@@ -246,36 +156,13 @@ async fn connection_run_loop_client_drop() {
 async fn connection_run_loop_inbound_close() {
     zebra_test::init();
 
-    let (client_tx, client_rx) = mpsc::channel(1);
-
+    // The real stream and sink are from a split TCP connection,
+    // but that doesn't change how the state machine behaves.
     let (mut peer_inbound_tx, peer_inbound_rx) = mpsc::channel(1);
 
     let mut peer_outbound_bytes = Vec::<u8>::new();
-    let peer_outbound_tx = FramedWrite::new(
-        &mut peer_outbound_bytes,
-        Codec::builder()
-            .for_network(Network::Mainnet)
-            .with_metrics_addr_label("test".into())
-            .finish(),
-    );
 
-    let unused_inbound_service =
-        service_fn(|_| async { unreachable!("inbound service should never be called") });
-
-    let shared_error_slot = ErrorSlot::default();
-
-    let connection = Connection {
-        state: State::AwaitingRequest,
-        request_timer: None,
-        cached_addrs: Vec::new(),
-        svc: unused_inbound_service,
-        client_rx: ClientRequestReceiver::from(client_rx),
-        error_slot: shared_error_slot.clone(),
-        peer_tx: peer_outbound_tx,
-        connection_tracker: ActiveConnectionCounter::new_counter().track_connection(),
-        metrics_label: "test".to_string(),
-        last_metrics_state: None,
-    };
+    let (connection, client_tx, shared_error_slot) = new_test_connection(&mut peer_outbound_bytes);
 
     let connection = connection.run(peer_inbound_rx);
 
@@ -303,36 +190,13 @@ async fn connection_run_loop_inbound_close() {
 async fn connection_run_loop_inbound_drop() {
     zebra_test::init();
 
-    let (client_tx, client_rx) = mpsc::channel(1);
-
+    // The real stream and sink are from a split TCP connection,
+    // but that doesn't change how the state machine behaves.
     let (peer_inbound_tx, peer_inbound_rx) = mpsc::channel(1);
 
     let mut peer_outbound_bytes = Vec::<u8>::new();
-    let peer_outbound_tx = FramedWrite::new(
-        &mut peer_outbound_bytes,
-        Codec::builder()
-            .for_network(Network::Mainnet)
-            .with_metrics_addr_label("test".into())
-            .finish(),
-    );
 
-    let unused_inbound_service =
-        service_fn(|_| async { unreachable!("inbound service should never be called") });
-
-    let shared_error_slot = ErrorSlot::default();
-
-    let connection = Connection {
-        state: State::AwaitingRequest,
-        request_timer: None,
-        cached_addrs: Vec::new(),
-        svc: unused_inbound_service,
-        client_rx: ClientRequestReceiver::from(client_rx),
-        error_slot: shared_error_slot.clone(),
-        peer_tx: peer_outbound_tx,
-        connection_tracker: ActiveConnectionCounter::new_counter().track_connection(),
-        metrics_label: "test".to_string(),
-        last_metrics_state: None,
-    };
+    let (connection, client_tx, shared_error_slot) = new_test_connection(&mut peer_outbound_bytes);
 
     let connection = connection.run(peer_inbound_rx);
 
@@ -359,41 +223,20 @@ async fn connection_run_loop_inbound_drop() {
 async fn connection_run_loop_failed() {
     zebra_test::init();
 
-    let (client_tx, client_rx) = mpsc::channel(1);
-
+    // The real stream and sink are from a split TCP connection,
+    // but that doesn't change how the state machine behaves.
     let (peer_inbound_tx, peer_inbound_rx) = mpsc::channel(1);
 
     let mut peer_outbound_bytes = Vec::<u8>::new();
-    let peer_outbound_tx = FramedWrite::new(
-        &mut peer_outbound_bytes,
-        Codec::builder()
-            .for_network(Network::Mainnet)
-            .with_metrics_addr_label("test".into())
-            .finish(),
-    );
 
-    let unused_inbound_service =
-        service_fn(|_| async { unreachable!("inbound service should never be called") });
-
-    let shared_error_slot = ErrorSlot::default();
+    let (mut connection, client_tx, shared_error_slot) =
+        new_test_connection(&mut peer_outbound_bytes);
 
     // Simulate an internal connection error.
+    connection.state = State::Failed;
     shared_error_slot
         .try_update_error(PeerError::ClientRequestTimeout.into())
         .expect("unexpected previous error in tests");
-
-    let connection = Connection {
-        state: State::Failed,
-        request_timer: None,
-        cached_addrs: Vec::new(),
-        svc: unused_inbound_service,
-        client_rx: ClientRequestReceiver::from(client_rx),
-        error_slot: shared_error_slot.clone(),
-        peer_tx: peer_outbound_tx,
-        connection_tracker: ActiveConnectionCounter::new_counter().track_connection(),
-        metrics_label: "test".to_string(),
-        last_metrics_state: None,
-    };
 
     let connection = connection.run(peer_inbound_rx);
 
@@ -414,4 +257,42 @@ async fn connection_run_loop_failed() {
     // We need to drop the future, because it holds a mutable reference to the bytes.
     std::mem::drop(connection_guard);
     assert_eq!(peer_outbound_bytes, Vec::<u8>::new());
+}
+
+/// Creates a new [`Connection`] instance for testing.
+fn new_test_connection(
+    peer_outbound_bytes: &mut Vec<u8>,
+) -> (
+    Connection<MockService<Request, Response, PanicAssertion>, FramedWrite<&mut Vec<u8>, Codec>>,
+    mpsc::Sender<ClientRequest>,
+    ErrorSlot,
+) {
+    let (client_tx, client_rx) = mpsc::channel(1);
+
+    let peer_outbound_tx = FramedWrite::new(
+        peer_outbound_bytes,
+        Codec::builder()
+            .for_network(Network::Mainnet)
+            .with_metrics_addr_label("test".into())
+            .finish(),
+    );
+
+    let unused_inbound_service = MockService::build().for_unit_tests();
+
+    let shared_error_slot = ErrorSlot::default();
+
+    let connection = Connection {
+        state: State::AwaitingRequest,
+        request_timer: None,
+        cached_addrs: Vec::new(),
+        svc: unused_inbound_service,
+        client_rx: ClientRequestReceiver::from(client_rx),
+        error_slot: shared_error_slot.clone(),
+        peer_tx: peer_outbound_tx,
+        connection_tracker: ActiveConnectionCounter::new_counter().track_connection(),
+        metrics_label: "test".to_string(),
+        last_metrics_state: None,
+    };
+
+    (connection, client_tx, shared_error_slot)
 }
