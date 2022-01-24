9a5c79ac2f0e50794d05df4400c58cc87aa1f996
teor <teor@riseup.net>
2022-01-24 14:57:19 +1000

    Refactor common test code into a function

--- zebra-network/src/isolated/tests/vectors.rs
+++ zebra-network/src/isolated/tests/vectors.rs
@@ -46,61 +46,9 @@ async fn connect_isolated_sends_anonymised_version_message_tcp_net(network: Netw
 
     let (inbound_conn, _) = listener.accept().await.unwrap();
 
-    let mut inbound_stream =
-        Framed::new(inbound_conn, Codec::builder().for_network(network).finish());
+    let inbound_stream = Framed::new(inbound_conn, Codec::builder().for_network(network).finish());
 
-    // We don't need to send any bytes to get a version message.
-    if let Message::Version {
-        version,
-        services,
-        timestamp,
-        address_recv,
-        address_from,
-        nonce: _,
-        user_agent,
-        start_height,
-        relay,
-    } = inbound_stream
-        .next()
-        .await
-        .expect("stream item")
-        .expect("item is Ok(msg)")
-    {
-        // Check that the version message sent by connect_isolated
-        // anonymises all the fields that it possibly can.
-        //
-        // The version field needs to be accurate, because it controls protocol features.
-        // The nonce must be randomised for security.
-        //
-        // SECURITY TODO: check if the timestamp field can be zeroed, to remove another distinguisher (#3300)
-
-        let mut fixed_isolated_addr: SocketAddr = "0.0.0.0:0".parse().unwrap();
-        fixed_isolated_addr.set_port(network.default_port());
-
-        // Required fields should be accurate and match most other peers.
-        // (We can't test nonce randomness here.)
-        assert_eq!(version, CURRENT_NETWORK_PROTOCOL_VERSION);
-        assert_eq!(timestamp.timestamp() % (5 * 60), 0);
-
-        // Other fields should be empty or zeroed.
-        assert_eq!(services, PeerServices::empty());
-        assert_eq!(
-            address_recv,
-            // Since we're connecting to the peer, we expect it to have the node flag.
-            //
-            // SECURITY TODO: should this just be zeroed anyway? (#3300)
-            AddrInVersion::new(fixed_isolated_addr, PeerServices::NODE_NETWORK),
-        );
-        assert_eq!(
-            address_from,
-            AddrInVersion::new(fixed_isolated_addr, PeerServices::empty()),
-        );
-        assert_eq!(user_agent, "");
-        assert_eq!(start_height.0, 0);
-        assert!(!relay);
-    } else {
-        panic!("handshake did not send version message");
-    }
+    check_version_message(network, inbound_stream).await;
 
     // Let the spawned task run for a short time.
     tokio::time::sleep(Duration::from_secs(1)).await;
@@ -142,11 +90,41 @@ async fn connect_isolated_sends_anonymised_version_message_mem_net(network: Netw
     let mut outbound_join_handle =
         tokio::spawn(connect_isolated(network, outbound_stream, "".to_string()));
 
-    let mut inbound_stream = Framed::new(
+    let inbound_stream = Framed::new(
         inbound_stream,
         Codec::builder().for_network(network).finish(),
     );
 
+    check_version_message(network, inbound_stream).await;
+
+    // Let the spawned task run for a short time.
+    tokio::time::sleep(Duration::from_secs(1)).await;
+
+    // Make sure that the isolated connection did not:
+    // - panic, or
+    // - return a service.
+    //
+    // This test doesn't send a version message on `inbound_conn`,
+    // so providing a service is incorrect behaviour.
+    // (But a timeout error would be acceptable.)
+    let outbound_result = futures::poll!(&mut outbound_join_handle);
+    assert!(matches!(
+        outbound_result,
+        Poll::Pending | Poll::Ready(Ok(Err(_)))
+    ));
+
+    outbound_join_handle.abort();
+}
+
+/// Wait to receive a version message on `inbound_stream`,
+/// then check that it is correctly anonymised.
+#[track_caller]
+async fn check_version_message<PeerTransport>(
+    network: Network,
+    mut inbound_stream: Framed<PeerTransport, Codec>,
+) where
+    PeerTransport: AsyncRead + Unpin,
+{
     // We don't need to send any bytes to get a version message.
     if let Message::Version {
         version,
@@ -199,22 +177,4 @@ async fn connect_isolated_sends_anonymised_version_message_mem_net(network: Netw
     } else {
         panic!("handshake did not send version message");
     }
-
-    // Let the spawned task run for a short time.
-    tokio::time::sleep(Duration::from_secs(1)).await;
-
-    // Make sure that the isolated connection did not:
-    // - panic, or
-    // - return a service.
-    //
-    // This test doesn't send a version message on `inbound_conn`,
-    // so providing a service is incorrect behaviour.
-    // (But a timeout error would be acceptable.)
-    let outbound_result = futures::poll!(&mut outbound_join_handle);
-    assert!(matches!(
-        outbound_result,
-        Poll::Pending | Poll::Ready(Ok(Err(_)))
-    ));
-
-    outbound_join_handle.abort();
 }
