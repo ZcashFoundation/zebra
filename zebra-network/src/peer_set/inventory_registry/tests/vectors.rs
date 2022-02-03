c5b624983e321cc9d8c4590c32681509e322ae7b
teor <teor@riseup.net>
2022-02-03 11:12:07 +1000

    test(network): check that current inventory is preferred over previous

--- zebra-network/src/peer_set/inventory_registry/tests/vectors.rs
+++ zebra-network/src/peer_set/inventory_registry/tests/vectors.rs
@@ -133,6 +133,61 @@ async fn inv_registry_prefer_missing_order(missing_first: bool) {
     );
 }
 
+/// Check inventory registration for one hash/peer prefers current over previous.
+#[tokio::test]
+async fn inv_registry_prefer_current_ok() {
+    inv_registry_prefer_current_order(true).await;
+    inv_registry_prefer_current_order(false).await;
+}
+
+async fn inv_registry_prefer_current_order(missing_current: bool) {
+    let test_hash = InventoryHash::Block(block::Hash([0; 32]));
+    let test_peer = "1.1.1.1:1"
+        .parse()
+        .expect("unexpected invalid peer address");
+
+    let missing_change = InventoryStatus::new_missing(test_hash, test_peer);
+    let advertised_change = InventoryStatus::new_advertised(test_hash, test_peer);
+
+    let (mut inv_registry, inv_stream_tx) = new_inv_registry();
+
+    let changes = if missing_current {
+        [advertised_change, missing_change]
+    } else {
+        [missing_change, advertised_change]
+    };
+
+    for change in changes {
+        // This rotation has no effect in the first loop iteration, because the registry is empty.
+        inv_registry.rotate();
+
+        let receiver_count = inv_stream_tx
+            .send(change)
+            .expect("unexpected failed inventory status send");
+        assert_eq!(receiver_count, 1);
+
+        // We must update after each change, so the rotation puts the first change in `prev`.
+        inv_registry
+            .update()
+            .await
+            .expect("unexpected dropped registry sender channel");
+    }
+
+    if missing_current {
+        assert_eq!(inv_registry.advertising_peers(test_hash).count(), 0);
+        assert_eq!(
+            inv_registry.missing_peers(test_hash).next(),
+            Some(&test_peer),
+        );
+    } else {
+        assert_eq!(
+            inv_registry.advertising_peers(test_hash).next(),
+            Some(&test_peer),
+        );
+        assert_eq!(inv_registry.missing_peers(test_hash).count(), 0);
+    }
+}
+
 /// Returns a newly initialised inventory registry, and a sender for its inventory channel.
 fn new_inv_registry() -> (InventoryRegistry, broadcast::Sender<InventoryChange>) {
     let (inv_stream_tx, inv_stream_rx) = broadcast::channel(MAX_PENDING_CHANGES);
