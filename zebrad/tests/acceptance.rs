89a7ca103f8c5c14738f80c40853950c1d1e3c4f
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2022-02-15 23:01:44 +0000

    Allow enabling colored output from Zebra in tests
    Force Zebrad instances to use colored output if the
    `ZEBRA_FORCE_USE_COLOR` environment variable is set.

--- zebrad/tests/acceptance.rs
+++ zebrad/tests/acceptance.rs
@@ -27,7 +27,7 @@ use color_eyre::{
 };
 use tempfile::TempDir;
 
-use std::{collections::HashSet, convert::TryInto, path::Path, path::PathBuf, time::Duration};
+use std::{collections::HashSet, convert::TryInto, env, path::Path, path::PathBuf, time::Duration};
 
 use zebra_chain::{
     block::Height,
@@ -42,7 +42,7 @@ use zebra_test::{
 };
 use zebrad::{
     components::{mempool, sync},
-    config::{SyncSection, ZebradConfig},
+    config::{SyncSection, TracingSection, ZebradConfig},
 };
 
 /// The amount of time we wait after launching `zebrad`.
@@ -82,12 +82,22 @@ fn default_test_config() -> Result<ZebradConfig> {
         ..zebra_consensus::Config::default()
     };
 
+    let force_use_color = !matches!(
+        env::var("ZEBRA_FORCE_USE_COLOR"),
+        Err(env::VarError::NotPresent)
+    );
+    let tracing = TracingSection {
+        force_use_color,
+        ..TracingSection::default()
+    };
+
     let config = ZebradConfig {
         network,
         state: zebra_state::Config::ephemeral(),
         sync,
         mempool,
         consensus,
+        tracing,
         ..ZebradConfig::default()
     };
 
