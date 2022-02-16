4a8509244fbdaadcbb2d7820b9ab17a1c95f0131
Alfredo Garcia <oxarbitrage@gmail.com>
2022-02-16 13:11:03 -0300

    use `unique_by` in place of `sorted`

--- zebra-chain/src/sapling/shielded_data.rs
+++ zebra-chain/src/sapling/shielded_data.rs
@@ -199,6 +199,7 @@ where
         // TODO: use TransferData::shared_anchor to improve performance for V5 transactions
         self.spends_per_anchor()
             .map(|spend| spend.per_spend_anchor)
+            .unique_by(|raw| raw.0.to_bytes())
             .dedup()
     }
 
