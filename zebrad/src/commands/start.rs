c707705c06f69acfcbf573bc716873642a3cf62c
teor <teor@riseup.net>
2022-02-17 06:03:18 +1000

    fix(test): warn when the state hasn't committed a block for a long time
    This also improves the full sync tests,
    because the warning is checked before logging a successful sync.

--- zebrad/src/commands/start.rs
+++ zebrad/src/commands/start.rs
@@ -64,7 +64,11 @@ use tokio::{pin, select, sync::oneshot};
 use tower::{builder::ServiceBuilder, util::BoxService};
 use tracing_futures::Instrument;
 
-use zebra_chain::{chain_tip::ChainTip, parameters::Network};
+use zebra_chain::{
+    block::Height,
+    chain_tip::ChainTip,
+    parameters::{Network, NetworkUpgrade},
+};
 use zebra_consensus::CheckpointList;
 
 use crate::{
@@ -333,9 +337,28 @@ impl StartCmd {
             .add(min_after_checkpoint_blocks)
             .expect("hard-coded checkpoint height is far below Height::MAX");
 
+        let target_block_spacing = NetworkUpgrade::target_spacing_for_height(network, Height::MAX);
+        let max_block_spacing =
+            NetworkUpgrade::minimum_difficulty_spacing_for_height(network, Height::MAX);
+
+        // We expect the state height to increase at least once in this interval.
+        //
+        // Most chain forks are 1-7 blocks long.
+        let min_state_block_interval = max_block_spacing.unwrap_or(target_block_spacing * 4) * 2;
+
+        // The last time we downloaded and verified at least one block.
+        //
+        // Initialized to the start time to simplify the code.
+        let mut last_state_change_time = Utc::now();
+
+        // The state tip height, when we last downloaded and verified at least one block.
+        //
+        // Initialized to the genesis height to simplify the code.
+        let mut last_state_change_height = Height(0);
+
         loop {
             let now = Utc::now();
-            let is_close_to_tip = sync_status.is_close_to_tip();
+            let is_syncer_stopped = sync_status.is_close_to_tip();
 
             if let Some(estimated_height) =
                 latest_chain_tip.estimate_network_chain_tip_height(network, now)
@@ -346,6 +369,7 @@ impl StartCmd {
                     .best_tip_height()
                     .expect("unexpected empty state: estimate requires a block height");
 
+                // Work out the sync progress towards the estimated tip.
                 let sync_progress = f64::from(current_height.0) / f64::from(estimated_height.0);
                 let sync_percent = format!(
                     "{:.frac$}",
@@ -355,11 +379,38 @@ impl StartCmd {
 
                 let remaining_sync_blocks = estimated_height - current_height;
 
+                // Work out how long it has been since the state height has increased.
+                //
+                // Non-finalized forks can decrease the height, we only want to track increases.
+                if current_height > last_state_change_height {
+                    last_state_change_height = current_height;
+                    last_state_change_time = now;
+                }
+
+                let time_since_last_state_block = last_state_change_time.signed_duration_since(now);
+
                 // TODO:
                 // - log progress, remaining blocks, and remaining time to next network upgrade
                 // - add some of this info to the metrics
 
-                if is_close_to_tip && remaining_sync_blocks > MIN_SYNC_WARNING_BLOCKS {
+                if time_since_last_state_block > min_state_block_interval {
+                    // The state tip height hasn't increased for a long time.
+                    //
+                    // Block verification can fail if the local node's clock is wrong.
+                    warn!(
+                        %sync_percent,
+                        ?current_height,
+                        ?time_since_last_state_block,
+                        ?target_block_spacing,
+                        ?max_block_spacing,
+                        ?is_syncer_stopped,
+                        "chain updates have stalled, \
+                         state height has not increased for {} minutes. \
+                         Hint: check your network connection, \
+                         and your computer clock and time zone",
+                        time_since_last_state_block.num_minutes(),
+                    );
+                } else if is_syncer_stopped && remaining_sync_blocks > MIN_SYNC_WARNING_BLOCKS {
                     // We've stopped syncing blocks, but we estimate we're a long way from the tip.
                     //
                     // TODO: warn after fixing slow syncing near tip (#3375)
@@ -368,30 +419,31 @@ impl StartCmd {
                         ?current_height,
                         ?remaining_sync_blocks,
                         ?after_checkpoint_height,
+                        ?time_since_last_state_block,
                         "initial sync is very slow, or estimated tip is wrong. \
                          Hint: check your network connection, \
                          and your computer clock and time zone",
                     );
-                } else if is_close_to_tip && current_height <= after_checkpoint_height {
+                } else if is_syncer_stopped && current_height <= after_checkpoint_height {
                     // We've stopped syncing blocks,
                     // but we're below the height our checkpoints were generated from.
-                    //
-                    // Block verification can fail if the local node's clock is wrong.
                     warn!(
                         %sync_percent,
                         ?current_height,
                         ?remaining_sync_blocks,
                         ?after_checkpoint_height,
+                        ?time_since_last_state_block,
                         "initial sync is very slow, and state is below the highest checkpoint. \
                          Hint: check your network connection, \
                          and your computer clock and time zone",
                     );
-                } else if is_close_to_tip {
+                } else if is_syncer_stopped {
                     // We've stayed near the tip for a while, and we've stopped syncing lots of blocks.
                     info!(
                         %sync_percent,
                         ?current_height,
                         ?remaining_sync_blocks,
+                        ?time_since_last_state_block,
                         "finished initial sync to chain tip, and activated mempool",
                     );
                 } else if remaining_sync_blocks <= MAX_CLOSE_TO_TIP_BLOCKS {
@@ -400,6 +452,7 @@ impl StartCmd {
                         %sync_percent,
                         ?current_height,
                         ?remaining_sync_blocks,
+                        ?time_since_last_state_block,
                         "finished initial sync, \
                          waiting for syncer to stabilise before activating mempool",
                     );
@@ -409,13 +462,14 @@ impl StartCmd {
                         %sync_percent,
                         ?current_height,
                         ?remaining_sync_blocks,
+                        ?time_since_last_state_block,
                         "estimated progress to chain tip",
                     );
                 }
             } else {
                 let sync_percent = format!("{:.frac$}", 0.0f64, frac = SYNC_PERCENT_FRAC_DIGITS,);
 
-                if is_close_to_tip {
+                if is_syncer_stopped {
                     // We've stopped syncing blocks,
                     // but we haven't downloaded and verified the genesis block.
                     warn!(
