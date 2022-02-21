6ae180bec6932c387d8578bd55cbf4f2d8cd485a
Gustavo Valverde <gustavo@iterativo.do>
2022-02-21 14:48:55 -0400

    refactor(startup): add a custom entrypoint

---
+++ docker/entrypoint.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -x
+
+if [ ! -f zebrad.toml ]; then
+echo "
+[consensus]
+checkpoint_sync = ${CHECKPOINT_SYNC}
+[metrics]
+endpoint_addr = '0.0.0.0:9999'
+[network]
+network = ${NETWORK}
+[state]
+cache_dir = '/zebrad-cache'
+[tracing]
+endpoint_addr = '0.0.0.0:3000'
+" > $zebrad.toml
+fi
+
+case "$1" in
+    -- | cargo)
+        if [[ "$RUN_TESTS" -eq "1" ]]; then
+            if [[ "$TEST_FULL_SYNC" -eq "1" ]]; then
+                exec cargo "test" "--locked" "--release" "--features" "enable-sentry" "--test" "acceptance" "--" "--nocapture" "--ignored" "full_sync_mainnet"
+            else
+                exec cargo "test" "--locked" "--release" "--features" "enable-sentry" "--workspace" "--" "--include-ignored"
+            fi
+        fi
+        ;;
+    zebrad)
+        exec zebrad "$@"
+        ;;
+    *)
+        exec "$@"
+esac
+
+exit 1
