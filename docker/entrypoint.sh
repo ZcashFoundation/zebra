06ccd6ea90456411b0606ae749dc8c18bfb0a661
Gustavo Valverde <gustavo@iterativo.do>
2022-02-22 15:56:38 -0400

    ref: use previous approach

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,20 +2,20 @@
 
 set -x
 
-# if [ ! -f /app/zebrad.toml ]; then
-# echo "
-# [consensus]
-# checkpoint_sync = ${CHECKPOINT_SYNC}
-# [metrics]
-# endpoint_addr = 0.0.0.0:9999
-# [network]
-# network = ${NETWORK}
-# [state]
-# cache_dir = /zebrad-cache
-# [tracing]
-# force_use_color = true
-# endpoint_addr = 0.0.0.0:3000" > /app/zebrad.toml
-# fi
+if [ ! -f /app/zebrad.toml ]; then
+echo "
+[consensus]
+checkpoint_sync = ${CHECKPOINT_SYNC}
+[metrics]
+endpoint_addr = 0.0.0.0:9999
+[network]
+network = ${NETWORK}
+[state]
+cache_dir = /zebrad-cache
+[tracing]
+force_use_color = true
+endpoint_addr = 0.0.0.0:3000" > /app/zebrad.toml
+fi
 
 case "$1" in
     -- | cargo)
