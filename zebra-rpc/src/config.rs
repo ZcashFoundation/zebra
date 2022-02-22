8559d79624748ed4e74d8bd5a4e028833eccea70
teor <teor@riseup.net>
2022-02-22 11:22:14 +1000

    doc(rpc): fix testnet port, add security note

--- zebra-rpc/src/config.rs
+++ zebra-rpc/src/config.rs
@@ -12,8 +12,14 @@ pub struct Config {
     ///
     /// Note: RPC server will not start if this is `None`.
     ///
-    // The recommended ports for the RPC server is:
-    /// - Mainnet: 8232
-    /// - Testnet: 83232
+    /// The recommended ports for the RPC server are:
+    /// - Mainnet: Some(127.0.0.1:8232)
+    /// - Testnet: Some(127.0.0.1:18232)
+    ///
+    /// # Security
+    ///
+    /// If you bind Zebra's RPC port to a public IP address,
+    /// anyone on the internet can send transactions via your node.
+    /// They can also query your node's state.
     pub listen_addr: Option<SocketAddr>,
 }
