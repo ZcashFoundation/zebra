bfc8f1acbf4c5ac13d552c9c8b7b4c405213b429
teor <teor@riseup.net>
2022-02-24 18:18:35 +1000

    test(db): snapshot column family data for the empty state

--- zebra-state/src/service/finalized_state/disk_format/tests/snapshot.rs
+++ zebra-state/src/service/finalized_state/disk_format/tests/snapshot.rs
@@ -21,19 +21,47 @@ use zebra_chain::parameters::Network::*;
 use crate::{service::finalized_state::FinalizedState, Config};
 
 /// Snapshot test for RocksDB column families, and their key-value data.
+///
+/// These snapshots contain the `default` column family, but it is not used by Zebra.
 #[test]
 fn test_raw_rocksdb_column_family_data() {
     zebra_test::init();
 
     let empty_state = FinalizedState::new(&Config::ephemeral(), Mainnet);
-    let mut cf_names = empty_state.db.list_cf().expect("empty database is valid");
+    let db = empty_state.db;
+
+    // TODO: repeat for genesis, block 1, block 2,
+    //       asserting that the column family names remain the same each time
+    //
+    // https://docs.rs/insta/latest/insta/macro.with_settings.html
+    // https://docs.rs/insta/latest/insta/struct.Settings.html#method.set_snapshot_suffix
+
+    // Snapshot the column family names
+
+    let mut cf_names = db.list_cf().expect("empty database is valid");
 
     // The order that RocksDB returns column families is irrelevant,
     // because we always access them by name.
     cf_names.sort();
 
-    // This snapshot contains the `default` column family, but it is not used by Zebra.
-    insta::assert_ron_snapshot!(cf_names);
+    insta::assert_ron_snapshot!("column_family_names", cf_names);
 
-    // TODO: snapshot the data as well
+    // Snapshot the data in each column family
+
+    for cf_name in cf_names {
+        let cf_handle = db
+            .cf_handle(&cf_name)
+            .expect("RocksDB provided correct names");
+
+        let mut cf_iter = db.forward_iterator(cf_handle);
+        let cf_data: Vec<_> = cf_iter.by_ref().collect();
+
+        insta::assert_ron_snapshot!(format!("{}_raw_data", cf_name), cf_data);
+
+        assert_eq!(
+            cf_iter.status(),
+            Ok(()),
+            "unexpected column family iterator error",
+        );
+    }
 }
