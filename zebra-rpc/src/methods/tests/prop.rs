97d45fdcc7b78e766b8612175d48bd18e484a31e
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2022-03-01 20:54:22 +0000

    Test if mempool errors are forwarded to caller
    Mempool service errors should be sent back to the remote caller as
    server errors.

--- zebra-rpc/src/methods/tests/prop.rs
+++ zebra-rpc/src/methods/tests/prop.rs
@@ -1,5 +1,6 @@
 use jsonrpc_core::{Error, ErrorCode};
 use proptest::prelude::*;
+use thiserror::Error;
 use tower::buffer::Buffer;
 
 use zebra_chain::{
@@ -43,6 +44,51 @@ proptest! {
                 .expect("Sending raw transactions should not panic");
 
             prop_assert_eq!(result, Ok(hash));
+
+            Ok::<_, TestCaseError>(())
+        })?;
+    }
+
+    /// Test that mempool errors are forwarded to the caller.
+    ///
+    /// Mempool service errors should become server errors.
+    #[test]
+    fn mempool_errors_are_forwarded(transaction in any::<Transaction>()) {
+        let runtime = zebra_test::init_async();
+
+        runtime.block_on(async move {
+            let mut mempool = MockService::build().for_prop_tests();
+            let rpc = RpcImpl::new("RPC test".to_owned(), Buffer::new(mempool.clone(), 1));
+
+            let transaction_bytes = transaction
+                .zcash_serialize_to_vec()
+                .expect("Transaction serializes successfully");
+            let transaction_hex = hex::encode(&transaction_bytes);
+
+            let send_task = tokio::spawn(rpc.send_raw_transaction(transaction_hex));
+
+            let unmined_transaction = UnminedTx::from(transaction);
+            let expected_request = mempool::Request::Queue(vec![unmined_transaction.into()]);
+
+            mempool
+                .expect_request(expected_request)
+                .await?
+                .respond(Err(DummyError));
+
+            let result = send_task
+                .await
+                .expect("Sending raw transactions should not panic");
+
+            prop_assert!(
+                matches!(
+                    result,
+                    Err(Error {
+                        code: ErrorCode::ServerError(_),
+                        ..
+                    })
+                ),
+                "Result is not a server error: {result:?}"
+            );
 
             Ok::<_, TestCaseError>(())
         })?;
@@ -128,3 +174,7 @@ proptest! {
         })?;
     }
 }
+
+#[derive(Clone, Copy, Debug, Error)]
+#[error("a dummy error type")]
+pub struct DummyError;
