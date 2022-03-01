18792d4e504814618caee1b4a6dc007f15493233
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2022-03-01 23:35:46 +0000

    Update test vector to use a mock mempool service
    Update the test to be compatible with the changes to `RpcImpl`. The mock
    mempool service is expected to not be used during the test.

--- zebra-rpc/src/methods/tests/vectors.rs
+++ zebra-rpc/src/methods/tests/vectors.rs
@@ -1,15 +1,23 @@
 //! Fixed test vectors for RPC methods.
 
-use super::super::*;
-use zebra_network::constants::USER_AGENT;
+use tower::buffer::Buffer;
 
-#[test]
-fn rpc_getinfo() {
+use zebra_network::constants::USER_AGENT;
+use zebra_node_services::BoxError;
+use zebra_test::mock_service::MockService;
+
+use super::super::*;
+
+#[tokio::test]
+async fn rpc_getinfo() {
     zebra_test::init();
 
-    let rpc = RpcImpl {
-        app_version: "Zebra version test".to_string(),
-    };
+    let mut mempool: MockService<_, _, _, BoxError> = MockService::build().for_unit_tests();
+
+    let rpc = RpcImpl::new(
+        "Zebra version test".to_string(),
+        Buffer::new(mempool.clone(), 1),
+    );
 
     let get_info = rpc.get_info().expect("We should have a GetInfo struct");
 
@@ -20,4 +28,6 @@ fn rpc_getinfo() {
     // make sure there is a `subversion` field,
     // and that is equal to the Zebra user agent.
     assert_eq!(get_info.subversion, USER_AGENT);
+
+    mempool.expect_no_requests().await;
 }
