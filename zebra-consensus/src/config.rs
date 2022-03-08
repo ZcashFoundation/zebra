38f49ecf9b3b51de692a4b4450ef524999faeebb
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2022-03-08 15:40:30 -0300

    Improve documentation
    Remove `post-Canopy`, because Zebra will likely change checkpoint
    heights in the future. Also explain better what the checkpoints are and
    why it helps with debugging.
    
    Co-authored-by: teor <teor@riseup.net>

--- zebra-consensus/src/config.rs
+++ zebra-consensus/src/config.rs
@@ -4,12 +4,17 @@ use serde::{Deserialize, Serialize};
 #[derive(Clone, Debug, Deserialize, Serialize)]
 #[serde(deny_unknown_fields, default)]
 pub struct Config {
-    /// Should Zebra sync using post-Canopy checkpoints?
+    /// Should Zebra use its optional checkpoints to sync?
     ///
     /// This option is `true` by default, and allows for faster chain synchronization.
     ///
-    /// Disabling this option forces Zebra to only use checkpoints until Canopy activation, which
-    /// helps with debugging by forcing Zebra to validate more blocks.
+    /// Zebra requires some checkpoints to validate legacy network upgrades.
+    /// But it also ships with optional checkpoints, which can be used instead of full block validation.
+    ///
+    /// Disabling this option makes Zebra start full validation as soon as possible.
+    /// This helps developers debug Zebra, by running full validation on more blocks.
+    ///
+    /// Future versions of Zebra may change the required and optional checkpoints.
     pub checkpoint_sync: bool,
 
     /// Skip the pre-download of Groth16 parameters if this option is true.
