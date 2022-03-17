3b7887adc817eaabbc87c8d83d44cade4ab3f45c
teor <teor@riseup.net>
2022-03-18 09:02:13 +1000

    Revert Option<Child> process handling

--- zebra-test/src/command.rs
+++ zebra-test/src/command.rs
@@ -751,9 +751,7 @@ impl<T> ContextFrom<&mut TestChild<T>> for Report {
 
         // Reading test child process output could hang if the child process is still running,
         // so kill it first.
-        if let Some(child) = source.child.as_mut() {
-            let _ = child.kill();
-        }
+        let _ = source.child.kill();
 
         let mut stdout_buf = String::new();
         let mut stderr_buf = String::new();
