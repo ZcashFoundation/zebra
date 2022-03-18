b4deca2912d4477feaa5d8453e115d43acf0875a
teor <teor@riseup.net>
2022-03-19 06:30:16 +1000

    6. add(test): add more tests for height and transaction location serialization (#3879)
    * Add round-trip test for OutputLocation disk serialization
    
    * Make the transaction snapshot tests more accurate
    
    Previously, we were showing the genesis transaction hash at height 0, index 0.
    
    But its hash is actually not stored by location in the database,
    because the genesis transaction is skipped due to a consensus rule.
    
    * Update the transaction snapshot data
    
    * Add history tree snapshot tests
    
    At the current test heights, the history trees are all empty.
    
    * Add the history tree snapshot data
    
    * Update comments
    
    * Simplify snapshot test code
    
    * Make some serde::Serialize impls test-only, so we can hex-encode them
    
    This should also speed up release compilation a bit.
    
    * Add snapshot test code for UTXOs
    
    * Add snapshot data for UTXOs

--- zebra-chain/src/transparent.rs
+++ zebra-chain/src/transparent.rs
@@ -48,7 +48,8 @@ use std::{collections::HashMap, fmt, iter};
 pub const MIN_TRANSPARENT_COINBASE_MATURITY: u32 = 100;
 
 /// Arbitrary data inserted by miners into a coinbase transaction.
-#[derive(Clone, Eq, PartialEq, Serialize, Deserialize)]
+#[derive(Clone, Eq, PartialEq)]
+#[cfg_attr(any(test, feature = "proptest-impl"), derive(Serialize))]
 pub struct CoinbaseData(
     /// Invariant: this vec, together with the coinbase height, must be less than
     /// 100 bytes. We enforce this by only constructing CoinbaseData fields by
@@ -92,10 +93,16 @@ impl std::fmt::Debug for CoinbaseData {
 /// OutPoint
 ///
 /// A particular transaction output reference.
-#[derive(Copy, Clone, Debug, Eq, PartialEq, Serialize, Deserialize, Hash)]
-#[cfg_attr(any(test, feature = "proptest-impl"), derive(Arbitrary))]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
+#[cfg_attr(any(test, feature = "proptest-impl"), derive(Arbitrary, Serialize))]
 pub struct OutPoint {
     /// References the transaction that contains the UTXO being spent.
+    ///
+    /// # Correctness
+    ///
+    /// Consensus-critical serialization uses [`ZcashSerialize`].
+    /// [`serde`]-based hex serialization must only be used for testing.
+    #[cfg_attr(any(test, feature = "proptest-impl"), serde(with = "hex"))]
     pub hash: transaction::Hash,
 
     /// Identifies which UTXO from that transaction is referenced; the
@@ -103,8 +110,25 @@ pub struct OutPoint {
     pub index: u32,
 }
 
+impl OutPoint {
+    /// Returns a new OutPoint from an in-memory output `index`.
+    ///
+    /// # Panics
+    ///
+    /// If `index` doesn't fit in a [`u32`].
+    pub fn from_usize(hash: transaction::Hash, index: usize) -> OutPoint {
+        OutPoint {
+            hash,
+            index: index
+                .try_into()
+                .expect("valid in-memory output indexes fit in a u32"),
+        }
+    }
+}
+
 /// A transparent input to a transaction.
-#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
+#[derive(Clone, Debug, Eq, PartialEq)]
+#[cfg_attr(any(test, feature = "proptest-impl"), derive(Serialize))]
 pub enum Input {
     /// A reference to an output of a previous transaction.
     PrevOut {
@@ -289,8 +313,8 @@ impl Input {
 /// I only own one UTXO worth 2 ZEC, I would construct a transaction
 /// that spends my UTXO and sends 1 ZEC to you and 1 ZEC back to me
 /// (just like receiving change).
-#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize, Hash)]
-#[cfg_attr(any(test, feature = "proptest-impl"), derive(Arbitrary))]
+#[derive(Clone, Debug, Eq, PartialEq, Hash)]
+#[cfg_attr(any(test, feature = "proptest-impl"), derive(Arbitrary, Serialize))]
 pub struct Output {
     /// Transaction value.
     // At https://en.bitcoin.it/wiki/Protocol_documentation#tx, this is an i64.
