d6628eadb7ecfc8c4ea5ab3dbd13d2f62a7abc08
teor <teor@riseup.net>
2022-03-22 10:50:14 +1000

    Re-order column family initialization to match the design

--- zebra-state/src/service/finalized_state/disk_db.rs
+++ zebra-state/src/service/finalized_state/disk_db.rs
@@ -189,26 +189,35 @@ impl DiskDb {
         let db_options = DiskDb::options();
 
         let column_families = vec![
+            // Blocks
             rocksdb::ColumnFamilyDescriptor::new("hash_by_height", db_options.clone()),
             rocksdb::ColumnFamilyDescriptor::new("height_by_hash", db_options.clone()),
+            // TODO: rename to block_header_by_height (#3151)
             rocksdb::ColumnFamilyDescriptor::new("block_by_height", db_options.clone()),
+            // Transactions
+            // TODO: rename to tx_loc_by_hash (#3151)
             rocksdb::ColumnFamilyDescriptor::new("tx_by_hash", db_options.clone()),
+            // Transparent
             rocksdb::ColumnFamilyDescriptor::new("utxo_by_outpoint", db_options.clone()),
+            // Sprout
             rocksdb::ColumnFamilyDescriptor::new("sprout_nullifiers", db_options.clone()),
-            rocksdb::ColumnFamilyDescriptor::new("sapling_nullifiers", db_options.clone()),
-            rocksdb::ColumnFamilyDescriptor::new("orchard_nullifiers", db_options.clone()),
             rocksdb::ColumnFamilyDescriptor::new("sprout_anchors", db_options.clone()),
-            rocksdb::ColumnFamilyDescriptor::new("sapling_anchors", db_options.clone()),
-            rocksdb::ColumnFamilyDescriptor::new("orchard_anchors", db_options.clone()),
             rocksdb::ColumnFamilyDescriptor::new("sprout_note_commitment_tree", db_options.clone()),
+            // Sapling
+            rocksdb::ColumnFamilyDescriptor::new("sapling_nullifiers", db_options.clone()),
+            rocksdb::ColumnFamilyDescriptor::new("sapling_anchors", db_options.clone()),
             rocksdb::ColumnFamilyDescriptor::new(
                 "sapling_note_commitment_tree",
                 db_options.clone(),
             ),
+            // Orchard
+            rocksdb::ColumnFamilyDescriptor::new("orchard_nullifiers", db_options.clone()),
+            rocksdb::ColumnFamilyDescriptor::new("orchard_anchors", db_options.clone()),
             rocksdb::ColumnFamilyDescriptor::new(
                 "orchard_note_commitment_tree",
                 db_options.clone(),
             ),
+            // Chain
             rocksdb::ColumnFamilyDescriptor::new("history_tree", db_options.clone()),
             rocksdb::ColumnFamilyDescriptor::new("tip_chain_value_pool", db_options.clone()),
         ];
