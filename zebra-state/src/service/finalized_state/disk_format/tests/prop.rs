94203bdad8c460cbc56cb46381a547aa0c94293d
teor <teor@riseup.net>
2022-03-25 13:40:53 +1000

    Add missing round-trip and serialized equality tests

--- zebra-state/src/service/finalized_state/disk_format/tests/prop.rs
+++ zebra-state/src/service/finalized_state/disk_format/tests/prop.rs
@@ -5,6 +5,8 @@ use proptest::{arbitrary::any, prelude::*};
 use zebra_chain::{
     amount::{Amount, NonNegative},
     block::{self, Height},
+    orchard, sapling, sprout,
+    transaction::{self, Transaction},
     transparent,
     value_balance::ValueBalance,
 };
@@ -18,36 +20,8 @@ use crate::service::finalized_state::{
     },
 };
 
-#[test]
-fn serialized_transparent_address_equal() {
-    zebra_test::init();
-    proptest!(|(addr1 in any::<transparent::Address>(), addr2 in any::<transparent::Address>())| {
-        if addr1 == addr2 {
-            prop_assert_eq!(
-                addr1.as_bytes(),
-                addr2.as_bytes(),
-                "transparent addresses were equal, but serialized bytes were not.\n\
-                 Addresses:\n\
-                 {:?}\n\
-                 {:?}",
-                addr1,
-                addr2,
-            );
-        } else {
-            prop_assert_ne!(
-                addr1.as_bytes(),
-                addr2.as_bytes(),
-                "transparent addresses were not equal, but serialized bytes were equal:\n\
-                 Addresses:\n\
-                 {:?}\n\
-                 {:?}",
-                addr1,
-                addr2,
-            );
-        }
-    }
-    );
-}
+// Block
+// TODO: split these tests into the disk_format sub-modules
 
 #[test]
 fn roundtrip_block_height() {
@@ -65,6 +39,22 @@ fn roundtrip_block_height() {
 }
 
 #[test]
+fn roundtrip_block_hash() {
+    zebra_test::init();
+
+    proptest!(|(val in any::<block::Hash>())| assert_value_properties(val));
+}
+
+#[test]
+fn roundtrip_block_header() {
+    zebra_test::init();
+
+    proptest!(|(val in any::<block::Header>())| assert_value_properties(val));
+}
+
+// Transaction
+
+#[test]
 fn roundtrip_transaction_location() {
     zebra_test::init();
 
@@ -77,38 +67,77 @@ fn roundtrip_transaction_location() {
 }
 
 #[test]
+fn roundtrip_transaction_hash() {
+    zebra_test::init();
+
+    proptest!(|(val in any::<transaction::Hash>())| assert_value_properties(val));
+}
+
+#[test]
+fn roundtrip_transaction() {
+    zebra_test::init();
+
+    proptest!(|(val in any::<Transaction>())| assert_value_properties(val));
+}
+
+// Transparent
+
+// TODO: turn this into a generic function like assert_value_properties()
+#[test]
+fn serialized_transparent_address_equal() {
+    zebra_test::init();
+
+    proptest!(|(val1 in any::<transparent::Address>(), val2 in any::<transparent::Address>())| {
+        if val1 == val2 {
+            prop_assert_eq!(
+                val1.as_bytes(),
+                val2.as_bytes(),
+                "struct values were equal, but serialized bytes were not.\n\
+                 Values:\n\
+                 {:?}\n\
+                 {:?}",
+                val1,
+                val2,
+            );
+        } else {
+            prop_assert_ne!(
+                val1.as_bytes(),
+                val2.as_bytes(),
+                "struct values were not equal, but serialized bytes were equal.\n\
+                 Values:\n\
+                 {:?}\n\
+                 {:?}",
+                val1,
+                val2,
+            );
+        }
+    }
+    );
+}
+
+#[test]
 fn roundtrip_output_location() {
     zebra_test::init();
+
     proptest!(|(val in any::<OutputLocation>())| assert_value_properties(val));
 }
 
 #[test]
 fn roundtrip_address_location() {
     zebra_test::init();
+
     proptest!(|(val in any::<AddressLocation>())| assert_value_properties(val));
 }
 
 #[test]
 fn roundtrip_address_balance_location() {
     zebra_test::init();
+
     proptest!(|(val in any::<AddressBalanceLocation>())| assert_value_properties(val));
 }
 
 #[test]
-fn roundtrip_block_hash() {
-    zebra_test::init();
-    proptest!(|(val in any::<block::Hash>())| assert_value_properties(val));
-}
-
-#[test]
-fn roundtrip_block_header() {
-    zebra_test::init();
-
-    proptest!(|(val in any::<block::Header>())| assert_value_properties(val));
-}
-
-#[test]
-fn roundtrip_transparent_output() {
+fn roundtrip_unspent_transparent_output() {
     zebra_test::init();
 
     proptest!(
@@ -120,11 +149,226 @@ fn roundtrip_transparent_output() {
 }
 
 #[test]
+fn roundtrip_transparent_output() {
+    zebra_test::init();
+
+    proptest!(|(val in any::<transparent::Output>())| assert_value_properties(val));
+}
+
+#[test]
 fn roundtrip_amount() {
     zebra_test::init();
 
     proptest!(|(val in any::<Amount::<NonNegative>>())| assert_value_properties(val));
 }
+
+// Sprout
+
+#[test]
+fn serialized_sprout_nullifier_equal() {
+    zebra_test::init();
+
+    proptest!(|(val1 in any::<sprout::Nullifier>(), val2 in any::<sprout::Nullifier>())| {
+        if val1 == val2 {
+            prop_assert_eq!(
+                val1.as_bytes(),
+                val2.as_bytes(),
+                "struct values were equal, but serialized bytes were not.\n\
+                 Values:\n\
+                 {:?}\n\
+                 {:?}",
+                val1,
+                val2,
+            );
+        } else {
+            prop_assert_ne!(
+                val1.as_bytes(),
+                val2.as_bytes(),
+                "struct values were not equal, but serialized bytes were equal.\n\
+                 Values:\n\
+                 {:?}\n\
+                 {:?}",
+                val1,
+                val2,
+            );
+        }
+    }
+    );
+}
+
+#[test]
+fn serialized_sprout_tree_root_equal() {
+    zebra_test::init();
+
+    proptest!(|(val1 in any::<sprout::tree::Root>(), val2 in any::<sprout::tree::Root>())| {
+        if val1 == val2 {
+            prop_assert_eq!(
+                val1.as_bytes(),
+                val2.as_bytes(),
+                "struct values were equal, but serialized bytes were not.\n\
+                 Values:\n\
+                 {:?}\n\
+                 {:?}",
+                val1,
+                val2,
+            );
+        } else {
+            prop_assert_ne!(
+                val1.as_bytes(),
+                val2.as_bytes(),
+                "struct values were not equal, but serialized bytes were equal.\n\
+                 Values:\n\
+                 {:?}\n\
+                 {:?}",
+                val1,
+                val2,
+            );
+        }
+    }
+    );
+}
+
+// TODO: test note commitment tree round-trip, after implementing proptest::Arbitrary
+
+// Sapling
+
+#[test]
+fn serialized_sapling_nullifier_equal() {
+    zebra_test::init();
+
+    proptest!(|(val1 in any::<sapling::Nullifier>(), val2 in any::<sapling::Nullifier>())| {
+        if val1 == val2 {
+            prop_assert_eq!(
+                val1.as_bytes(),
+                val2.as_bytes(),
+                "struct values were equal, but serialized bytes were not.\n\
+                 Values:\n\
+                 {:?}\n\
+                 {:?}",
+                val1,
+                val2,
+            );
+        } else {
+            prop_assert_ne!(
+                val1.as_bytes(),
+                val2.as_bytes(),
+                "struct values were not equal, but serialized bytes were equal.\n\
+                 Values:\n\
+                 {:?}\n\
+                 {:?}",
+                val1,
+                val2,
+            );
+        }
+    }
+    );
+}
+
+#[test]
+fn serialized_sapling_tree_root_equal() {
+    zebra_test::init();
+
+    proptest!(|(val1 in any::<sapling::tree::Root>(), val2 in any::<sapling::tree::Root>())| {
+        if val1 == val2 {
+            prop_assert_eq!(
+                val1.as_bytes(),
+                val2.as_bytes(),
+                "struct values were equal, but serialized bytes were not.\n\
+                 Values:\n\
+                 {:?}\n\
+                 {:?}",
+                val1,
+                val2,
+            );
+        } else {
+            prop_assert_ne!(
+                val1.as_bytes(),
+                val2.as_bytes(),
+                "struct values were not equal, but serialized bytes were equal.\n\
+                 Values:\n\
+                 {:?}\n\
+                 {:?}",
+                val1,
+                val2,
+            );
+        }
+    }
+    );
+}
+
+// TODO: test note commitment tree round-trip, after implementing proptest::Arbitrary
+
+// Orchard
+
+#[test]
+fn serialized_orchard_nullifier_equal() {
+    zebra_test::init();
+
+    proptest!(|(val1 in any::<orchard::Nullifier>(), val2 in any::<orchard::Nullifier>())| {
+        if val1 == val2 {
+            prop_assert_eq!(
+                val1.as_bytes(),
+                val2.as_bytes(),
+                "struct values were equal, but serialized bytes were not.\n\
+                 Values:\n\
+                 {:?}\n\
+                 {:?}",
+                val1,
+                val2,
+            );
+        } else {
+            prop_assert_ne!(
+                val1.as_bytes(),
+                val2.as_bytes(),
+                "struct values were not equal, but serialized bytes were equal.\n\
+                 Values:\n\
+                 {:?}\n\
+                 {:?}",
+                val1,
+                val2,
+            );
+        }
+    }
+    );
+}
+
+#[test]
+fn serialized_orchard_tree_root_equal() {
+    zebra_test::init();
+
+    proptest!(|(val1 in any::<orchard::tree::Root>(), val2 in any::<orchard::tree::Root>())| {
+        if val1 == val2 {
+            prop_assert_eq!(
+                val1.as_bytes(),
+                val2.as_bytes(),
+                "struct values were equal, but serialized bytes were not.\n\
+                 Values:\n\
+                 {:?}\n\
+                 {:?}",
+                val1,
+                val2,
+            );
+        } else {
+            prop_assert_ne!(
+                val1.as_bytes(),
+                val2.as_bytes(),
+                "struct values were not equal, but serialized bytes were equal.\n\
+                 Values:\n\
+                 {:?}\n\
+                 {:?}",
+                val1,
+                val2,
+            );
+        }
+    }
+    );
+}
+
+// TODO: test note commitment tree round-trip, after implementing proptest::Arbitrary
+
+// Chain
+
+// TODO: test NonEmptyHistoryTree round-trip, after implementing proptest::Arbitrary
 
 #[test]
 fn roundtrip_value_balance() {
