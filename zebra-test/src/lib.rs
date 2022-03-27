74e6b7e2ceb5c4a34b6c413c71d626b8916921f2
Deirdre Connolly <durumcrustulum@gmail.com>
2022-03-27 19:42:47 -0400

    Repoint zebra image links to our new zfnd.org site for now (#3949)
    * Repoint zebra image links to our new zfnd.org site for now
    
    * Remove images/
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- zebra-test/src/lib.rs
+++ zebra-test/src/lib.rs
@@ -1,6 +1,6 @@
 //! Miscellaneous test code for Zebra.
-#![doc(html_favicon_url = "https://www.zfnd.org/images/zebra-favicon-128.png")]
-#![doc(html_logo_url = "https://www.zfnd.org/images/zebra-icon.png")]
+#![doc(html_favicon_url = "https://zfnd.org/wp-content/uploads/2022/03/zebra-favicon-128.png")]
+#![doc(html_logo_url = "https://zfnd.org/wp-content/uploads/2022/03/zebra-icon.png")]
 #![doc(html_root_url = "https://doc.zebra.zfnd.org/zebra_test")]
 // Each lazy_static variable uses additional recursion
 #![recursion_limit = "512"]
