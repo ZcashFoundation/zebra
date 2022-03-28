82303ede81cb9a8c47c607bb87e3d9de634ebc8e
teor <teor@riseup.net>
2022-03-29 08:07:15 +1000

    Improve link formatting
    Co-authored-by: Deirdre Connolly <durumcrustulum@gmail.com>

--- zebra-rpc/src/methods.rs
+++ zebra-rpc/src/methods.rs
@@ -32,7 +32,7 @@ mod tests;
 /// The RPC error code used by `zcashd` for missing blocks.
 ///
 /// `lightwalletd` expects error code `-8` when a block is not found:
-/// https://github.com/adityapk00/lightwalletd/blob/c1bab818a683e4de69cd952317000f9bb2932274/common/common.go#L251-L254
+/// <https://github.com/adityapk00/lightwalletd/blob/c1bab818a683e4de69cd952317000f9bb2932274/common/common.go#L251-L254>
 pub const MISSING_BLOCK_ERROR_CODE: ErrorCode = ErrorCode::ServerError(-8);
 
 #[rpc(server)]
