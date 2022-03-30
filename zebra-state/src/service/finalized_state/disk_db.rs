78441bf99cee1b7887694bfa308280ab4b794a9e
teor <teor@riseup.net>
2022-03-30 10:31:47 +1000

    Add an empty utxo_by_transparent_addr_loc column family

--- zebra-state/src/service/finalized_state/disk_db.rs
+++ zebra-state/src/service/finalized_state/disk_db.rs
@@ -369,8 +369,10 @@ impl DiskDb {
             //rocksdb::ColumnFamilyDescriptor::new("tx_by_transparent_addr_loc", db_options.clone()),
             // TODO: rename to utxo_by_out_loc (#3952)
             rocksdb::ColumnFamilyDescriptor::new("utxo_by_outpoint", db_options.clone()),
-            // TODO: #3953
-            //rocksdb::ColumnFamilyDescriptor::new("utxo_by_transparent_addr_loc", db_options.clone()),
+            rocksdb::ColumnFamilyDescriptor::new(
+                "utxo_by_transparent_addr_loc",
+                db_options.clone(),
+            ),
             // Sprout
             rocksdb::ColumnFamilyDescriptor::new("sprout_nullifiers", db_options.clone()),
             rocksdb::ColumnFamilyDescriptor::new("sprout_anchors", db_options.clone()),
