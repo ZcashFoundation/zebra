e074e783553da57b0bdea6255d145b5433f46289
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2022-04-20 22:31:17 +0000

    Increase the block verification timeout
    Attempt to reduce the synchronization restarts and consequently improve
    performance.

--- zebrad/src/components/sync.rs
+++ zebrad/src/components/sync.rs
@@ -141,7 +141,7 @@ pub(super) const BLOCK_DOWNLOAD_TIMEOUT: Duration = Duration::from_secs(15);
 ///
 /// If this timeout is set too low, the syncer will sometimes get stuck in a
 /// failure loop.
-pub(super) const BLOCK_VERIFY_TIMEOUT: Duration = Duration::from_secs(180);
+pub(super) const BLOCK_VERIFY_TIMEOUT: Duration = Duration::from_secs(6 * 60);
 
 /// Controls how long we wait to restart syncing after finishing a sync run.
 ///
