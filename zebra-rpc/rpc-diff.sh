6f348f0e723fca61441bd4ed797bd2312b612605
teor <teor@riseup.net>
2022-04-28 19:32:37 +1000

    Add the network to the RPC diff logs
    This helps diagnose main/test mismatches.

--- zebra-rpc/rpc-diff.sh
+++ zebra-rpc/rpc-diff.sh
@@ -25,26 +25,35 @@ shift
 
 TMP_DIR=$(mktemp -d)
 
-echo "Checking zebrad tip height..."
-ZEBRAD_HEIGHT=$($ZCASH_CLI -rpcport="$ZEBRAD_RPC_PORT" getblockchaininfo | grep blocks | cut -d: -f2 | tr -d ' ,')
+ZEBRAD_BLOCKCHAIN_INFO="$TMP_DIR/zebrad-check-getblockchaininfo.json"
+ZCASHD_BLOCKCHAIN_INFO="$TMP_DIR/zcashd-check-getblockchaininfo.json"
 
-echo "Checking zcashd tip height..."
-ZCASHD_HEIGHT=$($ZCASH_CLI getblockchaininfo | grep blocks | cut -d: -f2 | tr -d ' ,')
+echo "Checking zebrad network and tip height..."
+$ZCASH_CLI -rpcport="$ZEBRAD_RPC_PORT" getblockchaininfo > "$ZEBRAD_BLOCKCHAIN_INFO"
+
+ZEBRAD_NET=$(cat "$ZEBRAD_BLOCKCHAIN_INFO" | grep '"chain"' | cut -d: -f2 | tr -d ' ,"')
+ZEBRAD_HEIGHT=$(cat "$ZEBRAD_BLOCKCHAIN_INFO" | grep '"blocks"' | cut -d: -f2 | tr -d ' ,"')
+
+echo "Checking zcashd network and tip height..."
+$ZCASH_CLI getblockchaininfo > "$ZCASHD_BLOCKCHAIN_INFO"
+
+ZCASHD_NET=$(cat "$ZCASHD_BLOCKCHAIN_INFO" | grep '"chain"' | cut -d: -f2 | tr -d ' ,"')
+ZCASHD_HEIGHT=$(cat "$ZCASHD_BLOCKCHAIN_INFO" | grep '"blocks"' | cut -d: -f2 | tr -d ' ,"')
 
 echo
 
-ZEBRAD_RESPONSE="$TMP_DIR/zebrad-$ZEBRAD_HEIGHT-$1.json"
-ZCASHD_RESPONSE="$TMP_DIR/zcashd-$ZCASHD_HEIGHT-$1.json"
+ZEBRAD_RESPONSE="$TMP_DIR/zebrad-$ZEBRAD_NET-$ZEBRAD_HEIGHT-$1.json"
+ZCASHD_RESPONSE="$TMP_DIR/zcashd-$ZCASHD_NET-$ZCASHD_HEIGHT-$1.json"
 
 echo "Request:"
 echo "$@"
 echo
 
-echo "Querying zebrad at height $ZEBRAD_HEIGHT..."
-$ZCASH_CLI -rpcport="$ZEBRAD_RPC_PORT" "$@" >> "$ZEBRAD_RESPONSE"
+echo "Querying zebrad $ZEBRAD_NET chain at height $ZEBRAD_HEIGHT..."
+$ZCASH_CLI -rpcport="$ZEBRAD_RPC_PORT" "$@" > "$ZEBRAD_RESPONSE"
 
-echo "Querying zcashd at height $ZCASHD_HEIGHT..."
-$ZCASH_CLI "$@" >> "$ZCASHD_RESPONSE"
+echo "Querying zcashd $ZCASHD_NET chain at height $ZCASHD_HEIGHT..."
+$ZCASH_CLI "$@" > "$ZCASHD_RESPONSE"
 
 echo
 
