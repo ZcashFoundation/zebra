aa3ba9952dea94db615bbd79fd8c33c8c99346aa
Gustavo Valverde <gustavo@iterativo.do>
2022-05-04 23:47:31 -0400

    fix(ci): do not create extra zebrad.toml, as tests does it

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,21 +2,6 @@
 
 set -x
 
-if [ ! -f /app/zebrad.toml ]; then
-echo "
-[consensus]
-checkpoint_sync = ${CHECKPOINT_SYNC}
-[metrics]
-endpoint_addr = 0.0.0.0:9999
-[network]
-network = ${NETWORK}
-[state]
-cache_dir = /zebrad-cache
-[tracing]
-force_use_color = true
-endpoint_addr = 0.0.0.0:3000" > /app/zebrad.toml
-fi
-
 case "$1" in
     -- | cargo)
         if [[ "$RUN_ALL_TESTS" -eq "1" ]]; then
