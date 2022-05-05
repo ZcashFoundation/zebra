702912ee8a619d34dd01aee5d36f2e588e4ddb7c
Gustavo Valverde <gustavo@iterativo.do>
2022-05-04 21:51:41 -0400

    fix(deps): remove dependencies

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -27,8 +27,6 @@ case "$1" in
             exec cargo "test" "--locked" "--release" "--features" "enable-sentry,test_sync_to_mandatory_checkpoint_${NETWORK,,}" "--manifest-path" "zebrad/Cargo.toml" "sync_to_mandatory_checkpoint_${NETWORK,,}"
         elif [[ "$TEST_CHECKPOINT_SYNC" -eq "1" ]]; then
             exec cargo "test" "--locked" "--release" "--features" "enable-sentry,test_sync_past_mandatory_checkpoint_${NETWORK,,}" "--manifest-path" "zebrad/Cargo.toml" "sync_past_mandatory_checkpoint_${NETWORK,,}"
-        elif [[ "$TEST_LWD_RPC_CALL" -eq "1" ]]; then
-            exec cargo "test" "--locked" "--release" "--features" "enable-sentry" "--test" "acceptance" "--" "--nocapture" "--ignored" "fully_synced_rpc_test"
         elif [[ "$TEST_LWD_TRANSACTIONS" -eq "1" ]]; then
             exec cargo "test" "--locked" "--release" "--features" "enable-sentry" "--test" "acceptance" "--" "--nocapture" "--ignored" "sending_transactions_using_lightwalletd"
         else
