0eb41df97fcc06112bb8d5289bcee6652e8f57de
teor <teor@riseup.net>
2022-07-25 12:27:48 +1000

    Wrap RPC executor startup and shutdown in a std::thread

--- zebra-rpc/src/server.rs
+++ zebra-rpc/src/server.rs
@@ -7,6 +7,8 @@
 //! See the full list of
 //! [Differences between JSON-RPC 1.0 and 2.0.](https://www.simple-is-better.org/rpc/#differences-between-1-0-and-2-0)
 
+use std::panic;
+
 use jsonrpc_core::{Compatibility, MetaIoHandler};
 use jsonrpc_http_server::ServerBuilder;
 use tokio::task::JoinHandle;
@@ -68,22 +70,30 @@ impl RpcServer {
                 MetaIoHandler::new(Compatibility::Both, TracingMiddleware);
             io.extend_with(rpc_impl.to_delegate());
 
-            // Use a different tokio executor from the rest of Zebra,
-            // so that large RPCs and any task handling bugs don't impact Zebra.
-            //
-            // TODO: make the number of RPC threads configurable?
-            let server = ServerBuilder::new(io)
-                .threads(1)
-                // TODO: disable this security check if we see errors from lightwalletd.
-                //.allowed_hosts(DomainsValidation::Disabled)
-                .request_middleware(FixHttpRequestMiddleware)
-                .start_http(&listen_addr)
-                .expect("Unable to start RPC server");
-
-            // The server is a blocking task, so we need to spawn it on a blocking thread.
+            // The server is a blocking task, which blocks on executor shutdown.
+            // So we need to create and spawn it on a std::thread, inside a tokio blocking task.
+            // (Otherwise tokio panics when we shut down the RPC server.)
             let span = Span::current();
             let server = move || {
                 span.in_scope(|| {
+                    // Use a different tokio executor from the rest of Zebra,
+                    // so that large RPCs and any task handling bugs don't impact Zebra.
+                    //
+                    // TODO:
+                    // - return server.close_handle(), which can shut down the RPC server,
+                    //   and add it to the server tests
+                    // - allow multiple RPC threads
+                    //   (when jsonrpc_http_server has multiple threads,
+                    //   it lets any process share its port - do we need to warn users?)
+                    // - make the number of RPC threads configurable
+                    let server = ServerBuilder::new(io)
+                        .threads(1)
+                        // TODO: disable this security check if we see errors from lightwalletd
+                        //.allowed_hosts(DomainsValidation::Disabled)
+                        .request_middleware(FixHttpRequestMiddleware)
+                        .start_http(&listen_addr)
+                        .expect("Unable to start RPC server");
+
                     info!("Opened RPC endpoint at {}", server.address());
 
                     server.wait();
@@ -93,7 +103,15 @@ impl RpcServer {
             };
 
             (
-                tokio::task::spawn_blocking(server),
+                tokio::task::spawn_blocking(|| {
+                    let thread_handle = std::thread::spawn(server);
+
+                    // Propagate panics from the inner std::thread to the outer tokio blocking task
+                    match thread_handle.join() {
+                        Ok(()) => (),
+                        Err(panic_object) => panic::resume_unwind(panic_object),
+                    }
+                }),
                 rpc_tx_queue_task_handle,
             )
         } else {
