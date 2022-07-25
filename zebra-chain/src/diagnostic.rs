ed553a9ecabcf1f975eb0cc88e6b290db41eed11
teor <teor@riseup.net>
2022-07-26 08:33:00 +1000

    2. add(log): Log when state requests take a long time (#4815)
    * Fix clippy::let_and_return
    
    * Increase lightwalletd test timeouts for zebrad slowness
    
    * Add a `zebrad_update_sync()` test, that update syncs Zebra without lightwalletd
    
    * Run the zebrad-update-sync test in CI
    
    * Add extra zebrad time to workaround lightwalletd bugs
    
    * Add a CodeTimer diagnostic struct for long-running code
    
    * Time state init and each state request, log when it takes too long
    
    * Add code locations to execution timers
    
    * Instrument state futures and functions with tracing spans
    
    * Only log each code timer once
    
    * Make displayed times shorter

---
+++ zebra-chain/src/diagnostic.rs
@@ -0,0 +1,120 @@
+//! Tracing the execution time of functions.
+//!
+//! TODO: also trace polling time for futures, using a `Future` wrapper
+
+use std::time::{Duration, Instant};
+
+use crate::fmt::duration_short;
+
+/// The default minimum info-level message time.
+pub const DEFAULT_MIN_INFO_TIME: Duration = Duration::from_secs(5);
+
+/// The default minimum warning message time.
+pub const DEFAULT_MIN_WARN_TIME: Duration = Duration::from_secs(20);
+
+/// A guard that logs code execution time when dropped.
+#[derive(Debug)]
+pub struct CodeTimer {
+    /// The time that the code started executing.
+    start: Instant,
+
+    /// The minimum duration for info-level messages.
+    min_info_time: Duration,
+
+    /// The minimum duration for warning messages.
+    min_warn_time: Duration,
+
+    /// `true` if this timer has finished.
+    has_finished: bool,
+}
+
+impl CodeTimer {
+    /// Start timing the execution of a function, method, or other code region.
+    ///
+    /// Returns a guard that finishes timing the code when dropped,
+    /// or when [`CodeTimer::finish()`] is called.
+    #[track_caller]
+    pub fn start() -> Self {
+        let start = Instant::now();
+        trace!(?start, "starting code timer");
+
+        Self {
+            start,
+            min_info_time: DEFAULT_MIN_INFO_TIME,
+            min_warn_time: DEFAULT_MIN_WARN_TIME,
+            has_finished: false,
+        }
+    }
+
+    /// Finish timing the execution of a function, method, or other code region.
+    #[track_caller]
+    pub fn finish<S>(
+        mut self,
+        module_path: &'static str,
+        line: u32,
+        description: impl Into<Option<S>>,
+    ) where
+        S: ToString,
+    {
+        self.finish_inner(Some(module_path), Some(line), description);
+    }
+
+    /// Finish timing the execution of a function, method, or other code region.
+    ///
+    /// This private method can be called from [`CodeTimer::finish()`] or `drop()`.
+    #[track_caller]
+    fn finish_inner<S>(
+        &mut self,
+        module_path: impl Into<Option<&'static str>>,
+        line: impl Into<Option<u32>>,
+        description: impl Into<Option<S>>,
+    ) where
+        S: ToString,
+    {
+        if self.has_finished {
+            return;
+        }
+
+        self.has_finished = true;
+
+        let execution = self.start.elapsed();
+        let execution_time = duration_short(execution);
+
+        let module_path = module_path.into();
+        let line = line.into();
+        let description = description
+            .into()
+            .map(|desc| desc.to_string() + " ")
+            .unwrap_or_default();
+
+        if execution >= self.min_warn_time {
+            warn!(
+                ?execution_time,
+                ?module_path,
+                ?line,
+                "{description}code took a long time to execute",
+            );
+        } else if execution >= self.min_info_time {
+            info!(
+                ?execution_time,
+                ?module_path,
+                ?line,
+                "{description}code took longer than expected to execute",
+            );
+        } else {
+            trace!(
+                ?execution_time,
+                ?module_path,
+                ?line,
+                "{description}code timer finished",
+            );
+        }
+    }
+}
+
+impl Drop for CodeTimer {
+    #[track_caller]
+    fn drop(&mut self) {
+        self.finish_inner(None, None, "(dropped, cancelled, or aborted)")
+    }
+}
