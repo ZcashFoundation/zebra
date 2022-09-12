896190086b2d7b3579c0c7992d32976b9d896f6b
teor <teor@riseup.net>
2022-09-12 11:04:15 +1000

    Explain how spent UTXOs are treated by the state

--- zebra-state/src/service/read/block.rs
+++ zebra-state/src/service/read/block.rs
@@ -96,7 +96,12 @@ where
 /// Returns the [`Utxo`] for [`transparent::OutPoint`], if it exists in the
 /// non-finalized `chain` or finalized `db`.
 ///
-/// UTXOs may be returned regardless of whether they have been spent.
+/// Non-finalized UTXOs are returned regardless of whether they have been spent.
+///
+/// Finalized UTXOs are only returned if they are unspent in the finalized chain.
+/// They may have been spent in the non-finalized chain,
+/// but this function returns them without checking for non-finalized spends,
+/// because we don't know which non-finalized chain will be committed to the finalized state.
 pub fn utxo<C>(chain: Option<C>, db: &ZebraDb, outpoint: transparent::OutPoint) -> Option<Utxo>
 where
     C: AsRef<Chain>,
@@ -115,10 +120,18 @@ where
         .or_else(|| db.utxo(&outpoint).map(|utxo| utxo.utxo))
 }
 
-/// Returns the [`Utxo`] for [`transparent::OutPoint`], if it exists in the
-/// `non_finalized_state` or finalized `db`.
+/// Returns the [`Utxo`] for [`transparent::OutPoint`], if it exists in any chain
+/// in the `non_finalized_state`, or in the finalized `db`.
 ///
-/// UTXOs may be returned regardless of whether they have been spent.
+/// Non-finalized UTXOs are returned regardless of whether they have been spent.
+///
+/// Finalized UTXOs are only returned if they are unspent in the finalized chain.
+/// They may have been spent in one or more non-finalized chains,
+/// but this function returns them without checking for non-finalized spends,
+/// because we don't know which non-finalized chain the request belongs to.
+///
+/// UTXO spends are checked once the block reaches the non-finalized state,
+/// by [`check::utxo::transparent_spend()`](crate::service::check::utxo::transparent_spend).
 pub fn any_utxo(
     non_finalized_state: NonFinalizedState,
     db: &ZebraDb,
