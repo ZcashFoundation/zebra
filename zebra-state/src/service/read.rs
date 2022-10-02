211dbb437b4a0967065fe370bca53ceefea87a54
teor <teor@riseup.net>
2022-10-03 09:34:44 +1000

    1. fix(rpc): Fix slow getblock RPC (verbose=1) using transaction ID index (#5307)
    * Add RPC timing to zcash-rpc-diff
    
    * Use transaction hash index for verbose block requests, rather than block data

--- zebra-state/src/service/read.rs
+++ zebra-state/src/service/read.rs
@@ -27,7 +27,7 @@ pub use address::{
     tx_id::transparent_tx_ids,
     utxo::{address_utxos, AddressUtxos, ADDRESS_HEIGHTS_FULL_RANGE},
 };
-pub use block::{any_utxo, block, block_header, transaction, utxo};
+pub use block::{any_utxo, block, block_header, transaction, transaction_hashes_for_block, utxo};
 pub use find::{
     block_locator, chain_contains_hash, depth, find_chain_hashes, find_chain_headers,
     hash_by_height, height_by_hash, tip, tip_height,
