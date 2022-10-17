4f3d64d520c9dd441ad56889782c77bbc277f1b2
Alfredo Garcia <oxarbitrage@gmail.com>
2022-10-17 16:58:20 -0300

    leave server as it was

--- zebra-rpc/src/server.rs
+++ zebra-rpc/src/server.rs
@@ -77,7 +77,7 @@ impl RpcServer {
             // Create handler compatible with V1 and V2 RPC protocols
             let mut io: MetaIoHandler<(), _> =
                 MetaIoHandler::new(Compatibility::Both, TracingMiddleware);
-            io.extend_with(Rpc::to_delegate(rpc_impl));
+            io.extend_with(rpc_impl.to_delegate());
 
             // If zero, automatically scale threads to the number of CPU cores
             let mut parallel_cpu_threads = config.parallel_cpu_threads;
