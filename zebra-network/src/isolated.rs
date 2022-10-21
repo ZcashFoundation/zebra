5572f748efea36091438217984b2e1d4d73e2b1e
Alfredo Garcia <oxarbitrage@gmail.com>
2022-10-21 15:57:47 -0300

    remove link from function behind feature

--- zebra-network/src/isolated.rs
+++ zebra-network/src/isolated.rs
@@ -120,7 +120,8 @@ where
 /// Transactions sent over this connection can be linked to the sending and receiving IP address
 /// by passive internet observers.
 ///
-/// Prefer [`connect_isolated_tor`](tor::connect_isolated_tor) if available.
+///
+/// Prefer `connect_isolated_tor` if available.
 pub fn connect_isolated_tcp_direct(
     network: Network,
     addr: SocketAddr,
