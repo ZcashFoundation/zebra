04c24dc10e6ce374bf344c1241c86cf0bc0d3f4b
teor <teor@riseup.net>
2022-10-25 13:13:41 +1000

    Remove unused, buggy Sapling address, key, and commitment code

--- zebra-chain/src/sapling/note/nullifiers.rs
+++ zebra-chain/src/sapling/note/nullifiers.rs
@@ -1,28 +1,4 @@
-#![allow(clippy::unit_arg)]
-
-use super::super::{
-    commitment::{pedersen_hashes::mixing_pedersen_hash, NoteCommitment},
-    keys::NullifierDerivingKey,
-    tree::Position,
-};
-
-/// Invokes Blake2s-256 as PRF^nfSapling to derive the nullifier for a
-/// Sapling note.
-///
-/// PRF^nfSapling(ρ*) := BLAKE2s-256("Zcash_nf", nk* || ρ*)
-///
-/// <https://zips.z.cash/protocol/protocol.pdf#concreteprfs>
-fn prf_nf(nk: [u8; 32], rho: [u8; 32]) -> [u8; 32] {
-    let hash = blake2s_simd::Params::new()
-        .hash_length(32)
-        .personal(b"Zcash_nf")
-        .to_state()
-        .update(&nk[..])
-        .update(&rho[..])
-        .finalize();
-
-    *hash.as_array()
-}
+//! Sapling nullifiers.
 
 /// A Nullifier for Sapling transactions
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Serialize, Deserialize, Hash)]
@@ -35,14 +11,6 @@ pub struct Nullifier(pub [u8; 32]);
 impl From<[u8; 32]> for Nullifier {
     fn from(buf: [u8; 32]) -> Self {
         Self(buf)
-    }
-}
-
-impl<'a> From<(NoteCommitment, Position, &'a NullifierDerivingKey)> for Nullifier {
-    fn from((cm, pos, nk): (NoteCommitment, Position, &'a NullifierDerivingKey)) -> Self {
-        let rho = jubjub::AffinePoint::from(mixing_pedersen_hash(cm.0.into(), pos.0.into()));
-
-        Nullifier(prf_nf(nk.into(), rho.to_bytes()))
     }
 }
 
