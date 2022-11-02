1c4672cf37996380bac10134e13bab1305dd1f90
teor <teor@riseup.net>
2022-11-03 06:21:11 +1000

    Update default roots docs

--- zebra-rpc/src/methods/get_block_template_rpcs/types/default_roots.rs
+++ zebra-rpc/src/methods/get_block_template_rpcs/types/default_roots.rs
@@ -9,21 +9,26 @@ use zebra_chain::block::{
 #[derive(Clone, Debug, Eq, PartialEq, serde::Serialize, serde::Deserialize)]
 pub struct DefaultRoots {
     /// The merkle root of the transaction IDs in the block.
+    /// Used in the new block's header.
     #[serde(rename = "merkleroot")]
     #[serde(with = "hex")]
     pub merkle_root: merkle::Root,
 
-    /// Add documentation.
+    /// The root of the merkle mountain range of the chain history roots from the last network upgrade to the previous block.
+    /// Unlike the other roots, this not cover any data from this new block, only from previous blocks.
     #[serde(rename = "chainhistoryroot")]
     #[serde(with = "hex")]
     pub chain_history_root: ChainHistoryMmrRootHash,
 
-    /// The merkle root of the authorizing data hashes of the transactions in the block.
+    /// The merkle root of the authorizing data hashes of the transactions in the new block.
     #[serde(rename = "authdataroot")]
     #[serde(with = "hex")]
     pub auth_data_root: AuthDataRoot,
 
-    /// Add documentation.
+    /// The block commitment for the new block's header.
+    /// This hash covers `chain_history_root` and `auth_data_root`.
+    ///
+    /// `merkle_root` has its own field in the block header.
     #[serde(rename = "blockcommitmentshash")]
     #[serde(with = "hex")]
     pub block_commitments_hash: ChainHistoryBlockTxAuthCommitmentHash,
