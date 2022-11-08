6445cadaf1f2609376ea5ec2ca88c6047e90bb17
arya2 <aryasolhi@gmail.com>
2022-11-07 19:26:31 -0500

    updates get_raw_future_blocks fn with rpc calls

--- zebrad/tests/common/get_block_template_rpcs/submit_block.rs
+++ zebrad/tests/common/get_block_template_rpcs/submit_block.rs
@@ -11,9 +11,11 @@
 use color_eyre::eyre::{eyre, Context, Result};
 
 use reqwest::Client;
-use zebra_chain::parameters::Network;
+use zebra_chain::{block::Height, parameters::Network};
+use zebra_state::MAX_BLOCK_REORG_HEIGHT;
 
 use crate::common::{
+    cached_state::load_tip_height_from_state_directory,
     launch::{can_spawn_zebrad_for_rpc, spawn_zebrad_for_rpc},
     lightwalletd::LightwalletdTestType,
     sync::{check_sync_logs_until, MempoolBehavior, SYNC_FINISHED_REGEX},
@@ -45,7 +47,7 @@ pub(crate) async fn run() -> Result<()> {
     // - get first X blocks in non-finalized state via getblock rpc calls
     // - restart zebra without peers
     // - submit block(s) above the finalized tip height
-    let raw_blocks: Vec<String> = get_raw_future_blocks(network, test_type, test_name).await?;
+    let raw_blocks: Vec<String> = get_raw_future_blocks(network, test_type, test_name, 3).await?;
 
     tracing::info!("got raw future blocks, spawning isolated zebrad...",);
 
@@ -71,13 +73,13 @@ pub(crate) async fn run() -> Result<()> {
 
     for raw_block in raw_blocks {
         let res = client
-        .post(format!("http://{}", &rpc_address))
-        .body(format!(
-            r#"{{"jsonrpc": "2.0", "method": "submitblock", "params": ["{raw_block}"], "id":123 }}"#
-        ))
-        .header("Content-Type", "application/json")
-        .send()
-        .await?;
+            .post(format!("http://{}", &rpc_address))
+            .body(format!(
+                r#"{{"jsonrpc": "2.0", "method": "submitblock", "params": ["{raw_block}"], "id":123 }}"#
+            ))
+            .header("Content-Type", "application/json")
+            .send()
+            .await?;
 
         assert!(res.status().is_success());
         let res_text = res.text().await?;
@@ -99,7 +101,7 @@ pub(crate) async fn run() -> Result<()> {
     Ok(())
 }
 
-/// Accepts a network, a test_type, and test_name.
+/// Accepts a network, test_type, test_name, and num_blocks (how many blocks past the finalized tip to try getting)
 ///
 /// Syncs zebra until the tip, gets some blocks near the tip, via getblock rpc calls,
 /// shuts down zebra, and gets the finalized tip height of the updated cached state.
@@ -109,7 +111,11 @@ async fn get_raw_future_blocks(
     network: Network,
     test_type: LightwalletdTestType,
     test_name: &str,
+    max_num_blocks: u32,
 ) -> Result<Vec<String>> {
+    let max_num_blocks = max_num_blocks.min(MAX_BLOCK_REORG_HEIGHT);
+    let mut raw_blocks = Vec::with_capacity(max_num_blocks as usize);
+
     assert!(
         test_type.needs_zebra_cached_state() && test_type.needs_zebra_rpc_server(),
         "get_raw_future_blocks needs zebra cached state and rpc server"
@@ -133,7 +139,61 @@ async fn get_raw_future_blocks(
     // Create an http client
     let client = Client::new();
 
+    let send_rpc_request = |method, params| {
+        client
+            .post(format!("http://{}", &rpc_address))
+            .body(format!(
+                r#"{{"jsonrpc": "2.0", "method": "{method}", "params": {params}, "id":123 }}"#
+            ))
+            .header("Content-Type", "application/json")
+            .send()
+    };
+
+    let blockchain_info: serde_json::Value =
+        send_rpc_request("getblockchaininfo", "[]".to_string())
+            .await?
+            .text()
+            .await?
+            .try_into()?;
+
+    let tip_height: u32 = blockchain_info["result"]["blocks"]
+        .as_u64()
+        .expect("unexpected block height: doesn't fit in u64")
+        .try_into()
+        .expect("unexpected block height: doesn't fit in u32");
+
+    let estimated_finalized_tip_height = tip_height - MAX_BLOCK_REORG_HEIGHT;
+
+    for block_height in (0..max_num_blocks).map(|idx| idx + estimated_finalized_tip_height) {
+        let raw_block: serde_json::Value =
+            send_rpc_request("getblock", format!(r#"["{block_height}", 0]"#))
+                .await?
+                .text()
+                .await?
+                .try_into()?;
+
+        raw_blocks.push((
+            block_height,
+            raw_block["result"]
+                .as_str()
+                .expect("unexpected getblock result: not a string")
+                .to_string(),
+        ));
+    }
+
     zebrad.kill(true)?;
 
-    Ok(vec![])
+    let zebrad_state_path = test_type
+        .zebrad_state_path(test_name)
+        .expect("already checked that there is a cached state path");
+
+    let Height(finalized_tip_height) =
+        load_tip_height_from_state_directory(network, zebrad_state_path.as_ref()).await?;
+
+    let raw_future_blocks = raw_blocks
+        .into_iter()
+        .filter_map(|(height, raw_block)| height.gt(&finalized_tip_height).then_some(raw_block))
+        .collect();
+
+    Ok(raw_future_blocks)
 }
