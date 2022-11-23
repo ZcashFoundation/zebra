f47439e1c238a38b0b5126a9d3e564c46c71f097
teor <teor@riseup.net>
2022-11-23 13:08:22 +1000

    Add ZIP-317 constants

--- zebra-chain/src/transaction/unmined/zip317.rs
+++ zebra-chain/src/transaction/unmined/zip317.rs
@@ -10,6 +10,20 @@ use crate::{
 #[allow(unused_imports)]
 use crate::transaction::UnminedTx;
 
+/// The marginal fee for the ZIP-317 fee calculation, in zatoshis per logical action.
+//
+// TODO: allow Amount<NonNegative> in constants
+const MARGINAL_FEE: i64 = 5_000;
+
+/// The number of grace logical actions allowed by the ZIP-317 fee calculation.
+const GRACE_ACTIONS: u64 = 2;
+
+/// The standard size of p2pkh inputs for the ZIP-317 fee calculation, in bytes.
+const P2PKH_STANDARD_INPUT_SIZE: usize = 150;
+
+/// The standard size of p2pkh outputs for the ZIP-317 fee calculation, in bytes.
+const P2PKH_STANDARD_OUTPUT_SIZE: usize = 34;
+
 /// Returns the conventional fee for `transaction`, as defined by [ZIP-317].
 ///
 /// [ZIP-317]: https://zips.z.cash/zip-0317#fee-calculation
