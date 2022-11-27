b1c03e53b191767bdd5124a2546b60531f853e61
teor <teor@riseup.net>
2022-11-28 09:01:17 +1000

    Split picking a transaction into its own function

--- zebra-rpc/src/methods/get_block_template_rpcs/zip317.rs
+++ zebra-rpc/src/methods/get_block_template_rpcs/zip317.rs
@@ -45,18 +45,21 @@ where
     let mut remaining_block_sigops = MAX_BLOCK_SIGOPS;
     let mut remaining_block_bytes: usize = MAX_BLOCK_BYTES.try_into().expect("fits in memory");
 
-    if let Some((mut conventional_fee_tx_weights, _total_weight)) =
+    if let Some((conventional_fee_tx_weights, _total_weight)) =
         setup_fee_weighted_index(&conventional_fee_txs)
     {
+        let mut conventional_fee_tx_weights = Some(conventional_fee_tx_weights);
+
         // > Repeat while there is any mempool transaction that:
         // > - pays at least the conventional fee,
         // > - is within the block sigop limit, and
         // > - fits in the block...
-        loop {
+        while let Some(tx_weights) = conventional_fee_tx_weights {
             // > Pick one of those transactions at random with probability in direct proportion
             // > to its weight, and add it to the block.
-            let candidate_index = conventional_fee_tx_weights.sample(&mut thread_rng());
-            let candidate_tx = &conventional_fee_txs[candidate_index];
+            let (tx_weights, candidate_tx) =
+                choose_transaction_weighted_random(&conventional_fee_txs, tx_weights);
+            conventional_fee_tx_weights = tx_weights;
 
             if candidate_tx.legacy_sigop_count <= remaining_block_sigops
                 && candidate_tx.transaction.size <= remaining_block_bytes
@@ -66,22 +69,12 @@ where
                 remaining_block_sigops -= candidate_tx.legacy_sigop_count;
                 remaining_block_bytes -= candidate_tx.transaction.size;
             }
-
-            // Only pick each transaction once, by setting picked transaction weights to zero
-            if conventional_fee_tx_weights
-                .update_weights(&[(candidate_index, &0.0)])
-                .is_err()
-            {
-                // All weights are zero, so each transaction has either been selected or rejected
-                break;
-            }
         }
     }
 
     // > Let `N` be the number of remaining transactions with `tx.weight < 1`.
     // > Calculate their sum of weights.
-    if let Some((mut low_fee_tx_weights, remaining_weight)) = setup_fee_weighted_index(&low_fee_txs)
-    {
+    if let Some((low_fee_tx_weights, remaining_weight)) = setup_fee_weighted_index(&low_fee_txs) {
         let low_fee_tx_count = low_fee_txs.len() as f32;
 
         // > Calculate `size_target = ...`
@@ -94,17 +87,20 @@ where
             remaining_block_bytes as f32 * average_remaining_weight.min(1.0);
         let mut remaining_block_bytes = remaining_block_bytes as usize;
 
-        loop {
-            // > Pick a transaction with probability in direct proportion to its weight
+        let mut low_fee_tx_weights = Some(low_fee_tx_weights);
+
+        while let Some(tx_weights) = low_fee_tx_weights {
+            // > Pick a transaction with probability in direct proportion to its weight...
+            let (tx_weights, candidate_tx) =
+                choose_transaction_weighted_random(&low_fee_txs, tx_weights);
+            low_fee_tx_weights = tx_weights;
+
             // > and add it to the block. If that transaction would exceed the `size_target`
             // > or the block sigop limit, stop without adding it.
-            let candidate_index = low_fee_tx_weights.sample(&mut thread_rng());
-            let candidate_tx = &low_fee_txs[candidate_index];
-
             if candidate_tx.legacy_sigop_count > remaining_block_sigops
                 || candidate_tx.transaction.size > remaining_block_bytes
             {
-                // We've exceeded the (scaled quota) limits
+                // We've exceeded the scaled quota size limit, or the absolute sigop limit
                 break;
             }
 
@@ -112,15 +108,6 @@ where
 
             remaining_block_sigops -= candidate_tx.legacy_sigop_count;
             remaining_block_bytes -= candidate_tx.transaction.size;
-
-            // Only pick each transaction once, by setting picked transaction weights to zero
-            if low_fee_tx_weights
-                .update_weights(&[(candidate_index, &0.0)])
-                .is_err()
-            {
-                // All weights are zero, so every remaining transaction has been selected
-                break;
-            }
         }
     }
 
@@ -173,4 +160,27 @@ fn setup_fee_weighted_index(
     let tx_weights = WeightedIndex::new(tx_weights).ok()?;
 
     Some((tx_weights, total_tx_weight))
+}
+
+/// Choose a transaction from `transactions`, using the previously set up `weighted_index`.
+///
+/// If some transactions have not yet been chosen, returns the weighted index and the transaction.
+/// Otherwise, just returns the transaction.
+fn choose_transaction_weighted_random(
+    transactions: &[VerifiedUnminedTx],
+    mut weighted_index: WeightedIndex<f32>,
+) -> (Option<WeightedIndex<f32>>, VerifiedUnminedTx) {
+    let candidate_position = weighted_index.sample(&mut thread_rng());
+    let candidate_tx = transactions[candidate_position].clone();
+
+    // Only pick each transaction once, by setting picked transaction weights to zero
+    if weighted_index
+        .update_weights(&[(candidate_position, &0.0)])
+        .is_err()
+    {
+        // All weights are zero, so each transaction has either been selected or rejected
+        (None, candidate_tx)
+    } else {
+        (Some(weighted_index), candidate_tx)
+    }
 }
