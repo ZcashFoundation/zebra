4eb310bc67637ea280b749b1c55c040e2cd1a784
teor <teor@riseup.net>
2022-12-01 13:25:30 +1000

    Try the RPC again after waiting for transactions to verify

--- zebrad/tests/common/get_block_template_rpcs/get_block_template.rs
+++ zebrad/tests/common/get_block_template_rpcs/get_block_template.rs
@@ -5,6 +5,8 @@
 //!
 //! After finishing the sync, it will call getblocktemplate.
 
+use std::time::Duration;
+
 use color_eyre::eyre::{eyre, Context, Result};
 
 use zebra_chain::parameters::Network;
@@ -16,6 +18,12 @@ use crate::common::{
     test_type::TestType,
 };
 
+/// How long the test waits for the mempool to download and verify transactions.
+///
+/// We've seen it take anywhere from 1-45 seconds for the mempool to have some transactions in it.
+pub const EXPECTED_MEMPOOL_TRANSACTION_TIME: Duration = Duration::from_secs(45);
+
+/// Launch Zebra, wait for it to sync, and check the getblocktemplate RPC returns without errors.
 pub(crate) async fn run() -> Result<()> {
     let _init_guard = zebra_test::init();
 
@@ -50,7 +58,10 @@ pub(crate) async fn run() -> Result<()> {
         true,
     )?;
 
-    tracing::info!("calling getblocktemplate RPC method at {rpc_address}...",);
+    tracing::info!(
+        "calling getblocktemplate RPC method at {rpc_address}, \
+         with a mempool that is likely empty...",
+    );
     let getblocktemplate_response = RPCRequestClient::new(rpc_address)
         .call("getblocktemplate", "[]".to_string())
         .await?;
@@ -58,7 +69,30 @@ pub(crate) async fn run() -> Result<()> {
     let is_response_success = getblocktemplate_response.status().is_success();
     let response_text = getblocktemplate_response.text().await?;
 
-    tracing::info!(response_text, "got getblocktemplate response",);
+    tracing::info!(
+        response_text,
+        "got getblocktemplate response, might not have transactions"
+    );
+
+    assert!(is_response_success);
+
+    tokio::time::sleep(EXPECTED_MEMPOOL_TRANSACTION_TIME).await;
+
+    tracing::info!(
+        "calling getblocktemplate RPC method at {rpc_address}, \
+         with a mempool that likely has transactions...",
+    );
+    let getblocktemplate_response = RPCRequestClient::new(rpc_address)
+        .call("getblocktemplate", "[]".to_string())
+        .await?;
+
+    let is_response_success = getblocktemplate_response.status().is_success();
+    let response_text = getblocktemplate_response.text().await?;
+
+    tracing::info!(
+        response_text,
+        "got getblocktemplate response, hopefully with transactions"
+    );
 
     assert!(is_response_success);
 
