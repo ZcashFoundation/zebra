a3ab31d59d41c3776ae70e48d0956fa94308cc18
teor <teor@riseup.net>
2023-01-16 12:26:45 +1000

    Revert "Temporarily fix the code for a disabled test"
    This reverts commit d915624417cc946e53aac76449e0b5b719e03d2a.

--- zebrad/tests/common/get_block_template_rpcs/get_block_template.rs
+++ zebrad/tests/common/get_block_template_rpcs/get_block_template.rs
@@ -145,12 +145,7 @@ async fn try_validate_block_template(client: &RPCRequestClient) -> Result<()> {
     // Propose a new block with an empty solution and nonce field
     tracing::info!("calling getblocktemplate with a block proposal...",);
 
-    // TODO: update this to use all valid time sources in the next PR
-    #[allow(clippy::single_element_loop)]
-    for proposal_block in [proposal_block_from_template(
-        response_json_result,
-        TimeSource::CurTime,
-    )?] {
+    for proposal_block in proposal_block_from_template(response_json_result)? {
         let raw_proposal_block = hex::encode(proposal_block.zcash_serialize_to_vec()?);
 
         let json_result = client
