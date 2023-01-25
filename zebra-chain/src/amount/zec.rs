9efb8f9cdd461ff28e2b7fbd1e7f64ccc6652ba5
teor <teor@riseup.net>
2023-01-25 15:17:48 +1000

    Switch to formatting Zec with f64, because the getblocksubsidy RPC requires JSON numbers

--- zebra-chain/src/amount/zec.rs
+++ zebra-chain/src/amount/zec.rs
@@ -1,4 +1,7 @@
-//! ZEC-specific formatting, similar to the `zcashd` implementation.
+//! ZEC amount formatting.
+//!
+//! The `f64` values returned by this type should not be used in consensus-critical code.
+//! The values themselves are accurate, but any calculations using them could be lossy.
 
 use std::{
     cmp::Ordering,
@@ -7,8 +10,6 @@ use std::{
     ops,
     str::FromStr,
 };
-
-use serde_with::{DeserializeFromStr, SerializeDisplay};
 
 use crate::{
     amount::{self, Amount, Constraint, COIN},
@@ -20,93 +21,103 @@ use crate::{
 ///
 /// This is the same as the `getblocksubsidy` RPC in `zcashd`:
 /// <https://github.com/zcash/zcash/blob/f6a4f68115ea4c58d55c8538579d0877ba9c8f79/src/rpc/server.cpp#L134>
-pub const MAX_ZAT_PRECISION: usize = 8;
+pub const MAX_ZEC_FORMAT_PRECISION: usize = 8;
 
-/// A wrapper type that formats [`Amount`]s as ZEC,
-/// using fixed-point integer calculations.
-#[derive(Clone, Copy, SerializeDisplay, DeserializeFromStr)]
+/// A wrapper type that formats [`Amount`]s as ZEC, using double-precision floating point.
+///
+/// This formatting is accurate to the nearest zatoshi, as long as the number of floating-point
+/// calculations is very small. This is because [`MAX_MONEY`] uses 51 bits, but [`f64`] has
+/// [53 bits of precision](f64::MANTISSA_DIGITS).
+///
+/// Rust uses [`roundTiesToEven`](f32), which can lose one bit of precision per calculation
+/// in the worst case. (Assuming the platform implements it correctly.)
+///
+/// Unlike `zcashd`, Zebra doesn't have control over its JSON number precision,
+/// because it uses `serde_json`'s formatter. But `zcashd` uses a fixed-point calculation:
+/// <https://github.com/zcash/zcash/blob/f6a4f68115ea4c58d55c8538579d0877ba9c8f79/src/rpc/server.cpp#L134>
+#[derive(Clone, Copy, Serialize, Deserialize)]
+#[serde(try_from = "f64")]
+#[serde(into = "f64")]
+#[serde(bound = "C: Constraint + Clone")]
 pub struct Zec<C: Constraint>(Amount<C>);
 
-impl<C: Constraint> fmt::Display for Zec<C> {
-    // We don't use floating-point here, because it is inaccurate
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+impl<C: Constraint> Zec<C> {
+    /// Returns the `f64` ZEC value for the inner amount.
+    ///
+    /// The returned value should not be used for consensus-critical calculations,
+    /// because it is lossy.
+    pub fn lossy_zec(&self) -> f64 {
         let zats = self.zatoshis();
+        // These conversions are exact, because f64 has 53 bits of precision,
+        // MAX_MONEY has <51, and COIN has <27, so we have 2 extra bits of precision.
+        let zats = zats as f64;
+        let coin = COIN as f64;
 
-        // Get the fixed-point ZEC and remaining zats values
-        let abs_coins = zats
-            .checked_abs()
-            .expect("-MAX_MONEY is much smaller than i64::MIN")
-            / COIN;
-        let positive_remainder = zats.rem_euclid(COIN);
+        // After this calculation, we might have lost one bit of precision,
+        // leaving us with only 1 extra bit.
+        zats / coin
+    }
 
-        // Format just like `zcashd` by default
-        let decimals = f.precision().unwrap_or(MAX_ZAT_PRECISION);
-        let string = format!("{abs_coins}.{positive_remainder:.decimals$}");
-        f.pad_integral(zats >= 0, "", &string)
+    /// Converts a `f64` ZEC value to a [`Zec`] amount.
+    ///
+    /// This method should not be used for consensus-critical calculations, because it is lossy.
+    pub fn from_lossy_zec(lossy_zec: f64) -> Result<Self, BoxError> {
+        // This conversion is exact, because f64 has 53 bits of precision, but COIN has <27
+        let coin = COIN as f64;
+
+        // After this calculation, we might have lost one bit of precision
+        let zats = lossy_zec * coin;
+
+        if zats != zats.trunc() {
+            return Err(
+                "loss of precision parsing ZEC value: floating point had fractional zatoshis"
+                    .into(),
+            );
+        }
+
+        // We know this conversion is exact, because we just checked.
+        let zats = zats as i64;
+        let zats = Amount::try_from(zats)?;
+
+        Ok(Self(zats))
     }
 }
 
-// This is mainly used in tests
+// These conversions are lossy, so they should not be used in consensus-critical code
+impl<C: Constraint> From<Zec<C>> for f64 {
+    fn from(zec: Zec<C>) -> f64 {
+        zec.lossy_zec()
+    }
+}
+
+impl<C: Constraint> TryFrom<f64> for Zec<C> {
+    type Error = BoxError;
+
+    fn try_from(value: f64) -> Result<Self, Self::Error> {
+        Self::from_lossy_zec(value)
+    }
+}
+
+// This formatter should not be used for consensus-critical outputs.
+impl<C: Constraint> fmt::Display for Zec<C> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let zec = self.lossy_zec();
+
+        // Try to format like `zcashd` by default
+        let decimals = f.precision().unwrap_or(MAX_ZEC_FORMAT_PRECISION);
+        let string = format!("{zec:.decimals$}");
+        f.pad_integral(zec >= 0.0, "", &string)
+    }
+}
+
+// This parser should not be used for consensus-critical inputs.
 impl<C: Constraint> FromStr for Zec<C> {
     type Err = BoxError;
 
-    // We don't use floating-point here, because it is inaccurate
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        // Get the fixed-point ZEC and remaining zats values
-        let (signed_coins, abs_remainder) = match s.trim().split_once('.') {
-            Some(("", positive_zats)) => ("0", positive_zats),
-            Some((signed_coins, "")) => (signed_coins, "0"),
-            Some((signed_coins, abs_remainder)) => (signed_coins, abs_remainder),
-            None => (s, "0"),
-        };
+        let lossy_zec: f64 = s.parse()?;
 
-        // Convert ZEC to integer
-        let signed_coins: i64 = signed_coins.parse()?;
-
-        // Check for spurious + or - after the decimal point
-        if !abs_remainder
-            .chars()
-            .next()
-            .expect("just checked for empty strings")
-            .is_numeric()
-        {
-            return Err("invalid ZEC amount: fractional part must start with 0-9".into());
-        }
-
-        // Check for loss of precision, after removing trailing zeroes
-        let abs_remainder = abs_remainder.trim_end_matches('0');
-        if abs_remainder.len() > MAX_ZAT_PRECISION {
-            return Err("loss of precision: ZEC value contains fractional zatoshis".into());
-        }
-
-        // Zero-pad to an amount in zats, then parse
-        let abs_remainder =
-            abs_remainder.to_owned() + &"0".repeat(MAX_ZAT_PRECISION - abs_remainder.len());
-        let abs_remainder: u32 = abs_remainder.parse()?;
-        let abs_remainder: i64 = abs_remainder.into();
-        assert!(
-            abs_remainder < COIN,
-            "unexpected parsing error: just checked zat value was less than one ZEC in length"
-        );
-
-        // Now convert everything to zatoshi amounts, with the correct sign
-        let zats = signed_coins
-            .checked_mul(COIN)
-            .ok_or("ZEC value too large")?;
-        let remainder = if zats > 0 {
-            abs_remainder
-        } else {
-            -abs_remainder
-        };
-
-        let mut zats: Amount<C> = zats.try_into()?;
-        let remainder: Amount<C> = remainder
-            .try_into()
-            .expect("already checked range and sign");
-
-        zats = (zats + remainder)?;
-
-        Ok(Self(zats))
+        Self::from_lossy_zec(lossy_zec)
     }
 }
 
