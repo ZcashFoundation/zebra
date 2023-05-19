778cce7fa97343ec0935adf110218d34d7599e54
Pili Guerra <pili@piliguerra.com>
2023-05-19 15:14:35 +0200

    #4794 was fixed by #6122

--- zebra-rpc/src/methods/get_block_template_rpcs.rs
+++ zebra-rpc/src/methods/get_block_template_rpcs.rs
@@ -110,8 +110,7 @@ pub trait GetBlockTemplateRpc {
     /// - the parent block is a valid block that Zebra already has, or will receive soon.
     ///
     /// Zebra verifies blocks in parallel, and keeps recent chains in parallel,
-    /// so moving between chains is very cheap. (But forking a new chain may take some time,
-    /// until bug #4794 is fixed.)
+    /// so moving between chains is very cheap.
     ///
     /// This rpc method is available only if zebra is built with `--features getblocktemplate-rpcs`.
     #[rpc(name = "getblocktemplate")]
