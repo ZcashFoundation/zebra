0ec306917422e75eac99a9ced961fa3c5c049f0a
teor <teor@riseup.net>
2023-05-24 12:23:14 +1000

    Add more specific logs for some RPC tests

--- zebra-rpc/src/methods/get_block_template_rpcs.rs
+++ zebra-rpc/src/methods/get_block_template_rpcs.rs
@@ -737,7 +737,7 @@ where
             let block: Block = match block_bytes.zcash_deserialize_into() {
                 Ok(block_bytes) => block_bytes,
                 Err(error) => {
-                    tracing::info!(?error, "submit block failed");
+                    tracing::info!(?error, "submit block failed: block bytes could not be deserialized into a structurally valid block");
 
                     return Ok(submit_block::ErrorResponse::Rejected.into());
                 }
@@ -778,8 +778,7 @@ where
                         .downcast::<VerifyChainError>()
                         .map(|boxed_chain_error| *boxed_chain_error);
 
-                    // TODO: add block hash to error?
-                    tracing::info!(?error, ?block_height, "submit block failed");
+                    tracing::info!(?error, ?block_hash, ?block_height, "submit block failed verification");
 
                     error
                 }
