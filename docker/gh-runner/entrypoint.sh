c46e9137c42549087c093bb82ad24f1c7ae79981
Gustavo Valverde <gustavo@iterativo.do>
2023-05-24 16:19:12 -0400

    fix(runner): use correct directory for scripts

--- docker/gh-runner/entrypoint.sh
+++ docker/gh-runner/entrypoint.sh
@@ -32,6 +32,6 @@ ACTIONS_RUNNER_INPUT_NAME=$HOSTNAME
 #get regsistration token for this runnner
 ACTIONS_RUNNER_INPUT_TOKEN="$(curl -sS --request POST --url "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/actions/runners/registration-token" --header "authorization: Bearer ${GITHUB_TOKEN}"  --header 'content-type: application/json' | jq -r .token)"
 # Create the runner and start the configuration script in unattended mode
-/runner/config.sh --unattended --disableupdate --replace --work "/tmp" --url "$ACTIONS_RUNNER_INPUT_URL" --token "$ACTIONS_RUNNER_INPUT_TOKEN"
+home/runner/config.sh --unattended --disableupdate --replace --work "/tmp" --url "$ACTIONS_RUNNER_INPUT_URL" --token "$ACTIONS_RUNNER_INPUT_TOKEN"
 #start runner
-/runner/bin/run.sh
+home/runner/bin/run.sh
