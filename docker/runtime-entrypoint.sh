a3a87bfa22c56227c2aa0abaa2f1c8eb352c21f3
teor <teor@riseup.net>
2023-06-07 10:33:10 +1000

    Add a runtime entrypoint to the Dockerfile and move config creation into it

---
+++ docker/runtime-entrypoint.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+# show the commands we are executing
+set -x
+# exit if a command fails
+set -e
+# exit if any command in a pipeline fails
+set -o pipefail
+
+echo "Runtime variables:"
+echo "NETWORK=$NETWORK"
+echo "RPC_PORT=$RPC_PORT"
+echo "ZEBRA_CONF_DIR=$ZEBRA_CONF_DIR"
+echo "ZEBRA_CONF_FILE=$ZEBRA_CONF_FILE"
+echo "ZEBRA_CONF_PATH=$ZEBRA_CONF_PATH"
+
+
+
+# Create the conf path and file if it does not exist.
+mkdir -p "$ZEBRA_CONF_DIR"
+touch "$ZEBRA_CONF_PATH"
+
+# Populate `zebrad.toml` before starting zebrad, using the environmental
+# variables set by the Dockerfile.
+#
+# We disable most ports by default, so the default config is secure.
+# Users have to opt-in to additional functionality by setting environmental variables.
+#
+# TODO:
+#  - make `cache_dir`, `metrics.endpoint_addr`, and `tracing.endpoint_addr` into Docker arguments
+#  - add an $EXTRA_CONFIG or $REPLACEMENT_CONFIG environmental variable
+cat <<EOF > "$ZEBRA_CONF_PATH"
+[network]
+network = "$NETWORK"
+listen_addr = "0.0.0.0"
+
+[state]
+cache_dir = "/zebrad-cache"
+
+[metrics]
+#endpoint_addr = "0.0.0.0:9999"
+
+[tracing]
+#endpoint_addr = "0.0.0.0:3000"
+EOF
+
+if [[ -n "$RPC_PORT" ]]; then
+cat <<EOF >> "$ZEBRA_CONF_PATH"
+[rpc]
+listen_addr = "0.0.0.0:${RPC_PORT}"
+EOF
+fi
+
+exec zebrad -c "$ZEBRA_CONF_PATH" "$@"
