cf676a174aad8e36268d93591adca7cdec993880
Marek <mail@marek.onl>
2023-07-18 00:51:26 +0200

    Support the retrieval of deduped Orchard trees

--- zebra-state/src/service/non_finalized_state/chain.rs
+++ zebra-state/src/service/non_finalized_state/chain.rs
@@ -806,7 +806,10 @@ impl Chain {
         let height =
             hash_or_height.height_or_else(|hash| self.height_by_hash.get(&hash).cloned())?;
 
-        self.orchard_trees_by_height.get(&height).cloned()
+        self.orchard_trees_by_height
+            .range(..=height)
+            .next_back()
+            .map(|(_height, tree)| tree.clone())
     }
 
     /// Add the Orchard `tree` to the tree and anchor indexes at `height`.
