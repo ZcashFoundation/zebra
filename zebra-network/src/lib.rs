000c7b4bc6202186a167bb451c1a2a24dc670ba9
Alfredo Garcia <oxarbitrage@gmail.com>
2023-08-25 01:08:13 -0300

    fix(clippy): Fix clippy and rustfmt after last releases today (#7381)
    * fix clippy and rustfmt after last releases today
    
    * Ignore clippy::arc_with_non_send_sync false positives in proptest code only
    
    * Remove .cargo/config.toml changes that didn't work
    
    ---------
    
    Co-authored-by: teor <teor@riseup.net>

--- zebra-network/src/lib.rs
+++ zebra-network/src/lib.rs
@@ -132,6 +132,16 @@
 #![doc(html_favicon_url = "https://zfnd.org/wp-content/uploads/2022/03/zebra-favicon-128.png")]
 #![doc(html_logo_url = "https://zfnd.org/wp-content/uploads/2022/03/zebra-icon.png")]
 #![doc(html_root_url = "https://doc.zebra.zfnd.org/zebra_network")]
+//
+// Rust 1.72 has a false positive when nested generics are used inside Arc.
+// This makes the `arc_with_non_send_sync` lint trigger on a lot of proptest code.
+//
+// TODO: remove this allow when Rust 1.73 is stable, because this lint bug is fixed in that release:
+// <https://github.com/rust-lang/rust-clippy/issues/11076>
+#![cfg_attr(
+    any(test, feature = "proptest-impl"),
+    allow(clippy::arc_with_non_send_sync)
+)]
 
 #[macro_use]
 extern crate pin_project;
