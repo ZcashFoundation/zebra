49123208d0347283e03d97577a0ed19e6c3bc3c0
arya2 <aryasolhi@gmail.com>
2023-10-11 16:05:47 -0400

    uses zs_range_iter for other read methods

--- zebra-state/src/service/finalized_state/zebra_db/shielded.rs
+++ zebra-state/src/service/finalized_state/zebra_db/shielded.rs
@@ -209,7 +209,7 @@ impl ZebraDb {
         R: std::ops::RangeBounds<Height>,
     {
         let sapling_trees = self.db.cf_handle("sapling_note_commitment_tree").unwrap();
-        self.db.zs_range_iter(&sapling_trees, range)
+        self.db.zs_range_iter(&sapling_trees, range, false)
     }
 
     /// Returns the Sapling note commitment trees in the reversed range, in decreasing height order.
@@ -282,7 +282,7 @@ impl ZebraDb {
         if let Some(exclusive_end_bound) = exclusive_end_bound {
             list = self
                 .db
-                .zs_range_iter(&sapling_subtrees, start_index..exclusive_end_bound)
+                .zs_range_iter(&sapling_subtrees, start_index..exclusive_end_bound, false)
                 .collect();
         } else {
             // If there is no end bound, just return all the trees.
@@ -291,7 +291,7 @@ impl ZebraDb {
             // the trees run out.)
             list = self
                 .db
-                .zs_range_iter(&sapling_subtrees, start_index..)
+                .zs_range_iter(&sapling_subtrees, start_index.., false)
                 .collect();
         }
 
@@ -376,7 +376,7 @@ impl ZebraDb {
         R: std::ops::RangeBounds<Height>,
     {
         let orchard_trees = self.db.cf_handle("orchard_note_commitment_tree").unwrap();
-        self.db.zs_range_iter(&orchard_trees, range)
+        self.db.zs_range_iter(&orchard_trees, range, false)
     }
 
     /// Returns the Orchard note commitment trees in the reversed range, in decreasing height order.
@@ -449,7 +449,7 @@ impl ZebraDb {
         if let Some(exclusive_end_bound) = exclusive_end_bound {
             list = self
                 .db
-                .zs_range_iter(&orchard_subtrees, start_index..exclusive_end_bound)
+                .zs_range_iter(&orchard_subtrees, start_index..exclusive_end_bound, false)
                 .collect();
         } else {
             // If there is no end bound, just return all the trees.
@@ -458,7 +458,7 @@ impl ZebraDb {
             // the trees run out.)
             list = self
                 .db
-                .zs_range_iter(&orchard_subtrees, start_index..)
+                .zs_range_iter(&orchard_subtrees, start_index.., false)
                 .collect();
         }
 
