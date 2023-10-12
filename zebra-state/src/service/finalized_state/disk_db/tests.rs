5fc6b306c875df69c2ddb2d1347ade7cb73406ea
arya2 <aryasolhi@gmail.com>
2023-10-11 23:11:51 -0400

    Adds test & updates method docs

--- zebra-state/src/service/finalized_state/disk_db/tests.rs
+++ zebra-state/src/service/finalized_state/disk_db/tests.rs
@@ -24,3 +24,28 @@ impl DiskDb {
         rocksdb::DB::list_cf(&opts, path)
     }
 }
+
+/// Check that the sprout tree database serialization format has not changed.
+#[test]
+fn zs_iter_opts_increments_key_by_one() {
+    let _init_guard = zebra_test::init();
+
+    // let (config, network) = Default::default();
+    // let db = DiskDb::new(&config, network);
+
+    let keys: [u32; 13] = [
+        0, 1, 200, 255, 256, 257, 65535, 65536, 65537, 16777215, 16777216, 16777217, 16777218,
+    ];
+
+    for key in keys {
+        let (_, upper_bound_bytes) = DiskDb::zs_iter_bounds(&..=key.to_be_bytes().to_vec());
+        let upper_bound_bytes = upper_bound_bytes.expect("there should be an upper bound");
+        let upper_bound = u32::from_be_bytes(upper_bound_bytes.try_into().unwrap());
+        let expected_upper_bound = key + 1;
+
+        assert_eq!(
+            expected_upper_bound, upper_bound,
+            "the upper bound should be 1 greater than the original key"
+        );
+    }
+}
