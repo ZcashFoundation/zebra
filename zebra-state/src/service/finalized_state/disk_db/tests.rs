600aa5f6087dc0b6af9d9fcce54b614cedaee5c4
arya2 <aryasolhi@gmail.com>
2023-10-13 00:14:36 -0400

    adds test case

--- zebra-state/src/service/finalized_state/disk_db/tests.rs
+++ zebra-state/src/service/finalized_state/disk_db/tests.rs
@@ -30,19 +30,41 @@ impl DiskDb {
 fn zs_iter_opts_increments_key_by_one() {
     let _init_guard = zebra_test::init();
 
-    let keys: [u32; 13] = [
-        0, 1, 200, 255, 256, 257, 65535, 65536, 65537, 16777215, 16777216, 16777217, 16777218,
+    let keys: [u32; 14] = [
+        0,
+        1,
+        200,
+        255,
+        256,
+        257,
+        65535,
+        65536,
+        65537,
+        16777215,
+        16777216,
+        16777217,
+        16777218,
+        u32::MAX,
     ];
 
     for key in keys {
-        let (_, upper_bound_bytes) = DiskDb::zs_iter_bounds(&..=key.to_be_bytes().to_vec());
-        let upper_bound_bytes = upper_bound_bytes.expect("there should be an upper bound");
+        let (_, bytes) = DiskDb::zs_iter_bounds(&..=key.to_be_bytes().to_vec());
+        let mut bytes = bytes.expect("there should be an upper bound");
+        let upper_bound_bytes = bytes.split_off(bytes.len() - 4);
         let upper_bound = u32::from_be_bytes(upper_bound_bytes.try_into().unwrap());
-        let expected_upper_bound = key + 1;
+        let expected_upper_bound = key.wrapping_add(1);
 
         assert_eq!(
             expected_upper_bound, upper_bound,
             "the upper bound should be 1 greater than the original key"
         );
+
+        if expected_upper_bound == 0 {
+            assert_eq!(
+                bytes,
+                vec![1],
+                "there should be an extra byte with a value of 1"
+            );
+        }
     }
 }
