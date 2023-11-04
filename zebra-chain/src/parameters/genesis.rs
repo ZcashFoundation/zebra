081e146c5b9e22a2d929a83efbd2bf9e896a6110
Arya <aryasolhi@gmail.com>
2023-11-04 19:15:34 -0400

    uses genesis hash from params

--- zebra-chain/src/parameters/genesis.rs
+++ zebra-chain/src/parameters/genesis.rs
@@ -14,7 +14,9 @@ pub fn genesis_hash(network: Network) -> block::Hash {
         // zcash-cli getblockhash 0
         Network::Mainnet => "00040fe8ec8471911baa1db1266ea15dd06b4a8a5c453883c000b031973dce08",
         // zcash-cli -testnet getblockhash 0
-        Network::Testnet => "05a60a92d99d85997cce3b87616c089f6124d7342af37106edc76126334a2c38",
+        Network::Testnet(params) => params
+            .genesis_hash()
+            .unwrap_or("05a60a92d99d85997cce3b87616c089f6124d7342af37106edc76126334a2c38"),
     }
     .parse()
     .expect("hard-coded hash parses")
