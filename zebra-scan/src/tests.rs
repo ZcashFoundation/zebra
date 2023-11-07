52958eb609ab01db21abc69d92a40fa28b24d8c0
Arya <aryasolhi@gmail.com>
2023-11-07 13:46:59 -0500

    scans all cached blocks for zecpages viewing key expecting Ok results.

--- zebra-scan/src/tests.rs
+++ zebra-scan/src/tests.rs
@@ -6,6 +6,7 @@
 use std::sync::Arc;
 
 use zcash_client_backend::{
+    data_api::BlockMetadata,
     encoding::decode_extended_full_viewing_key,
     proto::compact_formats::{
         self as compact, ChainMetadata, CompactBlock, CompactSaplingOutput, CompactSaplingSpend,
@@ -183,7 +184,6 @@ async fn scanning_zecpages_from_populated_zebra_state() -> Result<()> {
     .unwrap();
 
     let account = AccountId::from(1);
-    let nf = Nullifier([7; 32]);
 
     // Build a vector of viewing keys `vks` to scan for.
     let fvk = efvk.fvk;
@@ -191,28 +191,30 @@ async fn scanning_zecpages_from_populated_zebra_state() -> Result<()> {
     let vks: Vec<(&AccountId, &SaplingIvk)> = vec![(&account, &ivk)];
 
     let network = zebra_chain::parameters::Network::default();
+    let state_config = Default::default();
 
-    // Create a continuous chain of mainnet blocks from genesis
-    let blocks: Vec<Arc<Block>> = zebra_test::vectors::CONTINUOUS_MAINNET_BLOCKS
-        .iter()
-        .map(|(_height, block_bytes)| block_bytes.zcash_deserialize_into().unwrap())
-        .collect();
-
-    // Create a populated state service.
     let (_state_service, read_only_state_service, latest_chain_tip, _chain_tip_change) =
-        zebra_state::populated_state(blocks.clone(), network).await;
-
+        zebra_state::spawn_init(state_config, network, zebra_chain::block::Height::MAX, 3000)
+            .await?;
     let db = read_only_state_service.db();
 
     // use the tip as starting height
     let mut height = latest_chain_tip.best_tip_height().unwrap();
 
-    let mut transactions_found = 0;
-    let mut transactions_scanned = 0;
-    let mut blocks_scanned = 0;
     while let Some(block) = db.block(height.into()) {
-        // We fake the sapling tree size to 1 because we are not in Sapling heights.
-        let sapling_tree_size = 1;
+        let sapling_tree_size = db
+            .sapling_tree_by_hash_or_height(height.into())
+            .expect("should exist")
+            .count();
+
+        let previous_sapling_tree_size = if height.is_min() {
+            0
+        } else {
+            db.sapling_tree_by_hash_or_height(block.header.previous_block_hash.into())
+                .expect("should exist")
+                .count()
+        };
+
         let orchard_tree_size = db
             .orchard_tree_by_hash_or_height(height.into())
             .expect("should exist")
@@ -227,20 +229,24 @@ async fn scanning_zecpages_from_populated_zebra_state() -> Result<()> {
                 .expect("position should fit in u32"),
         };
 
+        let block_metadata = BlockMetadata::from_parts(
+            height.previous()?.0.into(),
+            BlockHash(block.header.previous_block_hash.0),
+            previous_sapling_tree_size
+                .try_into()
+                .expect("should fit in u32"),
+        );
+
         let compact_block = block_to_compact(block, chain_metadata);
 
-        let res = scan_block(
+        scan_block(
             &zcash_primitives::consensus::MainNetwork,
             compact_block.clone(),
             &vks[..],
-            &[(account, nf)],
-            None,
+            &[],
+            Some(&block_metadata),
         )
-        .unwrap();
-
-        transactions_found += res.transactions().len();
-        transactions_scanned += compact_block.vtx.len();
-        blocks_scanned += 1;
+        .expect("should scan block successfully");
 
         // scan backwards
         if height.is_min() {
@@ -248,13 +254,6 @@ async fn scanning_zecpages_from_populated_zebra_state() -> Result<()> {
         }
         height = height.previous()?;
     }
-
-    // make sure all blocks and transactions were scanned
-    assert_eq!(blocks_scanned, 11);
-    assert_eq!(transactions_scanned, 11);
-
-    // no relevant transactions should be found
-    assert_eq!(transactions_found, 0);
 
     Ok(())
 }
