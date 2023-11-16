a22c8d5f425996b7ad80e0ce6103583514878fd9
Alfredo Garcia <oxarbitrage@gmail.com>
2023-11-15 22:32:22 -0300

    feat(config): Allow to add keys to be scanned by the zebra-scan crate to config (#7949)
    * allow user to add sapling keys to config
    
    * apply code review suggestions
    
    Co-authored-by: teor <teor@riseup.net>
    
    ---------
    
    Co-authored-by: teor <teor@riseup.net>

--- zebrad/src/config.rs
+++ zebrad/src/config.rs
@@ -43,4 +43,8 @@ pub struct ZebradConfig {
     #[serde(skip_serializing_if = "zebra_rpc::config::mining::Config::skip_getblocktemplate")]
     /// Mining configuration
     pub mining: zebra_rpc::config::mining::Config,
+
+    #[cfg(feature = "zebra-scan")]
+    /// Scanner configuration
+    pub shielded_scan: zebra_scan::config::Config,
 }
