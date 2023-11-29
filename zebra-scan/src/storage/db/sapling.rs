a230dc01f030802d21df9d1c75cde85595c277cf
teor <teor@riseup.net>
2023-11-30 08:34:34 +1000

    Add a read method for a specific index, and document it

--- zebra-scan/src/storage/db/sapling.rs
+++ zebra-scan/src/storage/db/sapling.rs
@@ -1,6 +1,18 @@
 //! Sapling-specific database reading and writing.
+//!
+//! The sapling scanner database has the following format:
+//!
+//! | name             | key                           | value                    |
+//! |------------------|-------------------------------|--------------------------|
+//! | `sapling_tx_ids` | `SaplingScannedDatabaseIndex` | `Vec<transaction::Hash>` |
+//!
+//! And types:
+//! SaplingScannedDatabaseIndex = `SaplingScanningKey` | `Height`
+//!
+//! This format allows us to efficiently find all the results for each key, and the latest height
+//! for each key.
 
-use zebra_state::AsColumnFamilyRef;
+use zebra_state::{AsColumnFamilyRef, ReadDisk, SaplingScannedDatabaseIndex, SaplingScannedResult};
 
 use crate::storage::Storage;
 
@@ -10,8 +22,19 @@ use crate::storage::Storage;
 pub const SAPLING_TX_IDS: &str = "sapling_tx_ids";
 
 impl Storage {
+    // Reading Sapling database entries
+
+    /// Returns the results for a specific key and block height.
+    pub fn sapling_tx_ids(&self, index: &SaplingScannedDatabaseIndex) -> Vec<SaplingScannedResult> {
+        self.db
+            .zs_get(&self.sapling_tx_ids_cf(), &index)
+            .unwrap_or_default()
+    }
+
+    // Column family convenience methods
+
     /// Returns a handle to the `sapling_tx_ids` column family.
-    pub fn sapling_tx_ids_cf(&self) -> impl AsColumnFamilyRef + '_ {
+    pub(crate) fn sapling_tx_ids_cf(&self) -> impl AsColumnFamilyRef + '_ {
         self.db.cf_handle(SAPLING_TX_IDS).unwrap()
     }
 }
