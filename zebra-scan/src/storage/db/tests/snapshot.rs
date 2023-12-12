a72029473fa05ee4dfa89d2323521ec9c492fc84
Marek <mail@marek.onl>
2023-12-12 14:15:54 +0100

    Fix errors caused by the merge

--- zebra-scan/src/storage/db/tests/snapshot.rs
+++ zebra-scan/src/storage/db/tests/snapshot.rs
@@ -57,7 +57,7 @@ fn test_raw_rocksdb_column_families_with_network(network: Network) {
     let mut net_suffix = network.to_string();
     net_suffix.make_ascii_lowercase();
 
-    let mut storage = Storage::new(&Config::ephemeral(), network);
+    let mut storage = Storage::new(&Config::ephemeral(), network, false);
 
     // Snapshot the column family names
     let mut cf_names = storage.db.list_cf().expect("empty database is valid");
