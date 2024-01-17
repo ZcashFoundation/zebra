69704e8b8c16bbf30ac188e7cdd809cf9eed540c
Alfredo Garcia <oxarbitrage@gmail.com>
2024-01-17 17:18:16 -0300

    convert to a lib

---
+++ zebra-grpc/src/lib.rs
@@ -0,0 +1,5 @@
+//! Zebra gRPC interface.
+
+#![doc(html_favicon_url = "https://zfnd.org/wp-content/uploads/2022/03/zebra-favicon-128.png")]
+#![doc(html_logo_url = "https://zfnd.org/wp-content/uploads/2022/03/zebra-icon.png")]
+#![doc(html_root_url = "https://docs.rs/zebra_grpc")]
