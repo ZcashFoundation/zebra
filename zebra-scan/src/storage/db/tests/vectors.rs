dc4e8dd7548f5e7bb851bb1d6671dc86aadbdb89
Arya <aryasolhi@gmail.com>
2024-01-31 12:22:49 -0500

    updates test to insert and delete results for a second key

--- zebra-scan/src/storage/db/tests/vectors.rs
+++ zebra-scan/src/storage/db/tests/vectors.rs
@@ -15,27 +15,39 @@ pub fn deletes_keys_and_results_correctly() {
 
     let zec_pages_sapling_efvk = ZECPAGES_SAPLING_VIEWING_KEY.to_string();
 
-    for fake_result_height in [Height::MIN, Height(1), Height::MAX] {
-        db.insert_sapling_results(
-            &zec_pages_sapling_efvk,
-            fake_result_height,
-            fake_sapling_results([
-                TransactionIndex::MIN,
-                TransactionIndex::from_index(40),
-                TransactionIndex::MAX,
-            ]),
-        );
+    // Replace the last letter of the zec_pages efvk
+    let fake_efvk = format!(
+        "{}t",
+        &ZECPAGES_SAPLING_VIEWING_KEY[..ZECPAGES_SAPLING_VIEWING_KEY.len() - 1]
+    );
+
+    let efvks = [&zec_pages_sapling_efvk, &fake_efvk];
+
+    for efvk in efvks {
+        for fake_result_height in [Height::MIN, Height(1), Height::MAX] {
+            db.insert_sapling_results(
+                efvk,
+                fake_result_height,
+                fake_sapling_results([
+                    TransactionIndex::MIN,
+                    TransactionIndex::from_index(40),
+                    TransactionIndex::MAX,
+                ]),
+            );
+        }
     }
 
-    assert!(
-        !db.sapling_results(&zec_pages_sapling_efvk).is_empty(),
-        "there should be some results for this key in the db"
-    );
+    for efvk in efvks {
+        assert!(
+            !db.sapling_results(efvk).is_empty(),
+            "there should be some results for this key in the db"
+        );
 
-    db.delete_sapling_results(vec![zec_pages_sapling_efvk.clone()]);
+        db.delete_sapling_results(vec![efvk.clone()]);
 
-    assert!(
-        db.sapling_results(&zec_pages_sapling_efvk).is_empty(),
-        "all results for this key should have been deleted"
-    );
+        assert!(
+            db.sapling_results(efvk).is_empty(),
+            "all results for this key should have been deleted"
+        );
+    }
 }
