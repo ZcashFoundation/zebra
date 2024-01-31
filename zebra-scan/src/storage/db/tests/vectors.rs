74f570382e5ad475c32b83f51693c68d58ad7cd4
Arya <aryasolhi@gmail.com>
2024-01-30 20:50:23 -0500

    adds fake_sapling_results helper function

--- zebra-scan/src/storage/db/tests/vectors.rs
+++ zebra-scan/src/storage/db/tests/vectors.rs
@@ -1,11 +1,12 @@
 //! Fixed test vectors for the scanner Storage.
 
-use std::collections::BTreeMap;
+use zebra_chain::{block::Height, parameters::Network};
+use zebra_state::TransactionIndex;
 
-use zebra_chain::{block::Height, parameters::Network, transaction};
-use zebra_state::{SaplingScannedResult, TransactionIndex};
-
-use crate::{storage::db::tests::new_test_storage, tests::ZECPAGES_SAPLING_VIEWING_KEY};
+use crate::{
+    storage::db::tests::{fake_sapling_results, new_test_storage},
+    tests::ZECPAGES_SAPLING_VIEWING_KEY,
+};
 
 /// Tests that keys are deleted correctly
 #[test]
@@ -15,24 +16,14 @@ pub fn deletes_keys_and_results_correctly() {
     let zec_pages_sapling_efvk = ZECPAGES_SAPLING_VIEWING_KEY.to_string();
 
     for fake_result_height in [Height::MIN, Height(1), Height::MAX] {
-        let mut fake_sapling_results: BTreeMap<TransactionIndex, SaplingScannedResult> =
-            BTreeMap::new();
-        for transaction_index in [
-            TransactionIndex::MIN,
-            TransactionIndex::from_index(40),
-            TransactionIndex::MAX,
-        ] {
-            let fake_transaction_id = [0; 32];
-            fake_sapling_results.insert(
-                transaction_index,
-                SaplingScannedResult::from(transaction::Hash::from(fake_transaction_id)),
-            );
-        }
-
         db.insert_sapling_results(
             &zec_pages_sapling_efvk,
             fake_result_height,
-            fake_sapling_results,
+            fake_sapling_results([
+                TransactionIndex::MIN,
+                TransactionIndex::from_index(40),
+                TransactionIndex::MAX,
+            ]),
         );
     }
 
