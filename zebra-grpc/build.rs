96c0d42d74c693f01c87f036834468455dcb3f01
Marek <mail@marek.onl>
2024-02-19 14:09:52 +0100

    Add the `experimental_allow_proto3_optional` flag (#8282)

--- zebra-grpc/build.rs
+++ zebra-grpc/build.rs
@@ -3,6 +3,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .btree_map(["."])
+        .protoc_arg("--experimental_allow_proto3_optional")
         .type_attribute(".", "#[derive(serde::Deserialize, serde::Serialize)]")
         .compile(&["proto/scanner.proto"], &[""])?;
     Ok(())
