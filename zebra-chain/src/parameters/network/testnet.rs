997e26563607ecf2790b3b67a83e87f07fc999a1
Arya <aryasolhi@gmail.com>
2024-04-11 16:47:19 -0400

    fixes doc links

--- zebra-chain/src/parameters/network/testnet.rs
+++ zebra-chain/src/parameters/network/testnet.rs
@@ -11,14 +11,14 @@ use crate::{
 pub struct Parameters {
     /// The network upgrade activation heights for this network.
     ///
-    /// Note: This value is ignored by [`Network::activation_list()`] when `zebra-chain` is
+    /// Note: This value is ignored by `Network::activation_list()` when `zebra-chain` is
     ///       compiled with the `zebra-test` feature flag AND the `TEST_FAKE_ACTIVATION_HEIGHTS`
     ///       environment variable is set.
     pub activation_heights: BTreeMap<Height, NetworkUpgrade>,
 }
 
 impl Default for Parameters {
-    /// Returns an instance of the default public testnet [`NetworkParameters`].
+    /// Returns an instance of the default public testnet [`Parameters`].
     fn default() -> Self {
         Self {
             activation_heights: TESTNET_ACTIVATION_HEIGHTS.iter().cloned().collect(),
@@ -27,7 +27,7 @@ impl Default for Parameters {
 }
 
 impl Parameters {
-    /// Returns true if the instance of [`NetworkParameters`] represents the default public Testnet.
+    /// Returns true if the instance of [`Parameters`] represents the default public Testnet.
     pub fn is_default_testnet(&self) -> bool {
         self == &Self::default()
     }
