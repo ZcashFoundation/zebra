941a082de1a63fe2c82c69739baa5a5de9cac710
Alfredo Garcia <oxarbitrage@gmail.com>
2024-04-22 10:51:58 -0300

    disable single thread

--- zebra-rpc/src/server/tests/vectors.rs
+++ zebra-rpc/src/server/tests/vectors.rs
@@ -23,13 +23,13 @@ use super::super::*;
 
 /// Test that the JSON-RPC server spawns when configured with a single thread.
 #[test]
+#[cfg(not(target_os = "windows"))]
 fn rpc_server_spawn_single_thread() {
     rpc_server_spawn(false)
 }
 
 /// Test that the JSON-RPC server spawns when configured with multiple threads.
 #[test]
-#[cfg(not(target_os = "windows"))]
 fn rpc_server_spawn_parallel_threads() {
     rpc_server_spawn(true)
 }
