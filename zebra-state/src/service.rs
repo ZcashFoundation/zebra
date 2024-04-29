52ff68f4c8caad8be611450b15872ca957a57fc9
Arya <aryasolhi@gmail.com>
2024-04-29 19:03:34 -0400

    - Returns early when there is no Heartwood activation height when creating or updating HistoryTree - Skips call to `check::legacy_chain()` when no NU5 activation height is set (it would return immediately anyway) - Replaces `.map()` with `.filter_map()` in `NetworkUpgrade::target_spacings()` - Removes outdated TODO

--- zebra-state/src/service.rs
+++ zebra-state/src/service.rs
@@ -422,11 +422,10 @@ impl StateService {
         tracing::info!("starting legacy chain check");
         let timer = CodeTimer::start();
 
-        if let Some(tip) = state.best_tip() {
-            let nu5_activation_height = NetworkUpgrade::Nu5
-                .activation_height(network)
-                .expect("NU5 activation height is set");
-
+        if let (Some(tip), Some(nu5_activation_height)) = (
+            state.best_tip(),
+            NetworkUpgrade::Nu5.activation_height(network),
+        ) {
             if let Err(error) = check::legacy_chain(
                 nu5_activation_height,
                 any_ancestor_blocks(
