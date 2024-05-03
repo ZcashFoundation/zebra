99cb52b13c994020226cb15caa39dc47f884aaf0
Arya <aryasolhi@gmail.com>
2024-05-03 17:43:11 -0400

    removes outdated TODO

--- zebra-chain/src/block/commitment.rs
+++ zebra-chain/src/block/commitment.rs
@@ -118,7 +118,6 @@ impl Commitment {
             // NetworkUpgrade::current() returns the latest network upgrade that's activated at the provided height, so
             // on Regtest for heights above height 0, it returns NU5, and it's possible for the current network upgrade
             // to be NU5 (or Canopy, or any network upgrade above Heartwood) at the Heartwood activation height.
-            // TODO: Check Canopy too once Zebra can construct Canopy block templates.
             Heartwood | Canopy | Nu5 if Some(height) == Heartwood.activation_height(network) => {
                 if bytes == CHAIN_HISTORY_ACTIVATION_RESERVED {
                     Ok(ChainHistoryActivationReserved)
