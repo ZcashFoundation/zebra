46942f2d058ad40a7ecdf3ca6ea51b5d7fd76c78
Marek <mail@marek.onl>
2024-05-13 15:33:02 +0200

    Derive `Default` for `GetTreestate`

--- zebra-rpc/src/methods/trees.rs
+++ zebra-rpc/src/methods/trees.rs
@@ -36,7 +36,7 @@ pub struct GetSubtrees {
 ///
 /// Contains the hex-encoded Sapling & Orchard note commitment trees, and their
 /// corresponding [`block::Hash`], [`Height`], and block time.
-#[derive(Clone, Debug, Eq, PartialEq, serde::Serialize)]
+#[derive(Clone, Debug, Eq, PartialEq, serde::Serialize, Default)]
 pub struct GetTreestate {
     /// The block hash corresponding to the treestate, hex-encoded.
     #[serde(with = "hex")]
