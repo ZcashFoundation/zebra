ba7fc7b8cf52fdbf5a0febbc9c982e25c2a3c5e5
Alfredo Garcia <oxarbitrage@gmail.com>
2024-05-17 13:50:10 -0300

    leave doc as it was 2

--- zebra-chain/src/transaction/serialize.rs
+++ zebra-chain/src/transaction/serialize.rs
@@ -292,7 +292,7 @@ impl ZcashDeserialize for Option<sapling::ShieldedData<SharedAnchor>> {
         //
         // This rule is also implemented in
         // [`zebra_state::service::check::anchor`] and
-        // [`zebra_chain::sapling::Spend`].
+        // [`zebra_chain::sapling::spend`].
         //
         // The "anchor encoding for v5 transactions" is implemented here.
         let transfers = match shared_anchor {
