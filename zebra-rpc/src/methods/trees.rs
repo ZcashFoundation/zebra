829ae6b558f0d4cc90cd87394e272b9eb0349867
Marek <mail@marek.onl>
2024-05-20 12:40:47 +0200

    Describe the serialization format of treestates

--- zebra-rpc/src/methods/trees.rs
+++ zebra-rpc/src/methods/trees.rs
@@ -34,8 +34,19 @@ pub struct GetSubtrees {
 
 /// Response to a `z_gettreestate` RPC request.
 ///
-/// Contains the hex-encoded Sapling & Orchard note commitment trees, and their corresponding
+/// Contains hex-encoded Sapling & Orchard note commitment trees and their corresponding
 /// [`struct@Hash`], [`Height`], and block time.
+///
+/// The format of the serialized trees represents `CommitmentTree`s from the crate
+/// `incrementalmerkletree` and not `Frontier`s from the same crate, even though `zebrad`'s
+/// `NoteCommitmentTree`s are implemented using `Frontier`s. Zebra follows the former format to stay
+/// consistent with `zcashd`'s RPCs.
+///
+/// The formats are semantically equivalent. The difference is that in `Frontier`s, the vector of
+/// ommers is dense (we know where the gaps are from the position of the leaf in the overall tree);
+/// whereas in `CommitmentTree`, the vector of ommers is sparse with [`None`] values in the gaps.
+///
+/// The dense format might be used in future RPCs.
 #[derive(Clone, Debug, Eq, PartialEq, serde::Serialize)]
 pub struct GetTreestate {
     /// The block hash corresponding to the treestate, hex-encoded.
