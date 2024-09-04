2a6db14c33758c2de923b93834f44aa4578804aa
Tomek Piotrowski <tomek.piotrowski@atteo.com>
2024-09-04 14:20:03 +0200

    zfuture txids

--- zebra-chain/src/transaction/txid.rs
+++ zebra-chain/src/transaction/txid.rs
@@ -30,7 +30,7 @@ impl<'a> TxIdBuilder<'a> {
             | Transaction::V4 { .. } => self.txid_v1_to_v4(),
             Transaction::V5 { .. } => self.txid_v5(),
             #[cfg(feature = "zsf")]
-            Transaction::ZFuture { .. } => self.txid_v6(),
+            Transaction::ZFuture { .. } => self.txid_zfuture(),
         }
     }
 
@@ -55,10 +55,8 @@ impl<'a> TxIdBuilder<'a> {
         Ok(Hash(*alt_tx.txid().as_ref()))
     }
 
-    /// Compute the Transaction ID for a V6 transaction in the given network upgrade.
-    /// In this case it's the hash of a tree of hashes of specific parts of the
-    /// transaction, as specified in ZIP-2xx and ZIP-2xx.
-    fn txid_v6(self) -> Result<Hash, io::Error> {
+    /// Compute the Transaction ID for a ZFuture transaction in the given network upgrade.
+    fn txid_zfuture(self) -> Result<Hash, io::Error> {
         // The v5 txid (from ZIP-244) is computed using librustzcash. Convert the zebra
         // transaction to a librustzcash transaction.
         let alt_tx: zcash_primitives::transaction::Transaction = self.trans.try_into()?;
