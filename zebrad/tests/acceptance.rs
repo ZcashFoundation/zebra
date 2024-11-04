0d730bc5cdafa3b862e4aa16225d4d0117ccf461
Arya <aryasolhi@gmail.com>
2024-11-04 13:52:00 -0500

    Adds a test to check for crates in the Cargo.lock file that are being pulled in from a git source.

--- zebrad/tests/acceptance.rs
+++ zebrad/tests/acceptance.rs
@@ -3538,3 +3538,15 @@ async fn nu6_funding_streams_and_coinbase_balance() -> Result<()> {
 
     Ok(())
 }
+
+/// Check that Zebra does not depend on any crates from git sources.
+#[test]
+#[ignore]
+fn check_no_git_refs_in_cargo_lock() {
+    let cargo_lock_contents =
+        fs::read_to_string("../Cargo.lock").expect("should have Cargo.lock file in root dir");
+
+    if cargo_lock_contents.contains(r#"source = "git+"#) {
+        panic!("Cargo.lock includes git sources")
+    }
+}
