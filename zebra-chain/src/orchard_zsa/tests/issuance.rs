4855c25724fd15d8809304a933000766c90835dc
Dmitry Demin <dmitry@qed-it.com>
2024-11-11 22:02:36 +0100

    Add more checks after deserializing issuance block in issuance_block test

--- zebra-chain/src/orchard_zsa/tests/issuance.rs
+++ zebra-chain/src/orchard_zsa/tests/issuance.rs
@@ -1,4 +1,4 @@
-use crate::{block::Block, serialization::ZcashDeserialize};
+use crate::{block::Block, serialization::ZcashDeserialize, transaction::Transaction};
 
 use super::vectors::BLOCKS;
 
@@ -6,4 +6,20 @@ use super::vectors::BLOCKS;
 fn issuance_block() {
     let issuance_block =
         Block::zcash_deserialize(BLOCKS[0].as_ref()).expect("issuance block should deserialize");
+
+    for transaction in issuance_block.transactions {
+        if let Transaction::V6 {
+            orchard_zsa_issue_data,
+            ..
+        } = transaction.as_ref()
+        {
+            let issue_bundle = orchard_zsa_issue_data
+                .as_ref()
+                .expect("V6 transaction in the issuance test block has orchard_zsa_issue_data")
+                .inner();
+
+            assert_eq!(issue_bundle.actions().len(), 1);
+            assert_eq!(issue_bundle.actions()[0].notes().len(), 1);
+        }
+    }
 }
