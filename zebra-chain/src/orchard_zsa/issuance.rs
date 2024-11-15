3f7e68af6991406ca6aa53f5e2f8ac07c360c5e7
Arya <aryasolhi@gmail.com>
2024-11-15 18:13:17 -0500

    adds import

--- zebra-chain/src/orchard_zsa/issuance.rs
+++ zebra-chain/src/orchard_zsa/issuance.rs
@@ -7,6 +7,7 @@ use halo2::pasta::pallas;
 // For pallas::Base::from_repr only
 use group::ff::PrimeField;
 
+use nonempty::NonEmpty;
 use zcash_primitives::transaction::components::issuance::{read_v6_bundle, write_v6_bundle};
 
 use orchard::{
