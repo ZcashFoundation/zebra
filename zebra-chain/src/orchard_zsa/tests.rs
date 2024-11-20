90dac031999581cab77c0d43105d808e7a2fa8a2
Dmitry Demin <dmitry@qed-it.com>
2024-11-20 07:04:56 +0100

    Fix zsa consensus tests

--- zebra-chain/src/orchard_zsa/tests.rs
+++ zebra-chain/src/orchard_zsa/tests.rs
@@ -1,3 +1,6 @@
+/// FIXME: refactor orchard_zsa/tests (possibly move vectors to zebra-tests), remove cfg(test) here etc.
+#[cfg(test)]
 mod blocks;
+
 /// FIXME: pub is needed to access test vectors from another crates, remove it then
 pub mod vectors;
