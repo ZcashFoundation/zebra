9a5a5a121110280bd2d7b41319c5286f99cdabbf
Dmitry Demin <dmitry@qed-it.com>
2025-02-11 11:29:09 +0100

    Fix nu7 activation heights in network_upgrade.rs (nu6 value + 1 does not work - causes a test failure)

--- zebra-chain/src/parameters/network_upgrade.rs
+++ zebra-chain/src/parameters/network_upgrade.rs
@@ -96,7 +96,7 @@ pub(super) const MAINNET_ACTIVATION_HEIGHTS: &[(block::Height, NetworkUpgrade)] 
     (block::Height(2_726_400), Nu6),
     // FIXME: TODO: Use a proper value below.
     #[cfg(zcash_unstable = "nu6" /* TODO nu7 */ )]
-    (block::Height(2_726_401), Nu7),
+    (block::Height(3_111_000), Nu7),
 ];
 
 /// Fake mainnet network upgrade activation heights, used in tests.
@@ -136,7 +136,7 @@ pub(super) const TESTNET_ACTIVATION_HEIGHTS: &[(block::Height, NetworkUpgrade)] 
     (block::Height(2_976_000), Nu6),
     // FIXME: TODO: Use a proper value below.
     #[cfg(zcash_unstable = "nu6" /* TODO nu7 */ )]
-    (block::Height(2_942_001), Nu7),
+    (block::Height(3_222_000), Nu7),
 ];
 
 /// Fake testnet network upgrade activation heights, used in tests.
@@ -551,7 +551,7 @@ impl From<zcash_protocol::consensus::NetworkUpgrade> for NetworkUpgrade {
             zcash_protocol::consensus::NetworkUpgrade::Nu6 => Self::Nu6,
             // FIXME: TODO: Use a proper value below.
             #[cfg(zcash_unstable = "nu6" /* TODO nu7 */ )]
-            zcash_protocol::consensus::NetworkUpgrade::Nu7 => Self::Nu6,
+            zcash_protocol::consensus::NetworkUpgrade::Nu7 => Self::Nu7,
         }
     }
 }
