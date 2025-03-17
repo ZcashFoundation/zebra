bb6e35973d1fec0d657f6bfbc1acc857580ae863
Gustavo Valverde <g.valverde02@gmail.com>
2025-03-17 20:14:43 +0000

    Revert "refactor(entrypoint): use previous version"
    This reverts commit 0ac8f5e3f3c8d28ce4554dab379720b1db3b81a4.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,8 +3,24 @@
 # Entrypoint for running Zebra in Docker.
 #
 # The main script logic is at the bottom.
+#
+# ## Notes
+#
+# - `$ZEBRA_CONF_PATH` must point to a Zebra conf file.
 
 set -eo pipefail
+
+# Exit early if `ZEBRA_CONF_PATH` does not point to a file.
+if [[ ! -f "${ZEBRA_CONF_PATH}" ]]; then
+  echo "ERROR: No Zebra config file found at ZEBRA_CONF_PATH (${ZEBRA_CONF_PATH})."
+  echo "Please ensure the file exists or mount your custom config file and set ZEBRA_CONF_PATH accordingly."
+  exit 1
+fi
+
+# Use gosu to drop privileges
+exec_as_user() {
+  exec gosu "${UID}:${GID}" "$@"
+}
 
 # Modifies the existing Zebra config file at ZEBRA_CONF_PATH using environment variables.
 #
@@ -38,6 +54,12 @@ prepare_conf_file() {
     mkdir -p "${ZEBRA_CACHE_DIR//\"/}"
     chown -R "${UID}:${GID}" "${ZEBRA_CACHE_DIR//\"/}"
     sed -i 's|_dir = ".*"|_dir = "'"${ZEBRA_CACHE_DIR//\"/}"'"|' "${ZEBRA_CONF_PATH}"
+  fi
+
+  # Set a custom lightwalletd cache dir.
+  if [[ -n "${LWD_CACHE_DIR}" ]]; then
+    mkdir -p "${LWD_CACHE_DIR//\"/}"
+    chown -R "${UID}:${GID}" "${LWD_CACHE_DIR//\"/}"
   fi
 
   # Enable the Prometheus metrics endpoint.
@@ -74,6 +96,26 @@ prepare_conf_file() {
   echo "${ZEBRA_CONF_PATH}"
 }
 
+# Checks if a directory contains subdirectories
+#
+# Exits with 0 if it does, and 1 otherwise.
+check_directory_files() {
+  local dir="$1"
+  # Check if the directory exists
+  if [[ -d "${dir}" ]]; then
+    # Check if there are any subdirectories
+    if find "${dir}" -mindepth 1 -type d | read -r; then
+      :
+    else
+      echo "No subdirectories found in ${dir}."
+      exit 1
+    fi
+  else
+    echo "Directory ${dir} does not exist."
+    exit 1
+  fi
+}
+
 # Runs cargo test with an arbitrary number of arguments.
 #
 # ## Positional Parameters
@@ -83,31 +125,32 @@ prepare_conf_file() {
 #     https://doc.rust-lang.org/cargo/reference/features.html#command-line-feature-options,
 #   - or be empty.
 # - The remaining params will be appended to a command starting with
-#   `cargo test ... -- ...`
-run_test() {
-  # Start constructing the command, ensuring that $1 is enclosed in single
-  # quotes as it's a feature list
-  local cmd="cargo test --locked --release --features '$1' --package zebrad --test acceptance -- --nocapture --include-ignored"
-
+#   `exec_as_user cargo test ... -- ...`
+run_cargo_test() {
   # Shift the first argument, as it's already included in the cmd
+  local features="$1"
   shift
+
+  # Start constructing the command array
+  local cmd_args=(
+    cargo test --locked --release
+    --features "${features}"
+    --package zebrad
+    --test acceptance
+    -- --nocapture --include-ignored
+  )
 
   # Loop through the remaining arguments
   for arg in "$@"; do
     if [[ -n ${arg} ]]; then
       # If the argument is non-empty, add it to the command
-      cmd+=" ${arg}"
+      cmd_args+=("${arg}")
     fi
   done
 
-  # Run the command using eval. This will replace the current process with the
-  # cargo command.
-  echo "Running:"
-  echo "${cmd}"
-  eval "${cmd}" || {
-    echo "Cargo test failed"
-    exit 1
-  }
+  echo "Running: ${cmd_args[*]}"
+  # Execute directly to become PID 1
+  exec_as_user "${cmd_args[@]}"
 }
 
 # Runs tests depending on the env vars.
@@ -125,48 +168,48 @@ run_tests() {
 
   elif [[ "${RUN_CHECK_NO_GIT_REFS}" -eq "1" ]]; then
     # Run the check_no_git_refs_in_cargo_lock test.
-    cargo test --locked --release --workspace --features "${FEATURES}" \
+    exec_as_user cargo test --locked --release --workspace --features "${FEATURES}" \
       -- --nocapture --include-ignored check_no_git_refs_in_cargo_lock
 
   elif [[ "${TEST_FAKE_ACTIVATION_HEIGHTS}" -eq "1" ]]; then
     # Run state tests with fake activation heights.
-    cargo test --locked --release --lib --features "zebra-test" \
+    exec_as_user cargo test --locked --release --lib --features "zebra-test" \
       --package zebra-state \
       -- --nocapture --include-ignored with_fake_activation_heights
 
   elif [[ "${TEST_SCANNER}" -eq "1" ]]; then
     # Test the scanner.
-    cargo test --locked --release --package zebra-scan \
+    exec_as_user cargo test --locked --release --package zebra-scan \
       -- --nocapture --include-ignored scan_task_commands scan_start_where_left
 
   elif [[ "${TEST_ZEBRA_EMPTY_SYNC}" -eq "1" ]]; then
     # Test that Zebra syncs and checkpoints a few thousand blocks from an empty
     # state.
-    run_test "${FEATURES}" "sync_large_checkpoints_"
+    run_cargo_test "${FEATURES}" "sync_large_checkpoints_"
 
   elif [[ -n "${FULL_SYNC_MAINNET_TIMEOUT_MINUTES}" ]]; then
     # Run a Zebra full sync test on mainnet.
-    run_test "${FEATURES}" "full_sync_mainnet"
+    run_cargo_test "${FEATURES}" "full_sync_mainnet"
 
   elif [[ -n "${FULL_SYNC_TESTNET_TIMEOUT_MINUTES}" ]]; then
     # Run a Zebra full sync test on testnet.
-    run_test "${FEATURES}" "full_sync_testnet"
+    run_cargo_test "${FEATURES}" "full_sync_testnet"
 
   elif [[ "${TEST_DISK_REBUILD}" -eq "1" ]]; then
     # Run a Zebra sync up to the mandatory checkpoint.
-    run_test "${FEATURES} test_sync_to_mandatory_checkpoint_${NETWORK,,}" \
+    run_cargo_test "${FEATURES} test_sync_to_mandatory_checkpoint_${NETWORK,,}" \
       "sync_to_mandatory_checkpoint_${NETWORK,,}"
     echo "ran test_disk_rebuild"
 
   elif [[ "${TEST_UPDATE_SYNC}" -eq "1" ]]; then
     # Run a Zebra sync starting at the cached tip, and syncing to the latest
     # tip.
-    run_test "${FEATURES}" "zebrad_update_sync"
+    run_cargo_test "${FEATURES}" "zebrad_update_sync"
 
   elif [[ "${TEST_CHECKPOINT_SYNC}" -eq "1" ]]; then
     # Run a Zebra sync starting at the cached mandatory checkpoint, and syncing
     # past it.
-    run_test "${FEATURES} test_sync_past_mandatory_checkpoint_${NETWORK,,}" \
+    run_cargo_test "${FEATURES} test_sync_past_mandatory_checkpoint_${NETWORK,,}" \
       "sync_past_mandatory_checkpoint_${NETWORK,,}"
 
   elif [[ "${GENERATE_CHECKPOINTS_MAINNET}" -eq "1" ]]; then
@@ -174,105 +217,94 @@ run_tests() {
     #
     # TODO: disable or filter out logs like:
     # test generate_checkpoints_mainnet has been running for over 60 seconds
-    run_test "${FEATURES}" "generate_checkpoints_mainnet"
+    run_cargo_test "${FEATURES}" "generate_checkpoints_mainnet"
 
   elif [[ "${GENERATE_CHECKPOINTS_TESTNET}" -eq "1" ]]; then
     # Generate checkpoints after syncing Zebra on testnet.
     #
     # This test might fail if testnet is unstable.
-    run_test "${FEATURES}" "generate_checkpoints_testnet"
+    run_cargo_test "${FEATURES}" "generate_checkpoints_testnet"
 
   elif [[ "${TEST_LWD_RPC_CALL}" -eq "1" ]]; then
     # Starting at a cached Zebra tip, test a JSON-RPC call to Zebra.
     # Run both the fully synced RPC test and the subtree snapshot test, one test
     # at a time. Since these tests use the same cached state, a state problem in
     # the first test can fail the second test.
-    run_test "${FEATURES}" "--test-threads" "1" "fully_synced_rpc_"
+    run_cargo_test "${FEATURES}" "--test-threads" "1" "fully_synced_rpc_"
 
   elif [[ "${TEST_LWD_INTEGRATION}" -eq "1" ]]; then
     # Test launching lightwalletd with an empty lightwalletd and Zebra state.
-    run_test "${FEATURES}" "lightwalletd_integration"
+    run_cargo_test "${FEATURES}" "lightwalletd_integration"
 
   elif [[ "${TEST_LWD_FULL_SYNC}" -eq "1" ]]; then
     # Starting at a cached Zebra tip, run a lightwalletd sync to tip.
-    run_test "${FEATURES}" "lightwalletd_full_sync"
+    run_cargo_test "${FEATURES}" "lightwalletd_full_sync"
 
   elif [[ "${TEST_LWD_UPDATE_SYNC}" -eq "1" ]]; then
     # Starting with a cached Zebra and lightwalletd tip, run a quick update sync.
-    run_test "${FEATURES}" "lightwalletd_update_sync"
+    run_cargo_test "${FEATURES}" "lightwalletd_update_sync"
 
   # These tests actually use gRPC.
   elif [[ "${TEST_LWD_GRPC}" -eq "1" ]]; then
     # Starting with a cached Zebra and lightwalletd tip, test all gRPC calls to
     # lightwalletd, which calls Zebra.
-    run_test "${FEATURES}" "lightwalletd_wallet_grpc_tests"
+    run_cargo_test "${FEATURES}" "lightwalletd_wallet_grpc_tests"
 
   elif [[ "${TEST_LWD_TRANSACTIONS}" -eq "1" ]]; then
     # Starting with a cached Zebra and lightwalletd tip, test sending
     # transactions gRPC call to lightwalletd, which calls Zebra.
-    run_test "${FEATURES}" "sending_transactions_using_lightwalletd"
+    run_cargo_test "${FEATURES}" "sending_transactions_using_lightwalletd"
 
   # These tests use mining code, but don't use gRPC.
   elif [[ "${TEST_GET_BLOCK_TEMPLATE}" -eq "1" ]]; then
     # Starting with a cached Zebra tip, test getting a block template from
     # Zebra's RPC server.
-    run_test "${FEATURES}" "get_block_template"
+    run_cargo_test "${FEATURES}" "get_block_template"
 
   elif [[ "${TEST_SUBMIT_BLOCK}" -eq "1" ]]; then
     # Starting with a cached Zebra tip, test sending a block to Zebra's RPC
     # port.
-    run_test "${FEATURES}" "submit_block"
+    run_cargo_test "${FEATURES}" "submit_block"
 
   else
-    exec "$@"
+    exec_as_user "$@"
   fi
 }
 
-# Prepares the container for running Zebra.
-container_init() {
-  echo "INFO: Using the following environment variables:"
-  printenv
-
-  prepare_conf_file "${ZEBRA_CONF_PATH}"
-  echo "INFO: Using the following Zebra config:"
-  cat "${ZEBRA_CONF_PATH}"
-}
-
 # Main Script Logic
-#
+
+prepare_conf_file "${ZEBRA_CONF_PATH}"
+echo "INFO: Using the following environment variables:"
+printenv
+
+echo "Prepared the following Zebra config:"
+cat "${ZEBRA_CONF_PATH}"
+
 # - If "$1" is "--", "-", or "zebrad", run `zebrad` with the remaining params.
 # - If "$1" is "tests":
 #   - and "$2" is "zebrad", run `zebrad` with the remaining params,
 #   - else run tests with the remaining params.
 # - TODO: If "$1" is "monitoring", start a monitoring node.
 # - If "$1" doesn't match any of the above, run "$@" directly.
-entrypoint() {
-  case "$1" in
-  --* | -* | zebrad)
+case "$1" in
+--* | -* | zebrad)
+  shift
+  exec_as_user zebrad --config "${ZEBRA_CONF_PATH}" "$@"
+  ;;
+test)
+  shift
+  if [[ "$1" == "zebrad" ]]; then
     shift
-    zebrad --config "${ZEBRA_CONF_PATH}" "$@"
-    ;;
-  test)
-    shift
-    if [[ "$1" == "zebrad" ]]; then
-      shift
-      zebrad --config "${ZEBRA_CONF_PATH}" "$@"
-    else
-      run_tests "$@"
-    fi
-    ;;
-  monitoring)
-    #  TODO: Impl logic for starting a monitoring node.
-    :
-    ;;
-  *)
-    exec "$@"
-    ;;
-  esac
-}
-
-DECLARE_FUNS=$(declare -f entrypoint run_tests run_test)
-
-container_init
-
-gosu "${USER}" bash -c "${DECLARE_FUNS}; entrypoint $*"
+    exec_as_user zebrad --config "${ZEBRA_CONF_PATH}" "$@"
+  else
+    run_tests "$@"
+  fi
+  ;;
+monitoring)
+  #  TODO: Impl logic for starting a monitoring node.
+  :
+  ;;
+*)
+  exec_as_user "$@"
+  ;;
+esac
