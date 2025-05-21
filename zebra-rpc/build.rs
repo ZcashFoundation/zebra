2539b9f1abf836f349e6bb5f67fd33cddaa0828a
Alfredo Garcia <oxarbitrage@gmail.com>
2025-05-21 16:25:03 -0300

    build zallet in zebra under a feature

--- zebra-rpc/build.rs
+++ zebra-rpc/build.rs
@@ -11,5 +11,49 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             .compile_protos(&["proto/indexer.proto"], &[""])?;
     }
 
+    #[cfg(feature = "zallet")]
+    {
+        use std::{env, fs, path::PathBuf, process::Command};
+
+        // The following code will clone the zallet repo and build the binary,
+        // then copy the binary to the main target directory.
+        //
+        // Code below is fragile and will just build the main branch of the wallet repository
+        // so we can have it available for `qa` regtests.
+
+        let build_dir = env::var("OUT_DIR")
+            .map(PathBuf::from)
+            .expect("failed to get OUT_DIR");
+
+        let profile = env::var("PROFILE").unwrap_or_else(|_| "debug".to_string());
+
+        let target_dir = env::var("CARGO_TARGET_DIR")
+            .map(PathBuf::from)
+            .unwrap_or_else(|_| std::env::current_dir().expect("failed to get current dir"))
+            .join("../target")
+            .join(&profile);
+
+        let _ = Command::new("git")
+            .args([
+                "clone",
+                "https://github.com/zcash/wallet.git",
+                build_dir.to_str().unwrap(),
+            ])
+            .status()
+            .expect("failed to clone external binary");
+
+        let _ = Command::new("cargo")
+            .args(["build"])
+            .current_dir(&build_dir)
+            .status()
+            .expect("failed to build external binary");
+
+        fs::copy(
+            build_dir.join(format!("target/{}/zallet", profile)),
+            target_dir.join("zallet"),
+        )
+        .expect("failed to copy zallet binary");
+    }
+
     Ok(())
 }
