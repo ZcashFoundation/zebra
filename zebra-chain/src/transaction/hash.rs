acc3539a52b279b0623d25ee5183b9834cbe7416
Marek <mail@marek.onl>
2025-06-19 14:01:58 +0200

    Implement AsRef<[u8; 32]> for Hash

--- zebra-chain/src/transaction/hash.rs
+++ zebra-chain/src/transaction/hash.rs
@@ -62,6 +62,12 @@ use super::{txid::TxIdBuilder, AuthDigest, Transaction};
 #[cfg_attr(any(test, feature = "proptest-impl"), derive(Arbitrary))]
 pub struct Hash(pub [u8; 32]);
 
+impl AsRef<[u8; 32]> for Hash {
+    fn as_ref(&self) -> &[u8; 32] {
+        &self.0
+    }
+}
+
 impl From<Transaction> for Hash {
     fn from(transaction: Transaction) -> Self {
         // use the ref implementation, to avoid cloning the transaction
