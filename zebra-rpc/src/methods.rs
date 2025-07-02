78575d56f601e25be0ae50b9626956f9eab4c224
Marek <mail@marek.onl>
2025-07-02 17:35:20 +0200

    Make `zebra-chain::transparent::Address` Copy

--- zebra-rpc/src/methods.rs
+++ zebra-rpc/src/methods.rs
@@ -4058,7 +4058,7 @@ impl Utxo {
         Height,
     ) {
         (
-            self.address.clone(),
+            self.address,
             self.txid,
             self.output_index,
             self.script.clone(),
