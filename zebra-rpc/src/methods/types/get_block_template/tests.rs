02768eef35ecc7adc7e44566a37954b1c2ec374c
Marek <mail@marek.onl>
2025-07-02 18:06:09 +0200

    Refactor and expand coinbase transaction generation tests

--- zebra-rpc/src/methods/types/get_block_template/tests.rs
+++ zebra-rpc/src/methods/types/get_block_template/tests.rs
@@ -1,45 +1,125 @@
 //! Tests for types and functions for the `getblocktemplate` RPC.
 
+use std::iter;
+
 use zcash_keys::address::Address;
 use zcash_transparent::address::TransparentAddress;
 
+use zebra_chain::parameters::subsidy::{
+    FUNDING_STREAM_ECC_ADDRESSES_TESTNET, FUNDING_STREAM_MG_ADDRESSES_TESTNET,
+    FUNDING_STREAM_ZF_ADDRESSES_TESTNET,
+};
+use zebra_chain::parameters::testnet::ConfiguredFundingStreamRecipient;
 use zebra_chain::{
-    amount::Amount,
     block::Height,
-    parameters::testnet::{self, ConfiguredActivationHeights, ConfiguredFundingStreams},
-    serialization::{ZcashDeserializeInto, ZcashSerialize},
+    parameters::{
+        subsidy::FundingStreamReceiver,
+        testnet::{self, ConfiguredActivationHeights, ConfiguredFundingStreams},
+        Network, NetworkUpgrade,
+    },
+    serialization::ZcashDeserializeInto,
     transaction::Transaction,
 };
 
-use super::standard_coinbase_outputs;
+use crate::client::TransactionTemplate;
 
-/// Tests that a minimal coinbase transaction can be generated.
+/// Tests that coinbase transactions can be generated.
 #[test]
-fn minimal_coinbase() -> Result<(), Box<dyn std::error::Error>> {
+fn coinbase() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = Address::from(TransparentAddress::PublicKeyHash([0x42; 20]));
+
     let regtest = testnet::Parameters::build()
         .with_slow_start_interval(Height::MIN)
         .with_activation_heights(ConfiguredActivationHeights {
-            nu6: Some(1),
+            overwinter: Some(1),
+            sapling: Some(2),
+            blossom: Some(3),
+            heartwood: Some(4),
+            canopy: Some(5),
+            nu5: Some(6),
+            nu6: Some(7),
+            nu6_1: Some(8),
+            nu7: Some(9),
             ..Default::default()
         })?
-        .with_funding_streams(vec![ConfiguredFundingStreams {
-            height_range: Some(Height(1)..Height(10)),
-            recipients: None,
-        }])
+        .with_funding_streams(vec![
+            ConfiguredFundingStreams {
+                height_range: Some(Height(1)..Height(100)),
+                recipients: Some(vec![
+                    ConfiguredFundingStreamRecipient {
+                        receiver: FundingStreamReceiver::ZcashFoundation,
+                        numerator: 5,
+                        addresses: Some(
+                            FUNDING_STREAM_ZF_ADDRESSES_TESTNET
+                                .map(ToString::to_string)
+                                .to_vec(),
+                        ),
+                    },
+                    ConfiguredFundingStreamRecipient {
+                        receiver: FundingStreamReceiver::Ecc,
+                        numerator: 7,
+                        addresses: Some(
+                            FUNDING_STREAM_ECC_ADDRESSES_TESTNET
+                                .map(ToString::to_string)
+                                .to_vec(),
+                        ),
+                    },
+                    ConfiguredFundingStreamRecipient {
+                        receiver: FundingStreamReceiver::MajorGrants,
+                        numerator: 8,
+                        addresses: Some(
+                            FUNDING_STREAM_MG_ADDRESSES_TESTNET
+                                .map(ToString::to_string)
+                                .to_vec(),
+                        ),
+                    },
+                ]),
+            },
+            ConfiguredFundingStreams {
+                height_range: Some(Height(1)..Height(100)),
+                recipients: Some(vec![
+                    ConfiguredFundingStreamRecipient {
+                        receiver: FundingStreamReceiver::MajorGrants,
+                        numerator: 8,
+                        addresses: Some(
+                            FUNDING_STREAM_MG_ADDRESSES_TESTNET
+                                .map(ToString::to_string)
+                                .to_vec(),
+                        ),
+                    },
+                    ConfiguredFundingStreamRecipient {
+                        receiver: FundingStreamReceiver::Deferred,
+                        numerator: 12,
+                        addresses: None,
+                    },
+                ]),
+            },
+        ])
         .to_network()?;
 
-    let outputs = standard_coinbase_outputs(
-        &regtest,
-        Height(1),
-        &Address::from(TransparentAddress::PublicKeyHash([0x42; 20])),
-        Amount::zero(),
-    );
+    // The maximum length of miner data is 96 bytes.
+    let miner_datas = [
+        vec![],
+        vec![0x00; 1],
+        vec![0x00; 96],
+        vec![0xff; 1],
+        vec![0xff; 96],
+    ];
 
-    // It should be possible to generate a coinbase tx from these params.
-    Transaction::new_v5_coinbase(&regtest, Height(1), outputs, vec![])
-        .zcash_serialize_to_vec()?
-        // Deserialization contains checks for elementary consensus rules, which must pass.
-        .zcash_deserialize_into::<Transaction>()?;
+    for net in Network::iter().chain(iter::once(regtest)) {
+        for nu in NetworkUpgrade::iter().filter(|nu| nu >= &NetworkUpgrade::Overwinter) {
+            if let Some(height) = nu.activation_height(&net) {
+                for data in &miner_datas {
+                    // It should be possible to generate a coinbase tx from these params.
+                    TransactionTemplate::new_coinbase(&net, height, &addr, data.clone(), &[])?
+                        .data()
+                        .as_ref()
+                        // Deserialization contains checks for elementary consensus rules, which must pass.
+                        .zcash_deserialize_into::<Transaction>()?;
+                }
+            }
+        }
+    }
 
     Ok(())
 }
