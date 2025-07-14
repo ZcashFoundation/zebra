8e9e8e3898500ade74950ceee82963608d420b9a
Marek <mail@marek.onl>
2025-07-14 13:46:11 +0200

    Rename variable miner_address to miner_addr in getblocktemplate method

--- zebra-rpc/src/methods.rs
+++ zebra-rpc/src/methods.rs
@@ -2162,7 +2162,7 @@ where
 
         let client_long_poll_id = parameters.as_ref().and_then(|params| params.long_poll_id);
 
-        let miner_address = self
+        let miner_addr = self
             .gbt
             .miner_address()
             .ok_or_misc_error("miner_address not configured")?;
@@ -2419,7 +2419,7 @@ where
         let mempool_txs = select_mempool_transactions(
             &network,
             next_block_height,
-            &miner_address,
+            &miner_addr,
             miner_data.clone(),
             miner_memo.clone(),
             mempool_txs,
@@ -2440,7 +2440,7 @@ where
 
         let response = BlockTemplateResponse::new_internal(
             &network,
-            &miner_address,
+            &miner_addr,
             miner_data,
             miner_memo,
             &chain_tip_and_local_time,
