df7ff27126a48bc92e9c2742b50c130d5339b6cd
Marek <mail@marek.onl>
2025-07-14 13:41:33 +0200

    Rename extra_coinbase_data to miner_data

--- zebra-rpc/src/methods.rs
+++ zebra-rpc/src/methods.rs
@@ -2127,7 +2127,7 @@ where
 
         // Clone Configs
         let network = self.network.clone();
-        let extra_coinbase_data = self.gbt.extra_coinbase_data();
+        let miner_data = self.gbt.miner_data();
 
         // Clone Services
         let mempool = self.mempool.clone();
@@ -2412,9 +2412,9 @@ where
             &network,
             next_block_height,
             &miner_address,
+            miner_data.clone(),
             mempool_txs,
             mempool_tx_deps,
-            extra_coinbase_data.clone(),
         );
 
         tracing::debug!(
@@ -2430,11 +2430,11 @@ where
         let response = BlockTemplateResponse::new_internal(
             &network,
             &miner_address,
+            miner_data,
             &chain_tip_and_local_time,
             server_long_poll_id,
             mempool_txs,
             submit_old,
-            extra_coinbase_data,
         );
 
         Ok(response.into())
