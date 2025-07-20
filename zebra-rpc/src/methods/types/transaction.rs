cd0b2cecbedff8d9ec4fb4e13b204bf0489ef456
Marek <mail@marek.onl>
2025-07-20 17:16:32 +0200

    Remove obsolete comment about coinbase transaction inputs

--- zebra-rpc/src/methods/types/transaction.rs
+++ zebra-rpc/src/methods/types/transaction.rs
@@ -251,7 +251,6 @@ impl TransactionTemplate<NegativeOrZero> {
             data: data.into(),
             hash: tx.txid().as_ref().into(),
             auth_digest: tx.auth_commitment().as_ref().try_into()?,
-            // Always empty, coinbase transactions do not have inputs.
             depends: Vec::new(),
             fee: (-miner_fee).constrain()?,
             sigops: tx.sigops()?,
