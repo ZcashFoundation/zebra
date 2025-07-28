97772addcb5abd0c1c1dd4c979518c989611cb63
Marek <mail@marek.onl>
2025-07-28 10:09:02 +0200

    Verify Sapling shielded data using `sapling-crypto::BatchValidator`

--- zebra-consensus/src/primitives/groth16/params.rs
+++ zebra-consensus/src/primitives/groth16/params.rs
@@ -1,68 +1,44 @@
 //! Loading and checking correctness of Groth16 Sapling and Sprout parameters.
 
-use bellman::groth16::{prepare_verifying_key, Parameters, PreparedVerifyingKey, VerifyingKey};
+use bellman::groth16::{prepare_verifying_key, PreparedVerifyingKey, VerifyingKey};
 use bls12_381::Bls12;
+use derive_getters::Getters;
 use zcash_proofs::prover::LocalTxProver;
 
 lazy_static::lazy_static! {
-    /// Spend and output parameters for the Sapling circuit.
+    /// Sapling prover containing spend and output params for the Sapling circuit.
     ///
-    /// Used to construct Sapling outputs in coinbase txs.
-    pub static ref PROVER_PARAMS: LocalTxProver = LocalTxProver::bundled().clone();
-
-    /// Groth16 Zero-Knowledge Proof parameters for the Sapling and Sprout circuits.
+    /// Used to:
     ///
-    /// This static is accessed when Zebra needs to use these parameters for verification.
-    pub static ref GROTH16_PARAMETERS: Groth16Parameters = Groth16Parameters::new(PROVER_PARAMS.clone());
+    /// - construct Sapling outputs in coinbase txs, and
+    /// - verify Sapling shielded data in the tx verifier.
+    pub static ref SAPLING: LocalTxProver = LocalTxProver::bundled();
+
+    /// Spend parameters for the Sprout circuit.
+    ///
+    /// Used to verify Sprout shielded data in transactions.
+    ///
+    /// Note that adding value to the Sprout pool was disabled by the Canopy network upgrade.
+    pub static ref SPROUT: SproutParams = Default::default();
 }
 
-/// Groth16 Zero-Knowledge Proof parameters for the Sapling and Sprout circuits.
-pub struct Groth16Parameters {
-    /// The Sapling circuit Groth16 parameters.
-    pub sapling: SaplingParameters,
-    /// The Sprout circuit Groth16 spend parameters.
-    pub sprout: SproutParameters,
-}
-
-/// Groth16 Zero-Knowledge Proof spend and output parameters for the Sapling circuit.
-pub struct SaplingParameters {
-    pub spend: Parameters<Bls12>,
-    pub spend_prepared_verifying_key: PreparedVerifyingKey<Bls12>,
-
-    pub output: Parameters<Bls12>,
-    pub output_prepared_verifying_key: PreparedVerifyingKey<Bls12>,
-}
-
-/// Groth16 Zero-Knowledge Proof spend parameters for the Sprout circuit.
+/// Spend parameters for the Sprout circuit.
 ///
-/// Adding value to the Sprout pool was disabled by the Canopy network upgrade.
-pub struct SproutParameters {
-    pub joinsplit_prepared_verifying_key: PreparedVerifyingKey<Bls12>,
+/// Used to verify Sprout shielded data in transactions.
+///
+/// Note that adding value to the Sprout pool was disabled by the Canopy network upgrade.
+#[derive(Getters)]
+pub struct SproutParams {
+    prepared_verifying_key: PreparedVerifyingKey<Bls12>,
 }
 
-impl Groth16Parameters {
-    /// Loads the Groth16 parameters for Sapling and Sprout circuits.
-    fn new(prover_params: LocalTxProver) -> Groth16Parameters {
+impl Default for SproutParams {
+    fn default() -> Self {
         let sprout_vk = VerifyingKey::<Bls12>::read(&include_bytes!("sprout-groth16.vk")[..])
             .expect("should be able to read and parse Sprout verification key");
 
-        let (spend_params, output_params) = prover_params.params();
-        let (spend, output) = (spend_params.inner(), output_params.inner());
-        let (spend_prepared_verifying_key, output_prepared_verifying_key) = (
-            prepare_verifying_key(&spend.vk),
-            prepare_verifying_key(&output.vk),
-        );
-
-        Groth16Parameters {
-            sapling: SaplingParameters {
-                spend,
-                spend_prepared_verifying_key,
-                output,
-                output_prepared_verifying_key,
-            },
-            sprout: SproutParameters {
-                joinsplit_prepared_verifying_key: prepare_verifying_key(&sprout_vk),
-            },
+        Self {
+            prepared_verifying_key: prepare_verifying_key(&sprout_vk),
         }
     }
 }
