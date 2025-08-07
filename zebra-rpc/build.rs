8476e99cfae914b752b39bfc27d6481c79bd5ca6
Marek <mail@marek.onl>
2025-08-07 07:39:09 -0700

    Update tonic and prost dependencies to version 0.14

--- zebra-rpc/build.rs
+++ zebra-rpc/build.rs
@@ -3,7 +3,7 @@ use std::{env, fs, path::PathBuf, process::Command};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let out_dir = env::var("OUT_DIR").map(PathBuf::from);
-    tonic_build::configure()
+    tonic_prost_build::configure()
         .type_attribute(".", "#[derive(serde::Deserialize, serde::Serialize)]")
         .file_descriptor_set_path(out_dir.unwrap().join("indexer_descriptor.bin"))
         .compile_protos(&["proto/indexer.proto"], &[""])?;
