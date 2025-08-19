26161abfa96199d9aade6cbd236f83255bf6df80
arya2 <aryasolhi@gmail.com>
2025-08-18 23:00:21 -0400

    Updates `duration_short()` to print microseconds when formatting durations less than 5 ms

--- zebra-chain/src/fmt/time.rs
+++ zebra-chain/src/fmt/time.rs
@@ -5,14 +5,19 @@ use std::time::Duration;
 /// The minimum amount of time displayed with only seconds (no milliseconds).
 pub const MIN_SECONDS_ONLY_TIME: Duration = Duration::from_secs(5);
 
+/// The minimum amount of time displayed with only seconds and milliseconds (no microseconds).
+pub const MIN_MS_ONLY_TIME: Duration = Duration::from_millis(5);
+
 /// Returns a human-friendly formatted string for the whole number of seconds in `duration`.
 pub fn duration_short(duration: impl Into<Duration>) -> String {
     let duration = duration.into();
 
     if duration >= MIN_SECONDS_ONLY_TIME {
         humantime_seconds(duration)
-    } else {
+    } else if duration >= MIN_MS_ONLY_TIME {
         humantime_milliseconds(duration)
+    } else {
+        humantime_microseconds(duration)
     }
 }
 
@@ -39,6 +44,20 @@ pub fn humantime_milliseconds(duration: impl Into<Duration>) -> String {
     let duration_millis = Duration::from_millis(duration.subsec_millis().into());
 
     let duration = humantime::format_duration(duration_secs + duration_millis);
+
+    format!("{duration}")
+}
+
+/// Returns a human-friendly formatted string for the whole number of milliseconds in `duration`.
+pub fn humantime_microseconds(duration: impl Into<Duration>) -> String {
+    let duration = duration.into();
+
+    // Truncate fractional seconds.
+    let duration_secs = Duration::from_secs(duration.as_secs());
+    let duration_millis = Duration::from_millis(duration.subsec_millis().into());
+    let duration_micros = Duration::from_micros(duration.subsec_micros().into());
+
+    let duration = humantime::format_duration(duration_secs + duration_millis + duration_micros);
 
     format!("{duration}")
 }
