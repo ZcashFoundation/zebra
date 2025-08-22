9c807a47899818fa73df96ccca9e2c1cd5c1791a
arya2 <aryasolhi@gmail.com>
2025-08-22 15:34:42 -0400

    Removes protoc as a build dependency of zebra-rpc

--- zebra-rpc/build.rs
+++ zebra-rpc/build.rs
@@ -1,13 +1,51 @@
 //! Compile proto files
 use std::{env, fs, path::PathBuf, process::Command};
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let out_dir = env::var("OUT_DIR").map(PathBuf::from);
-    tonic_build::configure()
-        .type_attribute(".", "#[derive(serde::Deserialize, serde::Serialize)]")
-        .file_descriptor_set_path(out_dir.unwrap().join("indexer_descriptor.bin"))
-        .compile_protos(&["proto/indexer.proto"], &[""])?;
+const ZALLET_COMMIT: Option<&str> = None;
 
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    build_or_copy_proto()?;
+    build_zallet_for_qa_tests();
+
+    Ok(())
+}
+
+fn build_or_copy_proto() -> Result<(), Box<dyn std::error::Error>> {
+    let out_dir = env::var("OUT_DIR")
+        .map(PathBuf::from)
+        .expect("requires OUT_DIR environment variable definition");
+    let file_names = ["indexer_descriptor.bin", "zebra.indexer.rpc.rs"];
+
+    let is_protoc_available = env::var_os("PROTOC")
+        .map(PathBuf::from)
+        .or_else(|| which::which("protoc").ok())
+        .is_some();
+
+    if is_protoc_available {
+        tonic_build::configure()
+            .type_attribute(".", "#[derive(serde::Deserialize, serde::Serialize)]")
+            .file_descriptor_set_path(out_dir.join("indexer_descriptor.bin"))
+            .compile_protos(&["proto/indexer.proto"], &[""])?;
+
+        for file_name in file_names {
+            fs::copy(
+                out_dir.join(file_name),
+                format!("proto/__generated__/{file_name}"),
+            )?;
+        }
+    } else {
+        for file_name in file_names {
+            fs::copy(
+                format!("proto/__generated__/{file_name}"),
+                out_dir.join(file_name),
+            )?;
+        }
+    }
+
+    Ok(())
+}
+
+fn build_zallet_for_qa_tests() {
     if env::var_os("ZALLET").is_some() {
         // The following code will clone the zallet repo and build the binary,
         // then copy the binary to the project target directory.
@@ -15,7 +53,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         // Code below is fragile and will just build the main branch of the wallet repository
         // so we can have it available for `qa` regtests.
 
-        let build_dir = env::var("OUT_DIR").map(PathBuf::from).unwrap_or_default();
+        let build_dir = env::var("OUT_DIR")
+            .map(PathBuf::from)
+            .unwrap_or_default()
+            .join("zallet_build");
 
         let profile = "debug".to_string();
 
@@ -33,6 +74,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             ])
             .status()
             .expect("failed to clone external binary");
+
+        if let Some(zallet_commit) = ZALLET_COMMIT {
+            let _ = Command::new("git")
+                .args(["checkout", zallet_commit])
+                .current_dir(&build_dir)
+                .status()
+                .expect("failed to build external binary");
+        }
 
         let _ = Command::new("cargo")
             .args(["build"])
@@ -52,6 +101,4 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             )
         });
     }
-
-    Ok(())
 }
