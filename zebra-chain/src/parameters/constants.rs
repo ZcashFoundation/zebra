e422d2f4e5134ce2233282405a8578d3077c160f
Alfredo Garcia <oxarbitrage@gmail.com>
2025-10-07 10:32:48 -0300

    remove doc links

--- zebra-chain/src/parameters/constants.rs
+++ zebra-chain/src/parameters/constants.rs
@@ -34,23 +34,23 @@ pub mod activation_heights {
     pub mod testnet {
         use crate::block::Height;
 
-        /// The block height at which [`BeforeOverwinter`] activates on Testnet.
+        /// The block height at which `BeforeOverwinter` activates on Testnet.
         pub const BEFORE_OVERWINTER: Height = Height(1);
-        /// The block height at which [`Overwinter`] activates on Testnet.
+        /// The block height at which `Overwinter` activates on Testnet.
         pub const OVERWINTER: Height = Height(207_500);
-        /// The block height at which [`Sapling`] activates on Testnet.
+        /// The block height at which `Sapling` activates on Testnet.
         pub const SAPLING: Height = Height(280_000);
-        /// The block height at which [`Blossom`] activates on Testnet.
+        /// The block height at which `Blossom` activates on Testnet.
         pub const BLOSSOM: Height = Height(584_000);
-        /// The block height at which [`Heartwood`] activates on Testnet.
+        /// The block height at which `Heartwood` activates on Testnet.
         pub const HEARTWOOD: Height = Height(903_800);
-        /// The block height at which [`Canopy`] activates on Testnet.
+        /// The block height at which `Canopy` activates on Testnet.
         pub const CANOPY: Height = Height(1_028_500);
-        /// The block height at which [`NU5`] activates on Testnet.
+        /// The block height at which `NU5` activates on Testnet.
         pub const NU5: Height = Height(1_842_420);
-        /// The block height at which [`NU6`] activates on Testnet.
+        /// The block height at which `NU6` activates on Testnet.
         pub const NU6: Height = Height(2_976_000);
-        /// The block height at which [`NU6.1`](NU6_1) activates on Testnet.
+        /// The block height at which `NU6.1` activates on Testnet.
         pub const NU6_1: Height = Height(3_536_500);
     }
 
@@ -58,23 +58,23 @@ pub mod activation_heights {
     pub mod mainnet {
         use crate::block::Height;
 
-        /// The block height at which [`BeforeOverwinter`] activates on Mainnet.
+        /// The block height at which `BeforeOverwinter` activates on Mainnet.
         pub const BEFORE_OVERWINTER: Height = Height(1);
-        /// The block height at which [`Overwinter`] activates on Mainnet.
+        /// The block height at which `Overwinter` activates on Mainnet.
         pub const OVERWINTER: Height = Height(347_500);
-        /// The block height at which [`Sapling`] activates on Mainnet.
+        /// The block height at which `Sapling` activates on Mainnet.
         pub const SAPLING: Height = Height(419_200);
-        /// The block height at which [`Blossom`] activates on Mainnet.
+        /// The block height at which `Blossom` activates on Mainnet.
         pub const BLOSSOM: Height = Height(653_600);
-        /// The block height at which [`Heartwood`] activates on Mainnet.
+        /// The block height at which `Heartwood` activates on Mainnet.
         pub const HEARTWOOD: Height = Height(903_000);
-        /// The block height at which [`Canopy`] activates on Mainnet.
+        /// The block height at which `Canopy` activates on Mainnet.
         pub const CANOPY: Height = Height(1_046_400);
-        /// The block height at which [`NU5`] activates on Mainnet.
+        /// The block height at which `NU5` activates on Mainnet.
         pub const NU5: Height = Height(1_687_104);
-        /// The block height at which [`NU6`] activates on Mainnet.
+        /// The block height at which `NU6` activates on Mainnet.
         pub const NU6: Height = Height(2_726_400);
-        // /// The block height at which [`NU6.1`](NU6_1) activates on Mainnet.
+        // /// The block height at which `NU6.1` activates on Mainnet.
         // pub const NU6_1: Height = Height(3_146_400);
     }
 }
