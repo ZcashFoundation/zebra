53eee5990c131178be0380aa456f5df90e5040ab
Conrado Gouvea <conrado@zfnd.org>
2025-11-14 19:43:55 -0300

    change(zebrad): always enable backtraces, add debug info

--- zebrad/src/bin/zebrad/main.rs
+++ zebrad/src/bin/zebrad/main.rs
@@ -4,5 +4,11 @@ use zebrad::application::{boot, APPLICATION};
 
 /// Process entry point for `zebrad`
 fn main() {
+    std::env::set_var("RUST_BACKTRACE", "1");
+    // Disable library backtraces (i.e. eyre) to avoid performance hit for
+    // non-panic errors, but allow users to override it.
+    if std::env::var_os("RUST_LIB_BACKTRACE").is_none() {
+        std::env::set_var("RUST_LIB_BACKTRACE", "0");
+    }
     boot(&APPLICATION);
 }
