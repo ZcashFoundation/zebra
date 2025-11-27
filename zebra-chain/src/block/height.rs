f615d9095a5df98247924a4bc072be7f11ab9a27
Marek <m@rek.onl>
2025-11-27 11:05:26 +0100

    Add From implementation for Height to u64 conversion

--- zebra-chain/src/block/height.rs
+++ zebra-chain/src/block/height.rs
@@ -148,6 +148,12 @@ impl TryFrom<u32> for Height {
     }
 }
 
+impl From<Height> for u64 {
+    fn from(height: Height) -> Self {
+        height.0.into()
+    }
+}
+
 /// Convenience trait for converting a type into a valid Zcash [`Height`].
 pub trait TryIntoHeight {
     /// The error type returned by [`Height`] conversion failures.
