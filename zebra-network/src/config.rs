2da02d037e801d24ab779025a7fc97e23d960225
DC <alchemydc@proton.me>
2026-01-16 10:40:57 -0700

    chore: add new Shielded Labs DNS mainnet seeder.  remove unreachable/flaky z.cash seeders until ECC/bootstrap governance crisis is resolved.
    see [forum post](https://forum.zcashcommunity.com/t/expanding-zcash-network-infrastructure-new-dns-seeders-launched-in-the-united-states-and-europe/54256) for context

--- zebra-network/src/config.rs
+++ zebra-network/src/config.rs
@@ -546,17 +546,16 @@ impl Config {
 impl Default for Config {
     fn default() -> Config {
         let mainnet_peers = [
-            "dnsseed.z.cash:8233",
             "dnsseed.str4d.xyz:8233",
             "mainnet.seeder.zfnd.org:8233",
+            "mainnet.seeder.shieldedinfra.net:8233",
         ]
         .iter()
         .map(|&s| String::from(s))
         .collect();
 
         let testnet_peers = [
-            "dnsseed.testnet.z.cash:18233",
-            "testnet.seeder.zfnd.org:18233",
+            "testnet.seeder.zfnd.org:18233"
         ]
         .iter()
         .map(|&s| String::from(s))
