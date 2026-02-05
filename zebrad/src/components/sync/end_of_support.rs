6a3e22c78c547226069ecd979cec9bbf19f18762
Marek <m@rek.onl>
2026-02-05 22:50:59 +0100

    Update estimated release height to 3231000

--- zebrad/src/components/sync/end_of_support.rs
+++ zebrad/src/components/sync/end_of_support.rs
@@ -13,7 +13,7 @@ use zebra_chain::{
 use crate::application::release_version;
 
 /// The estimated height that this release will be published.
-pub const ESTIMATED_RELEASE_HEIGHT: u32 = 3_212_000;
+pub const ESTIMATED_RELEASE_HEIGHT: u32 = 3_231_000;
 
 /// The maximum number of days after `ESTIMATED_RELEASE_HEIGHT` where a Zebra server will run
 /// without halting.
