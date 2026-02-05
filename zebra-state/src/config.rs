43ad654a29298e5ce8aab2a6ee61db46f7cd9abc
GarmashAlex <garmasholeksii@gmail.com>
2026-02-05 14:24:35 +0300

    perf: cache restorable db versions during old db cleanup (#10245)

--- zebra-state/src/config.rs
+++ zebra-state/src/config.rs
@@ -277,6 +277,8 @@ fn delete_old_databases(config: Config, db_kind: String, major_version: u64, net
 
     info!(db_kind, "checking for old database versions");
 
+    let restorable_db_versions = restorable_db_versions();
+
     let mut db_path = config.db_path(&db_kind, major_version, network);
     // Check and remove the network path.
     assert_eq!(
@@ -303,7 +305,8 @@ fn delete_old_databases(config: Config, db_kind: String, major_version: u64, net
 
     if let Some(db_kind_dir) = read_dir(&db_path) {
         for entry in db_kind_dir.flatten() {
-            let deleted_db = check_and_delete_database(&config, major_version, &entry);
+            let deleted_db =
+                check_and_delete_database(&config, major_version, &restorable_db_versions, &entry);
 
             if let Some(deleted_db) = deleted_db {
                 info!(?deleted_db, "deleted outdated {db_kind} database directory");
@@ -331,6 +334,7 @@ fn read_dir(dir: &Path) -> Option<ReadDir> {
 fn check_and_delete_database(
     config: &Config,
     major_version: u64,
+    restorable_db_versions: &[u64],
     entry: &DirEntry,
 ) -> Option<PathBuf> {
     let dir_name = parse_dir_name(entry)?;
@@ -341,7 +345,7 @@ fn check_and_delete_database(
     }
 
     // Don't delete databases that can be reused.
-    if restorable_db_versions()
+    if restorable_db_versions
         .iter()
         .map(|v| v - 1)
         .any(|v| v == dir_major_version)
