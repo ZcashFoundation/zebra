1d983b54cf5cdb95ac2958b137c718dd357f426c
Conrado Gouvea <conradoplg@gmail.com>
2026-06-02 18:07:17 -0300

    fix more tests

--- zebra-consensus/src/transaction/tests.rs
+++ zebra-consensus/src/transaction/tests.rs
@@ -2249,7 +2249,7 @@ async fn v5_transaction_with_exceeding_expiry_height() {
         expiry_height,
         sapling_shielded_data: None,
         orchard_shielded_data: None,
-        network_upgrade: NetworkUpgrade::Nu6_1,
+        network_upgrade: NetworkUpgrade::Nu6_2,
     };
 
     let transaction_hash = transaction.hash();
