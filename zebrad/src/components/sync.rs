f1ce57761cd8ed477bba3a9aa93d462a069f5094
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2026-08-04 17:03:25 +0000

    Report stalled already-known obtain responses
    Mark an `obtain_tips` response as stalled when every returned hash is
    already known. Such a response offers no new blocks to download and
    must not be treated as neutral feedback.

--- zebrad/src/components/sync.rs
+++ zebrad/src/components/sync.rs
@@ -830,7 +830,10 @@ where
                 })
                 .map_err::<Report, _>(|e| eyre!(e))
             {
-                Ok(zn::Response::BlockHashes { hashes, feedback }) => {
+                Ok(zn::Response::BlockHashes {
+                    hashes,
+                    mut feedback,
+                }) => {
                     trace!(?hashes);
 
                     let hashes = hashes.as_slice();
@@ -851,6 +854,9 @@ where
                     let unknown_hashes = if let Some(index) = first_unknown {
                         &hashes[index..]
                     } else {
+                        if let Some(feedback) = feedback.take() {
+                            feedback.mark_stalled();
+                        }
                         continue;
                     };
 
@@ -894,7 +900,7 @@ where
                     metrics::histogram!("sync.obtain.response.hash.count")
                         .record(new_hashes as f64);
 
-                    if let Some(feedback) = feedback {
+                    if let Some(feedback) = feedback.take() {
                         feedback.mark_useful();
                     }
                 }
