bc1c3781ad30c0331d35b5af9398482619337bb4
Conrado Gouvea <conrado@zfnd.org>
2026-08-14 19:57:12 -0300

    add comment

--- zebra-state/src/service/tests.rs
+++ zebra-state/src/service/tests.rs
@@ -673,8 +673,8 @@ fn read_only_open_with_ephemeral_config_returns_error() {
     // checked first.
     let parent = tempfile::tempdir().expect("creating a temporary directory should succeed");
     let config = Config {
-        cache_dir: parent.path().join("missing"),
         ephemeral: true,
+        cache_dir: parent.path().join("missing"),
         ..Config::default()
     };
 
